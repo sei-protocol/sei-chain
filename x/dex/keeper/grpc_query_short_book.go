e3d35169270366248f5569cd3944e714ab062efd
Xiaoyu Chen <codchen03@gmail.com>
2022-05-29 12:43:49 -0700

    Fix order book query endpoint

--- x/dex/keeper/grpc_query_short_book.go
+++ x/dex/keeper/grpc_query_short_book.go
@@ -3,10 +3,8 @@ package keeper
 import (
 	"context"
 
-	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
-	"github.com/cosmos/cosmos-sdk/types/query"
 	"github.com/sei-protocol/sei-chain/x/dex/types"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -17,21 +15,9 @@ func (k Keeper) ShortBookAll(c context.Context, req *types.QueryAllShortBookRequ
 		return nil, status.Error(codes.InvalidArgument, "invalid request")
 	}
 
-	var shortBooks []types.ShortBook
 	ctx := sdk.UnwrapSDKContext(c)
 
-	store := ctx.KVStore(k.storeKey)
-	shortBookStore := prefix.NewStore(store, types.KeyPrefix(types.ShortBookKey))
-
-	pageRes, err := query.Paginate(shortBookStore, req.Pagination, func(key []byte, value []byte) error {
-		var shortBook types.ShortBook
-		if err := k.cdc.Unmarshal(value, &shortBook); err != nil {
-			return err
-		}
-
-		shortBooks = append(shortBooks, shortBook)
-		return nil
-	})
+	shortBooks, pageRes, err := k.GetAllShortBookForPairPaginated(ctx, req.ContractAddr, req.PriceDenom, req.AssetDenom, req.Pagination)
 	if err != nil {
 		return nil, status.Error(codes.Internal, err.Error())
 	}
