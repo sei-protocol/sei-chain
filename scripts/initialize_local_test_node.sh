8aee1e3c59da83aae14f523fa15e5c4471a3c72a
Xiaoyu Chen <codchen03@gmail.com>
2022-05-30 14:43:45 -0700

    Fix initialize local node script

--- scripts/initialize_local_test_node.sh
+++ scripts/initialize_local_test_node.sh
@@ -26,23 +26,20 @@ docker run -d --name jaeger \
   jaegertracing/all-in-one:1.33
 
 echo "Building..."
-make install
+go build -o build/seid ./cmd/seid/
 echo $password | sudo -S rm -r ~/.sei/
 echo $password | sudo -S rm -r ~/test_accounts/
-~/go/bin/seid tendermint unsafe-reset-all
-~/go/bin/seid init demo --chain-id sei-chain
-yes | ~/go/bin/seid keys add $keyname
-yes | ~/go/bin/seid keys add faucet
-printf '12345678\n' | ~/go/bin/seid add-genesis-account $(~/go/bin/seid keys show $keyname -a) 100000000000000000000usei
-printf '12345678\n' | ~/go/bin/seid add-genesis-account $(~/go/bin/seid keys show faucet -a) 100000000000000000000usei
+./build/seid unsafe-reset-all
+./build/seid init demo --chain-id sei-chain
+yes | ./build/seid keys add $keyname
+yes | ./build/seid keys add faucet
+./build/seid add-genesis-account $(./build/seid keys show $keyname -a) 100000000000000000000ust
+./build/seid add-genesis-account $(./build/seid keys show faucet -a) 100000000000000000000ust
 python ./loadtest/scripts/populate_genesis_accounts.py $numtestaccount loc
-printf '12345678\n' | ~/go/bin/seid gentx $keyname 70000000000000000000usei --chain-id sei-chain
-~/go/bin/seid collect-gentxs
-cat ~/.sei/config/genesis.json | jq '.app_state["crisis"]["constant_fee"]["denom"]="usei"' > ~/.sei/config/tmp_genesis.json && mv ~/.sei/config/tmp_genesis.json ~/.sei/config/genesis.json
-cat ~/.sei/config/genesis.json | jq '.app_state["gov"]["deposit_params"]["min_deposit"][0]["denom"]="usei"' > ~/.sei/config/tmp_genesis.json && mv ~/.sei/config/tmp_genesis.json ~/.sei/config/genesis.json
-cat ~/.sei/config/genesis.json | jq '.app_state["mint"]["params"]["mint_denom"]="usei"' > ~/.sei/config/tmp_genesis.json && mv ~/.sei/config/tmp_genesis.json ~/.sei/config/genesis.json
-cat ~/.sei/config/genesis.json | jq '.app_state["staking"]["params"]["bond_denom"]="usei"' > ~/.sei/config/tmp_genesis.json && mv ~/.sei/config/tmp_genesis.json ~/.sei/config/genesis.json
-cat ~/.sei/config/genesis.json | jq '.app_state["gov"]["deposit_params"]["max_deposit_period"]="300s"' > ~/.sei/config/tmp_genesis.json && mv ~/.sei/config/tmp_genesis.json ~/.sei/config/genesis.json
-cat ~/.sei/config/genesis.json | jq '.app_state["gov"]["voting_params"]["voting_period"]="300s"' > ~/.sei/config/tmp_genesis.json && mv ~/.sei/config/tmp_genesis.json ~/.sei/config/genesis.json
-cat ~/.sei/config/genesis.json | jq '.consensus_params["block"]["time_iota_ms"]="50"' > ~/.sei/config/tmp_genesis.json && mv ~/.sei/config/tmp_genesis.json ~/.sei/config/genesis.json
-~/go/bin/seid start --trace
+./build/seid gentx $keyname 70000000000000000000ust --chain-id sei-chain
+./build/seid collect-gentxs
+cat ~/.sei/config/genesis.json | jq '.app_state["crisis"]["constant_fee"]["denom"]="ust"' > ~/.sei/config/tmp_genesis.json && mv ~/.sei/config/tmp_genesis.json ~/.sei/config/genesis.json
+cat ~/.sei/config/genesis.json | jq '.app_state["gov"]["deposit_params"]["min_deposit"][0]["denom"]="ust"' > ~/.sei/config/tmp_genesis.json && mv ~/.sei/config/tmp_genesis.json ~/.sei/config/genesis.json
+cat ~/.sei/config/genesis.json | jq '.app_state["mint"]["params"]["mint_denom"]="ust"' > ~/.sei/config/tmp_genesis.json && mv ~/.sei/config/tmp_genesis.json ~/.sei/config/genesis.json
+cat ~/.sei/config/genesis.json | jq '.app_state["staking"]["params"]["bond_denom"]="ust"' > ~/.sei/config/tmp_genesis.json && mv ~/.sei/config/tmp_genesis.json ~/.sei/config/genesis.json
+./build/seid start --trace
