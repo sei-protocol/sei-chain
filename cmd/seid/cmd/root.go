8278f2b0eaf0e1d2d18974a58000a47c95ad7421
philipsu522 <philip.su.522@gmail.com>
2022-06-03 08:48:54 -0700

    change min gas price to usei

--- cmd/seid/cmd/root.go
+++ cmd/seid/cmd/root.go
@@ -325,7 +325,7 @@ func initAppConfig() (string, interface{}) {
 	//   own app.toml to override, or use this default value.
 	//
 	// In simapp, we set the min gas prices to 0.
-	srvCfg.MinGasPrices = "0stake"
+	srvCfg.MinGasPrices = "0.01usei"
 	srvCfg.API.Enable = true
 
 	customAppConfig := CustomAppConfig{
