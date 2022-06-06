0ac9e75a1da6dd45b7efb99b88eaa23b6b146857
philipsu522 <philip.su.522@gmail.com>
2022-06-06 11:34:45 -0700

    Add upgrade handler for ignite cli removal

--- app/upgrades.go
+++ app/upgrades.go
@@ -8,6 +8,7 @@ import (
 
 const UpgradeNameOracleModule = "create-oracle-mod"
 const UpgradeOracleStaleIndicator = "upgrade-oracle-stale-indicator"
+const UpgradeIgniteCliRemoval = "ignite-cli-removal-upgrade"
 const Upgrade102 = "1.0.2beta"
 
 func (app App) RegisterUpgradeHandlers() {
@@ -17,7 +18,9 @@ func (app App) RegisterUpgradeHandlers() {
 	app.UpgradeKeeper.SetUpgradeHandler(UpgradeOracleStaleIndicator, func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
 		return app.mm.RunMigrations(ctx, app.configurator, fromVM)
 	})
-
+	app.UpgradeKeeper.SetUpgradeHandler(UpgradeIgniteCliRemoval, func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+		return app.mm.RunMigrations(ctx, app.configurator, fromVM)
+	})
 	app.UpgradeKeeper.SetUpgradeHandler(Upgrade102, func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
 		return app.mm.RunMigrations(ctx, app.configurator, fromVM)
 	})
