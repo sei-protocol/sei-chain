f556c64de7b9056d280f65f742f826b0c656a521
philipsu522 <philip.su.522@gmail.com>
2022-06-06 14:05:50 -0700

    Cherry pick 1s commit timeout change

--- app/upgrades.go
+++ app/upgrades.go
@@ -6,10 +6,16 @@ import (
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 )
 
+// NOTE: When performing upgrades, make sure to keep / register the handlers
+// for both the current (n) and the previous (n-1) upgrade name. There is a bug
+// in a missing value in a log statement for which the fix is not released
 const UpgradeNameOracleModule = "create-oracle-mod"
 const UpgradeOracleStaleIndicator = "upgrade-oracle-stale-indicator"
 const UpgradeIgniteCliRemoval = "ignite-cli-removal-upgrade"
+
+// 1.0.2beta upgrades
 const Upgrade102 = "1.0.2beta"
+const Upgrade102CommitTimeout = "1.0.2beta-commit-timeout"
 
 func (app App) RegisterUpgradeHandlers() {
 	app.UpgradeKeeper.SetUpgradeHandler(UpgradeNameOracleModule, func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
@@ -22,6 +28,9 @@ func (app App) RegisterUpgradeHandlers() {
 		return app.mm.RunMigrations(ctx, app.configurator, fromVM)
 	})
 	app.UpgradeKeeper.SetUpgradeHandler(Upgrade102, func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+		return app.mm.RunMigrations(ctx, app.configurator, fromVM)
+	})
+	app.UpgradeKeeper.SetUpgradeHandler(Upgrade102CommitTimeout, func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
 		return app.mm.RunMigrations(ctx, app.configurator, fromVM)
 	})
 }
