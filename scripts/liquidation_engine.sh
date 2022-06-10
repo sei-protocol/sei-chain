fd5044970c5d1e5c90d0acabcc418f032522a395
branfordswbf <tony811.fu@gmail.com>
2022-06-10 10:59:43 +0800

    update

--- scripts/liquidation_engine.sh
+++ scripts/liquidation_engine.sh
@@ -1,4 +1,12 @@
+#!/bin/bash -i
+if [[ $# -eq 0 ]] ; then
+    echo 'Usage: ./liquidation_engine.sh CREATOR_ACCOUNT_NAME'
+    exit 0
+fi
+
 alias seid="./build/seid"
+# TODO: use secret fetcher
+keyring_passphrase='password'
 contract_code=$(seid query wasm list-code | grep code_id | cut -d':' -f2 | tr -d '"')
 contract_addresses=$(seid query wasm list-contracts $contract_code | grep '-' | cut -c 3-)
  
@@ -8,6 +16,6 @@ for contract_address in $contract_addresses; do
     seid query auth accounts | grep address | while read -r line ; do
         account_address=$(echo "$line" | cut -d ":" -f 2 | tr -d ' ')
         nonce=$RANDOM
-        seid tx dex liquidate $contract_address $nonce $account_address
+        yes $keyring_passphrase | seid tx dex liquidate $contract_address $nonce $account_address --chain-id=sei --from=$1 -y
     done
 done
