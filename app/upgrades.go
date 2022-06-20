5e00e483345cd775deadb17c1ef5cfcb072d5601
Uday Patil <udpatil@gmail.com>
2022-06-19 21:46:58 -0700

    [upgrades] Remove irrelevant upgrades

--- app/upgrades.go
+++ app/upgrades.go
@@ -9,8 +9,6 @@ import (
 // NOTE: When performing upgrades, make sure to keep / register the handlers
 // for both the current (n) and the previous (n-1) upgrade name. There is a bug
 // in a missing value in a log statement for which the fix is not released
-const IgniteCLIRemovalUpgradeHandler = "ignite-cli-removal-upgrade"
-const UpgradeDexDataType = "upgrade-dex-data-type"
 
 // 1.0.2beta upgrades
 const Upgrade102 = "1.0.2beta"
@@ -24,12 +22,6 @@ const Upgrade103 = "1.0.3beta"
 const Upgrade104 = "1.0.4beta"
 
 func (app App) RegisterUpgradeHandlers() {
-	app.UpgradeKeeper.SetUpgradeHandler(IgniteCLIRemovalUpgradeHandler, func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
-		return app.mm.RunMigrations(ctx, app.configurator, fromVM)
-	})
-	app.UpgradeKeeper.SetUpgradeHandler(UpgradeDexDataType, func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
-		return app.mm.RunMigrations(ctx, app.configurator, fromVM)
-	})
 	app.UpgradeKeeper.SetUpgradeHandler(Upgrade102, func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
 		return app.mm.RunMigrations(ctx, app.configurator, fromVM)
 	})
