52312272f5c85b4985c5071bc07972113002fbc9
Uday Patil <udpatil@gmail.com>
2022-06-20 14:56:59 -0400

    [upgrades] Re-add ignite cli removal upgrade handler so we can properly upgrade (#49)

--- app/upgrades.go
+++ app/upgrades.go
@@ -9,6 +9,7 @@ import (
 // NOTE: When performing upgrades, make sure to keep / register the handlers
 // for both the current (n) and the previous (n-1) upgrade name. There is a bug
 // in a missing value in a log statement for which the fix is not released
+const IgniteCLIRemovalUpgradeHandler = "ignite-cli-removal-upgrade"
 
 // 1.0.2beta upgrades
 const Upgrade102 = "1.0.2beta"
@@ -22,6 +23,9 @@ const Upgrade103 = "1.0.3beta"
 const Upgrade104 = "1.0.4beta"
 
 func (app App) RegisterUpgradeHandlers() {
+	app.UpgradeKeeper.SetUpgradeHandler(IgniteCLIRemovalUpgradeHandler, func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+		return app.mm.RunMigrations(ctx, app.configurator, fromVM)
+	})
 	app.UpgradeKeeper.SetUpgradeHandler(Upgrade102, func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
 		return app.mm.RunMigrations(ctx, app.configurator, fromVM)
 	})
