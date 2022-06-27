b56998e4349ae2e15284bed7823a31ae45bd5986
Uday Patil <udpatil@gmail.com>
2022-06-27 19:03:57 -0400

    [oracle] add codec stuff for combined vote

--- x/oracle/types/codec.go
+++ x/oracle/types/codec.go
@@ -10,15 +10,20 @@ import (
 func RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
 	cdc.RegisterConcrete(&MsgAggregateExchangeRatePrevote{}, "oracle/MsgAggregateExchangeRatePrevote", nil)
 	cdc.RegisterConcrete(&MsgAggregateExchangeRateVote{}, "oracle/MsgAggregateExchangeRateVote", nil)
+	cdc.RegisterConcrete(&MsgAggregateExchangeRateCombinedVote{}, "oracle/MsgAggregateExchangeRateCombinedVote", nil)
 	cdc.RegisterConcrete(&MsgDelegateFeedConsent{}, "oracle/MsgDelegateFeedConsent", nil)
 }
 
+// TODO: update here for combined vote
 func RegisterInterfaces(registry codectypes.InterfaceRegistry) {
 	registry.RegisterImplementations((*sdk.Msg)(nil),
 		&MsgAggregateExchangeRatePrevote{},
 	)
 	registry.RegisterImplementations((*sdk.Msg)(nil),
 		&MsgAggregateExchangeRateVote{},
+	)
+	registry.RegisterImplementations((*sdk.Msg)(nil),
+		&MsgAggregateExchangeRateCombinedVote{},
 	)
 	registry.RegisterImplementations((*sdk.Msg)(nil),
 		&MsgDelegateFeedConsent{},
