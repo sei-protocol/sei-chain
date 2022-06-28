c9332ee17418e344763dce7bf07b51690e7b9077
philipsu522 <philip.su.522@gmail.com>
2022-06-28 09:23:25 -0700

    Programmatically set tm configs

--- cmd/seid/main.go
+++ cmd/seid/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/cosmos/cosmos-sdk/server"
 	"github.com/sei-protocol/sei-chain/app/params"
 	"github.com/sei-protocol/sei-chain/cmd/seid/cmd"
 	"os"
@@ -12,6 +13,8 @@ import (
 func main() {
 	params.SetAddressPrefixes()
 	rootCmd, _ := cmd.NewRootCmd()
+	config := server.GetServerContextFromCmd(rootCmd).Config
+	params.SetTendermintConfigs(config)
 	if err := svrcmd.Execute(rootCmd, app.DefaultNodeHome); err != nil {
 		os.Exit(1)
 	}
