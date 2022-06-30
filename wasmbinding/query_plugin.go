5d2e6dbf3df9d7a2aaa1e679f91de713552052da
Uday Patil <udpatil@gmail.com>
2022-06-30 11:24:51 -0400

    [oracle][wasmbindings] Add twaps query and some more refactoring

--- wasmbinding/query_plugin.go
+++ wasmbinding/query_plugin.go
@@ -33,23 +33,3 @@ func CustomQuerier(qp *QueryPlugin) func(ctx sdk.Context, request json.RawMessag
 		}
 	}
 }
-
-// TODO: do we need this stuff from osmosis implementation? maybe remove
-// ConvertSdkCoinsToWasmCoins converts sdk type coins to wasm vm type coins
-func ConvertSdkCoinsToWasmCoins(coins []sdk.Coin) wasmvmtypes.Coins {
-	var toSend wasmvmtypes.Coins
-	for _, coin := range coins {
-		c := ConvertSdkCoinToWasmCoin(coin)
-		toSend = append(toSend, c)
-	}
-	return toSend
-}
-
-// ConvertSdkCoinToWasmCoin converts a sdk type coin to a wasm vm type coin
-func ConvertSdkCoinToWasmCoin(coin sdk.Coin) wasmvmtypes.Coin {
-	return wasmvmtypes.Coin{
-		Denom: coin.Denom,
-		// Note: gamm tokens have 18 decimal places, so 10^22 is common, no longer in u64 range
-		Amount: coin.Amount.String(),
-	}
-}
