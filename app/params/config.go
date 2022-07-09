eaf95ca44e5aabcb2ee5837f4057f7057f79e0be
Xiaoyu Chen <codchen03@gmail.com>
2022-07-08 22:28:06 -0700

    config tmp change

--- app/params/config.go
+++ app/params/config.go
@@ -1,9 +1,10 @@
 package params
 
 import (
+	"time"
+
 	"github.com/cosmos/cosmos-sdk/types/address"
 	tmcfg "github.com/tendermint/tendermint/config"
-	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
@@ -88,6 +89,6 @@ func SetTendermintConfigs(config *tmcfg.Config) {
 	// Consensus Configs
 	config.Consensus.TimeoutPrevote = 250 * time.Millisecond
 	config.Consensus.TimeoutPrecommit = 250 * time.Millisecond
-	config.Consensus.TimeoutCommit = 250 * time.Millisecond
-	config.Consensus.SkipTimeoutCommit = true
+	config.Consensus.TimeoutCommit = 1000 * time.Millisecond
+	// config.Consensus.SkipTimeoutCommit = true
 }
