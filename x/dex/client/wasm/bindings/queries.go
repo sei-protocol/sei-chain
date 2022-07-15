eaf2a9cdc3fe75b26daf0a5eb8082acabcf92e0f
Uday Patil <udpatil@gmail.com>
2022-07-15 11:09:19 -0700

    [dex] Add get order query functionality (#116)

--- x/dex/client/wasm/bindings/queries.go
+++ x/dex/client/wasm/bindings/queries.go
@@ -4,5 +4,7 @@ import "github.com/sei-protocol/sei-chain/x/dex/types"
 
 type SeiDexQuery struct {
 	// queries the dex TWAPs
-	DexTwaps *types.QueryGetTwapsRequest `json:"dex_twaps,omitempty"`
+	DexTwaps     *types.QueryGetTwapsRequest     `json:"dex_twaps,omitempty"`
+	GetOrders    *types.QueryGetOrdersRequest    `json:"get_orders,omitempty"`
+	GetOrderByID *types.QueryGetOrderByIDRequest `json:"get_order_by_id,omitempty"`
 }
