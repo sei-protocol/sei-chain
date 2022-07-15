bc60113aa356a1bc35153b6ea9cf6935b38fb210
codchen <codchen03@gmail.com>
2022-07-15 11:05:58 -0700

    do not merge

--- x/dex/keeper/end_block_liquidation.go
+++ x/dex/keeper/end_block_liquidation.go
@@ -56,10 +56,12 @@ func (k *Keeper) HandleEBLiquidation(ctx context.Context, sdkCtx sdk.Context, tr
 }
 
 func (k *Keeper) placeLiquidationOrders(ctx sdk.Context, contractAddr string, liquidationOrders []types.Order) {
+	ctx.Logger().Info(fmt.Sprintf("Placing liquidation orders..."))
 	nextID := k.GetNextOrderID(ctx)
 	for _, order := range liquidationOrders {
+		ctx.Logger().Info(fmt.Sprintf("Liquidation order %s", order.String()))
 		pair := types.Pair{PriceDenom: order.PriceDenom, AssetDenom: order.AssetDenom}
-		orders := k.MemState.GetBlockOrders(types.ContractAddress(contractAddr), types.PairString(pair.String()))
+		orders := k.MemState.GetBlockOrders(types.ContractAddress(contractAddr), types.GetPairString(&pair))
 		order.Id = nextID
 		orders.AddOrder(order)
 		nextID++
