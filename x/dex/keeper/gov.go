a756093bf7331c1bd461661892612231d7cb1c78
kbhat1 <kartikbhatri@gmail.com>
2022-07-19 15:12:46 -0700

    remove register pair restriction

--- x/dex/keeper/gov.go
+++ x/dex/keeper/gov.go
@@ -1,8 +1,6 @@
 package keeper
 
 import (
-	"fmt"
-
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/sei-protocol/sei-chain/x/dex/types"
@@ -14,15 +12,6 @@ func (k Keeper) HandleRegisterPairsProposal(ctx sdk.Context, p *types.RegisterPa
 	for _, batchContractPair := range p.Batchcontractpair {
 		contractAddress := batchContractPair.ContractAddr
 		for _, pair := range batchContractPair.Pairs {
-			// Only add registered pairs whose denoms are in AssetList
-			_, found := k.GetAssetMetadataByDenom(ctx, pair.PriceDenom)
-			if !found {
-				fmt.Println("Price Denom", pair.PriceDenom, "is not in the AssetList and can not be registered.")
-			}
-			_, found = k.GetAssetMetadataByDenom(ctx, pair.AssetDenom)
-			if !found {
-				fmt.Println("Asset Denom", pair.AssetDenom, "is not in the AssetList and can not be registered.")
-			}
 			k.AddRegisteredPair(ctx, contractAddress, *pair)
 			k.SetTickSizeForPair(ctx, contractAddress, *pair, *pair.Ticksize)
 		}
