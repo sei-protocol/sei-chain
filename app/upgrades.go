aeb9893ed4df64127535dcf26dab460303c2ecf4
Brandon Weng <18161326+BrandonWeng@users.noreply.github.com>
2022-07-26 21:37:39 -0700

    Assert that upgrade names are sorted (#147)
    * Assert that upgrade names are sorted
    
    * lint
    
    * comments
    
    * remove older versions

--- app/upgrades.go
+++ app/upgrades.go
@@ -1,6 +1,9 @@
 package app
 
 import (
+	"log"
+	"sort"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
@@ -9,57 +12,26 @@ import (
 // NOTE: When performing upgrades, make sure to keep / register the handlers
 // for both the current (n) and the previous (n-1) upgrade name. There is a bug
 // in a missing value in a log statement for which the fix is not released
-const IgniteCLIRemovalUpgradeHandler = "ignite-cli-removal-upgrade"
-
-// 1.0.2beta upgrades
-const Upgrade102 = "1.0.2beta"
-const Upgrade102CommitTimeout = "1.0.2beta-commit-timeout"
-
-// 1.0.3beta
-const Upgrade103 = "1.0.3beta"
-
-// 1.0.4beta
-// this will introduce the oracle module as well
-const Upgrade104 = "1.0.4beta"
-
-// 1.0.5beta
-const Upgrade105 = "1.0.5beta upgrade"
-
-// 1.0.6beta
-const Upgrade106 = "1.0.6beta"
-
-// 1.0.7beta
-const Upgrade107 = "1.0.7beta"
-
-// 1.0.7beta-postfix
-const Upgrade107PostFix = "1.0.7beta-postfix"
+var upgradesList = []string{
+	// 1.0.5beta
+	"1.0.5beta upgrade",
+	// 1.0.6beta
+	"1.0.6beta",
+	// 1.0.7beta
+	"1.0.7beta",
+	// 1.0.7beta-postfix
+	"1.0.7beta-postfix",
+}
 
 func (app App) RegisterUpgradeHandlers() {
-	app.UpgradeKeeper.SetUpgradeHandler(IgniteCLIRemovalUpgradeHandler, func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
-		return app.mm.RunMigrations(ctx, app.configurator, fromVM)
-	})
-	app.UpgradeKeeper.SetUpgradeHandler(Upgrade102, func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
-		return app.mm.RunMigrations(ctx, app.configurator, fromVM)
-	})
-	app.UpgradeKeeper.SetUpgradeHandler(Upgrade102CommitTimeout, func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
-		return app.mm.RunMigrations(ctx, app.configurator, fromVM)
-	})
-	app.UpgradeKeeper.SetUpgradeHandler(Upgrade103, func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
-		return app.mm.RunMigrations(ctx, app.configurator, fromVM)
-	})
-	app.UpgradeKeeper.SetUpgradeHandler(Upgrade104, func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
-		return app.mm.RunMigrations(ctx, app.configurator, fromVM)
-	})
-	app.UpgradeKeeper.SetUpgradeHandler(Upgrade105, func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
-		return app.mm.RunMigrations(ctx, app.configurator, fromVM)
-	})
-	app.UpgradeKeeper.SetUpgradeHandler(Upgrade106, func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
-		return app.mm.RunMigrations(ctx, app.configurator, fromVM)
-	})
-	app.UpgradeKeeper.SetUpgradeHandler(Upgrade107, func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
-		return app.mm.RunMigrations(ctx, app.configurator, fromVM)
-	})
-	app.UpgradeKeeper.SetUpgradeHandler(Upgrade107PostFix, func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
-		return app.mm.RunMigrations(ctx, app.configurator, fromVM)
-	})
+	// Upgrades names must be in alphabetical order
+	// https://github.com/cosmos/cosmos-sdk/issues/11707
+	if !sort.StringsAreSorted(upgradesList) {
+		log.Fatal("New upgrades must be appended to 'upgradesList' in alphabetical order")
+	}
+	for _, upgradeName := range upgradesList {
+		app.UpgradeKeeper.SetUpgradeHandler(upgradeName, func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+			return app.mm.RunMigrations(ctx, app.configurator, fromVM)
+		})
+	}
 }
