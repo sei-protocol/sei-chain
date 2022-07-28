1bf7219779b53916df609b0f320d637c4b4077fb
Eric Karlsson <eric.karlsson@gmail.com>
2022-07-28 21:38:34 +0200

    Added better argument handling and more flexible arguments (#165)
    * Improved argument handling for price feeder
    
    * Better help
    
    * Added interval argument
    
    Co-authored-by: Eric Karlsson <eric.karlsson@smhi.se>

--- scripts/oracle/price_feeder.py
+++ scripts/oracle/price_feeder.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+import argparse
 import time
 import subprocess
 import requests
@@ -5,27 +7,27 @@ from pathlib import Path
 import sys
 from price_fetcher import PriceFetcher
 
-HOME_PATH = str(Path.home())
-CMD = "printf '{password}\n' | {home_path}/go/bin/seid"
+CMD = "printf '{password}\n' | {binary}"
 PREVOTE_TMPL = (
     " tx oracle aggregate-prevote abc 100uusdc,50uatom {val_addr} --from={key} "
-    "--chain-id={chain_id} --fees={fees}usei --gas={gas} -y --broadcast-mode=sync"
+    "--chain-id={chain_id} --fees={fees}usei --gas={gas} -y --broadcast-mode=sync --node={node}"
 )
 VOTE_TMPL = (
     " tx oracle aggregate-vote abc 100uusdc,50uatom {val_addr} --from={key} "
-    "--chain-id={chain_id} --fees={fees}usei --gas={gas} -y --broadcast-mode=sync"
+    "--chain-id={chain_id} --fees={fees}usei --gas={gas} -y --broadcast-mode=sync --node={node}"
 )
 COMBINED_TMPL = (
     " tx oracle aggregate-combined-vote {salt} {prevote_prices} {salt} {vote_prices} {val_addr} --from {key} "
-    "--chain-id={chain_id} --fees={fees}usei --gas={gas} -y --broadcast-mode=sync"
+    "--chain-id={chain_id} --fees={fees}usei --gas={gas} -y --broadcast-mode=sync --node={node}"
 )
 
 class PriceFeeder:
-    def __init__(self, key="admin", password="12345678", chain_id="sei-chain", port="26657", fees="100000", gas="100000") -> None:
+    def __init__(self, key, password, binary, chain_id, node, fees, gas) -> None:
         self.key = key
         self.password = password
+        self.binary = binary
         self.chain_id = chain_id
-        self.port = port
+        self.node = node
         self.fees = fees
         self.gas = gas
         self.addr = ""
@@ -34,33 +36,34 @@ class PriceFeeder:
 
     def init_node_info(self):
         self.addr = subprocess.check_output(
-            [CMD.format(password=self.password, home_path=HOME_PATH) + f" keys show {self.key} -a"], 
+            [CMD.format(password=self.password, binary=self.binary) + f" keys show {self.key} -a"], 
             stderr=subprocess.STDOUT,
             shell=True,
         ).decode()[:-1]
 
         self.val_addr = subprocess.check_output(
-            [CMD.format(password=self.password, home_path=HOME_PATH) + f" query staking delegations {self.addr} | grep validator_address | cut -d':' -f2 | xargs"],
+            [CMD.format(password=self.password, binary=self.binary) + f" query staking delegations {self.addr} --node={self.node} | grep validator_address | cut -d':' -f2 | xargs"],
             stderr=subprocess.STDOUT,
             shell=True,
         ).decode()[:-1]
 
     def get_current_vote_period(self):
-        res = requests.get("http://localhost:{port_num}/blockchain".format(port_num=self.port))
+        res = requests.get("{node}/blockchain".format(node=self.node))
         body = res.json()
         return int(body["result"]["last_height"]) // 10
 
     def vote_for_period(self):
-        print("vote")
+        print("Vote")
         result = subprocess.check_output(
             [
-                CMD.format(password=self.password, home_path=HOME_PATH) + 
+                CMD.format(password=self.password, binary=self.binary) + 
                 VOTE_TMPL.format(
                     key=self.key, 
                     chain_id=self.chain_id, 
                     val_addr=self.val_addr, 
                     gas=self.gas, 
                     fees=self.fees, 
+                    node=self.node
                 )
             ],
             stderr=subprocess.STDOUT,
@@ -68,16 +71,17 @@ class PriceFeeder:
         )
 
     def prevote_for_period(self):
-        print("prevote")
+        print("Prevote")
         result = subprocess.check_output(
             [
-                CMD.format(password=self.password, home_path=HOME_PATH) + 
+                CMD.format(password=self.password, binary=self.binary) + 
                 PREVOTE_TMPL.format(
                     key=self.key, 
                     chain_id=self.chain_id, 
                     val_addr=self.val_addr, 
                     gas=self.gas, 
-                    fees=self.fees, 
+                    fees=self.fees,
+                    node=self.node
                 )
             ],
             stderr=subprocess.STDOUT,
@@ -87,7 +91,7 @@ class PriceFeeder:
     def combined_vote_for_period(self, vote_prices, prevote_prices, salt):
         result = subprocess.check_output(
             [
-                CMD.format(password=self.password, home_path=HOME_PATH) + 
+                CMD.format(password=self.password, binary=self.binary) + 
                 COMBINED_TMPL.format(
                     key=self.key, 
                     chain_id=self.chain_id, 
@@ -96,14 +100,15 @@ class PriceFeeder:
                     vote_prices=vote_prices,
                     gas=self.gas, 
                     fees=self.fees,
-                    salt=salt 
+                    salt=salt,
+                    node=self.node
                 )
             ],
             stderr=subprocess.STDOUT,
             shell=True,
         )
 
-    def vote_loop(self, coins, salt, interval=5):
+    def vote_loop(self, coins, salt, interval):
         last_prevoted_period = -1
         last_voted_period = -1
         last_combined_voted_period = -1
@@ -112,7 +117,7 @@ class PriceFeeder:
         vote_prices = pf.create_price_feed(coins)
         while True:
             time.sleep(interval)
-            print("submitting price feeds ", vote_prices)
+            print("Submitting price feeds ", vote_prices)
             current_vote_period = self.get_current_vote_period()
             if last_combined_voted_period < current_vote_period:
                 prevote_prices = pf.create_price_feed(coins)
@@ -121,23 +126,23 @@ class PriceFeeder:
                 last_combined_voted_period = current_vote_period
 
 def main():
-    args = sys.argv[1:]
-    salt = "abc"
+    parser=argparse.ArgumentParser()
+    parser.add_argument("key", help='Your wallet (key) name', type=str)
+    parser.add_argument("password", help='The keychain password', type=str)
+    parser.add_argument('coins', help='The coins to use', type=str)
+    parser.add_argument('--binary', help='Your seid binary path', type=str, default=str(Path.home()) + '/go/bin/seid')
+    parser.add_argument('--chain_id', help='Your wallet (key) name', type=str, default='sei-chain')
+    parser.add_argument('--node', help='The node to contact', type=str, default='http://localhost:26657')
+    parser.add_argument('--fees', help='The fees to use', type=int, default=100000)
+    parser.add_argument('--gas', help='The gas to use', type=int, default=100000)
+    parser.add_argument('--salt', help='The salt to use', type=str, default='abc')
+    parser.add_argument('--interval', help='How long time to sleep between price checks', type=int, default=5)
+    args=parser.parse_args()
 
-    if len(args) < 4:
-        print("You need to specify your chain-id, key name, password and the list of coins to send an oracle price feed.")
-        return
-    elif len(args) == 4:
-        pf = PriceFeeder(args[0], args[1], args[2])
-    elif len(args) > 4 and len(args) < 8:
-        print("You need to specify entire list of custom parameters to send an oracle price feed.")
-        return
-    else:
-        pf = PriceFeeder(args[0], args[1], args[2], args[4], args[5], args[6])
-        salt = args[7]
+    pf = PriceFeeder(args.key, args.password, args.binary, args.chain_id, args.node, args.fees, args.gas)
 
-    coins = args[3].split(',')
-    pf.vote_loop(coins, salt)
+    coins = args.coins.split(',')
+    pf.vote_loop(coins, args.salt, args.interval)
 
 if __name__ == "__main__":
     main()
