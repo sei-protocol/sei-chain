cd13b9ffc2250221c6306f1855eca8dc0be4c533
codchen <codchen03@gmail.com>
2022-08-03 17:09:41 -0700

    Add whitelist store utility (#171)
    * Add whiteliste store utility
    
    * check prefix

---
+++ store/whitelist/cachemulti/store_test.go
@@ -0,0 +1,53 @@
+package cachemulti_test
+
+import (
+	"testing"
+
+	storetypes "github.com/cosmos/cosmos-sdk/store/types"
+	"github.com/cosmos/cosmos-sdk/types"
+	"github.com/sei-protocol/sei-chain/store"
+	"github.com/sei-protocol/sei-chain/store/whitelist/cachemulti"
+	"github.com/stretchr/testify/require"
+)
+
+var WhitelistedStoreKey = storetypes.NewKVStoreKey("whitelisted")
+var NotWhitelistedStoreKey = storetypes.NewKVStoreKey("not-whitelisted")
+var TestStoreKeyToWriteWhitelist = map[storetypes.StoreKey][]string{
+	WhitelistedStoreKey: {"foo"},
+}
+
+func TestWhitelistEnabledStore(t *testing.T) {
+	stores := map[types.StoreKey]types.CacheWrapper{
+		WhitelistedStoreKey: store.NewTestKVStore(),
+	}
+	multistore := store.NewTestCacheMultiStore(stores)
+	whitelistMultistore := cachemulti.NewStore(multistore, TestStoreKeyToWriteWhitelist)
+	kvStore := whitelistMultistore.GetKVStore(WhitelistedStoreKey)
+	require.Panics(t, func() { kvStore.Delete([]byte("bar")) })
+	require.NotPanics(t, func() { kvStore.Delete([]byte("foo")) })
+}
+
+func TestWhitelistDisabledStore(t *testing.T) {
+	stores := map[types.StoreKey]types.CacheWrapper{
+		WhitelistedStoreKey:    store.NewTestKVStore(),
+		NotWhitelistedStoreKey: store.NewTestKVStore(),
+	}
+	multistore := store.NewTestCacheMultiStore(stores)
+	whitelistMultistore := cachemulti.NewStore(multistore, TestStoreKeyToWriteWhitelist)
+	kvStore := whitelistMultistore.GetKVStore(NotWhitelistedStoreKey)
+	require.NotPanics(t, func() { kvStore.Delete([]byte("bar")) })
+	require.NotPanics(t, func() { kvStore.Delete([]byte("foo")) })
+}
+
+func TestCacheStillWhitelist(t *testing.T) {
+	stores := map[types.StoreKey]types.CacheWrapper{
+		WhitelistedStoreKey:    store.NewTestKVStore(),
+		NotWhitelistedStoreKey: store.NewTestKVStore(),
+	}
+	multistore := store.NewTestCacheMultiStore(stores)
+	whitelistMultistore := cachemulti.NewStore(multistore, TestStoreKeyToWriteWhitelist)
+	cacheWhitelistMultistore := whitelistMultistore.CacheMultiStore()
+	kvStore := cacheWhitelistMultistore.GetKVStore(WhitelistedStoreKey)
+	require.Panics(t, func() { kvStore.Delete([]byte("bar")) })
+	require.NotPanics(t, func() { kvStore.Delete([]byte("foo")) })
+}
