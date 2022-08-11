4f7e9efaaff269526dbbcb8262e76ee8c1774645
Brandon Weng <wengbrandon@gmail.com>
2022-08-10 21:39:56 -0700

    fix

--- x/upgrade/types/proposal.go
+++ x/upgrade/types/proposal.go
@@ -19,7 +19,6 @@ func NewSoftwareUpgradeProposal(title, description string, plan Plan) gov.Conten
 var _ gov.Content = &SoftwareUpgradeProposal{}
 
 func init() {
-	println("Initing!")
 	gov.RegisterProposalType(ProposalTypeSoftwareUpgrade)
 	gov.RegisterProposalTypeCodec(&SoftwareUpgradeProposal{}, "cosmos-sdk/SoftwareUpgradeProposal")
 	gov.RegisterProposalType(ProposalTypeCancelSoftwareUpgrade)
