c0d5a18a64c740f9902b417c11faf1624ab2cf37
Brandon Weng <18161326+BrandonWeng@users.noreply.github.com>
2022-09-08 10:46:26 -0700

    Update params.go

--- x/mint/types/params.go
+++ x/mint/types/params.go
@@ -19,7 +19,7 @@ var (
 	KeyTokenReleaseSchedule   = []byte("TokenReleaseSchedule")
 )
 
-// ParamTable for minting m1odule.
+// ParamTable for minting module.
 func ParamKeyTable() paramtypes.KeyTable {
 	return paramtypes.NewKeyTable().RegisterParamSet(&Params{})
 }
