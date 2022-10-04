32d4a2b18adb4d290650e15423c982b60a14a6d4
Kbhat1 <kartikbhatri@gmail.com>
2022-10-04 17:48:02 -0400

    Remove Community Tax (#278)
    * Update gen state to remove community tax
    
    * Add store migration to distribution
    
    * Add distr migration test

--- app/upgrade_test.go
+++ app/upgrade_test.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/sei-protocol/sei-chain/app"
 )
 
@@ -13,4 +14,14 @@ func TestUpgradesListIsSorted(t *testing.T) {
 	valPub := secp256k1.GenPrivKey().PubKey()
 	testWrapper := app.NewTestWrapper(t, tm, valPub)
 	testWrapper.App.RegisterUpgradeHandlers()
+}
+
+// Test community tax param is set to 0 as part of upgrade 1.2.3beta
+func TestDistributionCommunityTaxParamMigration(t *testing.T) {
+	tm := time.Now().UTC()
+	valPub := secp256k1.GenPrivKey().PubKey()
+	testWrapper := app.NewTestWrapper(t, tm, valPub)
+	testWrapper.App.RegisterUpgradeHandlers()
+	params := testWrapper.App.DistrKeeper.GetParams(testWrapper.Ctx)
+	testWrapper.Require().Equal(params.CommunityTax, sdk.NewDec(0))
 }
