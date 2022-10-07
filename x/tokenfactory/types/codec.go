7769767d3c16a32d3667d0e8730903343e12ffb0
Kbhat1 <kartikbhatri@gmail.com>
2022-10-07 13:43:16 -0400

    Add AddCreatorsToDenomFeeWhitelistProposal and MsgRegisterContract to Codecs (#292)
    * Add to codecs
    
    * Add govtypes
    
    * Format

--- x/tokenfactory/types/codec.go
+++ x/tokenfactory/types/codec.go
@@ -4,6 +4,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
 
 	// this line is used by starport scaffolding # 1
 	"github.com/cosmos/cosmos-sdk/types/msgservice"
@@ -15,17 +16,29 @@ func RegisterCodec(cdc *codec.LegacyAmino) {
 	cdc.RegisterConcrete(&MsgBurn{}, "tokenfactory/burn", nil)
 	// cdc.RegisterConcrete(&MsgForceTransfer{}, "tokenfactory/force-transfer", nil)
 	cdc.RegisterConcrete(&MsgChangeAdmin{}, "tokenfactory/change-admin", nil)
+	cdc.RegisterConcrete(&AddCreatorsToDenomFeeWhitelistProposal{}, "tokenfactory/add-creators-to-denom-fee-whitelist-proposal", nil)
 }
 
 func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
-	registry.RegisterImplementations(
-		(*sdk.Msg)(nil),
+	registry.RegisterImplementations((*sdk.Msg)(nil),
 		&MsgCreateDenom{},
+	)
+	registry.RegisterImplementations((*sdk.Msg)(nil),
 		&MsgMint{},
+	)
+	registry.RegisterImplementations((*sdk.Msg)(nil),
 		&MsgBurn{},
-		// &MsgForceTransfer{},
+	)
+	registry.RegisterImplementations((*sdk.Msg)(nil),
 		&MsgChangeAdmin{},
 	)
+	registry.RegisterImplementations((*govtypes.Content)(nil),
+		&AddCreatorsToDenomFeeWhitelistProposal{},
+	)
+	// registry.RegisterImplementations((*govtypes.Content)(nil),
+	// 	&MsgForceTransfer{},
+	// )
+
 	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
 }
 
