567ffd951205b1be1f57460dcebd6191275054a6
Brandon Weng <18161326+BrandonWeng@users.noreply.github.com>
2022-10-09 20:00:02 -0700

    Add more parallel TX metrics (#296)
    * Add more parallel TX latency
    
    * lint

--- utils/metrics/labels.go
+++ utils/metrics/labels.go
@@ -1,6 +1,8 @@
 package metrics
 
 const (
-	CONCURRENT  = "concurrent"
-	SYNCHRONOUS = "synchronous"
+	CONCURRENT    = "concurrent"
+	SYNCHRONOUS   = "synchronous"
+	GovMsgInBlock = "gov-msg-in-block"
+	FailedToBuild = "failed-to-build"
 )
