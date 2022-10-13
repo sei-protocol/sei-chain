2ec2770e836c9c3ee7e663b22a81bed2a9788e52
philipsu522 <philip.su.522@gmail.com>
2022-10-13 10:01:16 -0700

    More fixes

--- sei-cosmos/x/accesscontrol/module.go
+++ sei-cosmos/x/accesscontrol/module.go
@@ -139,7 +139,7 @@ func (am AppModule) ExportGenesis(ctx sdk.Context, cdc codec.JSONCodec) json.Raw
 }
 
 // ConsensusVersion implements AppModule/ConsensusVersion.
-func (AppModule) ConsensusVersion() uint64 { return 2 }
+func (AppModule) ConsensusVersion() uint64 { return 1 }
 
 // BeginBlock returns the begin blocker for the accesscontrol module.
 func (am AppModule) BeginBlock(ctx sdk.Context, _ abci.RequestBeginBlock) {}
