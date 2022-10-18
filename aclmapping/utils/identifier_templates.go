18d885963e35e54c1294ebb8da07374d36350cff
codchen <codchen03@gmail.com>
2022-10-18 08:57:54 -0700

    Add dependencies for ante handlers that read/write accounts (#314)

--- aclmapping/utils/identifier_templates.go
+++ aclmapping/utils/identifier_templates.go
@@ -3,6 +3,7 @@ package util
 import "fmt"
 
 const (
+	ACCOUNT           = "acc"
 	BANK              = "bank"
 	AUTH              = "auth"
 	DefaultIDTemplate = "*"
@@ -10,4 +11,8 @@ const (
 
 func GetIdentifierTemplatePerModule(module string, identifier string) string {
 	return fmt.Sprintf("%s/%s", module, identifier)
+}
+
+func GetPrefixedIdentifierTemplatePerModule(module string, identifier string, prefix string) string {
+	return fmt.Sprintf("%s/%s/%s", module, prefix, identifier)
 }
