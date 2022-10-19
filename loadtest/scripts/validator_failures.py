eb967734c8692fe79a5c3572cd2242aad253ec20
philipsu522 <philip.su.522@gmail.com>
2022-10-19 08:05:07 -0700

    Fixes

--- loadtest/scripts/validator_failures.py
+++ loadtest/scripts/validator_failures.py
@@ -47,16 +47,14 @@ def delegation_change(chain_id):
     for i in range(len(validator_addrs // 4)):
         delegations[validator_addrs[i]] = 10 * total_delegation
         print(f"Delegating {10 * total_delegation} to {validator_addrs[i]}")
-        seid_staking_cmd = f"printf '12345678\n' seid tx staking delegate {validator_addrs[i]} {10 * total_delegation}usei --from admin --chain-id {chain_id} -b block -y"
-        subprocess.check_output([seid_staking_cmd], stderr=subprocess.STDOUT,
-                                shell=True)
+        seid_staking_cmd = f"seid tx staking delegate {validator_addrs[i]} {10 * total_delegation}usei --from admin --chain-id {chain_id} -b block -y"
+        _run_seid_cmd(seid_staking_cmd)
     time.sleep(random.randint(600, 3600))
     # Unbond
     for validator in delegations:
         print(f"Unbonding {10 * total_delegation} from {validator}")
-        seid_unbond_cmd = f"printf '12345678\n' seid tx staking unbond {validator} {delegations[validator]}usei --from admin -b block -y --chain-id {chain_id}"
-        subprocess.check_output([seid_unbond_cmd], stderr=subprocess.STDOUT,
-                                shell=True)
+        seid_unbond_cmd = f"seid tx staking unbond {validator} {delegations[validator]}usei --from admin -b block -y --chain-id {chain_id}"
+        _run_seid_cmd(seid_unbond_cmd)
 
 
 def slow_network(validators, ssh_key, num_concurrent):
@@ -87,27 +85,28 @@ def slow_network(validators, ssh_key, num_concurrent):
 
 
 def _get_admin_acc():
-    seid_query_cmd = "printf '12345678\n' seid keys list --output json"
-    accs_output = json.loads(
-        subprocess.check_output([seid_query_cmd], stderr=subprocess.STDOUT,
-                                shell=True))
+    seid_query_cmd = "seid keys list --output json"
+    accs_output = _run_seid_cmd(seid_query_cmd)
     return filter(lambda x: x['name'] == 'admin', accs_output)['address']
 
+def _run_seid_cmd(cmd)
+    seid_cmd = "printf '12345678\n' " + cmd
+    output = json.loads(
+        subprocess.check_output([seid_cmd], stderr=subprocess.STDOUT,
+                                shell=True))
+    return output
 
 def main():
     parser = argparse.ArgumentParser(
         description="Runs various failure scenarios on validators. Note that delegation change must be run on genesis validator")
     parser.add_argument('-s', '--ssh-key', type=str,
                         help='Ssh key for validator nodes')
-    parser.add_argument('-f', '--failure', type=str,
+    parser.add_argument('-f', '--failure', type=str, choices=["restart", "delegation-change", "slow-network"],
                         help='Type of failure to run. Should be one of ["restart", "delegation-change", "slow-network"]')
     parser.add_argument('-v', '--validators', type=str,
                         help='Comma separated list of validator nodes')
     parser.add_argument('-c', '--chain-id', type=str, help='Chain id')
     args = parser.parse_args()
-    assert args.failure in ["restart", "delegation-change",
-                            "slow-network"], "Failure unrecognized {failure}".format(
-        failure=args.failure)
     if args.failure == "restart":
         failure_restarts(args.validators.split(','), args.ssh_key, 2)
     elif args.failure == "delegation-change":
