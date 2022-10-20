0c1f69cdf236df18cd13d952d7d89fc749725c6f
Brandon Weng <brandon@Brandons-MacBook-Pro.local>
2022-10-20 15:26:42 -0400

    Add concurrency for BurnMsg and MintMsg

--- x/tokenfactory/types/expected_keepers.go
+++ x/tokenfactory/types/expected_keepers.go
@@ -15,6 +15,12 @@ type BankKeeper interface {
 
 	SendCoinsFromModuleToAccount(ctx sdk.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error
 	SendCoinsFromAccountToModule(ctx sdk.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error
+
+	DeferredSendCoinsFromModuleToAccount(ctx sdk.Context, senderModule string, recipientAddr sdk.AccAddress, amount sdk.Coins) error
+	DeferredSendCoinsFromAccountToModule(ctx sdk.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error
+	DeferredMintCoins(ctx sdk.Context, moduleName string, amt sdk.Coins) error
+	DeferredBurnCoins(ctx sdk.Context, moduleName string, amt sdk.Coins) error
+
 	DelegateCoinsFromAccountToModule(ctx sdk.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error
 	UndelegateCoinsFromModuleToAccount(ctx sdk.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error
 	MintCoins(ctx sdk.Context, moduleName string, amt sdk.Coins) error
