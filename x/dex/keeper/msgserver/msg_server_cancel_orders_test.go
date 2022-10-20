ebd585f9dfcaa8ff08419a9b101a2f70bd9669c2
codchen <codchen03@gmail.com>
2022-10-20 12:29:24 +0800

    fix tests

--- x/dex/keeper/msgserver/msg_server_cancel_orders_test.go
+++ x/dex/keeper/msgserver/msg_server_cancel_orders_test.go
@@ -56,35 +56,37 @@ func TestCancelOrder(t *testing.T) {
 	require.Nil(t, err)
 	require.Equal(t, 1, len(pairBlockCancellations.Get()))
 	require.Equal(t, uint64(1), pairBlockCancellations.Get()[0].Id)
+	require.Equal(t, sdk.OneDec(), pairBlockCancellations.Get()[0].Price)
+	require.Equal(t, "atom", pairBlockCancellations.Get()[0].AssetDenom)
+	require.Equal(t, "usdc", pairBlockCancellations.Get()[0].PriceDenom)
 	require.Equal(t, keepertest.TestAccount, pairBlockCancellations.Get()[0].Creator)
+	require.Equal(t, keepertest.TestContract, pairBlockCancellations.Get()[0].ContractAddr)
 }
 
 func TestInvalidCancels(t *testing.T) {
 	// nil cancel price
 	keeper, ctx := keepertest.DexKeeper(t)
-	keeper.AddRegisteredPair(ctx, keepertest.TestContract, keepertest.TestPair)
-	keeper.SetTickSizeForPair(ctx, keepertest.TestContract, keepertest.TestPair, *keepertest.TestPair.Ticksize)
-	keeper.SetLongBook(ctx, keepertest.TestContract, types.LongBook{
-		Price: sdk.OneDec(),
-		Entry: &types.OrderEntry{
-			Price:      sdk.OneDec(),
-			Quantity:   sdk.MustNewDecFromStr("2"),
-			PriceDenom: keepertest.TestPriceDenom,
-			AssetDenom: keepertest.TestAssetDenom,
-			Allocations: []*types.Allocation{
-				{
-					Account:  keepertest.TestAccount,
-					OrderId:  1,
-					Quantity: sdk.MustNewDecFromStr("2"),
-				},
+	msg := &types.MsgCancelOrders{
+		Creator:      keepertest.TestAccount,
+		ContractAddr: keepertest.TestContract,
+		Cancellations: []*types.Cancellation{
+			{
+				PositionDirection: types.PositionDirection_LONG,
+				PriceDenom:        keepertest.TestPriceDenom,
+				AssetDenom:        keepertest.TestAssetDenom,
+				Id:                1,
 			},
 		},
-	})
+	}
+	keeper.AddRegisteredPair(ctx, keepertest.TestContract, keepertest.TestPair)
+	keeper.SetTickSizeForPair(ctx, keepertest.TestContract, keepertest.TestPair, *keepertest.TestPair.Ticksize)
 	wctx := sdk.WrapSDKContext(ctx)
 	server := msgserver.NewMsgServerImpl(*keeper)
+	_, err := server.CancelOrders(wctx, msg)
+	require.NotNil(t, err)
 
 	// nil creator
-	msg := &types.MsgCancelOrders{
+	msg = &types.MsgCancelOrders{
 		ContractAddr: keepertest.TestContract,
 		Cancellations: []*types.Cancellation{
 			{
@@ -96,6 +98,54 @@ func TestInvalidCancels(t *testing.T) {
 			},
 		},
 	}
-	_, err := server.CancelOrders(wctx, msg)
+	_, err = server.CancelOrders(wctx, msg)
+	require.NotNil(t, err)
+
+	// nil contract address
+	msg = &types.MsgCancelOrders{
+		Creator: keepertest.TestAccount,
+		Cancellations: []*types.Cancellation{
+			{
+				Price:             sdk.OneDec(),
+				PositionDirection: types.PositionDirection_LONG,
+				PriceDenom:        keepertest.TestPriceDenom,
+				AssetDenom:        keepertest.TestAssetDenom,
+				Id:                1,
+			},
+		},
+	}
+	_, err = server.CancelOrders(wctx, msg)
+	require.NotNil(t, err)
+
+	// nil price denom
+	msg = &types.MsgCancelOrders{
+		Creator:      keepertest.TestAccount,
+		ContractAddr: keepertest.TestContract,
+		Cancellations: []*types.Cancellation{
+			{
+				Price:             sdk.OneDec(),
+				PositionDirection: types.PositionDirection_LONG,
+				AssetDenom:        keepertest.TestAssetDenom,
+				Id:                1,
+			},
+		},
+	}
+	_, err = server.CancelOrders(wctx, msg)
+	require.NotNil(t, err)
+
+	// nil asset denom
+	msg = &types.MsgCancelOrders{
+		Creator:      keepertest.TestAccount,
+		ContractAddr: keepertest.TestContract,
+		Cancellations: []*types.Cancellation{
+			{
+				Price:             sdk.OneDec(),
+				PositionDirection: types.PositionDirection_LONG,
+				PriceDenom:        keepertest.TestPriceDenom,
+				Id:                1,
+			},
+		},
+	}
+	_, err = server.CancelOrders(wctx, msg)
 	require.NotNil(t, err)
 }
