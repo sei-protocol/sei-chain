d387d497006c689440ba897005b4432ad2c466a5
Brandon Weng <18161326+BrandonWeng@users.noreply.github.com>
2022-10-24 13:56:48 -0400

    Refactor Loadtest script (#336)
    * Refactored
    
    * Refactor loadtest client
    
    * ido
    
    * undo old
    
    * lint
    
    * Update main.go

---
+++ loadtest/loadtest_client.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+	"sync"
+	"time"
+
+	typestx "github.com/cosmos/cosmos-sdk/types/tx"
+	"google.golang.org/grpc"
+)
+
+type LoadTestClient struct {
+	LoadTestConfig  Config
+	TestConfig      EncodingConfig
+	TxClient        typestx.ServiceClient
+	SignerClient    *SignerClient
+	ChainID         string
+	TxHashList      []string
+	TxHashListMutex *sync.Mutex
+	GrpcConn        *grpc.ClientConn
+}
+
+func NewLoadTestClient() *LoadTestClient {
+	grpcConn, _ := grpc.Dial(
+		"127.0.0.1:9090",
+		grpc.WithInsecure(),
+	)
+	TxClient := typestx.NewServiceClient(grpcConn)
+
+	config := Config{}
+	pwd, _ := os.Getwd()
+	file, _ := os.ReadFile(pwd + "/loadtest/config.json")
+	if err := json.Unmarshal(file, &config); err != nil {
+		panic(err)
+	}
+
+	return &LoadTestClient{
+		LoadTestConfig:  config,
+		TestConfig:      TestConfig,
+		TxClient:        TxClient,
+		SignerClient:    NewSignerClient(),
+		ChainID:         config.ChainID,
+		TxHashList:      []string{},
+		TxHashListMutex: &sync.Mutex{},
+		GrpcConn:        grpcConn,
+	}
+}
+
+func (c *LoadTestClient) Close() {
+	c.GrpcConn.Close()
+}
+
+func (c *LoadTestClient) AppendTxHash(txHash string) {
+	c.TxHashListMutex.Lock()
+	defer c.TxHashListMutex.Unlock()
+
+	c.TxHashList = append(c.TxHashList, txHash)
+}
+
+func (c *LoadTestClient) WriteTxHashToFile() {
+	userHomeDir, _ := os.UserHomeDir()
+	_ = os.Mkdir(filepath.Join(userHomeDir, "outputs"), os.ModePerm)
+	filename := filepath.Join(userHomeDir, "outputs", "test_tx_hash")
+	_ = os.Remove(filename)
+	file, _ := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0o644)
+	for _, txHash := range c.TxHashList {
+		txHashLine := fmt.Sprintf("%s\n", txHash)
+		if _, err := file.WriteString(txHashLine); err != nil {
+			panic(err)
+		}
+	}
+}
+
+func (c *LoadTestClient) BuildTxs() (workgroups []*sync.WaitGroup, sendersList [][]func()) {
+	config := c.LoadTestConfig
+	numberOfAccounts := config.TxsPerBlock / config.MsgsPerTx * 2 // * 2 because we need two sets of accounts
+	activeAccounts := []int{}
+	inactiveAccounts := []int{}
+
+	for i := 0; i < int(numberOfAccounts); i++ {
+		if i%2 == 0 {
+			activeAccounts = append(activeAccounts, i)
+		} else {
+			inactiveAccounts = append(inactiveAccounts, i)
+		}
+	}
+
+	for i := 0; i < int(config.Rounds); i++ {
+		fmt.Printf("Preparing %d-th round\n", i)
+
+		wg := &sync.WaitGroup{}
+		var senders []func()
+		workgroups = append(workgroups, wg)
+
+		for _, account := range activeAccounts {
+			key := c.SignerClient.GetKey(uint64(account))
+
+			msg := generateMessage(config, key, config.MsgsPerTx)
+			txBuilder := TestConfig.TxConfig.NewTxBuilder()
+			_ = txBuilder.SetMsgs(msg)
+			seqDelta := uint64(i / 2)
+			mode := typestx.BroadcastMode_BROADCAST_MODE_SYNC
+
+			// Note: There is a potential race condition here with seqnos
+			// in which a later seqno is delievered before an earlier seqno
+			// In practice, we haven't run into this issue so we'll leave this
+			// as is.
+			sender := SendTx(key, &txBuilder, mode, seqDelta, *c)
+			wg.Add(1)
+			senders = append(senders, func() {
+				defer wg.Done()
+				sender()
+			})
+		}
+
+		sendersList = append(sendersList, senders)
+		inactiveAccounts, activeAccounts = activeAccounts, inactiveAccounts
+	}
+
+	return workgroups, sendersList
+}
+
+func (c *LoadTestClient) SendTxs(workgroups []*sync.WaitGroup, sendersList [][]func()) {
+	lastHeight := getLastHeight()
+	for i := 0; i < int(c.LoadTestConfig.Rounds); i++ {
+		newHeight := getLastHeight()
+		for newHeight == lastHeight {
+			time.Sleep(10 * time.Millisecond)
+			newHeight = getLastHeight()
+		}
+		fmt.Printf("Sending %d-th block\n", i)
+		senders := sendersList[i]
+		wg := workgroups[i]
+		for _, sender := range senders {
+			go sender()
+		}
+		wg.Wait()
+		lastHeight = newHeight
+	}
+}
--- loadtest/main.go
+++ loadtest/main.go
@@ -4,105 +4,24 @@ import (
 	"encoding/json"
 	"fmt"
 	"math/rand"
-	"os"
 	"os/exec"
-	"path/filepath"
 	"strconv"
-	"sync"
 	"time"
 
 	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
+	"github.com/cosmos/cosmos-sdk/std"
 
+	"github.com/cosmos/cosmos-sdk/x/auth/tx"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 
-	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/codec/types"
-	"github.com/cosmos/cosmos-sdk/std"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	typestx "github.com/cosmos/cosmos-sdk/types/tx"
-	"github.com/cosmos/cosmos-sdk/x/auth/tx"
 	"github.com/sei-protocol/sei-chain/app"
-	"github.com/sei-protocol/sei-chain/utils"
 	dextypes "github.com/sei-protocol/sei-chain/x/dex/types"
-	"google.golang.org/grpc"
 )
 
-type EncodingConfig struct {
-	InterfaceRegistry types.InterfaceRegistry
-	// NOTE: this field will be renamed to Codec
-	Marshaler codec.Codec
-	TxConfig  client.TxConfig
-	Amino     *codec.LegacyAmino
-}
-
-type Config struct {
-	BatchSize      uint64                `json:"batch_size"`
-	ChainID        string                `json:"chain_id"`
-	OrdersPerBlock uint64                `json:"orders_per_block"`
-	Rounds         uint64                `json:"rounds"`
-	MessageType    string                `json:"message_type"`
-	PriceDistr     NumericDistribution   `json:"price_distribution"`
-	QuantityDistr  NumericDistribution   `json:"quantity_distribution"`
-	MsgTypeDistr   MsgTypeDistribution   `json:"message_type_distribution"`
-	ContractDistr  ContractDistributions `json:"contract_distribution"`
-}
-
-type NumericDistribution struct {
-	Min         sdk.Dec `json:"min"`
-	Max         sdk.Dec `json:"max"`
-	NumDistinct int64   `json:"number_of_distinct_values"`
-}
-
-func (d *NumericDistribution) Sample() sdk.Dec {
-	steps := sdk.NewDec(rand.Int63n(d.NumDistinct))
-	return d.Min.Add(d.Max.Sub(d.Min).QuoInt64(d.NumDistinct).Mul(steps))
-}
-
-type MsgTypeDistribution struct {
-	LimitOrderPct  sdk.Dec `json:"limit_order_percentage"`
-	MarketOrderPct sdk.Dec `json:"market_order_percentage"`
-}
-
-func (d *MsgTypeDistribution) Sample() string {
-	if !d.LimitOrderPct.Add(d.MarketOrderPct).Equal(sdk.OneDec()) {
-		panic("Distribution percentages must add up to 1")
-	}
-	randNum := sdk.MustNewDecFromStr(fmt.Sprintf("%f", rand.Float64()))
-	if randNum.LT(d.LimitOrderPct) {
-		return "limit"
-	}
-	return "market"
-}
-
-type ContractDistributions []ContractDistribution
-
-func (d *ContractDistributions) Sample() string {
-	if !utils.Reduce(*d, func(i ContractDistribution, o sdk.Dec) sdk.Dec { return o.Add(i.Percentage) }, sdk.ZeroDec()).Equal(sdk.OneDec()) {
-		panic("Distribution percentages must add up to 1")
-	}
-	randNum := sdk.MustNewDecFromStr(fmt.Sprintf("%f", rand.Float64()))
-	cumPct := sdk.ZeroDec()
-	for _, dist := range *d {
-		cumPct = cumPct.Add(dist.Percentage)
-		if randNum.LTE(cumPct) {
-			return dist.ContractAddr
-		}
-	}
-	panic("this should never be triggered")
-}
-
-type ContractDistribution struct {
-	ContractAddr string  `json:"contract_address"`
-	Percentage   sdk.Dec `json:"percentage"`
-}
-
-var (
-	TestConfig EncodingConfig
-	TxClient   typestx.ServiceClient
-	TxHashFile *os.File
-	ChainID    string
-)
+var TestConfig EncodingConfig
 
 const (
 	VortexData = "{\"position_effect\":\"Open\",\"leverage\":\"1\"}"
@@ -127,93 +46,30 @@ func init() {
 	app.ModuleBasics.RegisterInterfaces(TestConfig.InterfaceRegistry)
 }
 
-func run(config Config) {
-	ChainID = config.ChainID
-	grpcConn, _ := grpc.Dial(
-		"127.0.0.1:9090",
-		grpc.WithInsecure(),
-	)
-	defer grpcConn.Close()
-	TxClient = typestx.NewServiceClient(grpcConn)
-	userHomeDir, _ := os.UserHomeDir()
-	_ = os.Mkdir(filepath.Join(userHomeDir, "outputs"), os.ModePerm)
-	filename := filepath.Join(userHomeDir, "outputs", "test_tx_hash")
-	_ = os.Remove(filename)
-	file, _ := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0o644)
-	TxHashFile = file
-	var mu sync.Mutex
-	batchSize := config.BatchSize
-	if config.OrdersPerBlock < batchSize {
-		panic("Must have more orders per block than batch size")
-	}
+func run() {
+	client := NewLoadTestClient()
+	config := client.LoadTestConfig
 
-	numberOfAccounts := config.OrdersPerBlock / batchSize * 2 // * 2 because we need two sets of accounts
-	activeAccounts := []int{}
-	inactiveAccounts := []int{}
-	for i := 0; i < int(numberOfAccounts); i++ {
-		if i%2 == 0 {
-			activeAccounts = append(activeAccounts, i)
-		} else {
-			inactiveAccounts = append(inactiveAccounts, i)
-		}
+	defer client.Close()
+
+	if config.TxsPerBlock < config.MsgsPerTx {
+		panic("Must have more TxsPerBlock than MsgsPerTx")
 	}
-	wgs := []*sync.WaitGroup{}
-	sendersList := [][]func(){}
 
 	configString, _ := json.Marshal(config)
-	fmt.Printf("Running with \n %s \ns", string(configString))
+	fmt.Printf("Running with \n %s \n", string(configString))
 
 	fmt.Printf("%s - Starting block prepare\n", time.Now().Format("2006-01-02T15:04:05"))
-	for i := 0; i < int(config.Rounds); i++ {
-		fmt.Printf("Preparing %d-th round\n", i)
-		wg := &sync.WaitGroup{}
-		var senders []func()
-		wgs = append(wgs, wg)
-		for _, account := range activeAccounts {
-			key := GetKey(uint64(account))
+	workgroups, sendersList := client.BuildTxs()
 
-			msg := generateMessage(config, key, batchSize)
-			txBuilder := TestConfig.TxConfig.NewTxBuilder()
-			_ = txBuilder.SetMsgs(msg)
-			seqDelta := uint64(i / 2)
-			mode := typestx.BroadcastMode_BROADCAST_MODE_SYNC
+	client.SendTxs(workgroups, sendersList)
 
-			// Note: There is a potential race condition here with seqnos
-			// in which a later seqno is delievered before an earlier seqno
-			// In practice, we haven't run into this issue so we'll leave this
-			// as is.
-			sender := SendTx(key, &txBuilder, mode, seqDelta, &mu)
-			wg.Add(1)
-			senders = append(senders, func() {
-				defer wg.Done()
-				sender()
-			})
-		}
-		sendersList = append(sendersList, senders)
-
-		inactiveAccounts, activeAccounts = activeAccounts, inactiveAccounts
-	}
-
-	lastHeight := getLastHeight()
-	for i := 0; i < int(config.Rounds); i++ {
-		newHeight := getLastHeight()
-		for newHeight == lastHeight {
-			time.Sleep(10 * time.Millisecond)
-			newHeight = getLastHeight()
-		}
-		fmt.Printf("Sending %d-th block\n", i)
-		senders := sendersList[i]
-		wg := wgs[i]
-		for _, sender := range senders {
-			go sender()
-		}
-		wg.Wait()
-		lastHeight = newHeight
-	}
+	// Records the resulting TxHash to file
+	client.WriteTxHashToFile()
 	fmt.Printf("%s - Finished\n", time.Now().Format("2006-01-02T15:04:05"))
 }
 
-func generateMessage(config Config, key cryptotypes.PrivKey, batchSize uint64) sdk.Msg {
+func generateMessage(config Config, key cryptotypes.PrivKey, msgPerTx uint64) sdk.Msg {
 	var msg sdk.Msg
 	switch config.MessageType {
 	case "basic":
@@ -243,7 +99,7 @@ func generateMessage(config Config, key cryptotypes.PrivKey, batchSize uint64) s
 		price := config.PriceDistr.Sample()
 		quantity := config.QuantityDistr.Sample()
 		contract := config.ContractDistr.Sample()
-		for j := 0; j < int(batchSize); j++ {
+		for j := 0; j < int(msgPerTx); j++ {
 			orderPlacements = append(orderPlacements, &dextypes.Order{
 				Account:           sdk.AccAddress(key.PubKey().Address()).String(),
 				ContractAddr:      contract,
@@ -289,11 +145,5 @@ func getLastHeight() int {
 }
 
 func main() {
-	config := Config{}
-	pwd, _ := os.Getwd()
-	file, _ := os.ReadFile(pwd + "/loadtest/config.json")
-	if err := json.Unmarshal(file, &config); err != nil {
-		panic(err)
-	}
-	run(config)
+	run()
 }
