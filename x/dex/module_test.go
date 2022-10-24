f7471de8a697b97537503e9d38fd7e6a2afc1697
philipsu522 <philip.su.522@gmail.com>
2022-10-24 11:31:20 -0700

    Fix tokens

--- x/dex/module_test.go
+++ x/dex/module_test.go
@@ -54,7 +54,7 @@ func TestEndBlockMarketOrder(t *testing.T) {
 	pair := types.Pair{PriceDenom: "SEI", AssetDenom: "ATOM"}
 
 	testAccount, _ := sdk.AccAddressFromBech32("sei1yezq49upxhunjjhudql2fnj5dgvcwjj87pn2wx")
-	amounts := sdk.NewCoins(sdk.NewCoin("usei", sdk.NewInt(10000000)))
+	amounts := sdk.NewCoins(sdk.NewCoin("usei", sdk.NewInt(10000000)), sdk.NewCoin("uusdc", sdk.NewInt(10000000)))
 	bankkeeper := testApp.BankKeeper
 	bankkeeper.MintCoins(ctx, minttypes.ModuleName, amounts)
 	bankkeeper.SendCoinsFromModuleToAccount(ctx, minttypes.ModuleName, testAccount, amounts)
