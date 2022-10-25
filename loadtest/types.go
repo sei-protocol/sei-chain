1324183b965df5796aa05a776098bfacd8b0e4d6
Eric Zhu <ericzhu77@gmail.com>
2022-10-25 01:35:02 -0400

    Ezhu/staking dep gen mappings v2 rebase (#338)
    * staking dynamic dependency generation mapping
    
    * try staking messages
    
    * update param name
    
    * update config
    
    * update msg distribution json in config
    
    * fix linting error
    
    * revert to basic msg type

--- loadtest/types.go
+++ loadtest/types.go
@@ -66,20 +66,43 @@ func (d *NumericDistribution) InvalidSample() sdk.Dec {
 	return d.Max.Add(d.Max.Sub(d.Min).QuoInt64(d.NumDistinct).Mul(steps))
 }
 
-type MsgTypeDistribution struct {
+type DexMsgTypeDistribution struct {
 	LimitOrderPct  sdk.Dec `json:"limit_order_percentage"`
 	MarketOrderPct sdk.Dec `json:"market_order_percentage"`
 }
 
-func (d *MsgTypeDistribution) Sample() string {
-	if !d.LimitOrderPct.Add(d.MarketOrderPct).Equal(sdk.OneDec()) {
+type StakingMsgTypeDistribution struct {
+	DelegatePct        sdk.Dec `json:"delegate_percentage"`
+	UndelegatePct      sdk.Dec `json:"undelegate_percentage"`
+	BeginRedelegatePct sdk.Dec `json:"begin_redelegate_percentage"`
+}
+type MsgTypeDistribution struct {
+	Dex     DexMsgTypeDistribution     `json:"dex"`
+	Staking StakingMsgTypeDistribution `json:"staking"`
+}
+
+func (d *MsgTypeDistribution) SampleDexMsgs() string {
+	if !d.Dex.LimitOrderPct.Add(d.Dex.MarketOrderPct).Equal(sdk.OneDec()) {
 		panic("Distribution percentages must add up to 1")
 	}
 	randNum := sdk.MustNewDecFromStr(fmt.Sprintf("%f", rand.Float64()))
-	if randNum.LT(d.LimitOrderPct) {
+	if randNum.LT(d.Dex.LimitOrderPct) {
 		return Limit
 	}
 	return Market
+}
+
+func (d *MsgTypeDistribution) SampleStakingMsgs() string {
+	if !d.Staking.DelegatePct.Add(d.Staking.UndelegatePct).Add(d.Staking.BeginRedelegatePct).Equal(sdk.OneDec()) {
+		panic("Distribution percentages must add up to 1")
+	}
+	randNum := sdk.MustNewDecFromStr(fmt.Sprintf("%f", rand.Float64()))
+	if randNum.LT(d.Staking.DelegatePct) {
+		return "delegate"
+	} else if randNum.LT(d.Staking.DelegatePct.Add(d.Staking.UndelegatePct)) {
+		return "undelegate"
+	}
+	return "begin_redelegate"
 }
 
 type ContractDistributions []ContractDistribution
