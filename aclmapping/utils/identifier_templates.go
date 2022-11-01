91dca961a0f62d6d971c47babb7a40238d89f3e7
Uday Patil <udpatil@gmail.com>
2022-11-01 11:53:59 -0400

    Revert "Add dependencies for ante handlers that read/write accounts (#314)"
    This reverts commit 5d39f905aadbb15ae0a224c353e69ad7634dce8a.

--- aclmapping/utils/identifier_templates.go
+++ aclmapping/utils/identifier_templates.go
@@ -3,7 +3,6 @@ package util
 import "fmt"
 
 const (
-	ACCOUNT           = "acc"
 	BANK              = "bank"
 	AUTH              = "auth"
 	DefaultIDTemplate = "*"
@@ -11,8 +10,4 @@ const (
 
 func GetIdentifierTemplatePerModule(module string, identifier string) string {
 	return fmt.Sprintf("%s/%s", module, identifier)
-}
-
-func GetPrefixedIdentifierTemplatePerModule(module string, identifier string, prefix string) string {
-	return fmt.Sprintf("%s/%s/%s", module, prefix, identifier)
 }
