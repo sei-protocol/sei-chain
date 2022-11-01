8586b182d04a53c5f3f95d9650ba0c3b614341b6
Uday Patil <udpatil@gmail.com>
2022-11-01 11:53:59 -0400

    Revert "Lazy Deposit All Module Accounts During EndBlock (#313)"
    This reverts commit 5ad4d9b9af8989df69746ebc9b035f1d9dfc8421.

--- aclmapping/utils/identifier_templates.go
+++ aclmapping/utils/identifier_templates.go
@@ -1,11 +1,6 @@
 package util
 
-import (
-	"fmt"
-
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	sdkacltypes "github.com/cosmos/cosmos-sdk/types/accesscontrol"
-)
+import "fmt"
 
 const (
 	ACCOUNT           = "acc"
@@ -20,19 +15,4 @@ func GetIdentifierTemplatePerModule(module string, identifier string) string {
 
 func GetPrefixedIdentifierTemplatePerModule(module string, identifier string, prefix string) string {
 	return fmt.Sprintf("%s/%s/%s", module, prefix, identifier)
-}
-
-func GetOracleReadAccessOpsForValAndFeeder(feederAddr sdk.Address, valAddr sdk.Address) []sdkacltypes.AccessOperation {
-	return []sdkacltypes.AccessOperation{
-		{
-			AccessType:         sdkacltypes.AccessType_READ,
-			ResourceType:       sdkacltypes.ResourceType_KV,
-			IdentifierTemplate: GetIdentifierTemplatePerModule("oracle", feederAddr.String()),
-		},
-		{
-			AccessType:         sdkacltypes.AccessType_READ,
-			ResourceType:       sdkacltypes.ResourceType_KV,
-			IdentifierTemplate: GetIdentifierTemplatePerModule("oracle", valAddr.String()),
-		},
-	}
 }
