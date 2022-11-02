b2452a75d75643b6ca3ade7a3cba8402a24d3ef0
Brandon Weng <18161326+BrandonWeng@users.noreply.github.com>
2022-11-02 09:57:40 -0400

    Add ACL Mapping for Dex Send and Cancel orders  (#360)
    * Implement DEX parallelization
    
    * Remove print
    
    * sd
    
    * check out loadtest to 2.0.0beta
    
    * fix
    
    Co-authored-by: Eric Zhu <ericzhu77@gmail.com>

--- aclmapping/utils/resource_type.go
+++ aclmapping/utils/resource_type.go
@@ -14,13 +14,35 @@ import (
 
 var StoreKeyToResourceTypePrefixMap = aclsdktypes.StoreKeyToResourceTypePrefixMap{
 	aclsdktypes.ParentNodeKey: {
-		aclsdktypes.ResourceType_ANY: aclsdktypes.EmptyPrefix,
-		aclsdktypes.ResourceType_KV:  aclsdktypes.EmptyPrefix,
-		aclsdktypes.ResourceType_Mem: aclsdktypes.EmptyPrefix,
+		aclsdktypes.ResourceType_ANY:     aclsdktypes.EmptyPrefix,
+		aclsdktypes.ResourceType_KV:      aclsdktypes.EmptyPrefix,
+		aclsdktypes.ResourceType_Mem:     aclsdktypes.EmptyPrefix,
+		aclsdktypes.ResourceType_KV_WASM: aclsdktypes.EmptyPrefix,
 	},
 	dextypes.StoreKey: {
-		aclsdktypes.ResourceType_KV_DEX: aclsdktypes.EmptyPrefix,
-		aclsdktypes.ResourceType_DexMem: aclsdktypes.EmptyPrefix,
+		aclsdktypes.ResourceType_KV_DEX:                    aclsdktypes.EmptyPrefix,
+		aclsdktypes.ResourceType_DexMem:                    aclsdktypes.EmptyPrefix,
+		aclsdktypes.ResourceType_KV_DEX_CONTRACT_LONGBOOK:  dextypes.KeyPrefix(dextypes.LongBookKey),
+		aclsdktypes.ResourceType_KV_DEX_CONTRACT_SHORTBOOK: dextypes.KeyPrefix(dextypes.ShortBookKey),
+		// pricedenom and assetdenoms are the prefixes
+		aclsdktypes.ResourceType_KV_DEX_PAIR_PREFIX:           aclsdktypes.EmptyPrefix,
+		aclsdktypes.ResourceType_KV_DEX_TWAP:                  dextypes.KeyPrefix(dextypes.TwapKey),
+		aclsdktypes.ResourceType_KV_DEX_PRICE:                 dextypes.KeyPrefix(dextypes.PriceKey),
+		aclsdktypes.ResourceType_KV_DEX_SETTLEMENT_ENTRY:      dextypes.KeyPrefix(dextypes.SettlementEntryKey),
+		aclsdktypes.ResourceType_KV_DEX_REGISTERED_PAIR:       dextypes.KeyPrefix(dextypes.RegisteredPairKey),
+		aclsdktypes.ResourceType_KV_DEX_TICK_SIZE:             dextypes.KeyPrefix(dextypes.TickSizeKey),
+		aclsdktypes.ResourceType_KV_DEX_ORDER:                 dextypes.KeyPrefix(dextypes.OrderKey),
+		aclsdktypes.ResourceType_KV_DEX_CANCEL:                dextypes.KeyPrefix(dextypes.CancelKey),
+		aclsdktypes.ResourceType_KV_DEX_ACCOUNT_ACTIVE_ORDERS: dextypes.KeyPrefix(dextypes.AccountActiveOrdersKey),
+		aclsdktypes.ResourceType_KV_DEX_REGISTERED_PAIR_COUNT: dextypes.KeyPrefix(dextypes.RegisteredPairCount),
+		aclsdktypes.ResourceType_KV_DEX_ASSET_LIST:            dextypes.KeyPrefix(dextypes.AssetListKey),
+		aclsdktypes.ResourceType_KV_DEX_NEXT_ORDER_ID:         dextypes.KeyPrefix(dextypes.NextOrderIDKey),
+		aclsdktypes.ResourceType_KV_DEX_NEXT_SETTLEMENT_ID:    dextypes.KeyPrefix(dextypes.NextSettlementIDKey),
+		aclsdktypes.ResourceType_KV_DEX_MATCH_RESULT:          dextypes.KeyPrefix(dextypes.MatchResultKey),
+		aclsdktypes.ResourceType_KV_DEX_ORDER_BOOK:            dextypes.KeyPrefix(dextypes.NextOrderIDKey),
+		// SETTLEMENT keys are prefixed with account and order id
+		aclsdktypes.ResourceType_KV_DEX_SETTLEMENT_ORDER_ID: aclsdktypes.EmptyPrefix,
+		aclsdktypes.ResourceType_KV_DEX_SETTLEMENT:          aclsdktypes.EmptyPrefix,
 	},
 	banktypes.StoreKey: {
 		aclsdktypes.ResourceType_KV_BANK:          aclsdktypes.EmptyPrefix,
@@ -45,10 +67,13 @@ var StoreKeyToResourceTypePrefixMap = aclsdktypes.StoreKeyToResourceTypePrefixMa
 		aclsdktypes.ResourceType_KV_DISTRIBUTION_SLASH_EVENT:             distributiontypes.ValidatorSlashEventPrefix,
 	},
 	oracletypes.StoreKey: {
-		aclsdktypes.ResourceType_KV_ORACLE:                 aclsdktypes.EmptyPrefix,
-		aclsdktypes.ResourceType_KV_ORACLE_VOTE_TARGETS:    oracletypes.VoteTargetKey,
-		aclsdktypes.ResourceType_KV_ORACLE_AGGREGATE_VOTES: oracletypes.AggregateExchangeRateVoteKey,
-		aclsdktypes.ResourceType_KV_ORACLE_FEEDERS:         oracletypes.FeederDelegationKey,
+		aclsdktypes.ResourceType_KV_ORACLE:                      aclsdktypes.EmptyPrefix,
+		aclsdktypes.ResourceType_KV_ORACLE_VOTE_TARGETS:         oracletypes.VoteTargetKey,
+		aclsdktypes.ResourceType_KV_ORACLE_AGGREGATE_VOTES:      oracletypes.AggregateExchangeRateVoteKey,
+		aclsdktypes.ResourceType_KV_ORACLE_FEEDERS:              oracletypes.FeederDelegationKey,
+		aclsdktypes.ResourceType_KV_ORACLE_PRICE_SNAPSHOT:       oracletypes.PriceSnapshotKey,
+		aclsdktypes.ResourceType_KV_ORACLE_EXCHANGE_RATE:        oracletypes.ExchangeRateKey,
+		aclsdktypes.ResourceType_KV_ORACLE_VOTE_PENALTY_COUNTER: oracletypes.VotePenaltyCounterKey,
 	},
 	stakingtypes.StoreKey: {
 		aclsdktypes.ResourceType_KV_STAKING:                          aclsdktypes.EmptyPrefix,
