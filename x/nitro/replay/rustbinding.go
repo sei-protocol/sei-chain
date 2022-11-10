67b1ebee1a82472a0fc829e6f5982d7ae3f738c3
codchen <codchen03@gmail.com>
2022-11-10 17:18:15 +0800

    Add nitro artifacts

--- x/nitro/replay/rustbinding.go
+++ x/nitro/replay/rustbinding.go
@@ -12,7 +12,7 @@ import (
 	"github.com/sei-protocol/sei-chain/utils"
 )
 
-func callReplayer(accountFiles []string, programFiles []string, txFiles []string, outputDir string) (err error) {
+func callReplayer(accountFiles []string, sysvarFiles []string, programFiles []string, txFiles []string, outputDir string) (err error) {
 	defer func() {
 		if err := recover(); err != nil {
 			err = fmt.Errorf("received error when calling replayer: %s", err)
@@ -21,6 +21,11 @@ func callReplayer(accountFiles []string, programFiles []string, txFiles []string
 	C.replay(
 		makeFilePaths(
 			utils.Map(accountFiles, func(path string) C.ByteSliceView {
+				return makeView([]byte(path))
+			}),
+		),
+		makeFilePaths(
+			utils.Map(sysvarFiles, func(path string) C.ByteSliceView {
 				return makeView([]byte(path))
 			}),
 		),
