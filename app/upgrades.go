fb64ae851536b61a5f9cd5d4870c91cea65a2c1a
Philip Su <philip.su.522@gmail.com>
2022-11-14 08:27:42 -0800

    Sorted order diffs cherry pick (#379)
    * upgrade name
    
    * Totally sort components in MatchResult
    
    * fix order ordering
    
    * fix
    
    * fix
    
    * Add comment
    
    Co-authored-by: codchen <codchen03@gmail.com>

--- app/upgrades.go
+++ app/upgrades.go
@@ -47,10 +47,8 @@ var upgradesList = []string{
 	"1.2.1beta",
 	// 1.2.2beta
 	"1.2.2beta",
-	// 1.2.3beta
-	"1.2.3beta",
-	// 1.2.4beta
-	"1.2.4beta",
+	// 1.2.2beta-postfix
+	"1.2.2beta-postfix",
 }
 
 func (app App) RegisterUpgradeHandlers() {
