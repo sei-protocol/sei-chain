c9fee9a1f1cc99e6516da285a0b7b6f5f747c97e
Kbhat1 <kartikbhatri@gmail.com>
2022-11-19 14:17:41 -0500

    Add Tokenfactory Migration Unit Tests (#406)
    Add Migration test

--- x/tokenfactory/types/codec.go
+++ x/tokenfactory/types/codec.go
@@ -37,4 +37,7 @@ func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
 	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
 }
 
-var ModuleCdc = codec.NewProtoCodec(cdctypes.NewInterfaceRegistry())
+var (
+	Amino     = codec.NewLegacyAmino()
+	ModuleCdc = codec.NewProtoCodec(cdctypes.NewInterfaceRegistry())
+)
