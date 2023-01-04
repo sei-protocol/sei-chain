355aed3d91b0805c5b8ceacf7b0f2c3885651c6c
Brandon Weng <18161326+BrandonWeng@users.noreply.github.com>
2023-01-03 23:43:26 -0500

    Parse is_expedited json field (#123)
    ## Describe your changes and provide context
    is_expedited wasn't being parsed properly from the files 
    
    ## Testing performed to validate your change
    
    ```
    {
        "title": "Test Proposal",
        "description": "My awesome proposal",
        "is_expedited": true,
        "type": "Text",
        "deposit": "1000000000usei"
      }
    ```
    
    ```
     seid tx gov submit-proposal --proposal=./test.json --from admin  -y  --broadcast-mode=block --fees 1000sei
    ```
    
    
    ![image](https://user-images.githubusercontent.com/18161326/210484604-64beee2c-d798-4c3e-8129-a2dc220e17aa.png)

--- sei-cosmos/x/gov/client/cli/tx.go
+++ sei-cosmos/x/gov/client/cli/tx.go
@@ -30,11 +30,11 @@ const (
 )
 
 type proposal struct {
-	Title       string
-	Description string
-	Type        string
-	IsExpedited bool
-	Deposit     string
+	Title       string  `json:"title"`
+	Description string  `json:"description"`
+	Type        string  `json:"type"`
+	IsExpedited bool    `json:"is_expedited,omitempty"`
+	Deposit     string  `json:"deposit,omitempty"`
 }
 
 // ProposalFlags defines the core required fields of a proposal. It is used to
@@ -124,7 +124,6 @@ $ %s tx gov submit-proposal --title="Test Proposal" --description="My awesome pr
 			}
 
 			content := types.ContentFromProposalType(proposal.Title, proposal.Description, proposal.Type, proposal.IsExpedited)
-
 			msg, err := types.NewMsgSubmitProposalWithExpedite(content, amount, clientCtx.GetFromAddress(), proposal.IsExpedited)
 
 			if err != nil {
