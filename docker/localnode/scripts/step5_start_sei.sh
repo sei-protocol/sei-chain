c588b0acae76a30cb4566611372a517028df23b2
Yiming Zang <yzang@twitter.com>
2023-01-04 09:27:15 -0800

    Add some sleep time to avoid flaky test

--- docker/localnode/scripts/step5_start_sei.sh
+++ docker/localnode/scripts/step5_start_sei.sh
@@ -11,11 +11,14 @@ cp build/generated/genesis-sei.json ~/.sei/config/genesis.json
 ./build/seid start --chain-id sei > "$LOG_DIR"/seid-$NODE_ID.log 2>&1 &
 echo "Done" >> build/generated/launch.complete
 
-echo "Server started successfully! Check your logs under $LOG_DIR/"
+echo "SeiNode $NODE_ID started successfully! Check your logs under $LOG_DIR/"
 
 until [ $(cat build/generated/launch.complete |wc -l) = 4 ]
 do
   sleep 5
 done
+
+echo "All 4 Sei Nodes started successfully"
+
 
 tail -f /dev/null
