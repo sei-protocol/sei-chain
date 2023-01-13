949c2c992c56d48cbc314b772e806eddcae34416
Yiming Zang <yzang@twitter.com>
2023-01-12 20:23:09 -0800

    Fix libs

--- scripts/bundle_seid.sh
+++ scripts/bundle_seid.sh
@@ -6,7 +6,7 @@ export GOPATH=$HOME/go
 export PATH=$GOPATH/bin:$PATH
 make clean
 make build
-cp -r "$GOPATH/pkg/mod/github.com/!cosm!wasm" ./build/
-cp -r "x/nitro/replay/libnitro_replayer.x86_64.so" ./build/
-cp -r "x/nitro/replay/libnitro_replayer.dylib" ./build/
+mkdir -p build/libs
+cp "$GOPATH/pkg/mod/github.com/!cosm!wasm/wasmvm@v1.0.0/api/libwasmvm.x86_64.so" ./build/libs
+cp "x/nitro/replay/libnitro_replayer.x86_64.so" ./build/libs
 zip -r seid_bundle.zip ./build/*
