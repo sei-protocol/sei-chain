b8c5740136cbe478a7e6ac6c5e62ad3d1540b542
Yiming Zang <yzang@twitter.com>
2023-01-12 17:25:25 -0800

    Fix build

--- scripts/bundle_seid.sh
+++ scripts/bundle_seid.sh
@@ -5,9 +5,7 @@ cd "${SOURCE_ROOT}" || exit
 export GOPATH=$HOME/go
 export PATH=$GOPATH/bin:$PATH
 make clean
-make build-linux
+make build
 cd build || exit
-ls -l $GOPATH/pkg/mod
-ls -l $GOPATH/pkg/mod/github.com
 cp -r "$GOPATH/pkg/mod/github.com/!cosm!wasm" ./
 zip -r seid_bundle.zip ./*
