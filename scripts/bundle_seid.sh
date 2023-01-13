f611883a4cafd71e84db8929d0bf259d083d2340
Yiming Zang <yzang@twitter.com>
2023-01-12 16:00:25 -0800

    Add branch name

--- scripts/bundle_seid.sh
+++ scripts/bundle_seid.sh
@@ -4,8 +4,6 @@ SOURCE_ROOT=$(git rev-parse --show-toplevel)
 cd "${SOURCE_ROOT}" || exit
 make clean
 make build
-go env
-echo $GOPATH
 cd build || exit
 cp -r "$GOPATH/pkg/mod/github.com/\!cosm\!wasm" ./
 zip -r seid_bundle.zip ./*
