c45c49717ee9ab15b3ef7ad48ad31f4bd024702a
Yiming Zang <yzang@twitter.com>
2023-01-12 17:34:49 -0800

    Build nitro

--- scripts/bundle_seid.sh
+++ scripts/bundle_seid.sh
@@ -5,7 +5,7 @@ cd "${SOURCE_ROOT}" || exit
 export GOPATH=$HOME/go
 export PATH=$GOPATH/bin:$PATH
 make clean
-make build
+make build-all
 cd build || exit
 cp -r "$GOPATH/pkg/mod/github.com/!cosm!wasm" ./
 zip -r seid_bundle.zip ./*
