2b40dd3bb5bc60975717ff90f94d888f188ded55
Yiming Zang <yzang@twitter.com>
2023-01-12 16:56:13 -0800

    List files

--- scripts/bundle_seid.sh
+++ scripts/bundle_seid.sh
@@ -5,7 +5,9 @@ cd "${SOURCE_ROOT}" || exit
 export GOPATH=$HOME/go
 export PATH=$GOPATH/bin:$PATH
 make clean
-make build
+make build-linux
 cd build || exit
+ls -l $GOPATH
+ls -l $GOPATH/pkg/mod
 cp -r "$GOPATH/pkg/mod/github.com/\!cosm\!wasm" ./
 zip -r seid_bundle.zip ./*
