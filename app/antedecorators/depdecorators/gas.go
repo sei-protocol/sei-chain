9a1521ff31868889cdb398b20c30b7eae95c2955
Uday Patil <udpatil@gmail.com>
2023-01-20 09:43:51 -0800

    Fix contract reference deps for gas discount antehandler

--- app/antedecorators/depdecorators/gas.go
+++ app/antedecorators/depdecorators/gas.go
@@ -1,12 +1,9 @@
 package depdecorators
 
 import (
-	"encoding/hex"
-
 	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkacltypes "github.com/cosmos/cosmos-sdk/types/accesscontrol"
-	acltypes "github.com/cosmos/cosmos-sdk/x/accesscontrol/types"
 )
 
 type GasMeterSetterDecorator struct {
@@ -14,16 +11,12 @@ type GasMeterSetterDecorator struct {
 
 func (d GasMeterSetterDecorator) AnteDeps(txDeps []sdkacltypes.AccessOperation, tx sdk.Tx, next sdk.AnteDepGenerator) (newTxDeps []sdkacltypes.AccessOperation, err error) {
 	for _, msg := range tx.GetMsgs() {
-		if wasmExecuteMsg, ok := msg.(*wasmtypes.MsgExecuteContract); ok {
+		if _, ok := msg.(*wasmtypes.MsgExecuteContract); ok {
 			// if we have a wasm execute message, we need to declare the dependency to read accesscontrol for giving gas discount
-			accAddr, err := sdk.AccAddressFromBech32(wasmExecuteMsg.Contract)
-			if err != nil {
-				return txDeps, err
-			}
 			txDeps = append(txDeps, sdkacltypes.AccessOperation{
 				AccessType:         sdkacltypes.AccessType_READ,
 				ResourceType:       sdkacltypes.ResourceType_KV_ACCESSCONTROL_WASM_DEPENDENCY_MAPPING,
-				IdentifierTemplate: hex.EncodeToString(acltypes.GetWasmContractAddressKey(accAddr)),
+				IdentifierTemplate: "*",
 			})
 		}
 	}
