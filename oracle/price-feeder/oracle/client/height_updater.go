10c55371c6b3c3ef4c81825431b005e71870cdb9
Yiming Zang <yzang@twitter.com>
2023-01-25 18:33:42 -0800

    Fix lint issue

--- oracle/price-feeder/oracle/client/height_updater.go
+++ oracle/price-feeder/oracle/client/height_updater.go
@@ -55,18 +55,20 @@ func (heightUpdater HeightUpdater) subscribe(
 		eventDataNewBlockHeader, ok := eventData.(tmtypes.EventDataNewBlockHeader)
 		if !ok {
 			logger.Err(err).Msg("Failed to parse event from eventDataNewBlockHeader")
+			continue
 		} else {
 			eventHeight := eventDataNewBlockHeader.Header.Height
 			if eventHeight > heightUpdater.LastHeight {
 				logger.Info().Msg(fmt.Sprintf("Received new Chain Height: %d", eventHeight))
 				heightUpdater.LastHeight = eventHeight
+				if len(heightUpdater.ChBlockHeight) == 0 {
+					heightUpdater.ChBlockHeight <- eventHeight
+				} else {
+					// skip this block height since price feeder is still sending previous transaction
+					logger.Info().Msg(fmt.Sprintf("Skipped Block Height: %d", eventHeight))
+				}
 			}
-			if len(heightUpdater.ChBlockHeight) <= 0 {
-				heightUpdater.ChBlockHeight <- eventHeight
-			} else {
-				// skip this block height since price feeder is still sending previous transaction
-				logger.Info().Msg(fmt.Sprintf("Skipped Block Height: %d", eventHeight))
-			}
+
 		}
 		time.Sleep(queryInterval)
 	}
