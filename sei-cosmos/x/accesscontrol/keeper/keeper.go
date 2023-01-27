923693ed408850a9e3ad9abf0ed7f29999b0ac8a
Uday Patil <udpatil@gmail.com>
2023-01-26 20:57:20 -0800

    [acl] Add error logging for circular dependency (#144)
    ## Describe your changes and provide context
    This logs an error upon identifying a circular dependency
    
    ## Testing performed to validate your change

--- sei-cosmos/x/accesscontrol/keeper/keeper.go
+++ sei-cosmos/x/accesscontrol/keeper/keeper.go
@@ -150,6 +150,7 @@ func (k Keeper) GetWasmDependencyAccessOps(ctx sdk.Context, contractAddress sdk.
 	uniqueIdentifier := GetCircularDependencyIdentifier(contractAddress, msgInfo)
 	if _, ok := circularDepLookup[uniqueIdentifier]; ok {
 		// we've already seen this identifier, we should simply return synchronous access Ops
+		ctx.Logger().Error("Circular dependency encountered, using synchronous access ops instead")
 		return types.SynchronousAccessOps(), nil
 	}
 	// add to our lookup so we know we've seen this identifier
