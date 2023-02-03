c2db14249848febdd601b37cfc4db4eb052669ac
Brandon Weng <18161326+BrandonWeng@users.noreply.github.com>
2023-02-03 01:02:17 -0500

    [store] bump CommitKVStoreCache limit and switch to 2Q from arc (#152)
    ## Describe your changes and provide context
    IRRC we had discussions around bumping the limit for the inter-block
    cache since 1k is pretty little for our volume of TXs per block. Im
    setting it to 100k to be the same as the BoundedCacheKv store
    
    While I was investigating the race condition issue in the LRU cache, I
    saw that several repos brought up concerns around using ArcCache in
    their code as it's been patented by IBM (and later sold to Intel)
    
    https://github.com/hashicorp/golang-lru/issues/31 
    https://github.com/hashicorp/golang-lru/issues/73 
    
    https://github.com/ipfs/kubo/issues/6590
    https://github.com/ipfs/go-ipfs-blockstore/pull/20
    
    Postgres and IPFS replaced it with 2Q, which based on the whitepaper
    should have the same performance as ARC.
    ## Testing performed to validate your change
    Deployed a LT cluster with these changes and saw no impact to consensus
    and the performance (with LT clients running) is similar
    
    ![image](https://user-images.githubusercontent.com/18161326/216499319-be5ae693-242f-453c-8073-414bf5f810bd.png)

--- sei-cosmos/store/cache/cache.go
+++ sei-cosmos/store/cache/cache.go
@@ -16,7 +16,7 @@ var (
 
 	// DefaultCommitKVStoreCacheSize defines the persistent ARC cache size for a
 	// CommitKVStoreCache.
-	DefaultCommitKVStoreCacheSize uint = 1000
+	DefaultCommitKVStoreCacheSize uint = 100000
 )
 
 type (
@@ -28,7 +28,7 @@ type (
 	// CommitKVStore and below is completely irrelevant to this layer.
 	CommitKVStoreCache struct {
 		types.CommitKVStore
-		cache       *lru.ARCCache
+		cache       *lru.TwoQueueCache
 		cacheKVSize int
 
 		// the same CommitKVStoreCache may be accessed concurrently by multiple
@@ -48,7 +48,7 @@ type (
 )
 
 func NewCommitKVStoreCache(store types.CommitKVStore, size uint, cacheKVSize int) *CommitKVStoreCache {
-	cache, err := lru.NewARC(int(size))
+	cache, err := lru.New2Q(int(size))
 	if err != nil {
 		panic(fmt.Errorf("failed to create KVStore cache: %s", err))
 	}
