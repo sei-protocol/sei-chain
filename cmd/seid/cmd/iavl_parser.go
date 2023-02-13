802e22c57f2e7724a695baa1d2df792d21ea0382
Uday Patil <udpatil@gmail.com>
2023-02-13 13:37:32 -0600

    [debug] Add additional module parsers

--- cmd/seid/cmd/iavl_parser.go
+++ cmd/seid/cmd/iavl_parser.go
@@ -6,22 +6,33 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	dextypes "github.com/sei-protocol/sei-chain/x/dex/types"
+	minttypes "github.com/sei-protocol/sei-chain/x/mint/types"
 )
 
 type ModuleParser func([]byte) ([]string, error)
 
 var ModuleParserMap = map[string]ModuleParser{
 	"bank": BankParser,
+	"mint": MintParser,
+	"dex":  DexParser,
+}
+
+func MintParser(key []byte) ([]string, error) {
+	keyItems := []string{}
+	switch {
+	case bytes.HasPrefix(key, minttypes.MinterKey):
+		keyItems = append(keyItems, "MinterKey")
+	case bytes.HasPrefix(key, minttypes.LastTokenReleaseDate):
+		keyItems = append(keyItems, "LastTokenReleaseDate")
+	default:
+		keyItems = append(keyItems, "Unrecognized prefix")
+	}
+	return keyItems, nil
 }
 
 func BankParser(key []byte) ([]string, error) {
 	keyItems := []string{}
-	// split the stuff before any colons for JUST prefix
-	// key, err := hex.DecodeString(hexKey)
-	// if err != nil {
-	// 	return keyItems, err
-	// }
-	// iterate through, attempt to match against the Bank key prefixes
 	switch {
 	case bytes.HasPrefix(key, banktypes.SupplyKey):
 		keyItems = append(keyItems, "Supply")
@@ -37,19 +48,90 @@ func BankParser(key []byte) ([]string, error) {
 		keyItems = append(keyItems, "Balances")
 		// remaining is length prefixed addr + denom
 		remaining := bytes.TrimPrefix(key, banktypes.BalancesPrefix)
-		lengthPrefix, remaining := int(remaining[0]), remaining[1:]
-		keyItems = append(keyItems, fmt.Sprintf("AddrLength: %d", lengthPrefix))
-		accountAddr := remaining[0:lengthPrefix]
-		denom := remaining[lengthPrefix:]
-		bech32Addr, err := sdk.Bech32ifyAddressBytes("sei", accountAddr)
+		items, denom, err := parseLengthPrefixedAddress(remaining)
 		if err != nil {
 			return keyItems, err
 		}
-		keyItems = append(keyItems, fmt.Sprintf("AddrBech32: %s", bech32Addr))
+		keyItems = append(keyItems, items...)
 		keyItems = append(keyItems, fmt.Sprintf("Denom: %s", string(denom)))
 	default:
 		keyItems = append(keyItems, "Unrecognized prefix")
 	}
 
 	return keyItems, nil
+}
+
+func DexParser(key []byte) ([]string, error) {
+	keyItems := []string{}
+	switch {
+	case bytes.HasPrefix(key, dextypes.KeyPrefix(dextypes.LongBookKey)):
+		keyItems = append(keyItems, "LongBook")
+		remaining := bytes.TrimPrefix(key, dextypes.KeyPrefix(dextypes.LongBookKey))
+		items, remaining, err := parseLengthPrefixedAddress(remaining)
+		if err != nil {
+			return keyItems, err
+		}
+		keyItems = append(keyItems, items...)
+		// TODO: make this better
+		keyItems = append(keyItems, fmt.Sprintf("RemainingString: %s", string(remaining)))
+	case bytes.HasPrefix(key, dextypes.KeyPrefix(dextypes.ShortBookKey)):
+		keyItems = append(keyItems, "ShortBook")
+		remaining := bytes.TrimPrefix(key, dextypes.KeyPrefix(dextypes.ShortBookKey))
+		items, remaining, err := parseLengthPrefixedAddress(remaining)
+		if err != nil {
+			return keyItems, err
+		}
+		keyItems = append(keyItems, items...)
+		// TODO: make this better
+		keyItems = append(keyItems, fmt.Sprintf("RemainingString: %s", string(remaining)))
+	case bytes.HasPrefix(key, dextypes.KeyPrefix(dextypes.TriggerBookKey)):
+		keyItems = append(keyItems, "TriggerBook")
+		remaining := bytes.TrimPrefix(key, dextypes.KeyPrefix(dextypes.TriggerBookKey))
+		items, remaining, err := parseLengthPrefixedAddress(remaining)
+		if err != nil {
+			return keyItems, err
+		}
+		keyItems = append(keyItems, items...)
+		// TODO: make this better
+		keyItems = append(keyItems, fmt.Sprintf("RemainingString: %s", string(remaining)))
+	case bytes.HasPrefix(key, dextypes.KeyPrefix(dextypes.TwapKey)):
+		keyItems = append(keyItems, "TWAP")
+		remaining := bytes.TrimPrefix(key, dextypes.KeyPrefix(dextypes.TwapKey))
+		items, remaining, err := parseLengthPrefixedAddress(remaining)
+		if err != nil {
+			return keyItems, err
+		}
+		keyItems = append(keyItems, items...)
+		if len(remaining) > 0 {
+			keyItems = append(keyItems, fmt.Sprintf("RemainingString: %s", string(remaining)))
+		}
+	case bytes.HasPrefix(key, dextypes.KeyPrefix(dextypes.PriceKey)):
+		keyItems = append(keyItems, "Price")
+		remaining := bytes.TrimPrefix(key, dextypes.KeyPrefix(dextypes.PriceKey))
+		items, remaining, err := parseLengthPrefixedAddress(remaining)
+		if err != nil {
+			return keyItems, err
+		}
+		keyItems = append(keyItems, items...)
+		if len(remaining) > 0 {
+			keyItems = append(keyItems, fmt.Sprintf("RemainingString: %s", string(remaining)))
+		}
+	default:
+		keyItems = append(keyItems, "Unrecognized prefix")
+	}
+	return keyItems, nil
+}
+
+func parseLengthPrefixedAddress(remainingKey []byte) ([]string, []byte, error) {
+	keyItems := []string{}
+	lengthPrefix, remaining := int(remainingKey[0]), remainingKey[1:]
+	keyItems = append(keyItems, fmt.Sprintf("AddrLength: %d", lengthPrefix))
+	accountAddr := remaining[0:lengthPrefix]
+	remaining = remaining[lengthPrefix:]
+	bech32Addr, err := sdk.Bech32ifyAddressBytes("sei", accountAddr)
+	if err != nil {
+		return keyItems, remaining, err
+	}
+	keyItems = append(keyItems, fmt.Sprintf("AddrBech32: %s", bech32Addr))
+	return keyItems, remaining, nil
 }
