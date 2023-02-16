3c9ef50982a9caae5607e4c1f6506c5802bc9d11
Brandon Weng <18161326+BrandonWeng@users.noreply.github.com>
2023-02-15 19:01:47 -0500

    Add metrics around log not done util (#566)
    * Add metrics around log not doen util
    
    * comment
    
    * lint

--- utils/metrics/metrics_util.go
+++ utils/metrics/metrics_util.go
@@ -92,6 +92,20 @@ func IncrFailedConcurrentDeliverTxCounter() {
 	)
 }
 
+// Counts the number of operations that failed due to operation timeout
+// Metric Names:
+//
+//	sei_log_not_done_after_counter
+func IncrLogIfNotDoneAfter(label string) {
+	metrics.IncrCounterWithLabels(
+		[]string{"sei", "log", "not", "done", "after"},
+		1,
+		[]metrics.Label{
+			telemetry.NewLabel("label", label),
+		},
+	)
+}
+
 // Measures the time taken to execute a sudo msg
 // Metric Names:
 //
