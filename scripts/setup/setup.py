8fe0f1dcb432617212cefe99fb9eebe2a0cda1d3
Brandon Weng <18161326+BrandonWeng@users.noreply.github.com>
2023-02-16 23:39:37 -0500

    instructions

--- scripts/setup/setup.py
+++ scripts/setup/setup.py
@@ -216,49 +216,7 @@ def setup_oracle(args):
 
 def run():
     """
-        Setp script for decentralized launches of Sei networks.
-
-        setup-validator:
-            run this if you're trying to join an existing network and just need to provision the validator node.
-            Once provisioned, you'll need to request/get Sei tokens in order to stake as a validator
-        prepare-genesis:
-            run this if you're trying to launch a new network. This will generate the genesis file and the gentx file.
-            You will need to distribute the genesis file to all the other nodes and the gentx file to the validator node.
-        setup-price-feeder:
-            run this if you're trying to setup a price feeder service on a validator node post genesis
-
-        For seid and price-feeder processes, it's reccomended to run as a systemd service.
-
-        e.g seid
-            [Unit]
-            Description=Sei Node
-            After=network.target
-
-            [Service]
-            User=root
-            Type=simple
-            ExecStart=/root/go/bin/seid start --chain-id ${CHAIN_ID}
-            Restart=always
-            LimitNOFILE=6553500
-
-            [Install]
-            WantedBy=multi-user.target
-
-        e.g price-feeder
-            [Unit]
-            Description=Oracle Price Feeder
-            After=network.target
-
-            [Service]
-            User=root
-            Type=simple
-            Environment="PRICE_FEEDER_PASS={KEYRING_PASSWORD}"
-            ExecStart=/root/go/bin/price-feeder {PATH-TO-CONFIG-TOML}
-            Restart=on-failure
-            LimitNOFILE=6553500
-
-            [Install]
-            WantedBy=multi-user.target
+    Run the command line tool. See README.md for more details.
     """
     parser = argparse.ArgumentParser(description='Command line tool for specifying chain information')
     parser.add_argument('action', type=str, help='Action to preform', choices=[SETUP_VALIDATOR, PREPARE_GENESIS, SETUP_PRICE_FEEDER])
