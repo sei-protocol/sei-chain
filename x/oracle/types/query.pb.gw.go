925ccb2ad58a391d290e9e412f7919af04168b29
Uday Patil <udpatil@gmail.com>
2023-03-03 09:16:27 -0600

    fix nit

--- x/oracle/types/query.pb.gw.go
+++ x/oracle/types/query.pb.gw.go
@@ -376,7 +376,7 @@ func local_request_Query_AggregateVotes_0(ctx context.Context, marshaler runtime
 }
 
 func request_Query_SlashWindow_0(ctx context.Context, marshaler runtime.Marshaler, client QueryClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq QuerySlashWindow
+	var protoReq QuerySlashWindowRequest
 	var metadata runtime.ServerMetadata
 
 	msg, err := client.SlashWindow(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
@@ -385,7 +385,7 @@ func request_Query_SlashWindow_0(ctx context.Context, marshaler runtime.Marshale
 }
 
 func local_request_Query_SlashWindow_0(ctx context.Context, marshaler runtime.Marshaler, server QueryServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq QuerySlashWindow
+	var protoReq QuerySlashWindowRequest
 	var metadata runtime.ServerMetadata
 
 	msg, err := server.SlashWindow(ctx, &protoReq)
