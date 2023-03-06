680c3b5119241b549a9ba5f9833dd6189f886a36
Philip Su <philip.su.522@gmail.com>
2023-03-06 13:41:13 -0800

    bump

--- x/dex/types/wasm/utils.go
+++ x/dex/types/wasm/utils.go
@@ -6,12 +6,10 @@ import (
 	"golang.org/x/text/language"
 )
 
-//nolint:staticcheck // following the linter here requires changes in the sdk, I reckon.
 func GetContractPositionDirection(direction types.PositionDirection) string {
 	return cases.Title(language.English).String(cases.Lower(language.English).String(types.PositionDirection_name[int32(direction)]))
 }
 
-//nolint:staticcheck // following the linter here requires changes in the sdk, I reckon.
 func GetContractOrderType(orderType types.OrderType) string {
 	return cases.Title(language.English).String(cases.Lower(language.English).String(types.OrderType_name[int32(orderType)]))
 }
