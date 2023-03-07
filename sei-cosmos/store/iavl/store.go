f827acb9147b2e95c918a6d47eccc0c757fe5de8
Yiming Zang <50607998+yzang2019@users.noreply.github.com>
2023-03-07 09:24:56 -0800

    Turn on fsync for iavl store (#178)
    ## Describe your changes and provide context
    Testing performance impact for turning fsync on and decide next step.
    This change will prevent data loss when we encounter and power failure
    or disk failures.
    ## Testing performed to validate your change
    Performance tested using loadtest client, didn't observe any noticeable
    regression
    
    Co-authored-by: Yiming Zang <yzang@twitter.com>

--- sei-cosmos/store/iavl/store.go
+++ sei-cosmos/store/iavl/store.go
@@ -51,7 +51,10 @@ func LoadStore(db dbm.DB, logger log.Logger, key types.StoreKey, id types.Commit
 // provided DB. An error is returned if the version fails to load, or if called with a positive
 // version on an empty tree.
 func LoadStoreWithInitialVersion(db dbm.DB, logger log.Logger, key types.StoreKey, id types.CommitID, lazyLoading bool, initialVersion uint64, cacheSize int, disableFastNode bool) (types.CommitKVStore, error) {
-	tree, err := iavl.NewMutableTreeWithOpts(db, cacheSize, &iavl.Options{InitialVersion: initialVersion}, disableFastNode)
+	tree, err := iavl.NewMutableTreeWithOpts(db, cacheSize, &iavl.Options{
+		InitialVersion: initialVersion,
+		Sync:           true,
+	}, disableFastNode)
 	if err != nil {
 		return nil, err
 	}
