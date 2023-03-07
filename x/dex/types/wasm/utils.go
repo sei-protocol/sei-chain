911057cfd55a08aa3c5908533117efe71a83e79d
Philip Su <philip.su.522@gmail.com>
2023-03-07 09:49:49 -0800

    Change strings to case (#608)
    * Change strings to case
    
    * bump

--- x/dex/types/wasm/utils.go
+++ x/dex/types/wasm/utils.go
@@ -1,17 +1,15 @@
 package wasm
 
 import (
-	"strings"
-
 	"github.com/sei-protocol/sei-chain/x/dex/types"
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
 )
 
-//nolint:staticcheck // following the linter here requires changes in the sdk, I reckon.
 func GetContractPositionDirection(direction types.PositionDirection) string {
-	return strings.Title(strings.ToLower(types.PositionDirection_name[int32(direction)]))
+	return cases.Title(language.English).String(cases.Lower(language.English).String(types.PositionDirection_name[int32(direction)]))
 }
 
-//nolint:staticcheck // following the linter here requires changes in the sdk, I reckon.
 func GetContractOrderType(orderType types.OrderType) string {
-	return strings.Title(strings.ToLower(types.OrderType_name[int32(orderType)]))
+	return cases.Title(language.English).String(cases.Lower(language.English).String(types.OrderType_name[int32(orderType)]))
 }
