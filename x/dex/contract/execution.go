ef6d0498acbaf3138251905b19f3479837ce53e2
kbhat1 <kartikbhatri@gmail.com>
2023-03-09 20:22:39 -0500

    Update telemetry.ModuleSetGauge => telemetry.ModuleMeasureSince

--- x/dex/contract/execution.go
+++ x/dex/contract/execution.go
@@ -239,7 +239,7 @@ func HandleExecutionForContract(
 	tracer *otrace.Tracer,
 ) (map[string]dextypeswasm.ContractOrderResult, []*types.SettlementEntry, error) {
 	executionStart := time.Now()
-	defer telemetry.ModuleSetGauge(types.ModuleName, float32(time.Since(executionStart).Milliseconds()), "handle_execution_for_contract_ms")
+	defer telemetry.ModuleMeasureSince(types.ModuleName, float32(time.Since(executionStart).Milliseconds()), "handle_execution_for_contract_ms")
 	contractAddr := contract.ContractAddr
 	typedContractAddr := dextypesutils.ContractAddress(contractAddr)
 	orderResults := map[string]dextypeswasm.ContractOrderResult{}
