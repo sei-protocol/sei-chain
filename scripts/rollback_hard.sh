0b921bf226b360e5d52bb16c5a5724ad859966bd
Brandon Weng <18161326+BrandonWeng@users.noreply.github.com>
2023-03-21 11:01:30 -0400

    rollback

--- scripts/rollback_hard.sh
+++ scripts/rollback_hard.sh
@@ -7,12 +7,6 @@ echo "Rolling back to commit $ROLLBACK_COMMIT"
 COMMIT_INFO=$(seid version --long | grep commit)
 COMMIT=$(echo $COMMIT_INFO | awk '{ print $NF }' | xargs)
 
-if [ ${COMMIT} != $1 ]
-then
-  echo "incorrect seid version"
-  exit 1
-fi
-
 echo "Stopping seid systemctl process"
 systemctl stop seid
 
@@ -20,6 +14,11 @@ echo "Updating Binary to $ROLLBACK_COMMIT"
 git checkout $ROLLBACK_COMMIT
 make install
 
+if [ ${COMMIT} != $1 ]
+then
+  echo "incorrect seid version"
+  exit 1
+fi
 
 seid version --long | head -n 4
 
