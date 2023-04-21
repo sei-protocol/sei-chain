cdb62cf7385e430db1de1ff012cc33068c5263c8
codchen <codchen03@gmail.com>
2023-04-21 23:39:29 +0800

    Remove support for SR25519 (#224)
    ## Describe your changes and provide context
    We decided to not support SR25519 for sei
    
    ## Testing performed to validate your change
    local sei

--- sei-cosmos/client/keys/add.go
+++ sei-cosmos/client/keys/add.go
@@ -76,7 +76,7 @@ Example:
 	f.Uint32(flagCoinType, sdk.GetConfig().GetCoinType(), "coin type number for HD derivation")
 	f.Uint32(flagAccount, 0, "Account number for HD derivation")
 	f.Uint32(flagIndex, 0, "Address index number for HD derivation")
-	f.String(flags.FlagKeyAlgorithm, string(hd.Sr25519Type), "Key signing algorithm to generate keys for")
+	f.String(flags.FlagKeyAlgorithm, string(hd.Secp256k1Type), "Key signing algorithm to generate keys for")
 
 	return cmd
 }
