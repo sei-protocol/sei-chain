085e83e5eaa585c71a192363cba3128e24396ee0
Uday Patil <udpatil@gmail.com>
2023-04-24 15:49:02 -0500

    re-add commented out docker

--- scripts/initialize_local_chain.sh
+++ scripts/initialize_local_chain.sh
@@ -4,6 +4,20 @@ set -e
 
 # set key name
 keyname=admin
+#docker stop jaeger
+#docker rm jaeger
+#docker run -d --name jaeger \
+#  -e COLLECTOR_ZIPKIN_HOST_PORT=:9411 \
+#  -p 5775:5775/udp \
+#  -p 6831:6831/udp \
+#  -p 6832:6832/udp \
+#  -p 5778:5778 \
+#  -p 16686:16686 \
+#  -p 14250:14250 \
+#  -p 14268:14268 \
+#  -p 14269:14269 \
+#  -p 9411:9411 \
+#  jaegertracing/all-in-one:1.33
 # clean up old sei directory
 rm -rf ~/.sei
 echo "Building..."
