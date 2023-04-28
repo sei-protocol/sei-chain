f16dae37be94d9afeaf2079484593a236d312c4f
Xingchen Liao <liaox@usc.edu>
2023-04-28 10:46:50 -0700

    Remove x/nitro from genesis version (#732)
    * Remove x/nitro from genesis version
    
    * Change upgrade handler to no-ops
    
    ---------
    
    Co-authored-by: Cyson <cyson@Cysons-MBP.attlocal.net>

--- x/nitro/replay/link_glibclinux_x86_64.go
+++
@@ -1,7 +0,0 @@
-//go:build linux && amd64 && !sys_nitro_replayer
-// +build linux,amd64,!sys_nitro_replayer
-
-package replay
-
-// #cgo LDFLAGS: -Wl,-rpath,${SRCDIR} -L${SRCDIR} -lnitro_replayer.x86_64
-import "C"
--- x/nitro/replay/link_system.go
+++
@@ -1,7 +0,0 @@
-//go:build sys_replayer
-// +build sys_replayer
-
-package replay
-
-// #cgo LDFLAGS: -lreplayer
-import "C"
