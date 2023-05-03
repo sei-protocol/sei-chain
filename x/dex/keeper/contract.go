bde79bd46cf9d6c54dc60824127410d1472aa6c2
codchen <codchen03@gmail.com>
2023-05-03 21:28:41 +0800

    Add gas allowance for settlements

--- x/dex/keeper/contract.go
+++ x/dex/keeper/contract.go
@@ -82,12 +82,12 @@ func (k Keeper) GetAllContractInfo(ctx sdk.Context) []types.ContractInfoV2 {
 }
 
 // Reduce `RentBalance` of a contract if `userProvidedGas` cannot cover `gasUsed`
-func (k Keeper) ChargeRentForGas(ctx sdk.Context, contractAddr string, gasUsed uint64, userProvidedGas uint64) error {
-	if gasUsed <= userProvidedGas {
-		// User provided can fully cover the consumed gas. Doing nothing
+func (k Keeper) ChargeRentForGas(ctx sdk.Context, contractAddr string, gasUsed uint64, gasAllowance uint64) error {
+	if gasUsed <= gasAllowance {
+		// Allowance can fully cover the consumed gas. Doing nothing
 		return nil
 	}
-	gasUsed -= userProvidedGas
+	gasUsed -= gasAllowance
 	contract, err := k.GetContract(ctx, contractAddr)
 	if err != nil {
 		return err
