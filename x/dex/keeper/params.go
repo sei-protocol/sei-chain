bc377ef06a7cda4814bbcecd86953101d41a3aa1
codchen <codchen03@gmail.com>
2023-05-08 21:21:16 +0800

    migration

--- x/dex/keeper/params.go
+++ x/dex/keeper/params.go
@@ -20,6 +20,10 @@ func (k Keeper) GetMinProcessableRent(ctx sdk.Context) uint64 {
 	return k.GetParams(ctx).MinProcessableRent
 }
 
+func (k Keeper) GetOrderBookEntriesPerLoad(ctx sdk.Context) uint64 {
+	return k.GetParams(ctx).OrderBookEntriesPerLoad
+}
+
 // SetParams set the params
 func (k Keeper) SetParams(ctx sdk.Context, params types.Params) {
 	k.Paramstore.SetParamSet(ctx, &params)
