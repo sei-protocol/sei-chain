d2def26889a7ef74df356fd3b13277e550cd61dd
Uday Patil <udpatil@gmail.com>
2023-05-20 14:53:52 -0500

    Disable concurrency on non prio txs
    remove unnecessary statement

--- app/app.go
+++ app/app.go
@@ -1329,8 +1329,9 @@ func (app *App) ProcessBlock(ctx sdk.Context, txs [][]byte, req BlockProcessRequ
 	midBlockEvents := app.MidBlock(ctx, req.GetHeight())
 	events = append(events, midBlockEvents...)
 
-	// run other txs
-	otherResults, ctx := app.BuildDependenciesAndRunTxs(ctx, txs)
+	// run other txs - this will run them synchronously without building a dag or executing concurrently
+	// otherResults, ctx := app.BuildDependenciesAndRunTxs(ctx, txs)
+	otherResults := app.ProcessBlockSynchronous(ctx, txs)
 	txResults = append(txResults, otherResults...)
 
 	// Finalize all Bank Module Transfers here so that events are included
