de29c8c2b48bf9d950f2d907b4b9fb6b196795f3
Brandon Weng <18161326+BrandonWeng@users.noreply.github.com>
2023-06-07 11:00:38 -0400

    no type

--- x/epoch/handler.go
+++ x/epoch/handler.go
@@ -12,12 +12,7 @@ import (
 func NewHandler(k keeper.Keeper) sdk.Handler {
 	return func(ctx sdk.Context, msg sdk.Msg) (*sdk.Result, error) {
 		_ = ctx.WithEventManager(sdk.NewEventManager())
-
-		switch msg := msg.(type) {
-		// Epoch module has no message handlers
-		default:
-			errMsg := fmt.Sprintf("unrecognized %s message type: %T", types.ModuleName, msg)
-			return nil, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, errMsg)
-		}
+		errMsg := fmt.Sprintf("unrecognized %s message type: %T", types.ModuleName, msg)
+		return nil, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, errMsg)
 	}
 }
