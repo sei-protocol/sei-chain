885449303b19916a9a69d1b9d35d1a6947e56fc8
Philip Su <philip.su.522@gmail.com>
2023-06-08 15:00:55 -0700

    [Certik Audit] Fix key length check (#278)
    ## Describe your changes and provide context
    The code incorrectly checks padding for address. 3 should be outside the
    int cast
    ## Testing performed to validate your change

--- sei-cosmos/x/authz/keeper/keys.go
+++ sei-cosmos/x/authz/keeper/keys.go
@@ -41,7 +41,7 @@ func addressesFromGrantStoreKey(key []byte) (granterAddr, granteeAddr sdk.AccAdd
 	// 0x01<granterAddressLen (1 Byte)><granterAddress_Bytes><granteeAddressLen (1 Byte)><granteeAddress_Bytes><msgType_Bytes>
 	kv.AssertKeyAtLeastLength(key, 2)
 	granterAddrLen := key[1] // remove prefix key
-	kv.AssertKeyAtLeastLength(key, int(3+granterAddrLen))
+	kv.AssertKeyAtLeastLength(key, 3+int(granterAddrLen))
 	granterAddr = sdk.AccAddress(key[2 : 2+granterAddrLen])
 	granteeAddrLen := int(key[2+granterAddrLen])
 	kv.AssertKeyAtLeastLength(key, 4+int(granterAddrLen+byte(granteeAddrLen)))
