4c5bdc3ddf3c4e5b121127ff9cd2ce0c870c7f19
codchen <codchen03@gmail.com>
2023-06-19 23:17:57 +0800

    Add dex cancellatoon flow tests

--- testutil/processblock/verify/dex.go
+++ testutil/processblock/verify/dex.go
@@ -49,8 +49,8 @@ func DexOrders(t *testing.T, app *processblock.App, f BlockRunnable, txs []signi
 				}
 			}
 		}
-		expectedLongBookByMarket := map[string]map[sdk.Dec]*dextypes.OrderEntry{}
-		expectedShortBookByMarket := map[string]map[sdk.Dec]*dextypes.OrderEntry{}
+		expectedLongBookByMarket := map[string]map[string]*dextypes.OrderEntry{}
+		expectedShortBookByMarket := map[string]map[string]*dextypes.OrderEntry{}
 		for market := range markets {
 			orderPlacements := []*dextypes.Order{}
 			if o, ok := orderPlacementsByMarket[market]; ok {
@@ -75,7 +75,7 @@ func DexOrders(t *testing.T, app *processblock.App, f BlockRunnable, txs []signi
 			assetDenom := parts[2]
 			require.Equal(t, len(longBook), len(app.DexKeeper.GetAllLongBookForPair(app.Ctx(), contract, priceDenom, assetDenom)))
 			for price, entry := range longBook {
-				actual, found := app.DexKeeper.GetLongOrderBookEntryByPrice(app.Ctx(), contract, price, priceDenom, assetDenom)
+				actual, found := app.DexKeeper.GetLongOrderBookEntryByPrice(app.Ctx(), contract, sdk.MustNewDecFromStr(price), priceDenom, assetDenom)
 				require.True(t, found)
 				require.Equal(t, *entry, *(actual.GetOrderEntry()))
 			}
@@ -88,7 +88,7 @@ func DexOrders(t *testing.T, app *processblock.App, f BlockRunnable, txs []signi
 			assetDenom := parts[2]
 			require.Equal(t, len(shortBook), len(app.DexKeeper.GetAllShortBookForPair(app.Ctx(), contract, priceDenom, assetDenom)))
 			for price, entry := range shortBook {
-				actual, found := app.DexKeeper.GetShortOrderBookEntryByPrice(app.Ctx(), contract, price, priceDenom, assetDenom)
+				actual, found := app.DexKeeper.GetShortOrderBookEntryByPrice(app.Ctx(), contract, sdk.MustNewDecFromStr(price), priceDenom, assetDenom)
 				require.True(t, found)
 				require.Equal(t, *entry, *(actual.GetOrderEntry()))
 			}
@@ -108,10 +108,10 @@ func expectedOrdersForMarket(
 	contract string,
 	priceDenom string,
 	assetDenom string,
-) (longEntries map[sdk.Dec]*dextypes.OrderEntry, shortEntries map[sdk.Dec]*dextypes.OrderEntry) {
+) (longEntries map[string]*dextypes.OrderEntry, shortEntries map[string]*dextypes.OrderEntry) {
 	longBook := toOrderBookMap(keeper.GetAllLongBookForPair(ctx, contract, priceDenom, assetDenom))
 	shortBook := toOrderBookMap(keeper.GetAllShortBookForPair(ctx, contract, priceDenom, assetDenom))
-	books := map[dextypes.PositionDirection]map[sdk.Dec]*dextypes.OrderEntry{
+	books := map[dextypes.PositionDirection]map[string]*dextypes.OrderEntry{
 		dextypes.PositionDirection_LONG:  longBook,
 		dextypes.PositionDirection_SHORT: shortBook,
 	}
@@ -128,23 +128,23 @@ func expectedOrdersForMarket(
 }
 
 func cancelOrders(
-	books map[dextypes.PositionDirection]map[sdk.Dec]*dextypes.OrderEntry,
+	books map[dextypes.PositionDirection]map[string]*dextypes.OrderEntry,
 	orderCancellations []*dextypes.Cancellation,
 ) {
 	for _, cancel := range orderCancellations {
 		book := books[cancel.PositionDirection]
-		if entry, ok := book[cancel.Price]; ok {
+		if entry, ok := book[cancel.Price.String()]; ok {
 			entry.Allocations = removeMatched(entry.Allocations, func(a *dextypes.Allocation) bool { return a.OrderId == cancel.Id })
 			updateEntryQuantity(entry)
 			if entry.Quantity.IsZero() {
-				delete(book, cancel.Price)
+				delete(book, cancel.Price.String())
 			}
 		}
 	}
 }
 
 func addOrders(
-	books map[dextypes.PositionDirection]map[sdk.Dec]*dextypes.OrderEntry,
+	books map[dextypes.PositionDirection]map[string]*dextypes.OrderEntry,
 	orderPlacements []*dextypes.Order,
 ) {
 	for _, o := range orderPlacements {
@@ -157,11 +157,11 @@ func addOrders(
 			Quantity: o.Quantity,
 			OrderId:  o.Id,
 		}
-		if entry, ok := book[o.Price]; ok {
+		if entry, ok := book[o.Price.String()]; ok {
 			entry.Allocations = append(entry.Allocations, newAllocation)
 			updateEntryQuantity(entry)
 		} else {
-			book[o.Price] = &dextypes.OrderEntry{
+			book[o.Price.String()] = &dextypes.OrderEntry{
 				Price:       o.Price,
 				Quantity:    o.Quantity,
 				PriceDenom:  o.PriceDenom,
@@ -173,14 +173,14 @@ func addOrders(
 }
 
 func matchOrders(
-	longBook map[sdk.Dec]*dextypes.OrderEntry,
-	shortBook map[sdk.Dec]*dextypes.OrderEntry,
+	longBook map[string]*dextypes.OrderEntry,
+	shortBook map[string]*dextypes.OrderEntry,
 ) {
 	buyPrices := sortedPrices(longBook, true)
 	sellPrices := sortedPrices(shortBook, false)
 	for i, j := 0, 0; i < len(buyPrices) && j < len(sellPrices) && buyPrices[i].GTE(sellPrices[j]); {
-		buyEntry := longBook[buyPrices[i]]
-		sellEntry := shortBook[sellPrices[j]]
+		buyEntry := longBook[buyPrices[i].String()]
+		sellEntry := shortBook[sellPrices[j].String()]
 		if buyEntry.Quantity.GT(sellEntry.Quantity) {
 			takeLiquidity(longBook, buyPrices[i], sellEntry.Quantity)
 			takeLiquidity(shortBook, sellPrices[i], sellEntry.Quantity)
@@ -193,21 +193,21 @@ func matchOrders(
 	}
 }
 
-func toOrderBookMap(book []dextypes.OrderBookEntry) map[sdk.Dec]*dextypes.OrderEntry {
-	bookMap := map[sdk.Dec]*dextypes.OrderEntry{}
+func toOrderBookMap(book []dextypes.OrderBookEntry) map[string]*dextypes.OrderEntry {
+	bookMap := map[string]*dextypes.OrderEntry{}
 	for _, e := range book {
-		bookMap[e.GetPrice()] = e.GetOrderEntry()
+		bookMap[e.GetPrice().String()] = e.GetOrderEntry()
 	}
 	return bookMap
 }
 
-func getMarketOrderBookMap(orderPlacements []*dextypes.Order, direction dextypes.PositionDirection) map[sdk.Dec]*dextypes.OrderEntry {
-	bookMap := map[sdk.Dec]*dextypes.OrderEntry{}
+func getMarketOrderBookMap(orderPlacements []*dextypes.Order, direction dextypes.PositionDirection) map[string]*dextypes.OrderEntry {
+	bookMap := map[string]*dextypes.OrderEntry{}
 	for _, o := range orderPlacements {
 		if o.OrderType != dextypes.OrderType_MARKET || o.PositionDirection != direction {
 			continue
 		}
-		bookMap[o.Price] = orderToOrderEntry(o)
+		bookMap[o.Price.String()] = orderToOrderEntry(o)
 	}
 	return bookMap
 }
@@ -220,10 +220,10 @@ func updateEntryQuantity(entry *dextypes.OrderEntry) {
 	)
 }
 
-func takeLiquidity(book map[sdk.Dec]*dextypes.OrderEntry, price sdk.Dec, quantity sdk.Dec) {
-	entry := book[price]
+func takeLiquidity(book map[string]*dextypes.OrderEntry, price sdk.Dec, quantity sdk.Dec) {
+	entry := book[price.String()]
 	if entry.Quantity.Equal(quantity) {
-		delete(book, price)
+		delete(book, price.String())
 		return
 	}
 	if quantity.GT(entry.Quantity) {
@@ -247,10 +247,10 @@ func takeLiquidity(book map[sdk.Dec]*dextypes.OrderEntry, price sdk.Dec, quantit
 	entry.Quantity = entry.Quantity.Sub(quantity)
 }
 
-func sortedPrices(book map[sdk.Dec]*dextypes.OrderEntry, descending bool) []sdk.Dec {
+func sortedPrices(book map[string]*dextypes.OrderEntry, descending bool) []sdk.Dec {
 	prices := []sdk.Dec{}
 	for p := range book {
-		prices = append(prices, p)
+		prices = append(prices, sdk.MustNewDecFromStr(p))
 	}
 	if descending {
 		sort.Slice(prices, func(i, j int) bool { return prices[i].GT(prices[j]) })
