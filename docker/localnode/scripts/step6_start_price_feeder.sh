176788679d90f626b0be7cd3679f886cdc730690
Yiming Zang <yzang@twitter.com>
2023-06-20 06:45:56 -0700

    Add Integration Test for gov module

--- docker/localnode/scripts/step6_start_price_feeder.sh
+++ docker/localnode/scripts/step6_start_price_feeder.sh
@@ -5,6 +5,20 @@ NODE_ID=${ID:-0}
 LOG_DIR="build/generated/logs"
 mkdir -p $LOG_DIR
 ORACLE_CONFIG_FILE="build/generated/node_$NODE_ID/price_feeder_config.toml"
+ORACLE_ACCOUNT="oracle"
+VALIDATOR_ACCOUNT="node_admin"
+
+# Create an oracle account
+printf "12345678\n" | /root/go/bin/seid keys add $ORACLE_ACCOUNT --output json > /root/.sei/config/oracle_key.json
+ORACLE_ACCOUNT_ADDRESS=$(printf "12345678\n" | /root/go/bin/seid keys show $ORACLE_ACCOUNT -a)
+SEIVALOPER=$(printf "12345678\n" | /root/go/bin/seid keys show $VALIDATOR_ACCOUNT --bech=val -a)
+printf "12345678\n" | /root/go/bin/seid tx oracle set-feeder "$ORACLE_ACCOUNT_ADDRESS" --from $VALIDATOR_ACCOUNT --fees 2000usei -b block -y --chain-id sei >/dev/null 2>&1
+printf "12345678\n" | /root/go/bin/seid tx bank send $VALIDATOR_ACCOUNT "$ORACLE_ACCOUNT_ADDRESS" --from $VALIDATOR_ACCOUNT 1000sei --fees 2000usei -b block -y >/dev/null 2>&1
+
+
+sed -i.bak -e "s|^address *=.*|address = \"$ORACLE_ACCOUNT_ADDRESS\"|" $ORACLE_CONFIG_FILE
+sed -i.bak -e "s|^validator *=.*|validator = \"$SEIVALOPER\"|" $ORACLE_CONFIG_FILE
+
 
 # Starting oracle price feeder
 echo "Starting the oracle price feeder daemon"
