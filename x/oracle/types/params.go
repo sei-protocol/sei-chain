194bad589825414a74b2ff1edbc3f091aff1e9f2
Philip Su <philip.su.522@gmail.com>
2023-06-29 08:36:04 -0700

    67

--- x/oracle/types/params.go
+++ x/oracle/types/params.go
@@ -31,7 +31,7 @@ const (
 
 // Default parameter values
 var (
-	DefaultVoteThreshold = sdk.NewDecWithPrec(66, 2) // 66%
+	DefaultVoteThreshold = sdk.NewDecWithPrec(67, 2) // 67%
 	DefaultRewardBand    = sdk.NewDecWithPrec(2, 2)  // 2% (-1, 1)
 	DefaultWhitelist     = DenomList{
 		{Name: utils.MicroAtomDenom},
