c636325803cd2f82794d1483ac9b1a963809cbf4
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2023-07-05 12:37:51 -0700

    updates

--- oracle/price-feeder/oracle/provider/crypto_test.go
+++ oracle/price-feeder/oracle/provider/crypto_test.go
@@ -1,1 +1,29 @@
 package provider
+
+import (
+	"context"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/rs/zerolog"
+	"github.com/sei-protocol/sei-chain/oracle/price-feeder/config"
+	"github.com/sei-protocol/sei-chain/oracle/price-feeder/oracle/types"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCryptoProvider(t *testing.T) {
+	_, err := NewCryptoProvider(
+		context.TODO(),
+		zerolog.New(os.Stdout).With().Timestamp().Logger(),
+		config.ProviderEndpoint{
+			Name:      config.ProviderCrypto,
+			Rest:      cryptoRestHost,
+			Websocket: "wss://uat-stream.3ona.com",
+		},
+		types.CurrencyPair{},
+	)
+	require.NoError(t, err)
+
+	time.Sleep(1 * time.Minute)
+}
