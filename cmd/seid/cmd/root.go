45f5026f9137ec5423420d38854979e7a74e4f79
Philip Su <philip.su.522@gmail.com>
2023-07-11 16:43:08 -0700

    Increase default gas to 0.1usei (#947)
    * Increase default gas to 3.5usei
    
    * Set gas pricess
    
    * set gas

--- cmd/seid/cmd/root.go
+++ cmd/seid/cmd/root.go
@@ -390,7 +390,7 @@ func initAppConfig() (string, interface{}) {
 	//   own app.toml to override, or use this default value.
 	//
 	// In simapp, we set the min gas prices to 0.
-	srvCfg.MinGasPrices = "0.01usei"
+	srvCfg.MinGasPrices = "0.1usei"
 	srvCfg.API.Enable = true
 
 	// Pruning configs
