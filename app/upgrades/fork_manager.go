38b3df49bd4705ec11b3a7302ff7ea1e9733b49b
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2023-07-11 19:45:56 -0700

    init commit

--- app/upgrades/fork_manager.go
+++ app/upgrades/fork_manager.go
@@ -1,4 +1,4 @@
-package app
+package upgrades
 
 import (
 	"fmt"
---
+++ app/upgrades/v0/upgrade.go
@@ -0,0 +1,52 @@
+package v0
+
+import (
+	"fmt"
+
+	"github.com/CosmWasm/wasmd/x/wasm"
+	wasmkeeper "github.com/CosmWasm/wasmd/x/wasm/keeper"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/sei-protocol/sei-chain/app/upgrades"
+)
+
+const (
+	UpgradeName = "v0"
+)
+
+// HardForkUpgradeHandler defines an example hard fork handler that will be
+// executed during BeginBlock at a target height and chain-ID.
+type HardForkUpgradeHandler struct {
+	TargetHeight  int64
+	TargetChainID string
+	WasmKeeper    wasm.Keeper
+}
+
+func NewHardForkUpgradeHandler(height int64, chainID string, wk wasm.Keeper) upgrades.HardForkHandler {
+	return HardForkUpgradeHandler{
+		TargetHeight:  height,
+		TargetChainID: chainID,
+		WasmKeeper:    wk,
+	}
+}
+
+func (h HardForkUpgradeHandler) GetName() string {
+	return UpgradeName
+}
+
+func (h HardForkUpgradeHandler) GetTargetChainID() string {
+	return h.TargetChainID
+}
+
+func (h HardForkUpgradeHandler) GetTargetHeight() int64 {
+	return h.TargetHeight
+}
+
+func (h HardForkUpgradeHandler) ExecuteHandler(ctx sdk.Context) error {
+	govKeeper := wasmkeeper.NewGovPermissionKeeper(h.WasmKeeper)
+	_, err := govKeeper.Migrate(ctx, nil, nil, nil, nil)
+	if err != nil {
+		return fmt.Errorf("failed to execute wasm migration: %w", err)
+	}
+
+	return nil
+}
