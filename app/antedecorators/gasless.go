25353aaa22f80fb2f4e5176dbb59f64cbb78471c
Philip Su <philip.su.522@gmail.com>
2023-07-12 17:13:00 -0700

    undo revert

--- app/antedecorators/gasless.go
+++ app/antedecorators/gasless.go
@@ -32,14 +32,11 @@ func (gd GaslessDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool,
 	if err != nil {
 		return ctx, err
 	}
-	//feeTx, ok := tx.(sdk.FeeTx)
-	//if !ok {
-	//	return ctx, sdkerrors.Wrap(sdkerrors.ErrTxDecode, "Tx must be a FeeTx")
-	//}
-	//if !isGasless || feeTx.GetGas() > 0 {
-	//	ctx = ctx.WithGasMeter(originalGasMeter)
-	//}
-	if !isGasless {
+	feeTx, ok := tx.(sdk.FeeTx)
+	if !ok {
+		return ctx, sdkerrors.Wrap(sdkerrors.ErrTxDecode, "Tx must be a FeeTx")
+	}
+	if !isGasless || feeTx.GetGas() > 0 {
 		ctx = ctx.WithGasMeter(originalGasMeter)
 	}
 	isDeliverTx := !ctx.IsCheckTx() && !ctx.IsReCheckTx() && !simulate
