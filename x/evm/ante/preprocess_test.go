e6d0c01af5c6bb1c35f835723bfe1a5b25e3311a
codchen <codchen03@gmail.com>
2023-09-22 09:29:30 +0800

    EVM ante handler for signature verification (#1059)
    * evm wip
    
    * Create EVM module and add types/keeper
    
    * add tests
    
    * EVM signature verification
    
    * refactor

---
+++ x/evm/ante/preprocess_test.go
@@ -0,0 +1,53 @@
+package ante
+
+import (
+	"encoding/hex"
+	"math/big"
+	"testing"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/ethereum/go-ethereum/common"
+	ethtypes "github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/sei-protocol/sei-chain/x/evm/keeper"
+	"github.com/sei-protocol/sei-chain/x/evm/types"
+	"github.com/sei-protocol/sei-chain/x/evm/types/ethtx"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPreprocessAnteHandler(t *testing.T) {
+	k, ctx := keeper.MockEVMKeeper()
+	handler := NewEVMPreprocessDecorator(k, k.AccountKeeper())
+	privKey := keeper.MockPrivateKey()
+	testPrivHex := hex.EncodeToString(privKey.Bytes())
+	key, _ := crypto.HexToECDSA(testPrivHex)
+	to := new(common.Address)
+	copy(to[:], []byte("0x1234567890abcdef1234567890abcdef12345678"))
+	txData := ethtypes.LegacyTx{
+		Nonce:    1,
+		GasPrice: big.NewInt(10),
+		Gas:      1000,
+		To:       to,
+		Value:    big.NewInt(1000),
+		Data:     []byte("abc"),
+	}
+	chainID := k.ChainID()
+	evmParams := k.GetParams(ctx)
+	chainCfg := evmParams.GetChainConfig()
+	ethCfg := chainCfg.EthereumConfig(chainID)
+	blockNum := big.NewInt(ctx.BlockHeight())
+	signer := ethtypes.MakeSigner(ethCfg, blockNum, uint64(ctx.BlockTime().Unix()))
+	tx, err := ethtypes.SignTx(ethtypes.NewTx(&txData), signer, key)
+	require.Nil(t, err)
+	typedTx, err := ethtx.NewLegacyTx(tx)
+	require.Nil(t, err)
+	msg, err := types.NewMsgEVMTransaction(typedTx)
+	require.Nil(t, err)
+	ctx, err = handler.AnteHandle(ctx, mockTx{msgs: []sdk.Msg{msg}}, false, func(ctx sdk.Context, _ sdk.Tx, _ bool) (sdk.Context, error) {
+		return ctx, nil
+	})
+	require.Nil(t, err)
+	setAddr, found := types.GetContextSeiAddress(ctx)
+	require.True(t, found)
+	require.Equal(t, sdk.AccAddress(privKey.PubKey().Address()), setAddr)
+}
