3cbdb8d331b11b41865f0d00cca9c83fea29ea44
codchen <codchen03@gmail.com>
2023-09-27 10:46:13 +0800

    Add account state to EVM state interface implementation

--- x/evm/state/statedb.go
+++ x/evm/state/statedb.go
@@ -15,6 +15,10 @@ type StateDBImpl struct {
 	err error
 
 	k *keeper.Keeper
+
+	transientStorage   map[string]map[string][]byte
+	created            map[string]struct{}
+	selfDestructedAccs map[string]struct{}
 }
 
 func NewStateDBImpl(ctx sdk.Context, k *keeper.Keeper) *StateDBImpl {
