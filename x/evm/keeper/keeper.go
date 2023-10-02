daeaebc6696d14be927e9e77ab254d9652d89c4c
codchen <codchen03@gmail.com>
2023-10-02 22:05:46 +0800

    Add state-related function implementations in EVM state interface (#1071)
    * Add account state to EVM state interface implementation
    
    * add Snapshot and rollback

--- x/evm/keeper/keeper.go
+++ x/evm/keeper/keeper.go
@@ -3,6 +3,7 @@ package keeper
 import (
 	"math/big"
 
+	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	authkeeper "github.com/cosmos/cosmos-sdk/x/auth/keeper"
 	bankkeeper "github.com/cosmos/cosmos-sdk/x/bank/keeper"
@@ -49,4 +50,26 @@ func (k *Keeper) BankKeeper() bankkeeper.Keeper {
 
 func (k *Keeper) GetModuleBalance(ctx sdk.Context) *big.Int {
 	return k.BankKeeper().GetBalance(ctx, k.AccountKeeper().GetModuleAddress(types.ModuleName), k.GetBaseDenom(ctx)).Amount.BigInt()
+}
+
+func (k *Keeper) GetStoreKey() sdk.StoreKey {
+	return k.storeKey
+}
+
+func (k *Keeper) PrefixStore(ctx sdk.Context, pref []byte) sdk.KVStore {
+	store := ctx.KVStore(k.GetStoreKey())
+	return prefix.NewStore(store, pref)
+}
+
+func (k *Keeper) PurgePrefix(ctx sdk.Context, pref []byte) {
+	store := k.PrefixStore(ctx, pref)
+	iter := store.Iterator(nil, nil)
+	keys := [][]byte{}
+	for ; iter.Valid(); iter.Next() {
+		keys = append(keys, iter.Key())
+	}
+	iter.Close()
+	for _, key := range keys {
+		store.Delete(key)
+	}
 }
--- x/evm/state/balance.go
+++ x/evm/state/balance.go
@@ -11,11 +11,15 @@ import (
 	"github.com/sei-protocol/sei-chain/x/evm/types"
 )
 
-func (s *StateDBImpl) CreateAccount(common.Address) {
-	// noop
-	// EVM account creation is handled in ante handlers.
-	// State initialization is handled in Get/SetState
-}
+var (
+	// changes to EVM module balance because of balance movements. If this value
+	// does not equal to the change in EVM module account balance minus the minted
+	// amount at the end of the execution, the transaction should fail.
+	DeficitKey = []byte{0x01}
+	// the number of base tokens minted to temporarily facilitate balance movements.
+	// At the end of execution, `minted` number of base tokens will be burnt.
+	MintedKey = []byte{0x02}
+)
 
 func (s *StateDBImpl) SubBalance(evmAddr common.Address, amt *big.Int) {
 	if amt.Sign() == 0 {
@@ -26,7 +30,7 @@ func (s *StateDBImpl) SubBalance(evmAddr common.Address, amt *big.Int) {
 		return
 	}
 	defer func() {
-		s.deficit = new(big.Int).Sub(s.deficit, amt)
+		s.AddBigIntTransientModuleState(new(big.Int).Neg(amt), DeficitKey)
 	}()
 
 	if seiAddr, ok := s.k.GetSeiAddress(s.ctx, evmAddr); ok {
@@ -54,7 +58,7 @@ func (s *StateDBImpl) AddBalance(evmAddr common.Address, amt *big.Int) {
 		return
 	}
 	defer func() {
-		s.deficit = new(big.Int).Add(s.deficit, amt)
+		s.AddBigIntTransientModuleState(amt, DeficitKey)
 	}()
 
 	if seiAddr, ok := s.k.GetSeiAddress(s.ctx, evmAddr); ok {
@@ -67,7 +71,7 @@ func (s *StateDBImpl) AddBalance(evmAddr common.Address, amt *big.Int) {
 			if s.err != nil {
 				return
 			}
-			s.minted = new(big.Int).Add(s.minted, amt)
+			s.AddBigIntTransientModuleState(amt, MintedKey)
 		}
 		s.err = s.k.BankKeeper().SendCoinsFromModuleToAccount(s.ctx, types.ModuleName, seiAddr, coins)
 		return
@@ -94,14 +98,41 @@ func (s *StateDBImpl) CheckBalance() error {
 		return errors.New("should not call CheckBalance if there is already an error during execution")
 	}
 	currentModuleBalance := s.k.GetModuleBalance(s.ctx)
-	effectiveCurrentModuleBalance := new(big.Int).Sub(currentModuleBalance, s.minted)
-	expectedCurrentModuleBalance := new(big.Int).Sub(s.initialModuleBalance, s.deficit)
+	minted := s.GetBigIntTransientModuleState(MintedKey)
+	deficit := s.GetBigIntTransientModuleState(DeficitKey)
+	initialBalance := s.k.GetModuleBalance(s.snapshottedCtxs[0])
+	effectiveCurrentModuleBalance := new(big.Int).Sub(currentModuleBalance, minted)
+	expectedCurrentModuleBalance := new(big.Int).Sub(initialBalance, deficit)
 	if effectiveCurrentModuleBalance.Cmp(expectedCurrentModuleBalance) != 0 {
-		return fmt.Errorf("balance check failed. Initial balance: %s, current balance: %s, minted: %s, deficit: %s", s.initialModuleBalance, currentModuleBalance, s.minted, s.deficit)
+		return fmt.Errorf("balance check failed. Initial balance: %s, current balance: %s, minted: %s, deficit: %s", initialBalance, currentModuleBalance, minted, deficit)
 	}
 	// burn any minted token. If the function errors before, the state would be rolled back anyway
-	if err := s.k.BankKeeper().BurnCoins(s.ctx, types.ModuleName, sdk.NewCoins(sdk.NewCoin(s.k.GetBaseDenom(s.ctx), sdk.NewIntFromBigInt(s.minted)))); err != nil {
+	if err := s.k.BankKeeper().BurnCoins(s.ctx, types.ModuleName, sdk.NewCoins(sdk.NewCoin(s.k.GetBaseDenom(s.ctx), sdk.NewIntFromBigInt(minted)))); err != nil {
 		return err
 	}
 	return nil
+}
+
+func (s *StateDBImpl) AddBigIntTransientModuleState(delta *big.Int, key []byte) {
+	store := s.k.PrefixStore(s.ctx, types.TransientModuleStateKeyPrefix)
+	old := s.GetBigIntTransientModuleState(key)
+	new := new(big.Int).Add(old, delta)
+	sign := []byte{0}
+	if new.Sign() < 0 {
+		sign = []byte{1}
+	}
+	store.Set(key, append(sign, new.Bytes()...))
+}
+
+func (s *StateDBImpl) GetBigIntTransientModuleState(key []byte) *big.Int {
+	store := s.k.PrefixStore(s.ctx, types.TransientModuleStateKeyPrefix)
+	bz := store.Get(key)
+	if bz == nil {
+		return big.NewInt(0)
+	}
+	res := new(big.Int).SetBytes(bz[1:])
+	if bz[0] != 0 {
+		res = new(big.Int).Neg(res)
+	}
+	return res
 }
