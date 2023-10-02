6bdb3ba9ac182932337923c97cdac14563322fd0
codchen <codchen03@gmail.com>
2023-10-02 22:23:44 +0800

    [EVM] add remaining state interface functions (#1075)
    * [EVM] Add gas check ante handler
    
    * [EVM] implement balance related methods for state interface
    
    * Add account state to EVM state interface implementation
    
    * add Snapshot and rollback
    
    * wip
    
    * [EVM] add remaining state interface function
    
    * rebase

---
+++ x/evm/state/code.go
@@ -0,0 +1,39 @@
+package state
+
+import (
+	"encoding/binary"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/sei-protocol/sei-chain/x/evm/types"
+)
+
+func (s *StateDBImpl) GetCodeHash(addr common.Address) common.Hash {
+	store := s.k.PrefixStore(s.ctx, types.CodeHashKeyPrefix)
+	bz := store.Get(addr[:])
+	if bz == nil {
+		return common.Hash{}
+	}
+	return common.BytesToHash(bz)
+}
+
+func (s *StateDBImpl) GetCode(addr common.Address) []byte {
+	return s.k.PrefixStore(s.ctx, types.CodeKeyPrefix).Get(addr[:])
+}
+
+func (s *StateDBImpl) SetCode(addr common.Address, code []byte) {
+	s.k.PrefixStore(s.ctx, types.CodeKeyPrefix).Set(addr[:], code)
+	length := make([]byte, 8)
+	binary.BigEndian.PutUint64(length, uint64(len(code)))
+	s.k.PrefixStore(s.ctx, types.CodeSizeKeyPrefix).Set(addr[:], length)
+	h := crypto.Keccak256Hash(code)
+	s.k.PrefixStore(s.ctx, types.CodeHashKeyPrefix).Set(addr[:], h[:])
+}
+
+func (s *StateDBImpl) GetCodeSize(addr common.Address) int {
+	bz := s.k.PrefixStore(s.ctx, types.CodeSizeKeyPrefix).Get(addr[:])
+	if bz == nil {
+		return 0
+	}
+	return int(binary.BigEndian.Uint64(bz))
+}
