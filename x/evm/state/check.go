49a78d59a8565cbd20db5e62e4db3739ce5df8a6
codchen <codchen03@gmail.com>
2023-10-02 22:23:44 +0800

    [EVM] add remaining state interface functions (#1075)
    * [EVM] Add gas check ante handler
    
    * [EVM] implement balance related methods for state interface
    
    * Add account state to EVM state interface implementation
    
    * add Snapshot and rollback
    
    * wip
    
    * [EVM] add remaining state interface function
    
    * rebase

---
+++ x/evm/state/check.go
@@ -0,0 +1,33 @@
+package state
+
+import (
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/sei-protocol/sei-chain/x/evm/types"
+)
+
+// Exist reports whether the given account exists in state.
+// Notably this should also return true for self-destructed accounts.
+func (s *StateDBImpl) Exist(addr common.Address) bool {
+	// if there is any entry under addr, it exists
+	store := s.k.PrefixStore(s.ctx, types.StateKey(addr))
+	iter := store.Iterator(nil, nil)
+	if iter.Valid() {
+		return true
+	}
+
+	// if there is code under addr, it exists
+	if s.GetCodeHash(addr).Cmp(common.Hash{}) != 0 {
+		return true
+	}
+
+	// go-ethereum impl considers just-deleted accounts as "exist" as well
+	return s.HasSelfDestructed(addr)
+}
+
+// Empty returns whether the given account is empty. Empty
+// is defined according to EIP161 (balance = nonce = code = 0).
+func (s *StateDBImpl) Empty(addr common.Address) bool {
+	return s.GetBalance(addr).Cmp(big.NewInt(0)) == 0 && s.GetNonce(addr) == 0 && s.GetCodeHash(addr).Cmp(common.Hash{}) == 0
+}
---
+++ x/evm/state/refund_test.go
@@ -0,0 +1,20 @@
+package state
+
+import (
+	"testing"
+
+	"github.com/sei-protocol/sei-chain/x/evm/keeper"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGasRefund(t *testing.T) {
+	k, _, ctx := keeper.MockEVMKeeper()
+	statedb := NewStateDBImpl(ctx, k)
+
+	require.Equal(t, uint64(0), statedb.GetRefund())
+	statedb.AddRefund(2)
+	require.Equal(t, uint64(2), statedb.GetRefund())
+	statedb.SubRefund(1)
+	require.Equal(t, uint64(1), statedb.GetRefund())
+	require.Panics(t, func() { statedb.SubRefund(2) })
+}
