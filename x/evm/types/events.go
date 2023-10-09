3bed22a7ec5fb4f69457122497dae204bff2ac47
codchen <codchen03@gmail.com>
2023-10-09 11:05:51 +0800

    [EVM] implement message server that processes EVM transactions (#1083)
    * [EVM] implement message server that processes EVM transactions
    
    * add tests

---
+++ x/evm/types/events.go
@@ -0,0 +1,15 @@
+package types
+
+const (
+	EventTypeEVMLog = "evm_log"
+
+	AttributeTypeContractAddress = "contract_address"
+	AttributeTypeTopics          = "topics"
+	AttributeTypeData            = "data"
+	AttributeTypeBlockNumber     = "block_number"
+	AttributeTypeBlockHash       = "block_hash"
+	AttributeTypeTxHash          = "tx_hash"
+	AttributeTypeTxIndex         = "tx_idx"
+	AttributeTypeIndex           = "idx"
+	AttributeTypeRemoved         = "removed"
+)
