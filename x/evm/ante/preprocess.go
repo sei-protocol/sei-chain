ac2f910c6e6d211d0f728ad51e8fd114f2697da9
codchen <codchen03@gmail.com>
2023-10-10 13:05:42 +0800

    [EVM] add bank precompile (#1088)
    * [EVM] implement message server that processes EVM transactions
    
    * [EVM] add bank precompile
    
    * upgrade to Go 1.20
    
    * rename coinbase
    
    * tests & linter

--- x/evm/ante/preprocess.go
+++ x/evm/ante/preprocess.go
@@ -56,7 +56,7 @@ func (p EVMPreprocessDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate 
 		return ctx, ethtypes.ErrInvalidChainId
 	}
 	ctx = evmtypes.SetContextEVMVersion(ctx, version)
-	V = adjustV(V, version, ethTx.Type(), ethCfg.ChainID)
+	V = adjustV(V, ethTx.Type(), ethCfg.ChainID)
 	signer := SignerMap[version](ethCfg.ChainID)
 	pubkey, err := recoverPubkey(signer.Hash(ethTx), R, S, V, true)
 	if err != nil {
@@ -81,7 +81,9 @@ func (p EVMPreprocessDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate 
 	// set pubkey in acc object if not exist. Not doing it in the above block in case an account is created
 	// as a recipient of a send
 	if acc := p.accountKeeper.GetAccount(ctx, seiAddr); acc.GetPubKey() == nil {
-		acc.SetPubKey(&seiPubkey)
+		if err := acc.SetPubKey(&seiPubkey); err != nil {
+			return ctx, err
+		}
 		p.accountKeeper.SetAccount(ctx, acc)
 	}
 
@@ -90,7 +92,7 @@ func (p EVMPreprocessDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate 
 			return ctx, err
 		}
 	}
-	return ctx, nil
+	return next(ctx, tx, simulate)
 }
 
 // first half of go-ethereum/core/types/transaction_signing.go:recoverPlain
@@ -137,7 +139,7 @@ func isTxTypeAllowed(version evmtypes.SignerVersion, txType uint8) bool {
 	return false
 }
 
-func adjustV(V *big.Int, version evmtypes.SignerVersion, txType uint8, chainID *big.Int) *big.Int {
+func adjustV(V *big.Int, txType uint8, chainID *big.Int) *big.Int {
 	// Non-legacy TX always needs to be bumped by 27
 	if txType != ethtypes.LegacyTxType {
 		return new(big.Int).Add(V, big.NewInt(27))
