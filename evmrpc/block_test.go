42c7d3828b02a6b59fa7b829fe3521342ddc3eab
codchen <codchen03@gmail.com>
2023-10-13 12:42:06 +0800

    refactor tests

--- evmrpc/block_test.go
+++ evmrpc/block_test.go
@@ -1,172 +1,22 @@
 package evmrpc
 
 import (
-	"context"
 	"encoding/hex"
-	"errors"
 	"fmt"
 	"io"
 	"math/big"
 	"net/http"
 	"strings"
 	"testing"
-	"time"
 
 	"github.com/cosmos/cosmos-sdk/crypto/hd"
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/ethereum/go-ethereum/common"
 	ethtypes "github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/rpc"
-	"github.com/sei-protocol/sei-chain/app"
-	"github.com/sei-protocol/sei-chain/x/evm/keeper"
 	"github.com/sei-protocol/sei-chain/x/evm/types"
 	"github.com/sei-protocol/sei-chain/x/evm/types/ethtx"
 	"github.com/stretchr/testify/require"
-	abci "github.com/tendermint/tendermint/abci/types"
-	"github.com/tendermint/tendermint/libs/bytes"
-	"github.com/tendermint/tendermint/libs/log"
-	"github.com/tendermint/tendermint/rpc/client/mock"
-	"github.com/tendermint/tendermint/rpc/coretypes"
-	tmtypes "github.com/tendermint/tendermint/types"
 )
-
-const BlockTestPort = 7779
-const BadBlockTestPort = 7780
-
-var EncodingConfig = app.MakeEncodingConfig()
-var TxConfig = EncodingConfig.TxConfig
-var Encoder = TxConfig.TxEncoder()
-var Decoder = TxConfig.TxDecoder()
-var Tx sdk.Tx
-
-type MockClient struct {
-	mock.Client
-}
-
-func (c *MockClient) mockBlock() *coretypes.ResultBlock {
-	return &coretypes.ResultBlock{
-		BlockID: tmtypes.BlockID{
-			Hash: bytes.HexBytes([]byte("0000000000000000000000000000000000000000000000000000000000000001")),
-		},
-		Block: &tmtypes.Block{
-			Header: tmtypes.Header{
-				ChainID:         "test",
-				Height:          8,
-				Time:            time.Unix(1696941649, 0),
-				DataHash:        bytes.HexBytes([]byte("0000000000000000000000000000000000000000000000000000000000000002")),
-				AppHash:         bytes.HexBytes([]byte("0000000000000000000000000000000000000000000000000000000000000003")),
-				LastResultsHash: bytes.HexBytes([]byte("0000000000000000000000000000000000000000000000000000000000000004")),
-				ProposerAddress: tmtypes.Address([]byte("0000000000000000000000000000000000000000000000000000000000000005")),
-				LastBlockID: tmtypes.BlockID{
-					Hash: bytes.HexBytes([]byte("0000000000000000000000000000000000000000000000000000000000000006")),
-				},
-			},
-			Data: tmtypes.Data{
-				Txs: []tmtypes.Tx{func() []byte {
-					bz, _ := Encoder(Tx)
-					return bz
-				}()},
-			},
-		},
-	}
-}
-
-func (c *MockClient) Genesis(context.Context) (*coretypes.ResultGenesis, error) {
-	return &coretypes.ResultGenesis{Genesis: &tmtypes.GenesisDoc{InitialHeight: 1}}, nil
-}
-
-func (c *MockClient) Block(ctx context.Context, height *int64) (*coretypes.ResultBlock, error) {
-	return c.mockBlock(), nil
-}
-
-func (c *MockClient) BlockByHash(ctx context.Context, hash bytes.HexBytes) (*coretypes.ResultBlock, error) {
-	return c.mockBlock(), nil
-}
-
-func (c *MockClient) BlockResults(ctx context.Context, height *int64) (*coretypes.ResultBlockResults, error) {
-	return &coretypes.ResultBlockResults{
-		TxsResults: []*abci.ExecTxResult{
-			{
-				Data: func() []byte {
-					bz, _ := Encoder(Tx)
-					return bz
-				}(),
-				GasWanted: 10,
-				GasUsed:   5,
-				Events: []abci.Event{{
-					Type: types.EventTypeEVMLog,
-					Attributes: []abci.EventAttribute{{
-						Key:   []byte(types.AttributeTypeContractAddress),
-						Value: []byte("0x1111111111111111111111111111111111111111111111111111111111111111"),
-					}, {
-						Key:   []byte(types.AttributeTypeBlockHash),
-						Value: []byte("0x1111111111111111111111111111111111111111111111111111111111111111"),
-					}, {
-						Key:   []byte(types.AttributeTypeBlockNumber),
-						Value: []byte("8"),
-					}, {
-						Key:   []byte(types.AttributeTypeData),
-						Value: []byte("xyz"),
-					}, {
-						Key:   []byte(types.AttributeTypeIndex),
-						Value: []byte("1"),
-					}, {
-						Key:   []byte(types.AttributeTypeTxIndex),
-						Value: []byte("2"),
-					}, {
-						Key:   []byte(types.AttributeTypeRemoved),
-						Value: []byte("true"),
-					}, {
-						Key:   []byte(types.AttributeTypeTopics),
-						Value: []byte("0x1111111111111111111111111111111111111111111111111111111111111111,0x1111111111111111111111111111111111111111111111111111111111111112"),
-					}, {
-						Key:   []byte(types.AttributeTypeTxHash),
-						Value: []byte("0x1111111111111111111111111111111111111111111111111111111111111113"),
-					}},
-				}},
-			},
-		},
-	}, nil
-}
-
-type MockBadClient struct {
-	MockClient
-}
-
-func (m *MockBadClient) Block(ctx context.Context, height *int64) (*coretypes.ResultBlock, error) {
-	return nil, errors.New("error block")
-}
-
-func (m *MockBadClient) BlockByHash(ctx context.Context, hash bytes.HexBytes) (*coretypes.ResultBlock, error) {
-	return nil, errors.New("error block")
-}
-
-func (m *MockBadClient) Genesis(context.Context) (*coretypes.ResultGenesis, error) {
-	return nil, errors.New("error genesis")
-}
-
-var EVMKeeper *keeper.Keeper
-var Ctx sdk.Context
-
-func init() {
-	EVMKeeper, _, Ctx = keeper.MockEVMKeeper()
-	httpServer, err := NewEVMHTTPServer(log.NewNopLogger(), TestAddr, BlockTestPort, rpc.DefaultHTTPTimeouts, &MockClient{}, EVMKeeper, func() sdk.Context { return Ctx }, Decoder)
-	if err != nil {
-		panic(err)
-	}
-	if err := httpServer.Start(); err != nil {
-		panic(err)
-	}
-	badHttpServer, err := NewEVMHTTPServer(log.NewNopLogger(), TestAddr, BadBlockTestPort, rpc.DefaultHTTPTimeouts, &MockBadClient{}, EVMKeeper, func() sdk.Context { return Ctx }, Decoder)
-	if err != nil {
-		panic(err)
-	}
-	if err := badHttpServer.Start(); err != nil {
-		panic(err)
-	}
-	time.Sleep(1 * time.Second)
-}
 
 func TestGetBlockByHash(t *testing.T) {
 	types.RegisterInterfaces(EncodingConfig.InterfaceRegistry)
@@ -205,7 +55,7 @@ func TestGetBlockByHash(t *testing.T) {
 	}))
 
 	body := "{\"jsonrpc\": \"2.0\",\"method\": \"eth_getBlockByHash\",\"params\":[\"0x0000000000000000000000000000000000000000000000000000000000000001\",true],\"id\":\"test\"}"
-	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("http://%s:%d", TestAddr, BlockTestPort), strings.NewReader(body))
+	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("http://%s:%d", TestAddr, TestPort), strings.NewReader(body))
 	require.Nil(t, err)
 	req.Header.Set("Content-Type", "application/json")
 	res, err := http.DefaultClient.Do(req)
@@ -253,7 +103,7 @@ func TestGetBlockByNumber(t *testing.T) {
 
 	for _, num := range []string{"0x8", "earliest", "latest", "pending", "finalized", "safe"} {
 		body := fmt.Sprintf("{\"jsonrpc\": \"2.0\",\"method\": \"eth_getBlockByNumber\",\"params\":[\"%s\",true],\"id\":\"test\"}", num)
-		req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("http://%s:%d", TestAddr, BlockTestPort), strings.NewReader(body))
+		req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("http://%s:%d", TestAddr, TestPort), strings.NewReader(body))
 		require.Nil(t, err)
 		req.Header.Set("Content-Type", "application/json")
 		res, err := http.DefaultClient.Do(req)
@@ -264,7 +114,7 @@ func TestGetBlockByNumber(t *testing.T) {
 	}
 
 	body := "{\"jsonrpc\": \"2.0\",\"method\": \"eth_getBlockByNumber\",\"params\":[\"bad_num\",true],\"id\":\"test\"}"
-	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("http://%s:%d", TestAddr, BlockTestPort), strings.NewReader(body))
+	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("http://%s:%d", TestAddr, TestPort), strings.NewReader(body))
 	require.Nil(t, err)
 	req.Header.Set("Content-Type", "application/json")
 	res, err := http.DefaultClient.Do(req)
@@ -312,7 +162,7 @@ func TestGetBlockTransactionCount(t *testing.T) {
 	// get by block number
 	for _, num := range []string{"0x8", "earliest", "latest", "pending", "finalized", "safe"} {
 		body := fmt.Sprintf("{\"jsonrpc\": \"2.0\",\"method\": \"eth_getBlockTransactionCountByNumber\",\"params\":[\"%s\"],\"id\":\"test\"}", num)
-		req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("http://%s:%d", TestAddr, BlockTestPort), strings.NewReader(body))
+		req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("http://%s:%d", TestAddr, TestPort), strings.NewReader(body))
 		require.Nil(t, err)
 		req.Header.Set("Content-Type", "application/json")
 		res, err := http.DefaultClient.Do(req)
@@ -324,7 +174,7 @@ func TestGetBlockTransactionCount(t *testing.T) {
 
 	// get error returns null
 	body := "{\"jsonrpc\": \"2.0\",\"method\": \"eth_getBlockTransactionCountByNumber\",\"params\":[\"0x8\"],\"id\":\"test\"}"
-	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("http://%s:%d", TestAddr, BadBlockTestPort), strings.NewReader(body))
+	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("http://%s:%d", TestAddr, TestBadPort), strings.NewReader(body))
 	req.Header.Set("Content-Type", "application/json")
 	res, err := http.DefaultClient.Do(req)
 	require.Nil(t, err)
@@ -332,7 +182,7 @@ func TestGetBlockTransactionCount(t *testing.T) {
 	require.Nil(t, err)
 	require.Equal(t, "{\"jsonrpc\":\"2.0\",\"id\":\"test\",\"result\":null}\n", string(resBody))
 	body = "{\"jsonrpc\": \"2.0\",\"method\": \"eth_getBlockTransactionCountByNumber\",\"params\":[\"earliest\"],\"id\":\"test\"}"
-	req, err = http.NewRequest(http.MethodGet, fmt.Sprintf("http://%s:%d", TestAddr, BadBlockTestPort), strings.NewReader(body))
+	req, err = http.NewRequest(http.MethodGet, fmt.Sprintf("http://%s:%d", TestAddr, TestBadPort), strings.NewReader(body))
 	req.Header.Set("Content-Type", "application/json")
 	res, err = http.DefaultClient.Do(req)
 	require.Nil(t, err)
@@ -340,7 +190,7 @@ func TestGetBlockTransactionCount(t *testing.T) {
 	require.Nil(t, err)
 	require.Equal(t, "{\"jsonrpc\":\"2.0\",\"id\":\"test\",\"result\":null}\n", string(resBody))
 	body = "{\"jsonrpc\": \"2.0\",\"method\": \"eth_getBlockTransactionCountByHash\",\"params\":[\"0x0000000000000000000000000000000000000000000000000000000000000001\"],\"id\":\"test\"}"
-	req, err = http.NewRequest(http.MethodGet, fmt.Sprintf("http://%s:%d", TestAddr, BadBlockTestPort), strings.NewReader(body))
+	req, err = http.NewRequest(http.MethodGet, fmt.Sprintf("http://%s:%d", TestAddr, TestBadPort), strings.NewReader(body))
 	req.Header.Set("Content-Type", "application/json")
 	res, err = http.DefaultClient.Do(req)
 	require.Nil(t, err)
@@ -350,7 +200,7 @@ func TestGetBlockTransactionCount(t *testing.T) {
 
 	// get by hash
 	body = "{\"jsonrpc\": \"2.0\",\"method\": \"eth_getBlockTransactionCountByHash\",\"params\":[\"0x0000000000000000000000000000000000000000000000000000000000000001\"],\"id\":\"test\"}"
-	req, err = http.NewRequest(http.MethodGet, fmt.Sprintf("http://%s:%d", TestAddr, BlockTestPort), strings.NewReader(body))
+	req, err = http.NewRequest(http.MethodGet, fmt.Sprintf("http://%s:%d", TestAddr, TestPort), strings.NewReader(body))
 	require.Nil(t, err)
 	req.Header.Set("Content-Type", "application/json")
 	res, err = http.DefaultClient.Do(req)
--- evmrpc/tx_test.go
+++ evmrpc/tx_test.go
@@ -1,6 +1,7 @@
 package evmrpc
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
@@ -15,7 +16,8 @@ import (
 func TestGetTxReceipt(t *testing.T) {
 	types.RegisterInterfaces(EncodingConfig.InterfaceRegistry)
 
-	require.Nil(t, EVMKeeper.SetReceipt(Ctx, common.HexToHash("0x1234567890123456789012345678901234567890123456789012345678901234"), &types.Receipt{
+	hash := common.HexToHash("0x1234567890123456789012345678901234567890123456789012345678901234")
+	require.Nil(t, EVMKeeper.SetReceipt(Ctx, hash, &types.Receipt{
 		From:              "0x123456789012345678902345678901234567890",
 		To:                "0x123456789012345678902345678901234567890",
 		TransactionIndex:  0,
@@ -30,21 +32,55 @@ func TestGetTxReceipt(t *testing.T) {
 	}))
 
 	body := "{\"jsonrpc\": \"2.0\",\"method\": \"eth_getTransactionReceipt\",\"params\":[\"0x1234567890123456789012345678901234567890123456789012345678901234\"],\"id\":\"test\"}"
-	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("http://%s:%d", TestAddr, BlockTestPort), strings.NewReader(body))
+	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("http://%s:%d", TestAddr, TestPort), strings.NewReader(body))
 	require.Nil(t, err)
 	req.Header.Set("Content-Type", "application/json")
 	res, err := http.DefaultClient.Do(req)
 	require.Nil(t, err)
 	resBody, err := io.ReadAll(res.Body)
 	require.Nil(t, err)
-	require.Equal(t, "{\"jsonrpc\":\"2.0\",\"id\":\"test\",\"result\":{\"blockHash\":\"0x3030303030303030303030303030303030303030303030303030303030303031\",\"blockNumber\":\"0x8\",\"contractAddress\":\"0x0123456789012345678902345678901234567890\",\"cumulativeGasUsed\":\"0x7b\",\"effectiveGasPrice\":\"0xa\",\"from\":\"0x0123456789012345678902345678901234567890\",\"gasUsed\":\"0x37\",\"logs\":[{\"address\":\"0x1111111111111111111111111111111111111111\",\"topics\":[\"0x1111111111111111111111111111111111111111111111111111111111111111\",\"0x1111111111111111111111111111111111111111111111111111111111111112\"],\"data\":\"0x78797a\",\"blockNumber\":\"0x8\",\"transactionHash\":\"0x1111111111111111111111111111111111111111111111111111111111111113\",\"transactionIndex\":\"0x2\",\"blockHash\":\"0x1111111111111111111111111111111111111111111111111111111111111111\",\"logIndex\":\"0x1\",\"removed\":true}],\"logsBloom\":\"0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000\",\"status\":\"0x0\",\"to\":\"0x0123456789012345678902345678901234567890\",\"transactionHash\":\"0x0123456789012345678902345678901234567890123456789012345678901234\",\"transactionIndex\":\"0x0\",\"type\":\"0x1\"}}\n", string(resBody))
+	resObj := map[string]interface{}{}
+	require.Nil(t, json.Unmarshal(resBody, &resObj))
+	resObj = resObj["result"].(map[string]interface{})
+	require.Equal(t, "0x3030303030303030303030303030303030303030303030303030303030303031", resObj["blockHash"].(string))
+	require.Equal(t, "0x8", resObj["blockNumber"].(string))
+	require.Equal(t, "0x0123456789012345678902345678901234567890", resObj["contractAddress"].(string))
+	require.Equal(t, "0x7b", resObj["cumulativeGasUsed"].(string))
+	require.Equal(t, "0xa", resObj["effectiveGasPrice"].(string))
+	require.Equal(t, "0x0123456789012345678902345678901234567890", resObj["from"].(string))
+	require.Equal(t, "0x37", resObj["gasUsed"].(string))
+	logs := resObj["logs"].([]interface{})
+	require.Equal(t, 1, len(logs))
+	log := logs[0].(map[string]interface{})
+	require.Equal(t, "0x1111111111111111111111111111111111111111", log["address"].(string))
+	topics := log["topics"].([]interface{})
+	require.Equal(t, 2, len(topics))
+	require.Equal(t, "0x1111111111111111111111111111111111111111111111111111111111111111", topics[0].(string))
+	require.Equal(t, "0x1111111111111111111111111111111111111111111111111111111111111112", topics[1].(string))
+	require.Equal(t, "0x78797a", log["data"].(string))
+	require.Equal(t, "0x8", log["blockNumber"].(string))
+	require.Equal(t, "0x1111111111111111111111111111111111111111111111111111111111111113", log["transactionHash"].(string))
+	require.Equal(t, "0x2", log["transactionIndex"].(string))
+	require.Equal(t, "0x1111111111111111111111111111111111111111111111111111111111111111", log["blockHash"].(string))
+	require.Equal(t, "0x1", log["logIndex"].(string))
+	require.True(t, log["removed"].(bool))
+	require.Equal(t, "0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000", resObj["logsBloom"].(string))
+	require.Equal(t, "0x0", resObj["status"].(string))
+	require.Equal(t, "0x0123456789012345678902345678901234567890", resObj["to"].(string))
+	require.Equal(t, "0x0123456789012345678902345678901234567890123456789012345678901234", resObj["transactionHash"].(string))
+	require.Equal(t, "0x0", resObj["transactionIndex"].(string))
+	require.Equal(t, "0x1", resObj["type"].(string))
 
-	req, err = http.NewRequest(http.MethodGet, fmt.Sprintf("http://%s:%d", TestAddr, BadBlockTestPort), strings.NewReader(body))
+	req, err = http.NewRequest(http.MethodGet, fmt.Sprintf("http://%s:%d", TestAddr, TestBadPort), strings.NewReader(body))
 	require.Nil(t, err)
 	req.Header.Set("Content-Type", "application/json")
 	res, err = http.DefaultClient.Do(req)
 	require.Nil(t, err)
 	resBody, err = io.ReadAll(res.Body)
 	require.Nil(t, err)
-	require.Equal(t, "{\"jsonrpc\":\"2.0\",\"id\":\"test\",\"error\":{\"code\":-32000,\"message\":\"error block\"}}\n", string(resBody))
+	resObj = map[string]interface{}{}
+	require.Nil(t, json.Unmarshal(resBody, &resObj))
+	resObj = resObj["error"].(map[string]interface{})
+	require.Equal(t, float64(-32000), resObj["code"].(float64))
+	require.Equal(t, "error block", resObj["message"].(string))
 }
