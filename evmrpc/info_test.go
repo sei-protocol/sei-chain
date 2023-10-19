493cc9fc58c087d3f48acfd7bb80cca0d60274ed
codchen <codchen03@gmail.com>
2023-10-19 12:41:42 +0800

    [EVM] add info endpoints (#1107)

--- evmrpc/info_test.go
+++ evmrpc/info_test.go
@@ -1,37 +1,49 @@
 package evmrpc
 
 import (
-	"encoding/json"
-	"fmt"
-	"io"
 	"math/big"
-	"net/http"
-	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
+func TestBlockNumber(t *testing.T) {
+	resObj := sendRequestGood(t, "blockNumber")
+	result := resObj["result"].(string)
+	require.Equal(t, "0x8", result)
+}
+
+func TestChainID(t *testing.T) {
+	resObj := sendRequestGood(t, "chainId")
+	result := resObj["result"].(string)
+	require.Equal(t, "0x1", result)
+}
+
+func TestCoinbase(t *testing.T) {
+	resObj := sendRequestGood(t, "coinbase")
+	result := resObj["result"].(string)
+	require.Equal(t, "0x27f7b8b8b5a4e71e8e9aa671f4e4031e3773303f", result)
+}
+
+func TestGasPrice(t *testing.T) {
+	resObj := sendRequestGood(t, "gasPrice")
+	result := resObj["result"].(string)
+	require.Equal(t, "0xa", result)
+}
+
 func TestFeeHistory(t *testing.T) {
-	bodyByNumber := "{\"jsonrpc\": \"2.0\",\"method\": \"eth_feeHistory\",\"params\":[\"0x1\",\"0x8\",[0.5]],\"id\":\"test\"}"
-	bodyByLatest := "{\"jsonrpc\": \"2.0\",\"method\": \"eth_feeHistory\",\"params\":[\"0x1\",\"latest\",[0.5]],\"id\":\"test\"}"
-	bodyByEarliest := "{\"jsonrpc\": \"2.0\",\"method\": \"eth_feeHistory\",\"params\":[\"0x1\",\"earliest\",[0.5]],\"id\":\"test\"}"
-	bodyOld := "{\"jsonrpc\": \"2.0\",\"method\": \"eth_feeHistory\",\"params\":[\"0x1\",\"0x1\",[0.5]],\"id\":\"test\"}"
-	bodyFuture := "{\"jsonrpc\": \"2.0\",\"method\": \"eth_feeHistory\",\"params\":[\"0x1\",\"0x9\",[0.5]],\"id\":\"test\"}"
-	for body, expectedOldest := range map[string]string{
-		bodyByNumber: "0x8", bodyByLatest: "0x8", bodyByEarliest: "0x1", bodyOld: "0x1", bodyFuture: "0x8",
+	bodyByNumber := []interface{}{"0x1", "0x8", []interface{}{0.5}}
+	bodyByLatest := []interface{}{"0x1", "latest", []interface{}{0.5}}
+	bodyByEarliest := []interface{}{"0x1", "earliest", []interface{}{0.5}}
+	bodyOld := []interface{}{"0x1", "0x1", []interface{}{0.5}}
+	bodyFuture := []interface{}{"0x1", "0x9", []interface{}{0.5}}
+	expectedOldest := []string{"0x8", "0x8", "0x1", "0x1", "0x8"}
+	for i, body := range [][]interface{}{
+		bodyByNumber, bodyByLatest, bodyByEarliest, bodyOld, bodyFuture,
 	} {
-		req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("http://%s:%d", TestAddr, TestPort), strings.NewReader(body))
-		require.Nil(t, err)
-		req.Header.Set("Content-Type", "application/json")
-		res, err := http.DefaultClient.Do(req)
-		require.Nil(t, err)
-		resBody, err := io.ReadAll(res.Body)
-		require.Nil(t, err)
-		resObj := map[string]interface{}{}
-		require.Nil(t, json.Unmarshal(resBody, &resObj))
+		resObj := sendRequestGood(t, "feeHistory", body...)
 		resObj = resObj["result"].(map[string]interface{})
-		require.Equal(t, expectedOldest, resObj["oldestBlock"].(string))
+		require.Equal(t, expectedOldest[i], resObj["oldestBlock"].(string))
 		rewards := resObj["reward"].([]interface{})
 		require.Equal(t, 1, len(rewards))
 		reward := rewards[0].([]interface{})
@@ -46,19 +58,11 @@ func TestFeeHistory(t *testing.T) {
 	}
 
 	// bad percentile
-	outOfRangeBody1 := "{\"jsonrpc\": \"2.0\",\"method\": \"eth_feeHistory\",\"params\":[\"0x1\",\"0x8\",[-1]],\"id\":\"test\"}"
-	outOfRangeBody2 := "{\"jsonrpc\": \"2.0\",\"method\": \"eth_feeHistory\",\"params\":[\"0x1\",\"0x8\",[101]],\"id\":\"test\"}"
-	outOfOrderBody := "{\"jsonrpc\": \"2.0\",\"method\": \"eth_feeHistory\",\"params\":[\"0x1\",\"0x8\",[99, 1]],\"id\":\"test\"}"
-	for _, body := range []string{outOfRangeBody1, outOfRangeBody2, outOfOrderBody} {
-		req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("http://%s:%d", TestAddr, TestPort), strings.NewReader(body))
-		require.Nil(t, err)
-		req.Header.Set("Content-Type", "application/json")
-		res, err := http.DefaultClient.Do(req)
-		require.Nil(t, err)
-		resBody, err := io.ReadAll(res.Body)
-		require.Nil(t, err)
-		resObj := map[string]interface{}{}
-		require.Nil(t, json.Unmarshal(resBody, &resObj))
+	outOfRangeBody1 := []interface{}{"0x1", "0x8", []interface{}{-1}}
+	outOfRangeBody2 := []interface{}{"0x1", "0x8", []interface{}{101}}
+	outOfOrderBody := []interface{}{"0x1", "0x8", []interface{}{99, 1}}
+	for _, body := range [][]interface{}{outOfRangeBody1, outOfRangeBody2, outOfOrderBody} {
+		resObj := sendRequestGood(t, "feeHistory", body...)
 		errMap := resObj["error"].(map[string]interface{})
 		require.Equal(t, "invalid reward percentiles: must be ascending and between 0 and 100", errMap["message"].(string))
 	}
