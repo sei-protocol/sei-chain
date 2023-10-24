2687147a0ce6cb9e5100747ae9fa67eb90c2ce85
Kartik Bhat <kartikbhat@kartiks-mbp-2.mynetworksettings.com>
2023-10-24 14:02:44 -0400

    Remove sdk errs

--- sei-db/common/utils/errors.go
+++ sei-db/common/utils/errors.go
@@ -3,16 +3,11 @@ package utils
 import (
 	"errors"
 	"strings"
-
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
-// StoreCodespace defines the store package's unique error code space.
-const StoreCodespace = "store"
-
 var (
-	ErrKeyEmpty      = sdkerrors.Register(StoreCodespace, 1, "key empty")
-	ErrStartAfterEnd = sdkerrors.Register(StoreCodespace, 2, "start key after end key")
+	ErrKeyEmpty      = errors.New("key empty")
+	ErrStartAfterEnd = errors.New("start key after end key")
 )
 
 // Join returns an error that wraps the given errors.
