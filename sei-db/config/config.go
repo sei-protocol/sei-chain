9743558bf1d8b786f025d42da68a7aa1c15b5313
yzang2019 <zymfrank@gmail.com>
2023-10-24 13:57:43 -0700

    Add configs and code to set up seiDB

---
+++ sei-db/config/config.go
@@ -0,0 +1,58 @@
+package config
+
+const (
+	DefaultCacheSize        = 100000
+	DefaultSnapshotInterval = 10000
+)
+
+type StateCommitConfig struct {
+	// Enable defines if the state-commit should be enabled.
+	// If true, it will replace the existing IAVL db backend with memIAVL.
+	// defaults to false.
+	Enable bool `mapstructure:"enable"`
+
+	// ZeroCopy defines if the memiavl should return slices pointing to mmap-ed buffers directly (zero-copy),
+	// the zero-copied slices must not be retained beyond current block's execution.
+	// the sdk address cache will be disabled if zero-copy is enabled.
+	// defaults to false.
+	ZeroCopy bool `mapstructure:"zero-copy"`
+
+	// AsyncCommitBuffer defines the size of asynchronous commit queue, this greatly improve block catching-up
+	// performance, -1 or 0 means synchronous commit.
+	AsyncCommitBuffer int `mapstructure:"async-commit-buffer"`
+
+	// SnapshotKeepRecent defines what many old snapshots (excluding the latest one) to keep
+	// defaults to 1 to make sure ibc relayers work.
+	SnapshotKeepRecent uint32 `mapstructure:"snapshot-keep-recent"`
+
+	// SnapshotInterval defines the block interval the memiavl snapshot is taken, default to 10000.
+	SnapshotInterval uint32 `mapstructure:"snapshot-interval"`
+
+	// CacheSize defines the size of the cache for each memiavl store.
+	// defaults to 100000.
+	CacheSize int `mapstructure:"cache-size"`
+}
+
+type StateStoreConfig struct {
+
+	//Enable defines if the state-store should be enabled for historical queries.
+	Enable bool `mapstructure:"enable"`
+
+	//Backend defines the backend database used for state-store
+	Backend string `mapstructure:"backend"`
+}
+
+func DefaultStateCommitConfig() StateCommitConfig {
+	return StateCommitConfig{
+		AsyncCommitBuffer:  100,
+		CacheSize:          DefaultCacheSize,
+		SnapshotInterval:   DefaultSnapshotInterval,
+		SnapshotKeepRecent: 1,
+	}
+}
+
+func DefaultStateStoreConfig() StateStoreConfig {
+	return StateStoreConfig{
+		Backend: "pebbledb",
+	}
+}
---
+++ sei-db/config/toml.go
@@ -0,0 +1,42 @@
+package config
+
+// DefaultConfigTemplate defines the configuration template for the seiDB configuration
+const DefaultConfigTemplate = `
+###############################################################################
+###                             SeiDB Configuration                       ###
+###############################################################################
+
+[state-commit]
+
+# Enable defines if the state-commit (memiavl) should be enabled to override existing IAVL db backend.
+enable = {{ .StateCommit.Enable }}
+
+# ZeroCopy defines if memiavl should return slices pointing to mmap-ed buffers directly (zero-copy),
+# the zero-copied slices must not be retained beyond current block's execution.
+# the sdk address cache will be disabled if zero-copy is enabled.
+zero-copy = {{ .StateCommit.ZeroCopy }}
+
+# AsyncCommitBuffer defines the size of asynchronous commit queue, this greatly improve block catching-up
+# performance, <=0 means synchronous commit.
+async-commit-buffer = {{ .StateCommit.AsyncCommitBuffer }}
+
+# SnapshotKeepRecent defines how many state-commit snapshots (besides the latest one) to keep 
+# defaults to 1 to make sure ibc relayers work.
+snapshot-keep-recent = {{ .StateCommit.SnapshotKeepRecent }}
+
+# SnapshotInterval defines the block interval the SC snapshot is taken, default to 10000.
+snapshot-interval = {{ .StateCommit.SnapshotInterval }}
+
+# CacheSize defines the size of the cache for each memiavl store, default to 100000.
+cache-size = {{ .StateCommit.CacheSize }}
+
+[state-store]
+
+# Enable defines if the state-store should be enabled for historical queries.
+# In order to use state-store, you need to make sure to enable state-commit at the same time
+enable = {{ .StateStore.Enable }}
+
+# DBBackend defines the backend database used for state-store 
+backend = "{{ .StateStore.Backend }}"
+
+`
