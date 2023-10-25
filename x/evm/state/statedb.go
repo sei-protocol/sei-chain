b584cca30a67633e7c4f72cf8d6c114f902e3245
codchen <codchen03@gmail.com>
2023-10-25 15:29:43 +0800

    [EVM] get simulation endpoints (#1110)
    * simulate api
    
    * add tests
    
    * linter

--- x/evm/state/statedb.go
+++ x/evm/state/statedb.go
@@ -6,6 +6,7 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/vm"
 	"github.com/sei-protocol/sei-chain/utils"
 	"github.com/sei-protocol/sei-chain/x/evm/types"
 )
@@ -46,7 +47,7 @@ func (s *DBImpl) Finalize() error {
 		return err
 	}
 
-	logs, err := s.GetLogs()
+	logs, err := s.GetAllLogs()
 	if err != nil {
 		return err
 	}
@@ -82,6 +83,49 @@ func (s *DBImpl) flushCtx(ctx sdk.Context) {
 	s.k.PurgePrefix(ctx, types.AccountTransientStateKeyPrefix)
 	s.k.PurgePrefix(ctx, types.TransientModuleStateKeyPrefix)
 	ctx.MultiStore().(sdk.CacheMultiStore).Write()
+}
+
+// Backward-compatibility functions
+func (s *DBImpl) Error() error {
+	return s.Err()
+}
+
+func (s *DBImpl) GetStorageRoot(common.Address) common.Hash {
+	panic("GetStorageRoot is not implemented and called unexpectedly")
+}
+
+func (s *DBImpl) Copy() vm.StateDB {
+	newCtx := s.ctx.WithMultiStore(s.ctx.MultiStore().CacheMultiStore())
+	return &DBImpl{
+		ctx:             newCtx,
+		snapshottedCtxs: append(s.snapshottedCtxs, s.ctx),
+		k:               s.k,
+		err:             s.err,
+	}
+}
+
+func (s *DBImpl) Finalise(bool) {
+	s.ctx.Logger().Info("Finalise should only be called during simulation and will no-op")
+}
+
+func (s *DBImpl) Commit(uint64, bool) (common.Hash, error) {
+	panic("Commit is not implemented and called unexpectedly")
+}
+
+func (s *DBImpl) SetTxContext(common.Hash, int) {
+	//noop
+}
+
+func (s *DBImpl) IntermediateRoot(bool) common.Hash {
+	panic("IntermediateRoot is not implemented and called unexpectedly")
+}
+
+func (s *DBImpl) TxIndex() int {
+	return s.ctx.TxIndex()
+}
+
+func (s *DBImpl) Preimages() map[common.Hash][]byte {
+	return map[common.Hash][]byte{}
 }
 
 // ** TEST ONLY FUNCTIONS **//
