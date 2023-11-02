6ebad4c5947a85211f98a0c14cafa3ab226b8b04
Jeremy Wei <jeremy.t.wei@gmail.com>
2023-11-02 14:38:43 -0400

    addressed comments

--- scripts/authz_proposal_for_validator_ops.sh
+++ scripts/authz_proposal_for_validator_ops.sh
@@ -5,11 +5,13 @@ set -e
 #  - to run: ./authz_proposal_for_validator_ops.sh [grant/revoke]
 #  - put grantee addresses in grantee.txt
 
-if [[ -z "$1" ]]; then
-    operation="grant"
+if [[ "$1" == "grant" || "$1" == "revoke" ]]; then
+   operation=$1
 else
-    operation=$1 # should be "revoke"
+   echo "Invalid operation. Please pass either 'grant' or 'revoke'."
+   exit 1
 fi
+
 
 rm /tmp/grant-*.json || true
 rm /tmp/revoke-*.json || true
@@ -44,7 +46,7 @@ for GRANTEE in $(cat grantee.txt); do
     echo " - $GRANTEE"
 done
 
-if [[ $operation == "enable" ]]
+if [[ $operation == "grant" ]]
 then
     for GRANTEE in $(cat grantee.txt); do
         echo
@@ -57,6 +59,11 @@ then
         echo "generating feegrant for grantee: '$GRANTEE'"
         seid tx feegrant grant $GRANTER $GRANTEE --allowed-messages "/cosmos.authz.v1beta1.MsgExec" --spend-limit 10sei -b block -y --fees 20000usei --from $GRANTER --generate-only | jq > /tmp/grant-feegrant-$GRANTEE.json
     done
+
+    echo
+    echo "aggregating messages for grants"
+    echo
+    jq -s 'reduce .[] as $item ({"body": {"messages": []}, "auth_info": .[0].auth_info, "signatures": .[0].signatures}; .body.messages += $item.body.messages)' /tmp/grant-*.json > /tmp/combined_messages.json
 else
     for GRANTEE in $(cat grantee.txt); do
         echo
@@ -69,12 +76,12 @@ else
         echo "generating feegrant for grantee: '$GRANTEE'"
         seid tx feegrant revoke $GRANTER $GRANTEE --fees 20000usei --from $GRANTER -b block -y --generate-only | jq > /tmp/revoke-feegrant-$GRANTEE.json
     done
-fi
 
-echo
-echo "aggregating messages for grants"
-echo
-jq -s 'reduce .[] as $item ({"body": {"messages": []}, "auth_info": .[0].auth_info, "signatures": .[0].signatures}; .body.messages += $item.body.messages)' /tmp/grant-*.json > /tmp/combined_messages.json
+    echo
+    echo "aggregating messages for revokes"
+    echo
+    jq -s 'reduce .[] as $item ({"body": {"messages": []}, "auth_info": .[0].auth_info, "signatures": .[0].signatures}; .body.messages += $item.body.messages)' /tmp/revoke-*.json > /tmp/combined_messages.json
+fi
 
 echo "combined messages have been stored in /tmp/combined_messages.json, please sign and broadcast them manually"
 echo 
