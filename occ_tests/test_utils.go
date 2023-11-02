6d41dd9516e281ea9500bdd45d3f8794b12510a2
Steven Landers <steven.landers@gmail.com>
2023-11-02 14:00:41 -0400

    move const

--- occ_tests/test_utils.go
+++ occ_tests/test_utils.go
@@ -33,29 +33,6 @@ import (
 	minttypes "github.com/sei-protocol/sei-chain/x/mint/types"
 )
 
-const INSTANTIATE = `{"whitelist": ["sei1h9yjz89tl0dl6zu65dpxcqnxfhq60wxx8s5kag"],
-    "use_whitelist":false,"admin":"sei1h9yjz89tl0dl6zu65dpxcqnxfhq60wxx8s5kag",
-	"limit_order_fee":{"decimal":"0.0001","negative":false},
-	"market_order_fee":{"decimal":"0.0001","negative":false},
-	"liquidation_order_fee":{"decimal":"0.0001","negative":false},
-	"margin_ratio":{"decimal":"0.0625","negative":false},
-	"max_leverage":{"decimal":"4","negative":false},
-	"default_base":"USDC",
-	"native_token":"USDC","denoms": ["SEI","ATOM","USDC","SOL","ETH","OSMO","AVAX","BTC"],
-	"full_denom_mapping": [["usei","SEI","0.000001"],["uatom","ATOM","0.000001"],["uusdc","USDC","0.000001"]],
-	"funding_payment_lookback":3600,"spot_market_contract":"sei1h9yjz89tl0dl6zu65dpxcqnxfhq60wxx8s5kag",
-	"supported_collateral_denoms": ["USDC"],
-	"supported_multicollateral_denoms": ["ATOM"],
-	"oracle_denom_mapping": [["usei","SEI","1"],["uatom","ATOM","1"],["uusdc","USDC","1"],["ueth","ETH","1"]],
-	"multicollateral_whitelist": ["sei1h9yjz89tl0dl6zu65dpxcqnxfhq60wxx8s5kag"],
-	"multicollateral_whitelist_enable": true,
-	"funding_payment_pairs": [["USDC","ETH"]],
-	"default_margin_ratios":{
-		"initial":"0.3",
-		"partial":"0.25",
-		"maintenance":"0.06"
-	}}`
-
 func toTxBytes(testCtx *TestContext, msgs []sdk.Msg) [][]byte {
 	var txs [][]byte
 	tc := app.MakeEncodingConfig().TxConfig
