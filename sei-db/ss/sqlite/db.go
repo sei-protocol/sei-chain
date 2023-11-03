2fad52448a4561b8258333b2fa160990d9b235f6
Kartik Bhat <kartikbhat@Kartiks-MacBook-Pro-2.local>
2023-11-03 15:03:00 -0400

    Remove mmap

--- sei-db/ss/sqlite/db.go
+++ sei-db/ss/sqlite/db.go
@@ -65,7 +65,6 @@ func New(dataDir string) (*Database, error) {
 		`PRAGMA cache_size=-32000;`,
 		`PRAGMA auto_vacuum=FULL;`,
 		`PRAGMA temp_store=MEMORY;`,
-		`PRAGMA mmap_size=30000000000;`,
 	}
 
 	if err := execPragmas(db, pragmas); err != nil {
