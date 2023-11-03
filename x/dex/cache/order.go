0f9a03a0cc8828012941cb080b348665258ac804
Steven Landers <steven.landers@gmail.com>
2023-11-03 15:31:57 -0400

    fix unrelated lint errors

--- x/dex/cache/order.go
+++ x/dex/cache/order.go
@@ -20,11 +20,11 @@ func NewOrders(orderStore prefix.Store) *BlockOrders {
 func (o *BlockOrders) Add(newItem *types.Order) {
 	keybz := make([]byte, 8)
 	binary.BigEndian.PutUint64(keybz, newItem.Id)
-	if valbz, err := newItem.Marshal(); err != nil {
+	valbz, err := newItem.Marshal()
+	if err != nil {
 		panic(err)
-	} else {
-		o.orderStore.Set(keybz, valbz)
 	}
+	o.orderStore.Set(keybz, valbz)
 }
 
 func (o *BlockOrders) GetByID(id uint64) *types.Order {
