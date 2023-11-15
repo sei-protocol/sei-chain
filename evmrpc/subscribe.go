9ab65cabd85b56d0a629450e431195261588afab
Jeremy Wei <jeremy.t.wei@gmail.com>
2023-11-15 14:56:26 -0500

    [EVM] subscription based eth endpoints (#1132)
    * added new block filter
    
    * wip
    
    * finished tests
    
    * minor fix
    
    * fix lint
    
    * minor fix
    
    * added basic endpoint and created sendWSRequestAndListen
    
    * no goroutine in test
    
    * removed params
    
    * able to call my subscribe endpoint
    
    * subscribe2 works but not subscribe
    
    * got back msg from ws but hard to decode
    
    * subscription api uses sub manager, manager thread safe
    
    * able to send many messages through ws conn
    
    * newHeads tests work
    
    * newHeads works
    
    * subscribe works need to cleanup
    
    * test with multiple addrs, remove prints
    
    * cleanup
    
    * cleanup
    
    * fix test
    
    * fix linter
    
    * fix linter 2
    
    * fix linter 3
    
    * address comments
    
    * add unsubscribe in mock client
    
    * fix linter 4
    
    * fix extra subscribe param
    
    * fix test

--- evmrpc/subscribe.go
+++ evmrpc/subscribe.go
@@ -2,140 +2,217 @@ package evmrpc
 
 import (
 	"context"
-	"errors"
 	"fmt"
-	"strings"
+	"math/big"
+	"sync"
 
-	"github.com/sei-protocol/sei-chain/utils"
-	"github.com/sei-protocol/sei-chain/x/evm/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	ethtypes "github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/eth/filters"
+	"github.com/ethereum/go-ethereum/rpc"
 	rpcclient "github.com/tendermint/tendermint/rpc/client"
 	"github.com/tendermint/tendermint/rpc/coretypes"
+	tmtypes "github.com/tendermint/tendermint/types"
 )
+
+type SubscriptionAPI struct {
+	tmClient            rpcclient.Client
+	ctxProvider         func(int64) sdk.Context
+	subscriptionManager *SubscriptionManager
+	subscriptonConfig   *SubscriptionConfig
+}
+
+type SubscriptionConfig struct {
+	subscriptionCapacity int
+}
+
+func NewSubscriptionAPI(tmClient rpcclient.Client, ctxProvider func(int64) sdk.Context, subscriptionConfig *SubscriptionConfig) *SubscriptionAPI {
+	return &SubscriptionAPI{
+		tmClient:            tmClient,
+		ctxProvider:         ctxProvider,
+		subscriptionManager: NewSubscriptionManager(tmClient),
+		subscriptonConfig:   subscriptionConfig,
+	}
+}
+
+func (a *SubscriptionAPI) NewHeads(ctx context.Context) (*rpc.Subscription, error) {
+	notifier, supported := rpc.NotifierFromContext(ctx)
+	if !supported {
+		return &rpc.Subscription{}, rpc.ErrNotificationsUnsupported
+	}
+
+	rpcSub := notifier.CreateSubscription()
+	subscriberID, subCh, err := a.subscriptionManager.Subscribe(context.Background(), NewHeadQueryBuilder(), a.subscriptonConfig.subscriptionCapacity)
+	if err != nil {
+		return nil, err
+	}
+
+	go func() {
+		defer func() {
+			_ = a.subscriptionManager.Unsubscribe(context.Background(), subscriberID)
+		}()
+		for {
+			select {
+			case res := <-subCh:
+				ethHeader, err := encodeTmHeader(res.Data.(tmtypes.EventDataNewBlockHeader))
+				if err != nil {
+					return
+				}
+				err = notifier.Notify(rpcSub.ID, ethHeader)
+				if err != nil {
+					return
+				}
+			case <-rpcSub.Err():
+				return
+			case <-notifier.Closed():
+				return
+			}
+		}
+	}()
+	return rpcSub, nil
+}
+
+func (a *SubscriptionAPI) Logs(ctx context.Context, filter *filters.FilterCriteria) (*rpc.Subscription, error) {
+	notifier, supported := rpc.NotifierFromContext(ctx)
+	if !supported {
+		return &rpc.Subscription{}, rpc.ErrNotificationsUnsupported
+	}
+
+	rpcSub := notifier.CreateSubscription()
+
+	resultEventAllAddrs := make(chan coretypes.ResultEvent, len(filter.Addresses)*a.subscriptonConfig.subscriptionCapacity)
+	for _, address := range filter.Addresses {
+		q := getBuiltQuery(filter.BlockHash, filter.FromBlock, filter.ToBlock, address, filter.Topics)
+		subscriberID, subCh, err := a.subscriptionManager.Subscribe(context.Background(), q, a.subscriptonConfig.subscriptionCapacity)
+		if err != nil {
+			return nil, err
+		}
+		go func() {
+			defer func() {
+				_ = a.subscriptionManager.Unsubscribe(context.Background(), subscriberID)
+			}()
+			for {
+				select {
+				case res := <-subCh:
+					resultEventAllAddrs <- res
+				case <-rpcSub.Err():
+					return
+				case <-notifier.Closed():
+					return
+				}
+			}
+		}()
+	}
+
+	go func() {
+		for {
+			res := <-resultEventAllAddrs
+			for _, abciEvent := range res.Events {
+				ethLog, err := encodeEventToLog(abciEvent)
+				if err != nil {
+					if err == ErrInvalidEventAttribute {
+						continue
+					}
+					err = notifier.Notify(rpcSub.ID, err)
+					if err != nil {
+						return
+					}
+				}
+				err = notifier.Notify(rpcSub.ID, ethLog)
+				if err != nil {
+					return
+				}
+			}
+		}
+	}()
+	return rpcSub, nil
+}
 
 const SubscriberPrefix = "evm.rpc."
 
 type SubscriberID uint64
 
-type SubscriptionManager struct {
-	NextID        SubscriberID
-	Subscriptions map[SubscriberID]<-chan coretypes.ResultEvent
+type SubInfo struct {
+	Query          string
+	SubscriptionCh <-chan coretypes.ResultEvent
+}
 
-	tmClient rpcclient.Client
+type SubscriptionManager struct {
+	subMu            sync.Mutex
+	NextID           SubscriberID
+	SubscriptionInfo map[SubscriberID]SubInfo
+	tmClient         rpcclient.Client
 }
 
 func NewSubscriptionManager(tmClient rpcclient.Client) *SubscriptionManager {
 	return &SubscriptionManager{
-		NextID:        1,
-		Subscriptions: map[SubscriberID]<-chan coretypes.ResultEvent{},
-		tmClient:      tmClient,
+		subMu:            sync.Mutex{},
+		NextID:           1,
+		SubscriptionInfo: make(map[SubscriberID]SubInfo),
+		tmClient:         tmClient,
 	}
 }
 
-func (s *SubscriptionManager) Subscribe(ctx context.Context, q *QueryBuilder, limit int) (SubscriberID, error) {
+func (s *SubscriptionManager) Subscribe(ctx context.Context, q *QueryBuilder, limit int) (SubscriberID, <-chan coretypes.ResultEvent, error) {
+	query := q.Build()
+	s.subMu.Lock()
+	defer s.subMu.Unlock()
 	id := s.NextID
 	// ignore deprecation here since the new endpoint does not support polling
 	//nolint:staticcheck
-	res, err := s.tmClient.Subscribe(ctx, fmt.Sprintf("%s%d", SubscriberPrefix, id), q.Build(), limit)
+	res, err := s.tmClient.Subscribe(ctx, fmt.Sprintf("%s%d", SubscriberPrefix, id), query, limit)
 	if err != nil {
-		return 0, err
+		return 0, nil, err
 	}
-	s.Subscriptions[id] = res
+	s.SubscriptionInfo[id] = SubInfo{Query: query, SubscriptionCh: res}
 	s.NextID++
-	return id, nil
+	return id, res, nil
 }
 
-type QueryBuilder struct {
-	conditions []string
-}
-
-func NewBlockQueryBuilder() *QueryBuilder {
-	return &QueryBuilder{
-		conditions: []string{
-			"tm.event = 'NewBlock'",
-		},
-	}
-}
-
-func NewTxQueryBuilder() *QueryBuilder {
-	return &QueryBuilder{
-		conditions: []string{
-			"tm.event = 'Tx'", // needed for all transaction-generated events
-		},
-	}
-}
-
-func (q *QueryBuilder) Build() string {
-	return strings.Join(q.conditions, " AND ")
-}
-
-func (q *QueryBuilder) FilterContractAddress(contractAddr string) *QueryBuilder {
-	q.conditions = append(q.conditions, fmt.Sprintf("%s.%s = '%s'", types.EventTypeEVMLog, types.AttributeTypeContractAddress, contractAddr))
-	return q
-}
-
-func (q *QueryBuilder) FilterTopic(topic string) *QueryBuilder {
-	q.conditions = append(q.conditions, fmt.Sprintf("%s.%s CONTAINS '%s'", types.EventTypeEVMLog, types.AttributeTypeTopics, topic))
-	return q
-}
-
-func (q *QueryBuilder) FilterTopics(topics [][]string) *QueryBuilder {
-	if len(topics) == 0 {
-		return q
-	}
-	pattern, err := GetTopicsRegex(topics)
+func (s *SubscriptionManager) Unsubscribe(ctx context.Context, id SubscriberID) error {
+	s.subMu.Lock()
+	defer s.subMu.Unlock()
+	// ignore deprecation here since the new endpoint does not support polling
+	//nolint:staticcheck
+	err := s.tmClient.Unsubscribe(ctx, SubscriberPrefix, s.SubscriptionInfo[id].Query)
 	if err != nil {
-		panic(err)
+		return err
 	}
-	q.conditions = append(q.conditions, fmt.Sprintf("%s.%s = MATCHES '%s'", types.EventTypeEVMLog, types.AttributeTypeTopics, pattern))
-	return q
+	delete(s.SubscriptionInfo, id)
+	return nil
 }
 
-func GetTopicsRegex(topics [][]string) (string, error) {
-	if len(topics) == 0 {
-		return "", errors.New("topics array must be at least length 1")
+func encodeTmHeader(
+	header tmtypes.EventDataNewBlockHeader,
+) (map[string]interface{}, error) {
+	number := big.NewInt(header.Header.Height)
+	miner := common.HexToAddress(string(header.Header.ProposerAddress))
+	gasLimit, gasWanted := int64(0), int64(0)
+	lastHash := common.HexToHash(string(header.Header.LastBlockID.Hash))
+	resultHash := common.HexToHash(string(header.Header.LastResultsHash))
+	appHash := common.HexToHash(string(header.Header.AppHash))
+	txHash := common.HexToHash(string(header.Header.DataHash))
+	for _, txRes := range header.ResultFinalizeBlock.TxResults {
+		gasLimit += txRes.GasWanted
+		gasWanted += txRes.GasUsed
 	}
-
-	topicRegex := func(topic []string) string {
-		if len(topic) == 0 {
-			return ""
-		}
-		return fmt.Sprintf("(%s)", strings.Join(topic, "|"))
+	result := map[string]interface{}{
+		"difficulty":       (*hexutil.Big)(big.NewInt(0)), // inapplicable to Sei
+		"extraData":        hexutil.Bytes{},               // inapplicable to Sei
+		"gasLimit":         hexutil.Uint64(gasLimit),
+		"gasUsed":          hexutil.Uint64(gasWanted),
+		"logsBloom":        ethtypes.Bloom{}, // inapplicable to Sei
+		"miner":            miner,
+		"nonce":            ethtypes.BlockNonce{}, // inapplicable to Sei
+		"number":           (*hexutil.Big)(number),
+		"parentHash":       lastHash,
+		"receiptsRoot":     resultHash,
+		"sha3Uncles":       common.Hash{}, // inapplicable to Sei
+		"stateRoot":        appHash,
+		"timestamp":        hexutil.Uint64(header.Header.Time.Unix()),
+		"transactionsRoot": txHash,
 	}
-
-	return fmt.Sprintf("\\[%s.*\\]", strings.Join(utils.Map(topics, topicRegex), "[^\\,]*,")), nil
-}
-
-func (q *QueryBuilder) FilterBlockNumber(blockNumber int64) *QueryBuilder {
-	q.conditions = append(q.conditions, fmt.Sprintf("%s.%s = '%d'", types.EventTypeEVMLog, types.AttributeTypeBlockNumber, blockNumber))
-	return q
-}
-
-func (q *QueryBuilder) FilterBlockNumberStart(blockNumber int64) *QueryBuilder {
-	q.conditions = append(q.conditions, fmt.Sprintf("%s.%s >= '%d'", types.EventTypeEVMLog, types.AttributeTypeBlockNumber, blockNumber))
-	return q
-}
-
-func (q *QueryBuilder) FilterBlockNumberEnd(blockNumber int64) *QueryBuilder {
-	q.conditions = append(q.conditions, fmt.Sprintf("%s.%s <= '%d'", types.EventTypeEVMLog, types.AttributeTypeBlockNumber, blockNumber))
-	return q
-}
-
-func (q *QueryBuilder) FilterTxIndex(txIndex int64) *QueryBuilder {
-	q.conditions = append(q.conditions, fmt.Sprintf("%s.%s = '%d'", types.EventTypeEVMLog, types.AttributeTypeTxIndex, txIndex))
-	return q
-}
-
-func (q *QueryBuilder) FilterIndex(index int64) *QueryBuilder {
-	q.conditions = append(q.conditions, fmt.Sprintf("%s.%s = '%d'", types.EventTypeEVMLog, types.AttributeTypeIndex, index))
-	return q
-}
-
-func (q *QueryBuilder) FilterBlockHash(blockHash string) *QueryBuilder {
-	q.conditions = append(q.conditions, fmt.Sprintf("%s.%s = '%s'", types.EventTypeEVMLog, types.AttributeTypeBlockHash, blockHash))
-	return q
-}
-
-func (q *QueryBuilder) FilterTxHash(txHash string) *QueryBuilder {
-	q.conditions = append(q.conditions, fmt.Sprintf("%s.%s = '%s'", types.EventTypeEVMLog, types.AttributeTypeTxHash, txHash))
-	return q
+	return result, nil
 }
