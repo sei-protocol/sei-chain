f5903df25b0367ab80893cfcedc6f9c65fc31904
Kartik Bhat <kartikbhat@Kartiks-MacBook-Pro-2.local>
2023-11-15 16:47:04 -0800

    Add Prune tool

--- sei-db/tools/cmd/seidb/main.go
+++ sei-db/tools/cmd/seidb/main.go
@@ -25,7 +25,7 @@ func main() {
 		Short: "A tool to generate raw key value data from a node as well as benchmark different backends",
 	}
 
-	rootCmd.AddCommand(GenerateCmd(), BenchmarkWriteCmd(), BenchmarkReadCmd(), BenchmarkDBIterationCmd(), BenchmarkDBReverseIterationCmd(), DumpDbCmd())
+	rootCmd.AddCommand(GenerateCmd(), BenchmarkWriteCmd(), BenchmarkReadCmd(), BenchmarkDBIterationCmd(), BenchmarkDBReverseIterationCmd(), DumpDbCmd(), PruneCmd())
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
