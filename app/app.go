6de0d2713e6bcd0f42a53befa2c843b06eafa43d
yzang2019 <zymfrank@gmail.com>
2023-11-15 21:15:17 -0800

    Optimize and parallelize OCC estimation

--- app/app.go
+++ app/app.go
@@ -1313,17 +1313,28 @@ func (app *App) ExecuteTxsConcurrently(ctx sdk.Context, txs [][]byte) ([]*abci.E
 
 // ProcessTXsWithOCC runs the transactions concurrently via OCC
 func (app *App) ProcessTXsWithOCC(ctx sdk.Context, txs [][]byte) ([]*abci.ExecTxResult, sdk.Context) {
+	startTime := time.Now()
 	entries := make([]*sdk.DeliverTxEntry, 0, len(txs))
+	wg := sync.WaitGroup{}
+	mutex := sync.Mutex{}
 	for txIndex, tx := range txs {
-		deliverTxEntry := &sdk.DeliverTxEntry{Request: abci.RequestDeliverTx{Tx: tx}}
-		// get prefill estimate
-		estimatedWritesets, err := app.AccessControlKeeper.GenerateEstimatedWritesets(ctx, app.txDecoder, app.GetAnteDepGenerator(), txIndex, tx)
-		// if no error, then we assign the mapped writesets for prefill estimate
-		if err == nil {
-			deliverTxEntry.EstimatedWritesets = estimatedWritesets
-		}
-		entries = append(entries, deliverTxEntry)
+		wg.Add(1)
+		go func(index int, tx []byte) {
+			defer wg.Done()
+			deliverTxEntry := &sdk.DeliverTxEntry{Request: abci.RequestDeliverTx{Tx: tx}}
+			estimatedWritesets, err := app.AccessControlKeeper.GenerateEstimatedWritesets(ctx, app.txDecoder, app.GetAnteDepGenerator(), txIndex, tx)
+			// get prefill estimate
+			// if no error, then we assign the mapped writesets for prefill estimate
+			if err == nil {
+				deliverTxEntry.EstimatedWritesets = estimatedWritesets
+			}
+			mutex.Lock()
+			entries[index] = deliverTxEntry
+			mutex.Unlock()
+		}(txIndex, tx)
 	}
+	wg.Wait()
+	fmt.Printf("[OCC-Debug] build estimate writeset took %s\n", time.Since(startTime))
 
 	batchResult := app.DeliverTxBatch(ctx, sdk.DeliverTxBatchRequest{TxEntries: entries})
 
