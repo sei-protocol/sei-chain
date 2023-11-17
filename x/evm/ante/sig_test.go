98047a99b1580676249d2854d41e4b2658220b7c
codchen <codchen03@gmail.com>
2023-11-17 11:59:04 +0800

    [EVM] handle gapped nonce in CheckTx (#1142)
    * [EVM] handle nonce gap in CheckTx
    
    * disable compatibility check

--- x/evm/ante/sig_test.go
+++ x/evm/ante/sig_test.go
@@ -3,7 +3,9 @@ package ante_test
 import (
 	"encoding/hex"
 	"math/big"
+	"sync"
 	"testing"
+	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/ethereum/go-ethereum/common"
@@ -18,7 +20,7 @@ import (
 
 func TestEVMSigVerifyDecorator(t *testing.T) {
 	k, ctx := testkeeper.MockEVMKeeper()
-	handler := ante.NewEVMSigVerifyDecorator(k)
+	handler := ante.NewEVMSigVerifyDecorator(k, 5*time.Second)
 	privKey := testkeeper.MockPrivateKey()
 	testPrivHex := hex.EncodeToString(privKey.Bytes())
 	key, _ := crypto.HexToECDSA(testPrivHex)
@@ -89,4 +91,93 @@ func TestEVMSigVerifyDecorator(t *testing.T) {
 		return ctx, nil
 	})
 	require.Nil(t, err)
+}
+
+func TestSigVerifyCheckTxNonceGap(t *testing.T) {
+	_, addr := testkeeper.MockAddressPair()
+	k, ctx := testkeeper.MockEVMKeeper()
+	ctx = ctx.WithIsCheckTx(true)
+	ctx = types.SetContextEVMAddress(ctx, addr)
+	k.SetNonce(ctx, addr, 1)
+
+	var leadingPriority int64 = 1000
+	for _, test := range []struct {
+		name               string
+		nonces             []uint64
+		shouldSuccess      []bool
+		expectedPriorities []int64
+		delay              []time.Duration
+	}{{
+		"single tx with correct nonce",
+		[]uint64{1},
+		[]bool{true},
+		[]int64{1000},
+		[]time.Duration{0 * time.Second},
+	}, {
+		"single tx with incorrect nonce",
+		[]uint64{2},
+		[]bool{false},
+		[]int64{},
+		[]time.Duration{0 * time.Second},
+	}, {
+		"multiple txs with duplicated nonce",
+		[]uint64{1, 1},
+		[]bool{true, false},
+		[]int64{1000},
+		[]time.Duration{0 * time.Second, 2 * time.Second},
+	}, {
+		"multiple txs with incrementing nonce",
+		[]uint64{3, 1, 2},
+		[]bool{true, true, true},
+		[]int64{998, 1000, 999},
+		[]time.Duration{0 * time.Second, 0 * time.Second, 0 * time.Second},
+	}, {
+		"multiple txs with gapped nonce",
+		[]uint64{3, 1},
+		[]bool{false, true},
+		[]int64{0, 1000},
+		[]time.Duration{0 * time.Second, 0 * time.Second},
+	}} {
+		var wg sync.WaitGroup
+		handler := ante.NewEVMSigVerifyDecorator(k, 5*time.Second)
+		for i, nonce := range test.nonces {
+			wg.Add(1)
+			i, nonce := i, nonce
+			go func() {
+				defer wg.Done()
+				runCtx := types.SetContextEthTx(ctx, ethtypes.NewTx(&ethtypes.DynamicFeeTx{Nonce: nonce})).WithPriority(leadingPriority)
+				if test.delay[i] > 0 {
+					time.Sleep(test.delay[i])
+				}
+				resCtx, err := handler.AnteHandle(runCtx, nil, false, noop)
+				if test.shouldSuccess[i] {
+					require.Nil(t, err, test.name)
+					require.Equal(t, test.expectedPriorities[i], resCtx.Priority(), test.name)
+				} else {
+					require.NotNil(t, err, test.name)
+				}
+			}()
+		}
+		wg.Wait()
+	}
+}
+
+func TestSigVerifyCheckTxCleanup(t *testing.T) {
+	_, addr := testkeeper.MockAddressPair()
+	k, ctx := testkeeper.MockEVMKeeper()
+	ctx = ctx.WithIsCheckTx(true)
+	ctx = types.SetContextEVMAddress(ctx, addr)
+	k.SetNonce(ctx, addr, 1)
+	handler := ante.NewEVMSigVerifyDecorator(k, 5*time.Second)
+	ctx = types.SetContextEthTx(ctx, ethtypes.NewTx(&ethtypes.DynamicFeeTx{Nonce: 1}))
+	handler.AnteHandle(ctx, nil, false, noop) // checktx at height 8
+	ctx = ctx.WithBlockHeight(ctx.BlockHeight() + 1)
+	handler.AnteHandle(ctx, nil, false, noop)        // checktx at height 9, which should clean height 8's state
+	ctx = ctx.WithBlockHeight(ctx.BlockHeight() - 1) // revert back to height 8. Note that this would not happen in practice. We are doing it here merely to verify cleanup's effect.
+	_, err := handler.AnteHandle(ctx, nil, false, noop)
+	require.Nil(t, err) // there should be no error since the dedupe map should've been cleared already when the height 9 tx was checked.
+}
+
+func noop(ctx sdk.Context, _ sdk.Tx, _ bool) (sdk.Context, error) {
+	return ctx, nil
 }
