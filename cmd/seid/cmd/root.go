a8d6a792d20bae76718a9de90196a180d60de2af
codchen <codchen03@gmail.com>
2023-11-17 11:59:04 +0800

    [EVM] handle gapped nonce in CheckTx (#1142)
    * [EVM] handle nonce gap in CheckTx
    
    * disable compatibility check

--- cmd/seid/cmd/root.go
+++ cmd/seid/cmd/root.go
@@ -460,6 +460,9 @@ idle_timeout = "{{ .EVM.IdleTimeout }}"
 # Maximum gas limit for simulation
 simulation_gas_limit = {{ .EVM.SimulationGasLimit }}
 
+# Timeout for EVM call in simulation
+simulation_evm_timeout = "{{ .EVM.SimulationEVMTimeout }}"
+
 # list of CORS allowed origins, separated by comma
 cors_origins = "{{ .EVM.CORSOrigins }}"
 
@@ -468,6 +471,9 @@ ws_origins = "{{ .EVM.WSOrigins }}"
 
 # timeout for filters
 filter_timeout = "{{ .EVM.FilterTimeout }}"
+
+# checkTx timeout for sig verify
+checktx_timeout = "{{ .EVM.CheckTxTimeout }}"
 `
 
 	return customAppTemplate, customAppConfig
