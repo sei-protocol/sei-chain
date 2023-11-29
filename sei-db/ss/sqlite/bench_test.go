c65071747fd397ea88c212e184e42f0e0fdc3845
Kartik Bhat <kartikbhat@kartiks-mbp-2.mynetworksettings.com>
2023-11-28 19:44:23 -0500

    Refactor benchmarks

---
+++ sei-db/ss/sqlite/bench_test.go
@@ -0,0 +1,21 @@
+package sqlite
+
+import (
+	"testing"
+
+	sstest "github.com/sei-protocol/sei-db/ss/test"
+	"github.com/sei-protocol/sei-db/ss/types"
+)
+
+func BenchmarkDBBackend(b *testing.B) {
+	s := &sstest.StorageBenchSuite{
+		NewDB: func(dir string) (types.StateStore, error) {
+			return New(dir)
+		},
+		BenchBackendName: "Sqlite",
+	}
+
+	s.BenchmarkGet(b)
+	s.BenchmarkApplyChangeset(b)
+	s.BenchmarkIterate(b)
+}
