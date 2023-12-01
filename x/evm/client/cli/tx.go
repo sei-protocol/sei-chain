bd811c9065b339588297062eaf1267b4ce8d820c
Jeremy Wei <jeremy.t.wei@gmail.com>
2023-12-01 09:19:20 -0500

    [EVM] seid command to deploy erc20 contract for a native sei token (#1159)
    * able to txn deployment to go through
    
    * cleanup
    
    * remove prints
    
    * remove prints
    
    * fix linter
    
    * minor fix

--- x/evm/client/cli/tx.go
+++ x/evm/client/cli/tx.go
@@ -1,7 +1,9 @@
 package cli
 
 import (
+	"embed"
 	"encoding/hex"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
@@ -10,8 +12,10 @@ import (
 	"strconv"
 	"strings"
 
+	ethabi "github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/rlp"
 	"github.com/spf13/cobra"
 
 	"github.com/cosmos/cosmos-sdk/client"
@@ -19,6 +23,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/client/tx"
 	"github.com/cosmos/cosmos-sdk/codec/legacy"
 	"github.com/cosmos/cosmos-sdk/crypto/keyring"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	ethtypes "github.com/ethereum/go-ethereum/core/types"
 	"github.com/sei-protocol/sei-chain/evmrpc"
 	"github.com/sei-protocol/sei-chain/x/evm/state"
@@ -33,6 +38,12 @@ const (
 	FlagRPC        = "evm-rpc"
 )
 
+// Embed abi json file to the executable binary. Needed when importing as dependency.
+//
+//go:embed contract-artifacts/NativeSeiTokensERC20.bin
+//go:embed contract-artifacts/NativeSeiTokensERC20.abi
+var f embed.FS
+
 // GetTxCmd returns the transaction commands for this module
 func GetTxCmd() *cobra.Command {
 	cmd := &cobra.Command{
@@ -45,6 +56,7 @@ func GetTxCmd() *cobra.Command {
 
 	cmd.AddCommand(CmdAssociateAddress())
 	cmd.AddCommand(CmdSend())
+	cmd.AddCommand(CmdDeployErc20())
 
 	return cmd
 }
@@ -125,7 +137,7 @@ func CmdAssociateAddress() *cobra.Command {
 
 func CmdSend() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "send [to EVM address] [amount in usei] [nonce] --from=<sender> --gas-fee-cap=<cap> --gas-limt=<limit> --evm-chain-id=<chain-id> --rpc=<url>",
+		Use:   "send [to EVM address] [amount in usei] [nonce] --from=<sender> --gas-fee-cap=<cap> --gas-limit=<limit> --evm-chain-id=<chain-id> --evm-rpc=<url>",
 		Short: "send usei to EVM address",
 		Long:  "",
 		Args:  cobra.ExactArgs(3),
@@ -215,6 +227,166 @@ func CmdSend() *cobra.Command {
 			}
 			fmt.Printf("Response: %s\n", string(resBody))
 
+			return nil
+		},
+	}
+
+	cmd.Flags().Uint64(FlagGasFeeCap, 1000000000000, "Gas fee cap for the transaction")
+	cmd.Flags().Uint64(FlagGas, 21000, "Gas limit for the transaction")
+	cmd.Flags().Uint64(FlagEVMChainID, 713715, "EVM chain ID")
+	cmd.Flags().String(FlagRPC, fmt.Sprintf("http://%s:8545", evmrpc.LocalAddress), "RPC endpoint to send request to")
+	flags.AddTxFlagsToCmd(cmd)
+
+	return cmd
+}
+
+type Response struct {
+	Jsonrpc string `json:"jsonrpc"`
+	ID      string `json:"id"`
+	Result  string `json:"result"`
+}
+
+func CmdDeployErc20() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "deploy-erc20 [denom] [nonce] --from=<sender> --gas-fee-cap=<cap> --gas-limt=<limit> --evm-chain-id=<chain-id> --evm-rpc=<url>",
+		Short: "Deploy ERC20 contract for a native Sei token",
+		Long:  "",
+		Args:  cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			err = sdk.ValidateDenom(args[0])
+			if err != nil {
+				return err
+			}
+			denom := args[0]
+
+			nonce, err := strconv.ParseUint(args[1], 10, 64)
+			if err != nil {
+				return err
+			}
+			gasFeeCap, err := cmd.Flags().GetUint64(FlagGasFeeCap)
+			if err != nil {
+				return err
+			}
+			gasLimit, err := cmd.Flags().GetUint64(FlagGas)
+			if err != nil {
+				return err
+			}
+			chainID, err := cmd.Flags().GetUint64(FlagEVMChainID)
+			if err != nil {
+				return err
+			}
+
+			bytecodeString, err := f.ReadFile("contract-artifacts/NativeSeiTokensERC20.bin")
+			if err != nil {
+				fmt.Println("failed at reading bytecode")
+				return err
+			}
+
+			bytecode, err := hex.DecodeString(string(bytecodeString))
+			if err != nil {
+				return err
+			}
+
+			abi, err := f.ReadFile("contract-artifacts/NativeSeiTokensERC20.abi")
+			if err != nil {
+				fmt.Println("failed at reading abi")
+				return err
+			}
+			parsedABI, err := ethabi.JSON(strings.NewReader(string(abi)))
+			if err != nil {
+				fmt.Println("failed at parsing abi")
+				return err
+			}
+			constructorArguments := []interface{}{
+				denom,
+			}
+
+			packedArgs, err := parsedABI.Pack("", constructorArguments...)
+			if err != nil {
+				return err
+			}
+			contractData := append(bytecode, packedArgs...)
+
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+			txf := tx.NewFactoryCLI(clientCtx, cmd.Flags())
+			kb := txf.Keybase()
+			info, err := kb.Key(clientCtx.GetFromName())
+			if err != nil {
+				return err
+			}
+			localInfo, ok := info.(keyring.LocalInfo)
+			if !ok {
+				return errors.New("can only associate address for local keys")
+			}
+			priv, err := legacy.PrivKeyFromBytes([]byte(localInfo.PrivKeyArmor))
+			if err != nil {
+				return err
+			}
+			privHex := hex.EncodeToString(priv.Bytes())
+			key, _ := crypto.HexToECDSA(privHex)
+
+			txData := ethtypes.DynamicFeeTx{
+				Nonce:     nonce,
+				GasFeeCap: new(big.Int).SetUint64(gasFeeCap),
+				Gas:       gasLimit,
+				Value:     big.NewInt(0),
+				Data:      contractData,
+				ChainID:   new(big.Int).SetUint64(chainID),
+			}
+			ethCfg := types.DefaultChainConfig().EthereumConfig(txData.ChainID)
+			signer := ethtypes.MakeSigner(ethCfg, big.NewInt(1), 1)
+			tx := ethtypes.NewTx(&txData)
+			tx, err = ethtypes.SignTx(tx, signer, key)
+			if err != nil {
+				return err
+			}
+			bz, err := tx.MarshalBinary()
+			if err != nil {
+				return err
+			}
+			payload := "0x" + hex.EncodeToString(bz)
+
+			body := fmt.Sprintf("{\"jsonrpc\": \"2.0\",\"method\": \"eth_sendRawTransaction\",\"params\":[\"%s\"],\"id\":\"deploy-erc20\"}", payload)
+			rpc, err := cmd.Flags().GetString(FlagRPC)
+			if err != nil {
+				return err
+			}
+			req, err := http.NewRequest(http.MethodGet, rpc, strings.NewReader(body))
+			if err != nil {
+				return err
+			}
+			req.Header.Set("Content-Type", "application/json")
+			res, err := http.DefaultClient.Do(req)
+			if err != nil {
+				return err
+			}
+			defer res.Body.Close()
+
+			resBody, err := io.ReadAll(res.Body)
+			if err != nil {
+				return err
+			}
+			var resp Response
+			err = json.Unmarshal(resBody, &resp)
+			if err != nil {
+				return err
+			}
+
+			senderAddr := crypto.PubkeyToAddress(key.PublicKey)
+			data, err := rlp.EncodeToBytes([]interface{}{senderAddr, nonce})
+			if err != nil {
+				return err
+			}
+			hash := crypto.Keccak256Hash(data)
+			contractAddress := hash.Bytes()[12:]
+			contractAddressHex := hex.EncodeToString(contractAddress)
+
+			fmt.Println("Deployer:", senderAddr)
+			fmt.Println("Deployed to:", fmt.Sprintf("0x%s", contractAddressHex))
+			fmt.Println("Transaction hash:", resp.Result)
 			return nil
 		},
 	}
