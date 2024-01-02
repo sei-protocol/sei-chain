13c40a6e8695107047ab2b2ca3daccca2492c5c8
codchen <codchen03@gmail.com>
2024-01-02 19:48:56 +0800

    Fix ERC20 wrapper code hash not whitelisted (#1203)

--- x/evm/artifacts/artifacts.go
+++ x/evm/artifacts/artifacts.go
@@ -1,13 +1,17 @@
 package artifacts
 
 import (
+	"bytes"
 	"embed"
 	"encoding/hex"
+	"fmt"
 )
 
 //go:embed NativeSeiTokensERC20.abi
 //go:embed NativeSeiTokensERC20.bin
 var f embed.FS
+
+var cachedSeiTokensERC20Bin []byte
 
 func GetNativeSeiTokensERC20ABI() []byte {
 	bz, err := f.ReadFile("NativeSeiTokensERC20.abi")
@@ -18,6 +22,9 @@ func GetNativeSeiTokensERC20ABI() []byte {
 }
 
 func GetNativeSeiTokensERC20Bin() []byte {
+	if cachedSeiTokensERC20Bin != nil {
+		return cachedSeiTokensERC20Bin
+	}
 	code, err := f.ReadFile("NativeSeiTokensERC20.bin")
 	if err != nil {
 		panic("failed to read native ERC20 contract binary")
@@ -26,5 +33,27 @@ func GetNativeSeiTokensERC20Bin() []byte {
 	if err != nil {
 		panic("failed to decode native ERC20 contract binary")
 	}
+	cachedSeiTokensERC20Bin = bz
 	return bz
+}
+
+func IsCodeNativeSeiTokensERC20Wrapper(code []byte) bool {
+	binLen := len(GetNativeSeiTokensERC20Bin())
+	if len(code) < binLen {
+		return false
+	}
+	if !bytes.Equal(code[:binLen], GetNativeSeiTokensERC20Bin()) {
+		return false
+	}
+	abi, err := ArtifactsMetaData.GetAbi()
+	if err != nil {
+		fmt.Printf("error getting ERC20 wrapper metadata ABI: %s\n", err)
+		return false
+	}
+	args, err := abi.Constructor.Inputs.Unpack(code[binLen:])
+	if err != nil || len(args) != 1 {
+		return false
+	}
+	_, isString := args[0].(string)
+	return isString
 }
