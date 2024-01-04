f2f7d556b15eee289f5d6d998eec8741978d4d57
Steven Landers <steven@seinetwork.io>
2024-01-04 12:55:50 -0500

    [EVM] Fix pending nonce race issues (#1194)
    * add keeper methods for nonces
    
    * calculate nonce from list of nonces
    
    * avoid pending tx check (nonce)
    
    * fix test
    
    * hack test to make it pass
    
    * fix test
    
    * fix noisy lint error
    
    * save off working version
    
    * fix goimports for app.go
    
    * fix imports for keeper.go
    
    * use lock references
    
    * more lint issues
    
    * fix expiry of pending nonces
    
    * fix lint
    
    * goimports
    
    * fix test
    
    * cleanup imports for test
    
    * cleanup
    
    * update go.mod

--- x/evm/keeper/keeper_test.go
+++ x/evm/keeper/keeper_test.go
@@ -2,13 +2,18 @@ package keeper_test
 
 import (
 	"math"
+	"sync"
 	"testing"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/sei-protocol/sei-chain/testutil/keeper"
+	evmkeeper "github.com/sei-protocol/sei-chain/x/evm/keeper"
 	"github.com/sei-protocol/sei-chain/x/evm/types"
 	"github.com/stretchr/testify/require"
+	"github.com/tendermint/tendermint/libs/rand"
+	tmtypes "github.com/tendermint/tendermint/types"
 )
 
 func TestGetChainID(t *testing.T) {
@@ -34,20 +39,136 @@ func TestGetHashFn(t *testing.T) {
 	require.Equal(t, common.Hash{}, f(uint64(ctx.BlockHeight())-1))
 }
 
-func TestPendingTxCount(t *testing.T) {
-	k, _ := keeper.MockEVMKeeper()
-	_, evmAddr := keeper.MockAddressPair()
-	require.Equal(t, uint64(0), k.GetPendingTxCount(evmAddr))
-	k.IncrementPendingTxCount(evmAddr)
-	require.Equal(t, uint64(1), k.GetPendingTxCount(evmAddr))
-	k.IncrementPendingTxCount(evmAddr)
-	require.Equal(t, uint64(2), k.GetPendingTxCount(evmAddr))
-	k.DecrementPendingTxCount(evmAddr)
-	require.Equal(t, uint64(1), k.GetPendingTxCount(evmAddr))
-	k.DecrementPendingTxCount(evmAddr)
-	require.Equal(t, uint64(0), k.GetPendingTxCount(evmAddr))
-	k.DecrementPendingTxCount(evmAddr)
-	require.Equal(t, uint64(0), k.GetPendingTxCount(evmAddr))
-	k.IncrementPendingTxCount(evmAddr)
-	require.Equal(t, uint64(1), k.GetPendingTxCount(evmAddr))
+func TestKeeper_CalculateNextNonce(t *testing.T) {
+	address1 := common.BytesToAddress([]byte("addr1"))
+	key1 := tmtypes.TxKey(rand.NewRand().Bytes(32))
+	key2 := tmtypes.TxKey(rand.NewRand().Bytes(32))
+	tests := []struct {
+		name          string
+		address       common.Address
+		pending       bool
+		setup         func(ctx sdk.Context, k *evmkeeper.Keeper)
+		expectedNonce uint64
+	}{
+		{
+			name:          "latest block, no latest stored",
+			address:       address1,
+			pending:       false,
+			expectedNonce: 0,
+		},
+		{
+			name:    "latest block, latest stored",
+			address: address1,
+			pending: false,
+			setup: func(ctx sdk.Context, k *evmkeeper.Keeper) {
+				k.SetNonce(ctx, address1, 50)
+			},
+			expectedNonce: 50,
+		},
+		{
+			name:    "latest block, latest stored with pending nonces",
+			address: address1,
+			pending: false,
+			setup: func(ctx sdk.Context, k *evmkeeper.Keeper) {
+				k.SetNonce(ctx, address1, 50)
+				// because pending:false, these won't matter
+				k.AddPendingNonce(key1, address1, 50)
+				k.AddPendingNonce(key2, address1, 51)
+			},
+			expectedNonce: 50,
+		},
+		{
+			name:    "pending block, nonce should follow the last pending",
+			address: address1,
+			pending: true,
+			setup: func(ctx sdk.Context, k *evmkeeper.Keeper) {
+				k.SetNonce(ctx, address1, 50)
+				k.AddPendingNonce(key1, address1, 50)
+				k.AddPendingNonce(key2, address1, 51)
+			},
+			expectedNonce: 52,
+		},
+		{
+			name:    "pending block, nonce should be the value of hole",
+			address: address1,
+			pending: true,
+			setup: func(ctx sdk.Context, k *evmkeeper.Keeper) {
+				k.SetNonce(ctx, address1, 50)
+				k.AddPendingNonce(key1, address1, 50)
+				// missing 51, so nonce = 51
+				k.AddPendingNonce(key2, address1, 52)
+			},
+			expectedNonce: 51,
+		},
+		{
+			name:    "pending block, completed nonces should also be skipped",
+			address: address1,
+			pending: true,
+			setup: func(ctx sdk.Context, k *evmkeeper.Keeper) {
+				k.SetNonce(ctx, address1, 50)
+				k.AddPendingNonce(key1, address1, 50)
+				k.AddPendingNonce(key2, address1, 51)
+				k.CompletePendingNonce(key1)
+				k.CompletePendingNonce(key2)
+			},
+			expectedNonce: 52,
+		},
+		{
+			name:    "pending block, hole created by expiration",
+			address: address1,
+			pending: true,
+			setup: func(ctx sdk.Context, k *evmkeeper.Keeper) {
+				k.SetNonce(ctx, address1, 50)
+				k.AddPendingNonce(key1, address1, 50)
+				k.AddPendingNonce(key2, address1, 51)
+				k.ExpirePendingNonce(key1)
+			},
+			expectedNonce: 50,
+		},
+		{
+			name:    "pending block, skipped nonces all in pending",
+			address: address1,
+			pending: true,
+			setup: func(ctx sdk.Context, k *evmkeeper.Keeper) {
+				// next expected for latest is 50, but 51,52 were sent
+				k.SetNonce(ctx, address1, 50)
+				k.AddPendingNonce(key1, address1, 51)
+				k.AddPendingNonce(key2, address1, 52)
+			},
+			expectedNonce: 50,
+		},
+		{
+			name:    "try 1000 nonces concurrently",
+			address: address1,
+			pending: true,
+			setup: func(ctx sdk.Context, k *evmkeeper.Keeper) {
+				// next expected for latest is 50, but 51,52 were sent
+				k.SetNonce(ctx, address1, 50)
+				wg := sync.WaitGroup{}
+				for i := 50; i < 1000; i++ {
+					wg.Add(1)
+					go func(nonce int) {
+						defer wg.Done()
+						key := tmtypes.TxKey(rand.NewRand().Bytes(32))
+						// call this just to exercise locks
+						k.CalculateNextNonce(ctx, address1, true)
+						k.AddPendingNonce(key, address1, uint64(nonce))
+					}(i)
+				}
+				wg.Wait()
+			},
+			expectedNonce: 1000,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			k, ctx := keeper.MockEVMKeeper()
+			if test.setup != nil {
+				test.setup(ctx, k)
+			}
+			next := k.CalculateNextNonce(ctx, test.address, test.pending)
+			require.Equal(t, test.expectedNonce, next)
+		})
+	}
 }
