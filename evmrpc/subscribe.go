d09db8107b256994c91737d91af4f31bd87aa77a
Jeremy Wei <jeremy.t.wei@gmail.com>
2024-01-03 21:19:09 -0500

    remove print

--- evmrpc/subscribe.go
+++ evmrpc/subscribe.go
@@ -38,7 +38,6 @@ func NewSubscriptionAPI(tmClient rpcclient.Client, ctxProvider func(int64) sdk.C
 }
 
 func (a *SubscriptionAPI) NewHeads(ctx context.Context) (*rpc.Subscription, error) {
-	fmt.Println("SubscriptionAPI: in newHeads")
 	notifier, supported := rpc.NotifierFromContext(ctx)
 	if !supported {
 		return &rpc.Subscription{}, rpc.ErrNotificationsUnsupported
