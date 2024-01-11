ef457258052ecf00fff87aae681c5770ed0a4954
Philip Su <philip.su.522@gmail.com>
2024-01-10 17:13:19 -0800

    debug

--- loadtest/tx.go
+++ loadtest/tx.go
@@ -3,11 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
-	"sync/atomic"
-	"time"
-
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	typestx "github.com/cosmos/cosmos-sdk/types/tx"
+	"sync/atomic"
 )
 
 func SendTx(
@@ -17,7 +14,6 @@ func SendTx(
 	loadtestClient LoadTestClient,
 	sentCount *int64,
 ) {
-	fmt.Printf("PSUDEBUG - sending txs")
 	grpcRes, err := loadtestClient.GetTxClient().BroadcastTx(
 		context.Background(),
 		&typestx.BroadcastTxRequest{
@@ -40,25 +36,27 @@ func SendTx(
 		}
 	}
 
-	for grpcRes.TxResponse.Code == sdkerrors.ErrMempoolIsFull.ABCICode() {
-		// retry after a second until either succeed or fail for some other reason
-		fmt.Printf("Mempool full\n")
-		time.Sleep(1 * time.Second)
-		grpcRes, err = loadtestClient.GetTxClient().BroadcastTx(
-			context.Background(),
-			&typestx.BroadcastTxRequest{
-				Mode:    mode,
-				TxBytes: txBytes,
-			},
-		)
-		if err != nil {
-			if failureExpected {
-			} else {
-				panic(err)
-			}
-		}
-	}
+	//for grpcRes.TxResponse.Code == sdkerrors.ErrMempoolIsFull.ABCICode() {
+	//	// retry after a second until either succeed or fail for some other reason
+	//	fmt.Printf("Mempool full\n")
+	//	time.Sleep(1 * time.Second)
+	//	grpcRes, err = loadtestClient.GetTxClient().BroadcastTx(
+	//		context.Background(),
+	//		&typestx.BroadcastTxRequest{
+	//			Mode:    mode,
+	//			TxBytes: txBytes,
+	//		},
+	//	)
+	//	if err != nil {
+	//		if failureExpected {
+	//		} else {
+	//			panic(err)
+	//		}
+	//	}
+	//}
 	if grpcRes.TxResponse.Code == 0 {
 		atomic.AddInt64(sentCount, 1)
+	} else {
+		fmt.Printf("PSUDEBUTG - failed: %s\n", grpcRes.TxResponse)
 	}
 }
