93ab04a40433c556a2d960e0910c24f613b75b4b
codchen <codchen03@gmail.com>
2024-01-11 10:44:10 +0800

    Add timestamp during estimateGas and add tests (#1225)
    * Add timestamp during estimateGas and add tests
    
    * fix

--- x/evm/keeper/evm.go
+++ x/evm/keeper/evm.go
@@ -1,7 +1,6 @@
 package keeper
 
 import (
-	"fmt"
 	"math"
 	"math/big"
 
@@ -92,7 +91,6 @@ func (k *Keeper) callEVM(ctx sdk.Context, from sdk.AccAddress, to *common.Addres
 	if val != nil {
 		value = val.BigInt()
 	}
-	fmt.Printf("EVMTEST: %s sending to %s with %s and %s\n", sender.Hex(), to, string(data), value)
 	ret, leftoverGas, err := f(vm.AccountRef(sender), to, data, evmGasRemaining.Uint64(), value)
 	ctx.GasMeter().ConsumeGas(ctx.GasMeter().Limit()-new(big.Int).Mul(new(big.Int).SetUint64(leftoverGas), multiplier).Uint64(), "call EVM")
 	if err != nil {
