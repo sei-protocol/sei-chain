84bb2ef35206ac61aa503baa6bff6b86b9734fbf
Uday Patil <udpatil@gmail.com>
2024-01-11 11:38:16 -0600

    update to pass absoluteIndices

--- app/app.go
+++ app/app.go
@@ -1359,13 +1359,14 @@ func (app *App) ExecuteTxsConcurrently(ctx sdk.Context, txs [][]byte, typedTxs [
 }
 
 // ProcessTXsWithOCC runs the transactions concurrently via OCC
-func (app *App) ProcessTXsWithOCC(ctx sdk.Context, txs [][]byte, typedTxs []sdk.Tx) ([]*abci.ExecTxResult, sdk.Context) {
+func (app *App) ProcessTXsWithOCC(ctx sdk.Context, txs [][]byte, typedTxs []sdk.Tx, absoluteIndices []int) ([]*abci.ExecTxResult, sdk.Context) {
 	entries := make([]*sdk.DeliverTxEntry, 0, len(txs))
 	for txIndex, tx := range txs {
 		deliverTxEntry := &sdk.DeliverTxEntry{
-			Request:  abci.RequestDeliverTx{Tx: tx},
-			SdkTx:    typedTxs[txIndex],
-			Checksum: sha256.Sum256(tx),
+			Request:       abci.RequestDeliverTx{Tx: tx},
+			SdkTx:         typedTxs[txIndex],
+			Checksum:      sha256.Sum256(tx),
+			AbsoluteIndex: absoluteIndices[txIndex],
 		}
 		// get prefill estimate
 		estimatedWritesets, err := app.AccessControlKeeper.GenerateEstimatedWritesets(ctx, app.txDecoder, app.GetAnteDepGenerator(), txIndex, tx)
