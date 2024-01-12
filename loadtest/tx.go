b8bed1345d5eda8e0786573666d0e8245048e7f8
Philip Su <philip.su.522@gmail.com>
2024-01-12 10:24:14 -0800

    debug

--- loadtest/tx.go
+++ loadtest/tx.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	typestx "github.com/cosmos/cosmos-sdk/types/tx"
 	"sync/atomic"
 )
@@ -16,27 +15,13 @@ func SendTx(
 	sentCount *int64,
 ) {
 
-	grpcRes, err := loadtestClient.GetTxClient().BroadcastTx(
+	grpcRes, _ := loadtestClient.GetTxClient().BroadcastTx(
 		ctx,
 		&typestx.BroadcastTxRequest{
 			Mode:    mode,
 			TxBytes: txBytes,
 		},
 	)
-	if err != nil {
-		if failureExpected {
-			fmt.Printf("Error: %s\n", err)
-		} else {
-			panic(err)
-		}
-
-		if grpcRes == nil || grpcRes.TxResponse == nil {
-			return
-		}
-		if grpcRes.TxResponse.Code == 0 {
-			atomic.AddInt64(sentCount, 1)
-		}
-	}
 
 	if grpcRes.TxResponse.Code == 0 {
 		atomic.AddInt64(sentCount, 1)
