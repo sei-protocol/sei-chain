82ee40324e2f67b02fa07c156a7c74c89096fb6e
Jeremy Wei <jeremy.t.wei@gmail.com>
2024-01-14 09:58:19 -0500

    in progress

--- docker/localnode/scripts/step0_build.sh
+++ docker/localnode/scripts/step0_build.sh
@@ -12,8 +12,20 @@ make clean
 make build-linux
 make build-price-feeder-linux
 mkdir -p build/generated
+# echo "install foundry" > build/generated/build.complete
+echo "installing foundry"
+curl -L https://foundry.paradigm.xyz | bash
+echo "sourcing bashrc"
+. /root/.bashrc
+echo "running foundryup"
+foundryup
+# RUN curl -L https://foundry.paradigm.xyz | bash > build/generated/foundry.log 2>&1
+# echo "sourcing bashrc" >> build/generated/foundry.log
+# RUN source /root/.bashrc >> build/generated/foundry.log 2>&1
+# echo "running foundryup" >> build/generated/foundry.log
+# RUN foundryup >> build/generated/foundry.log 2>&1
 echo "DONE" > build/generated/build.complete
 
 # install foundry
 # TODO: can move to own script
-RUN curl -L https://foundry.paradigm.xyz | bash && source /root/.bashrc && foundryup
+# RUN curl -L https://foundry.paradigm.xyz | bash && source /root/.bashrc && foundryup
