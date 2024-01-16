4c233fe030514ea5bf17a1934fa765107ccf259d
codchen <codchen03@gmail.com>
2024-01-16 15:35:00 +0800

    add whitelist for CW code hash that can make delegatecall to EVM (#1234)
    * add whitelist for CW code hash that can make delegatecall to EVM
    
    * add migration
    
    * it

--- wasmbinding/encoder.go
+++ wasmbinding/encoder.go
@@ -3,6 +3,7 @@ package wasmbinding
 import (
 	"encoding/json"
 
+	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
 	wasmvmtypes "github.com/CosmWasm/wasmvm/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
@@ -12,17 +13,18 @@ import (
 )
 
 type SeiWasmMessage struct {
-	PlaceOrders  json.RawMessage `json:"place_orders,omitempty"`
-	CancelOrders json.RawMessage `json:"cancel_orders,omitempty"`
-	CreateDenom  json.RawMessage `json:"create_denom,omitempty"`
-	MintTokens   json.RawMessage `json:"mint_tokens,omitempty"`
-	BurnTokens   json.RawMessage `json:"burn_tokens,omitempty"`
-	ChangeAdmin  json.RawMessage `json:"change_admin,omitempty"`
-	SetMetadata  json.RawMessage `json:"set_metadata,omitempty"`
-	CallEVM      json.RawMessage `json:"call_evm,omitempty"`
+	PlaceOrders     json.RawMessage `json:"place_orders,omitempty"`
+	CancelOrders    json.RawMessage `json:"cancel_orders,omitempty"`
+	CreateDenom     json.RawMessage `json:"create_denom,omitempty"`
+	MintTokens      json.RawMessage `json:"mint_tokens,omitempty"`
+	BurnTokens      json.RawMessage `json:"burn_tokens,omitempty"`
+	ChangeAdmin     json.RawMessage `json:"change_admin,omitempty"`
+	SetMetadata     json.RawMessage `json:"set_metadata,omitempty"`
+	CallEVM         json.RawMessage `json:"call_evm,omitempty"`
+	DelegateCallEVM json.RawMessage `json:"delegate_call_evm,omitempty"`
 }
 
-func CustomEncoder(sender sdk.AccAddress, msg json.RawMessage, info wasmvmtypes.MessageInfo) ([]sdk.Msg, error) {
+func CustomEncoder(sender sdk.AccAddress, msg json.RawMessage, info wasmvmtypes.MessageInfo, codeInfo wasmtypes.CodeInfo) ([]sdk.Msg, error) {
 	var parsedMessage SeiWasmMessage
 	if err := json.Unmarshal(msg, &parsedMessage); err != nil {
 		return []sdk.Msg{}, sdkerrors.Wrap(err, "Error parsing Sei Wasm Message")
@@ -44,6 +46,8 @@ func CustomEncoder(sender sdk.AccAddress, msg json.RawMessage, info wasmvmtypes.
 		return tokenfactorywasm.EncodeTokenFactorySetMetadata(parsedMessage.SetMetadata, sender)
 	case parsedMessage.CallEVM != nil:
 		return evmwasm.EncodeCallEVM(parsedMessage.CallEVM, sender, info)
+	case parsedMessage.DelegateCallEVM != nil:
+		return evmwasm.EncodeDelegateCallEVM(parsedMessage.DelegateCallEVM, sender, info, codeInfo)
 	default:
 		return []sdk.Msg{}, wasmvmtypes.UnsupportedRequest{Kind: "Unknown Sei Wasm Message"}
 	}
