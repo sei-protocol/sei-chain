f1c4a763f06a432ec0e3775eaf587a9f6566632e
kbhat1 <kartikbhat@Kartiks-MacBook-Pro-2.local>
2024-01-16 22:04:48 -0800

    logging

--- integration_test/contracts/deploy_wasm_contracts.sh
+++ integration_test/contracts/deploy_wasm_contracts.sh
@@ -25,6 +25,14 @@ done
 first_set_block_height=$($seidbin status | jq -r '.SyncInfo.latest_block_height')
 echo "$first_set_block_height" > $seihome/integration_test/contracts/wasm_first_set_block_height.txt
 
+forward_first_set=$(seid q wasm list-code --count-total --limit 400 --height $beginning_block_height --output json)
+reverse_first_set=$(seid q wasm list-code --reverse --count-total --limit 400 --height $beginning_block_height --output json)
+
+echo "100 forward first set $forward_first_set"
+echo ""
+echo "100 reverse first set $reverse_first_set"
+echo ""
+
 sleep 5s
 
 # store second set of contracts
@@ -39,6 +47,14 @@ done
 
 second_set_block_height=$($seidbin status | jq -r '.SyncInfo.latest_block_height')
 echo "$second_set_block_height" > $seihome/integration_test/contracts/wasm_second_set_block_height.txt
+
+forward_first_set=$(seid q wasm list-code --count-total --limit 400 --height $beginning_block_height --output json)
+reverse_first_set=$(seid q wasm list-code --reverse --count-total --limit 400 --height $beginning_block_height --output json)
+
+echo "200 forward first set $forward_first_set"
+echo ""
+echo "200 reverse first set $reverse_first_set"
+echo ""
 
 sleep 5s
 
@@ -57,5 +73,13 @@ echo "$third_set_block_height" > $seihome/integration_test/contracts/wasm_third_
 
 num_stored=$(seid q wasm list-code --count-total --limit 400 --output json | jq -r ".code_infos | length")
 echo $num_stored
+
+forward_first_set=$(seid q wasm list-code --count-total --limit 400 --height $beginning_block_height --output json)
+reverse_first_set=$(seid q wasm list-code --reverse --count-total --limit 400 --height $beginning_block_height --output json)
+
+echo "300 forward first set $forward_first_set"
+echo ""
+echo "300 reverse first set $reverse_first_set"
+echo ""
 
 exit 0
