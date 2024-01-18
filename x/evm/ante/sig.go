ced89eff2b7183f5661e0f4901a3104c85bf2413
Steven Landers <steven.landers@gmail.com>
2024-01-18 08:21:55 -0500

    consider pending nonce for mempool accept

--- x/evm/ante/sig.go
+++ x/evm/ante/sig.go
@@ -1,6 +1,7 @@
 package ante
 
 import (
+	"fmt"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	abci "github.com/tendermint/tendermint/abci/types"
@@ -34,6 +35,11 @@ func (svd *EVMSigVerifyDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulat
 	nextNonce := svd.evmKeeper.GetNonce(ctx, evmAddr)
 	txNonce := ethTx.Nonce()
 
+	// set EVM properties
+	ctx = ctx.WithIsEVM(true)
+	ctx = ctx.WithEVMNonce(txNonce)
+	ctx = ctx.WithEVMSenderAddress(evmAddr.Hex())
+
 	if ctx.IsCheckTx() {
 		if txNonce < nextNonce {
 			return ctx, sdkerrors.ErrWrongSequence
@@ -53,17 +59,21 @@ func (svd *EVMSigVerifyDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulat
 		})
 
 		if txNonce > nextNonce {
+			fmt.Printf("NONCE: add pending tx checker, nextNonce=%d, txNonce=%d\n", nextNonce, txNonce)
 			// transaction shall be added to mempool as a pending transaction
 			ctx = ctx.WithPendingTxChecker(func() abci.PendingTxCheckerResponse {
 				latestCtx := svd.latestCtxGetter()
-				latestNonce := svd.evmKeeper.GetNonce(latestCtx, evmAddr)
+				latestNonce := svd.evmKeeper.GetNonce(ctx, evmAddr)
+				nextNonce := svd.evmKeeper.CalculateNextNonce(latestCtx, evmAddr, true)
 				if txNonce < latestNonce {
 					return abci.Rejected
-				} else if txNonce == latestNonce {
+				} else if txNonce < nextNonce {
 					return abci.Accepted
 				}
 				return abci.Pending
 			})
+		} else {
+			fmt.Printf("NONCE: NOT Pending, nextNonce=%d, txNonce=%d\n", nextNonce, txNonce)
 		}
 	} else if txNonce != nextNonce {
 		return ctx, sdkerrors.ErrWrongSequence
