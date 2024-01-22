ba5de62434bbda084a51b90d4a9e08b7f4503bd7
Steven Landers <steven.landers@gmail.com>
2024-01-22 15:43:48 -0500

    add a test for defaulting logic

--- x/evm/keeper/params_test.go
+++ x/evm/keeper/params_test.go
@@ -1,6 +1,7 @@
 package keeper_test
 
 import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"testing"
 
 	testkeeper "github.com/sei-protocol/sei-chain/testutil/keeper"
@@ -8,7 +9,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestParams(t *testing.T) {
+func TestParamsDefault(t *testing.T) {
 	k, ctx := testkeeper.MockEVMKeeper()
 	require.Equal(t, types.DefaultChainConfig(), k.GetChainConfig(ctx))
 	require.Equal(t, types.DefaultBaseDenom, k.GetBaseDenom(ctx))
@@ -16,6 +17,32 @@ func TestParams(t *testing.T) {
 	require.Equal(t, types.DefaultBaseFeePerGas, k.GetBaseFeePerGas(ctx))
 	require.Equal(t, types.DefaultMinFeePerGas, k.GetMinimumFeePerGas(ctx))
 	require.Equal(t, types.DefaultWhitelistedCodeHashesBankSend, k.WhitelistedCodehashesBankSend(ctx))
+
+	require.Nil(t, k.GetParams(ctx).Validate())
+}
+
+func TestParamsWithOverride(t *testing.T) {
+	k, ctx := testkeeper.MockEVMKeeper()
+	overrides := types.Params{
+		BaseDenom:          "test",
+		PriorityNormalizer: sdk.NewDec(1),
+		BaseFeePerGas:      sdk.NewDec(2),
+		MinimumFeePerGas:   sdk.NewDec(3),
+		ChainConfig: types.ChainConfig{
+			CancunTime: 1,
+			PragueTime: 2,
+			VerkleTime: 3,
+		},
+		ChainId:                       sdk.NewInt(4),
+		WhitelistedCodehashesBankSend: []string{"test"},
+	}
+	k.SetParams(ctx, overrides)
+	require.Equal(t, overrides.ChainConfig, k.GetChainConfig(ctx))
+	require.Equal(t, overrides.BaseDenom, k.GetBaseDenom(ctx))
+	require.Equal(t, overrides.PriorityNormalizer, k.GetPriorityNormalizer(ctx))
+	require.Equal(t, overrides.BaseFeePerGas, k.GetBaseFeePerGas(ctx))
+	require.Equal(t, overrides.MinimumFeePerGas, k.GetMinimumFeePerGas(ctx))
+	require.Equal(t, overrides.WhitelistedCodehashesBankSend, k.WhitelistedCodehashesBankSend(ctx))
 
 	require.Nil(t, k.GetParams(ctx).Validate())
 }
