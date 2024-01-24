340c9912eedbdf9ba6f3976ec9a0d22d390eea9b
Steven Landers <steven@seinetwork.io>
2024-01-24 17:34:28 -0500

    [EVM] update version for devnet v4.0.3 (#1280)
    add version to version list

--- app/upgrades.go
+++ app/upgrades.go
@@ -70,6 +70,7 @@ var upgradesList = []string{
 	"v3.7.0",
 	"v4.0.0-evm-devnet",
 	"v4.0.1-evm-devnet",
+	"v4.0.3-evm-devnet",
 }
 
 // if there is an override list, use that instead, for integration tests
