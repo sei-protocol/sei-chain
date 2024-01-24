4ad070350fcfac0c7a1d5c63aa84dd3a46e60a84
mj <riku850@gmail.com>
2024-01-24 22:41:22 +0800

    Add more CW20 methods to ERC20 Wrapper (#1255)
    * Initial commit for producer/consumer loadtest client (#1190)
    
    * Initial commit for producer/consumer loadtest client
    
    * update sei-cosmos
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * rm rounds
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * finalize
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * bump cosmos
    
    * gofmt
    
    * debug
    
    * debug
    
    * Bump version to v3.6.1 (#1222)
    
    * Bump version to v3.6.0
    
    * Fix empty branch
    
    * Bump cosmos to fix upgrade migration
    
    * Update version
    
    * Fix changelogs readme
    
    * Bump tendermint version
    
    * Fix version
    
    * Add 3.6.1 upgrade handler
    
    * Supress lint
    
    * Add migration process readme for SeiDB (#1221)
    
    * Add seidb migration steps
    
    * Add migration process for SeiDB
    
    * Fix
    
    * Update seidb_migration.md
    
    * Loadtest producer consumer various fixes (#1227)
    
    * debug
    
    * debug
    
    * debug
    
    * set default broadcast to sync
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * grpc
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * lint
    
    * int
    
    * Add TransferFrom method to contract and bindings
    
    * Lt client fix (#1260)
    
    * Fix account loading in loadtest client
    
    * Fix account loading in loadtest client
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * update
    
    * base
    
    * base with error
    
    * comments
    
    * fix bug
    
    * old additions
    
    * revert merge
    
    * pre merge
    
    * issues
    
    * add tests for transfer and transferFrom
    
    * add approve functions
    
    * add send_from
    
    ---------
    
    Co-authored-by: Philip Su <philip.su.522@gmail.com>
    Co-authored-by: Yiming Zang <50607998+yzang2019@users.noreply.github.com>
    Co-authored-by: Mingjun <mj@mingjuns-mbp.lan>

--- example/cosmwasm/cw20/src/querier.rs
+++ example/cosmwasm/cw20/src/querier.rs
@@ -1,6 +1,6 @@
 use cosmwasm_std::{QuerierWrapper, StdResult, Uint128};
 
-use crate::msg::{Route, EvmQuery, EvmQueryWrapper, Erc20TransferPayloadResponse};
+use crate::msg::{Erc20AllowanceResponse, ErcPayloadResponse, EvmQuery, EvmQueryWrapper, Route};
 
 pub struct EvmQuerier<'a> {
     querier: &'a QuerierWrapper<'a, EvmQueryWrapper>,
@@ -12,11 +12,50 @@ impl<'a> EvmQuerier<'a> {
     }
 
     // returns base64-encoded bytes
-    pub fn erc20_transfer_payload(&self, recipient: String, amount: Uint128) -> StdResult<Erc20TransferPayloadResponse> {
+    pub fn erc20_transfer_payload(&self, recipient: String, amount: Uint128) -> StdResult<ErcPayloadResponse> {
         let request = EvmQueryWrapper {
             route: Route::Evm,
             query_data: EvmQuery::Erc20TransferPayload {
                 recipient, amount,
+            },
+        }
+        .into();
+
+        self.querier.query(&request)
+    }
+
+    // returns base64-encoded bytes
+    pub fn erc20_transfer_from_payload(&self, owner: String, recipient: String, amount: Uint128) -> StdResult<ErcPayloadResponse> {
+        let request = EvmQueryWrapper {
+            route: Route::Evm,
+            query_data: EvmQuery::Erc20TransferFromPayload {
+                owner, recipient, amount,
+            },
+        }
+        .into();
+
+        self.querier.query(&request)
+    }
+
+    // returns base64-encoded bytes
+    pub fn erc20_approve_payload(&self, spender: String, amount: Uint128) -> StdResult<ErcPayloadResponse> {
+        let request = EvmQueryWrapper {
+            route: Route::Evm,
+            query_data: EvmQuery::Erc20ApprovePayload {
+                spender, amount,
+            },
+        }
+        .into();
+
+        self.querier.query(&request)
+    }
+
+    // returns base64-encoded bytes
+    pub fn erc20_allowance(&self, contract_address: String, owner: String, spender: String) -> StdResult<Erc20AllowanceResponse> {
+        let request = EvmQueryWrapper {
+            route: Route::Evm,
+            query_data: EvmQuery::Erc20Allowance {
+                contract_address, owner, spender,
             },
         }
         .into();
