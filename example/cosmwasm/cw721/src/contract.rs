66bef29be6509088e57da30c807163537121590c
codchen <codchen03@gmail.com>
2024-01-25 15:53:01 +0800

    add CW->ERC wrapper queries (#1281)

--- example/cosmwasm/cw721/src/contract.rs
+++ example/cosmwasm/cw721/src/contract.rs
@@ -1,10 +1,10 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{
-    DepsMut, Env, MessageInfo, Response, Binary,
+    DepsMut, Deps, Env, MessageInfo, Response, Binary, StdResult, to_json_binary,
 };
-use cw721::Cw721ReceiveMsg;
-use crate::msg::{EvmQueryWrapper, EvmMsg, InstantiateMsg, ExecuteMsg};
+use cw721::{Cw721ReceiveMsg, OwnerOfResponse, Approval, ApprovalResponse, ApprovalsResponse, OperatorResponse, ContractInfoResponse, NftInfoResponse, AllNftInfoResponse};
+use crate::msg::{EvmQueryWrapper, EvmMsg, InstantiateMsg, ExecuteMsg, QueryMsg};
 use crate::querier::EvmQuerier;
 use crate::error::ContractError;
 use crate::state::ERC721_ADDRESS;
@@ -119,7 +119,7 @@ pub fn execute_approve_all(
 }
 
 pub fn execute_burn() -> Result<Response<EvmMsg>, ContractError> {
-    Err(ContractError::BurnNotSupported {})
+    Err(ContractError::NotSupported {})
 }
 
 fn transfer_nft(
@@ -143,4 +143,82 @@ fn transfer_nft(
         .add_message(msg);
 
     Ok(res)
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn query(deps: Deps<EvmQueryWrapper>, env: Env, msg: QueryMsg) -> Result<Binary, ContractError> {
+    match msg {
+        QueryMsg::OwnerOf { token_id, include_expired: _ } => Ok(to_json_binary(&query_owner_of(deps, env, token_id)?)?),
+        QueryMsg::Approval { token_id, spender, include_expired: _ } => Ok(query_approval(deps, env, token_id, spender)?),
+        QueryMsg::Approvals { token_id, include_expired: _ } => Ok(query_approvals(deps, env, token_id)?),
+        QueryMsg::Operator { owner, operator, include_expired: _ } => Ok(query_operator(deps, env, owner, operator)?),
+        QueryMsg::ContractInfo {} => Ok(query_contract_info(deps, env)?),
+        QueryMsg::NftInfo { token_id } => Ok(query_nft_info(deps, env, token_id)?),
+        QueryMsg::AllNftInfo { token_id, include_expired: _ } => Ok(query_all_nft_info(deps, env, token_id)?),
+        _ => Err(ContractError::NotSupported {  }),
+    }
+}
+
+pub fn query_owner_of(deps: Deps<EvmQueryWrapper>, env: Env, token_id: String) -> StdResult<OwnerOfResponse> {
+    let erc_addr = ERC721_ADDRESS.load(deps.storage)?;
+    let querier = EvmQuerier::new(&deps.querier);
+    let owner = querier.erc721_owner(env.clone().contract.address.into_string(), erc_addr.clone(), token_id.clone())?.owner;
+    let approved = querier.erc721_approved(env.clone().contract.address.into_string(), erc_addr.clone(), token_id.clone())?.approved;
+    let mut approvals: Vec<Approval> = vec![];
+    if !approved.is_empty() {
+        approvals.push(Approval{spender:approved, expires: cw721::Expiration::Never {}});
+    }
+    Ok(OwnerOfResponse{owner, approvals})
+}
+
+pub fn query_approval(deps: Deps<EvmQueryWrapper>, env: Env, token_id: String, spender: String) -> StdResult<Binary> {
+    let erc_addr = ERC721_ADDRESS.load(deps.storage)?;
+    let querier = EvmQuerier::new(&deps.querier);
+    let approved = querier.erc721_approved(env.clone().contract.address.into_string(), erc_addr.clone(), token_id.clone())?.approved;
+    if !approved.is_empty() && approved == spender {
+        return to_json_binary(&ApprovalResponse{approval: Approval{spender, expires: cw721::Expiration::Never {}}});
+    }
+    Err(cosmwasm_std::StdError::NotFound { kind: "not approved".to_string() })
+}
+
+pub fn query_approvals(deps: Deps<EvmQueryWrapper>, env: Env, token_id: String) -> StdResult<Binary> {
+    let erc_addr = ERC721_ADDRESS.load(deps.storage)?;
+    let querier = EvmQuerier::new(&deps.querier);
+    let approved = querier.erc721_approved(env.clone().contract.address.into_string(), erc_addr.clone(), token_id.clone())?.approved;
+    if !approved.is_empty() {
+        return to_json_binary(&ApprovalsResponse{approvals: vec![Approval{spender: approved, expires: cw721::Expiration::Never {}}]});
+    }
+    to_json_binary(&ApprovalsResponse{approvals: vec![]})
+}
+
+pub fn query_operator(deps: Deps<EvmQueryWrapper>, env: Env, owner: String, operator: String) -> StdResult<Binary> {
+    let erc_addr = ERC721_ADDRESS.load(deps.storage)?;
+    let querier = EvmQuerier::new(&deps.querier);
+    let is_approved = querier.erc721_is_approved_for_all(env.clone().contract.address.into_string(), erc_addr.clone(), owner.clone(), operator.clone())?.is_approved;
+    if is_approved {
+        return to_json_binary(&OperatorResponse{approval: Approval{spender: operator.clone(), expires: cw721::Expiration::Never {}}});
+    }
+    Err(cosmwasm_std::StdError::NotFound { kind: "not approved".to_string() })
+}
+
+pub fn query_contract_info(deps: Deps<EvmQueryWrapper>, env: Env) -> StdResult<Binary> {
+    let erc_addr = ERC721_ADDRESS.load(deps.storage)?;
+    let querier = EvmQuerier::new(&deps.querier);
+    let res = querier.erc721_name_symbol(env.clone().contract.address.into_string(), erc_addr.clone())?;
+    to_json_binary(&ContractInfoResponse{name: res.name, symbol: res.symbol})
+}
+
+pub fn query_nft_info(deps: Deps<EvmQueryWrapper>, env: Env, token_id: String) -> StdResult<Binary> {
+    let erc_addr = ERC721_ADDRESS.load(deps.storage)?;
+    let querier = EvmQuerier::new(&deps.querier);
+    let res = querier.erc721_uri(env.clone().contract.address.into_string(), erc_addr.clone(), token_id.clone())?;
+    to_json_binary(&NftInfoResponse{token_uri: Some(res.uri), extension: ""})
+}
+
+pub fn query_all_nft_info(deps: Deps<EvmQueryWrapper>, env: Env, token_id: String) -> StdResult<Binary> {
+    let erc_addr = ERC721_ADDRESS.load(deps.storage)?;
+    let querier = EvmQuerier::new(&deps.querier);
+    let res = querier.erc721_uri(env.clone().contract.address.into_string(), erc_addr.clone(), token_id.clone())?;
+    let owner_of_res = query_owner_of(deps, env, token_id)?;
+    to_json_binary(&AllNftInfoResponse{access: owner_of_res, info: NftInfoResponse{token_uri: Some(res.uri), extension: ""}})
 }
--- example/cosmwasm/cw721/src/querier.rs
+++ example/cosmwasm/cw721/src/querier.rs
@@ -1,6 +1,6 @@
 use cosmwasm_std::{QuerierWrapper, StdResult};
 
-use crate::msg::{Route, EvmQuery, EvmQueryWrapper, ErcPayloadResponse, Erc721OwnerResponse};
+use crate::msg::{Route, EvmQuery, EvmQueryWrapper, ErcPayloadResponse, Erc721OwnerResponse, Erc721ApprovedResponse, Erc721IsApprovedForAllResponse, Erc721NameSymbolResponse, Erc721UriResponse};
 
 pub struct EvmQuerier<'a> {
     querier: &'a QuerierWrapper<'a, EvmQueryWrapper>,
@@ -15,6 +15,46 @@ impl<'a> EvmQuerier<'a> {
         let request = EvmQueryWrapper {
             route: Route::Evm,
             query_data: EvmQuery::Erc721Owner { caller, contract_address, token_id },
+        }
+        .into();
+
+        self.querier.query(&request)
+    }
+
+    pub fn erc721_approved(&self, caller: String, contract_address: String, token_id: String) -> StdResult<Erc721ApprovedResponse> {
+        let request = EvmQueryWrapper {
+            route: Route::Evm,
+            query_data: EvmQuery::Erc721Approved { caller, contract_address, token_id },
+        }
+        .into();
+
+        self.querier.query(&request)
+    }
+
+    pub fn erc721_is_approved_for_all(&self, caller: String, contract_address: String, owner: String, operator: String) -> StdResult<Erc721IsApprovedForAllResponse> {
+        let request = EvmQueryWrapper {
+            route: Route::Evm,
+            query_data: EvmQuery::Erc721IsApprovedForAll { caller, contract_address, owner, operator },
+        }
+        .into();
+
+        self.querier.query(&request)
+    }
+
+    pub fn erc721_name_symbol(&self, caller: String, contract_address: String) -> StdResult<Erc721NameSymbolResponse> {
+        let request = EvmQueryWrapper {
+            route: Route::Evm,
+            query_data: EvmQuery::Erc721NameSymbol { caller, contract_address },
+        }
+        .into();
+
+        self.querier.query(&request)
+    }
+
+    pub fn erc721_uri(&self, caller: String, contract_address: String, token_id: String,) -> StdResult<Erc721UriResponse> {
+        let request = EvmQueryWrapper {
+            route: Route::Evm,
+            query_data: EvmQuery::Erc721Uri { caller, contract_address, token_id },
         }
         .into();
 
