535c6f5e12a3d5d15cf4d59bc43760dc153cd6ac
codchen <codchen03@gmail.com>
2024-01-25 15:53:01 +0800

    add CW->ERC wrapper queries (#1281)

--- example/cosmwasm/cw721/src/msg.rs
+++ example/cosmwasm/cw721/src/msg.rs
@@ -3,7 +3,7 @@ use schemars::JsonSchema;
 use cosmwasm_schema::cw_serde;
 use serde::{Deserialize, Serialize};
 
-pub use cw721::Cw721ExecuteMsg as ExecuteMsg;
+pub use cw721::{Cw721ExecuteMsg as ExecuteMsg, Cw721QueryMsg as QueryMsg};
 
 #[cw_serde]
 pub struct InstantiateMsg {
@@ -46,9 +46,29 @@ pub enum EvmQuery {
         contract_address: String,
         token_id: String,
     },
+    Erc721Approved {
+        caller: String,
+        contract_address: String,
+        token_id: String,
+    },
+    Erc721IsApprovedForAll {
+        caller: String,
+        contract_address: String,
+        owner: String,
+        operator: String,
+    },
     Erc721SetApprovalAllPayload {
         to: String,
         approved: bool,
+    },
+    Erc721NameSymbol {
+        caller: String,
+        contract_address: String,
+    },
+    Erc721Uri {
+        caller: String,
+        contract_address: String,
+        token_id: String,
     },
 }
 
@@ -60,6 +80,27 @@ pub struct ErcPayloadResponse {
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct Erc721OwnerResponse {
     pub owner: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct Erc721ApprovedResponse {
+    pub approved: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct Erc721IsApprovedForAllResponse {
+    pub is_approved: bool,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct Erc721NameSymbolResponse {
+    pub name: String,
+    pub symbol: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct Erc721UriResponse {
+    pub uri: String,
 }
 
 // implement custom query
