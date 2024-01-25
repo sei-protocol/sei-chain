30a846d4d5008d0c6382b9f7ea95210722368b02
codchen <codchen03@gmail.com>
2024-01-25 15:53:01 +0800

    add CW->ERC wrapper queries (#1281)

--- x/evm/client/wasm/query.go
+++ x/evm/client/wasm/query.go
@@ -4,6 +4,7 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"errors"
+	"math/big"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/ethereum/go-ethereum/common"
@@ -51,6 +52,110 @@ func (h *EVMQueryHandler) HandleERC20TransferPayload(ctx sdk.Context, recipient 
 	}
 	res := bindings.ERCPayloadResponse{EncodedPayload: base64.StdEncoding.EncodeToString(bz)}
 	return json.Marshal(res)
+}
+
+func (h *EVMQueryHandler) HandleERC20TokenInfo(ctx sdk.Context, contractAddress string, caller string) ([]byte, error) {
+	callerAddr, err := sdk.AccAddressFromBech32(caller)
+	if err != nil {
+		return nil, err
+	}
+	contract := common.HexToAddress(contractAddress)
+	abi, err := native.NativeMetaData.GetAbi()
+	if err != nil {
+		return nil, err
+	}
+	response := bindings.ERC20TokenInfoResponse{}
+
+	bz, err := abi.Pack("totalSupply")
+	if err != nil {
+		return nil, err
+	}
+	res, err := h.k.StaticCallEVM(ctx, callerAddr, &contract, bz)
+	if err != nil {
+		return nil, err
+	}
+	unpacked, err := abi.Unpack("totalSupply", res)
+	if err != nil {
+		return nil, err
+	}
+	totalSupply := sdk.NewIntFromBigInt(unpacked[0].(*big.Int))
+	response.TotalSupply = &totalSupply
+
+	bz, err = abi.Pack("name")
+	if err != nil {
+		return nil, err
+	}
+	res, err = h.k.StaticCallEVM(ctx, callerAddr, &contract, bz)
+	if err != nil {
+		return nil, err
+	}
+	unpacked, err = abi.Unpack("name", res)
+	if err != nil {
+		return nil, err
+	}
+	response.Name = unpacked[0].(string)
+
+	bz, err = abi.Pack("symbol")
+	if err != nil {
+		return nil, err
+	}
+	res, err = h.k.StaticCallEVM(ctx, callerAddr, &contract, bz)
+	if err != nil {
+		return nil, err
+	}
+	unpacked, err = abi.Unpack("symbol", res)
+	if err != nil {
+		return nil, err
+	}
+	response.Symbol = unpacked[0].(string)
+
+	bz, err = abi.Pack("decimal")
+	if err != nil {
+		return nil, err
+	}
+	res, err = h.k.StaticCallEVM(ctx, callerAddr, &contract, bz)
+	if err != nil {
+		return nil, err
+	}
+	unpacked, err = abi.Unpack("decimal", res)
+	if err != nil {
+		return nil, err
+	}
+	response.Decimal = unpacked[0].(byte)
+
+	return json.Marshal(response)
+}
+
+func (h *EVMQueryHandler) HandleERC20Balance(ctx sdk.Context, contractAddress string, account string) ([]byte, error) {
+	addr, err := sdk.AccAddressFromBech32(account)
+	if err != nil {
+		return nil, err
+	}
+	evmAddr, found := h.k.GetEVMAddress(ctx, addr)
+	if !found {
+		evmAddr = common.Address{}
+		evmAddr.SetBytes(addr)
+	}
+	contract := common.HexToAddress(contractAddress)
+	abi, err := native.NativeMetaData.GetAbi()
+	if err != nil {
+		return nil, err
+	}
+
+	bz, err := abi.Pack("balanceOf", evmAddr)
+	if err != nil {
+		return nil, err
+	}
+	res, err := h.k.StaticCallEVM(ctx, addr, &contract, bz)
+	if err != nil {
+		return nil, err
+	}
+	unpacked, err := abi.Unpack("balanceOf", res)
+	if err != nil {
+		return nil, err
+	}
+	balance := sdk.NewIntFromBigInt(unpacked[0].(*big.Int))
+	return json.Marshal(bindings.ERC20BalanceResponse{Balance: &balance})
 }
 
 func (h *EVMQueryHandler) HandleERC721Owner(ctx sdk.Context, caller string, contractAddress string, tokenId string) ([]byte, error) {
@@ -280,7 +385,161 @@ func (h *EVMQueryHandler) HandleERC20Allowance(ctx sdk.Context, contractAddress 
 	if err != nil {
 		return nil, err
 	}
-	allowance := typed[0].(sdk.Int)
-	response := bindings.ERC20AllowanceResponse{Allowance: &allowance}
+	allowance := typed[0].(*big.Int)
+	allowanceSdk := sdk.NewIntFromBigInt(allowance)
+	response := bindings.ERC20AllowanceResponse{Allowance: &allowanceSdk}
+	return json.Marshal(response)
+}
+
+func (h *EVMQueryHandler) HandleERC721Approved(ctx sdk.Context, caller string, contractAddress string, tokenId string) ([]byte, error) {
+	callerAddr, err := sdk.AccAddressFromBech32(caller)
+	if err != nil {
+		return nil, err
+	}
+	contract := common.HexToAddress(contractAddress)
+	abi, err := cw721.Cw721MetaData.GetAbi()
+	if err != nil {
+		return nil, err
+	}
+	t, ok := sdk.NewIntFromString(tokenId)
+	if !ok {
+		return nil, errors.New("invalid token ID for ERC20, must be a big Int")
+	}
+	bz, err := abi.Pack("getApproved", t.BigInt())
+	if err != nil {
+		return nil, err
+	}
+	res, err := h.k.StaticCallEVM(ctx, callerAddr, &contract, bz)
+	if err != nil {
+		return nil, err
+	}
+	typed, err := abi.Unpack("getApproved", res)
+	if err != nil {
+		return nil, err
+	}
+	approved := typed[0].(common.Address)
+	a := ""
+	if (approved != common.Address{}) {
+		aSeiAddr, found := h.k.GetSeiAddress(ctx, approved)
+		if !found {
+			aSeiAddr = sdk.AccAddress(approved[:])
+		}
+		a = aSeiAddr.String()
+	}
+	response := bindings.ERC721ApprovedResponse{Approved: a}
+	return json.Marshal(response)
+}
+
+func (h *EVMQueryHandler) HandleERC721IsApprovedForAll(ctx sdk.Context, caller string, contractAddress string, owner string, operator string) ([]byte, error) {
+	callerAddr, err := sdk.AccAddressFromBech32(caller)
+	if err != nil {
+		return nil, err
+	}
+	ownerAddr, err := sdk.AccAddressFromBech32(owner)
+	if err != nil {
+		return nil, err
+	}
+	ownerEvmAddr, found := h.k.GetEVMAddress(ctx, ownerAddr)
+	if !found {
+		ownerEvmAddr = common.Address{}
+		ownerEvmAddr.SetBytes(ownerAddr)
+	}
+	operatorAddr, err := sdk.AccAddressFromBech32(operator)
+	if err != nil {
+		return nil, err
+	}
+	operatorEvmAddr, found := h.k.GetEVMAddress(ctx, operatorAddr)
+	if !found {
+		operatorEvmAddr = common.Address{}
+		operatorEvmAddr.SetBytes(operatorAddr)
+	}
+	contract := common.HexToAddress(contractAddress)
+	abi, err := cw721.Cw721MetaData.GetAbi()
+	if err != nil {
+		return nil, err
+	}
+	bz, err := abi.Pack("isApprovedForAll", owner, operator)
+	if err != nil {
+		return nil, err
+	}
+	res, err := h.k.StaticCallEVM(ctx, callerAddr, &contract, bz)
+	if err != nil {
+		return nil, err
+	}
+	typed, err := abi.Unpack("isApprovedForAll", res)
+	if err != nil {
+		return nil, err
+	}
+	response := bindings.ERC721IsApprovedForAllResponse{IsApproved: typed[0].(bool)}
+	return json.Marshal(response)
+}
+
+func (h *EVMQueryHandler) HandleERC721NameSymbol(ctx sdk.Context, caller string, contractAddress string) ([]byte, error) {
+	callerAddr, err := sdk.AccAddressFromBech32(caller)
+	if err != nil {
+		return nil, err
+	}
+	contract := common.HexToAddress(contractAddress)
+	abi, err := cw721.Cw721MetaData.GetAbi()
+	if err != nil {
+		return nil, err
+	}
+	bz, err := abi.Pack("name")
+	if err != nil {
+		return nil, err
+	}
+	res, err := h.k.StaticCallEVM(ctx, callerAddr, &contract, bz)
+	if err != nil {
+		return nil, err
+	}
+	typed, err := abi.Unpack("name", res)
+	if err != nil {
+		return nil, err
+	}
+	name := typed[0].(string)
+	bz, err = abi.Pack("symbol")
+	if err != nil {
+		return nil, err
+	}
+	res, err = h.k.StaticCallEVM(ctx, callerAddr, &contract, bz)
+	if err != nil {
+		return nil, err
+	}
+	typed, err = abi.Unpack("symbol", res)
+	if err != nil {
+		return nil, err
+	}
+	symbol := typed[0].(string)
+	response := bindings.ERC721NameSymbolResponse{Name: name, Symbol: symbol}
+	return json.Marshal(response)
+}
+
+func (h *EVMQueryHandler) HandleERC721Uri(ctx sdk.Context, caller string, contractAddress string, tokenId string) ([]byte, error) {
+	callerAddr, err := sdk.AccAddressFromBech32(caller)
+	if err != nil {
+		return nil, err
+	}
+	t, ok := sdk.NewIntFromString(tokenId)
+	if !ok {
+		return nil, errors.New("invalid token ID for ERC20, must be a big Int")
+	}
+	contract := common.HexToAddress(contractAddress)
+	abi, err := cw721.Cw721MetaData.GetAbi()
+	if err != nil {
+		return nil, err
+	}
+	bz, err := abi.Pack("tokenURI", t.BigInt())
+	if err != nil {
+		return nil, err
+	}
+	res, err := h.k.StaticCallEVM(ctx, callerAddr, &contract, bz)
+	if err != nil {
+		return nil, err
+	}
+	typed, err := abi.Unpack("tokenURI", res)
+	if err != nil {
+		return nil, err
+	}
+	response := bindings.ERC721UriResponse{Uri: typed[0].(string)}
 	return json.Marshal(response)
 }
