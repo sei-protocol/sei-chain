13169df640e878ce042c30b5fa08a7814ab91469
Jeremy Wei <jeremy.t.wei@gmail.com>
2024-01-29 16:57:35 -0500

    added prints

--- contracts/test/EVMCompatabilityTester.js
+++ contracts/test/EVMCompatabilityTester.js
@@ -726,6 +726,7 @@ describe("EVM Test", function () {
             };
           
             const logs = await ethers.provider.getLogs(filter);
+            console.log("block range filter logs = ", logs)
             expect(logs).to.be.an('array');
             expect(logs.length).to.equal(numTxs);
           });
