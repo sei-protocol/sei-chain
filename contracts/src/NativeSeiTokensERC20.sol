c4d7dd2d74aa9497102b1956d852eba89f686b08
codchen <codchen03@gmail.com>
2024-01-31 00:37:51 +0800

    Allow display info to be displayed in native ERC-20 wrappers

--- contracts/src/NativeSeiTokensERC20.sol
+++ contracts/src/NativeSeiTokensERC20.sol
@@ -9,19 +9,25 @@ contract NativeSeiTokensERC20 is ERC20 {
     address constant BANK_PRECOMPILE_ADDRESS = 0x0000000000000000000000000000000000001001;
 
     string public denom;
+    string public nname;
+    string public ssymbol;
+    uint8 public ddecimals;
     IBank public BankPrecompile;
 
-    constructor(string memory denom_) ERC20("", "") {
+    constructor(string memory denom_, string memory name_, string memory symbol_, uint8 decimals_) ERC20("", "") {
         BankPrecompile = IBank(BANK_PRECOMPILE_ADDRESS);
         denom = denom_;
+        nname = name_;
+        ssymbol = symbol_;
+        ddecimals = decimals_;
     }
 
     function name() public view override returns (string memory) {
-        return BankPrecompile.name(denom);
+        return nname;
     }
 
     function symbol() public view override returns (string memory) {
-        return BankPrecompile.symbol(denom);
+        return ssymbol;
     }
 
     function balanceOf(address account) public view override returns (uint256) {
@@ -29,7 +35,7 @@ contract NativeSeiTokensERC20 is ERC20 {
     }
 
     function decimals() public view override returns (uint8) {
-        return BankPrecompile.decimals(denom);
+        return ddecimals;
     }
 
     function totalSupply() public view override returns (uint256) {
