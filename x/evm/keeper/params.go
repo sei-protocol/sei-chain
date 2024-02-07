a96e6849ad28a4b9d82de6294f597e731eeff9b9
codchen <codchen03@gmail.com>
2024-02-07 14:09:12 +0800

    hardcode params

--- x/evm/keeper/params.go
+++ x/evm/keeper/params.go
@@ -12,39 +12,48 @@ func (k Keeper) SetParams(ctx sdk.Context, params types.Params) {
 }
 
 func (k *Keeper) GetParams(ctx sdk.Context) types.Params {
-	params := types.Params{}
-	k.Paramstore.GetParamSet(ctx, &params)
-	return params
+	return types.DefaultParams()
+	// params := types.Params{}
+	// k.Paramstore.GetParamSet(ctx, &params)
+	// return params
 }
 
 func (k *Keeper) GetBaseDenom(ctx sdk.Context) string {
-	return k.GetParams(ctx).BaseDenom
+	return types.DefaultBaseDenom
+	// return k.GetParams(ctx).BaseDenom
 }
 
 func (k *Keeper) GetChainConfig(ctx sdk.Context) types.ChainConfig {
-	return k.GetParams(ctx).ChainConfig
+	return types.DefaultChainConfig()
+	// return k.GetParams(ctx).ChainConfig
 }
 
 func (k *Keeper) GetPriorityNormalizer(ctx sdk.Context) sdk.Dec {
-	return k.GetParams(ctx).PriorityNormalizer
+	return types.DefaultPriorityNormalizer
+	// return k.GetParams(ctx).PriorityNormalizer
 }
 
 func (k *Keeper) GetBaseFeePerGas(ctx sdk.Context) sdk.Dec {
-	return k.GetParams(ctx).BaseFeePerGas
+	return types.DefaultBaseFeePerGas
+	// return k.GetParams(ctx).BaseFeePerGas
 }
 
 func (k *Keeper) GetMinimumFeePerGas(ctx sdk.Context) sdk.Dec {
-	return k.GetParams(ctx).MinimumFeePerGas
+	return types.DefaultMinFeePerGas
+	// return k.GetParams(ctx).MinimumFeePerGas
 }
 
 func (k *Keeper) ChainID(ctx sdk.Context) *big.Int {
-	return k.GetParams(ctx).ChainId.BigInt()
+	return types.DefaultChainID.BigInt()
+	// return k.GetParams(ctx).ChainId.BigInt()
 }
 
 func (k *Keeper) WhitelistedCodehashesBankSend(ctx sdk.Context) []string {
-	return k.GetParams(ctx).WhitelistedCodehashesBankSend
+	return types.DefaultWhitelistedCodeHashesBankSend
+	// return k.GetParams(ctx).WhitelistedCodehashesBankSend
 }
 
 func (k *Keeper) WhitelistedCwCodeHashesForDelegateCall(ctx sdk.Context) [][]byte {
-	return k.GetParams(ctx).WhitelistedCwCodeHashesForDelegateCall
+	return types.DefaultWhitelistedCwCodeHashesForDelegateCall
+	// return k.GetParams(ctx).WhitelistedCwCodeHashesForDelegateCall
 }
