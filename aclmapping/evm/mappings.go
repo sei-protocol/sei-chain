ec4820c257ed006a766f7f86c7eaf282e5cd93cb
codchen <codchen03@gmail.com>
2024-02-13 12:09:45 +0800

    Use per-tx escrow for Wei balance (#1314)
    * Use per-tx escrow for Wei balance
    
    * rebase

--- aclmapping/evm/mappings.go
+++ aclmapping/evm/mappings.go
@@ -42,8 +42,8 @@ func TransactionDependencyGenerator(_ aclkeeper.Keeper, evmKeeper evmkeeper.Keep
 		return []sdkacltypes.AccessOperation{}, err
 	}
 	ops := []sdkacltypes.AccessOperation{}
-	ops = appendRWBalanceOps(ops, state.GetMiddleManAddress(ctx))
-	ops = appendRWBalanceOps(ops, state.GetCoinbaseAddress(ctx))
+	ops = appendRWBalanceOps(ops, state.GetMiddleManAddress(ctx.TxIndex()))
+	ops = appendRWBalanceOps(ops, state.GetCoinbaseAddress(ctx.TxIndex()))
 	sender := evmMsg.Derived.SenderSeiAddr
 	ops = appendRWBalanceOps(ops, sender)
 	ops = append(ops,
