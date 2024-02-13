17e49cf698deeae19027f334be7d942a564929ca
Jeremy Wei <jeremy.t.wei@gmail.com>
2024-02-12 23:05:16 -0500

    add test for prestateTracer

--- evmrpc/tracers_test.go
+++ evmrpc/tracers_test.go
@@ -1,16 +1,18 @@
 package evmrpc_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestTraceTransaction(t *testing.T) {
-	hash := "0x1234567890123456789023456789012345678901234567890123456789000004"
 	args := map[string]interface{}{}
+
+	// test callTracer
 	args["tracer"] = "callTracer"
-	resObj := sendRequestGoodWithNamespace(t, "debug", "traceTransaction", hash, args)
+	resObj := sendRequestGoodWithNamespace(t, "debug", "traceTransaction", DebugTraceHashHex, args)
 	result := resObj["result"].(map[string]interface{})
 	require.Equal(t, "0x5b4eba929f3811980f5ae0c5d04fa200f837df4e", result["from"])
 	require.Equal(t, "0x55f0", result["gas"])
@@ -18,4 +20,17 @@ func TestTraceTransaction(t *testing.T) {
 	require.Equal(t, "0x0000000000000000000000000000000000010203", result["to"])
 	require.Equal(t, "CALL", result["type"])
 	require.Equal(t, "0x3e8", result["value"])
+
+	// test prestateTracer
+	args["tracer"] = "prestateTracer"
+	resObj = sendRequestGoodWithNamespace(t, "debug", "traceTransaction", DebugTraceHashHex, args)
+	result = resObj["result"].(map[string]interface{})
+	fmt.Println("result = ", result)
+	// iterate over keys of the map
+	for _, v := range result {
+		require.Contains(t, v, "balance")
+		balanceMap := v.(map[string]interface{})
+		balance := balanceMap["balance"].(string)
+		require.Greater(t, len(balance), 2)
+	}
 }
