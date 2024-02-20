4993a27e268a15ffeb070b3237399ed90499be9b
codchen <codchen03@gmail.com>
2024-02-21 00:07:47 +0800

    handle nil code (#1360)

--- x/evm/keeper/code_test.go
+++ x/evm/keeper/code_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common"
+	ethtypes "github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/sei-protocol/sei-chain/testutil/keeper"
 	"github.com/stretchr/testify/require"
@@ -22,4 +23,14 @@ func TestCode(t *testing.T) {
 	require.Equal(t, crypto.Keccak256Hash(code), k.GetCodeHash(ctx, addr))
 	require.Equal(t, code, k.GetCode(ctx, addr))
 	require.Equal(t, 5, k.GetCodeSize(ctx, addr))
+}
+
+func TestNilCode(t *testing.T) {
+	k, ctx := keeper.MockEVMKeeper()
+	_, addr := keeper.MockAddressPair()
+
+	k.SetCode(ctx, addr, nil)
+	require.Nil(t, k.GetCode(ctx, addr))
+	require.Equal(t, 0, k.GetCodeSize(ctx, addr))
+	require.Equal(t, ethtypes.EmptyCodeHash, k.GetCodeHash(ctx, addr))
 }
