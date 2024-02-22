e3d00ef0342ba33e42b5476b0538df19a713aec2
yzang2019 <zymfrank@gmail.com>
2024-02-22 21:51:29 +0800

    Add debug endpoints for traceBlockByNumber and traceBlockByHash

--- evmrpc/tracers_test.go
+++ evmrpc/tracers_test.go
@@ -31,3 +31,16 @@ func TestTraceTransaction(t *testing.T) {
 		require.Greater(t, len(balance), 2)
 	}
 }
+
+func TestTraceBlockByNumber(t *testing.T) {
+	args := map[string]interface{}{}
+	args["tracer"] = "callTracer"
+	resObj := sendRequestGoodWithNamespace(t, "debug", "traceBlockByNumber", "0x65", args)
+	result := resObj["result"].([]interface{})[0].(map[string]interface{})["result"].(map[string]interface{})
+	require.Equal(t, "0x5b4eba929f3811980f5ae0c5d04fa200f837df4e", result["from"])
+	require.Equal(t, "0x55f0", result["gas"])
+	require.Equal(t, "0x616263", result["input"])
+	require.Equal(t, "0x0000000000000000000000000000000000010203", result["to"])
+	require.Equal(t, "CALL", result["type"])
+	require.Equal(t, "0x3e8", result["value"])
+}
