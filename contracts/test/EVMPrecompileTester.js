4e95314b5b1f5329590f745c0bf582c52bac59e5
Jeremy Wei <jeremy.t.wei@gmail.com>
2024-02-25 21:22:29 -0500

    wip

---
+++ contracts/test/EVMPrecompileTester.js
@@ -0,0 +1,104 @@
+const { execSync } = require('child_process');
+const { expect } = require("chai");
+
+describe("EVM Test", function () {
+    describe("EVMPrecompileTester", function () {
+        let contractAddress;
+        let erc20;
+        let owner;
+        let owner2;
+        const setupScriptPath = './test/deploy_atom_erc20.sh';
+        before(async function() {
+            contractAddress = runSetupScript(setupScriptPath);
+            await sleep(1000);
+
+            // TODO: create a contract object
+            // Create a signer
+            const [signer, signer2] = await ethers.getSigners();
+            owner = await signer.getAddress();
+            owner2 = await signer2.getAddress();
+
+            // contractArtifact.abi
+            const contractABI = [{"type":"constructor","inputs":[{"name":"denom_","type":"string","internalType":"string"},{"name":"name_","type":"string","internalType":"string"},{"name":"symbol_","type":"string","internalType":"string"},{"name":"decimals_","type":"uint8","internalType":"uint8"}],"stateMutability":"nonpayable"},{"type":"function","name":"BankPrecompile","inputs":[],"outputs":[{"name":"","type":"address","internalType":"contract IBank"}],"stateMutability":"view"},{"type":"function","name":"allowance","inputs":[{"name":"owner","type":"address","internalType":"address"},{"name":"spender","type":"address","internalType":"address"}],"outputs":[{"name":"","type":"uint256","internalType":"uint256"}],"stateMutability":"view"},{"type":"function","name":"approve","inputs":[{"name":"spender","type":"address","internalType":"address"},{"name":"value","type":"uint256","internalType":"uint256"}],"outputs":[{"name":"","type":"bool","internalType":"bool"}],"stateMutability":"nonpayable"},{"type":"function","name":"balanceOf","inputs":[{"name":"account","type":"address","internalType":"address"}],"outputs":[{"name":"","type":"uint256","internalType":"uint256"}],"stateMutability":"view"},{"type":"function","name":"ddecimals","inputs":[],"outputs":[{"name":"","type":"uint8","internalType":"uint8"}],"stateMutability":"view"},{"type":"function","name":"decimals","inputs":[],"outputs":[{"name":"","type":"uint8","internalType":"uint8"}],"stateMutability":"view"},{"type":"function","name":"denom","inputs":[],"outputs":[{"name":"","type":"string","internalType":"string"}],"stateMutability":"view"},{"type":"function","name":"name","inputs":[],"outputs":[{"name":"","type":"string","internalType":"string"}],"stateMutability":"view"},{"type":"function","name":"nname","inputs":[],"outputs":[{"name":"","type":"string","internalType":"string"}],"stateMutability":"view"},{"type":"function","name":"ssymbol","inputs":[],"outputs":[{"name":"","type":"string","internalType":"string"}],"stateMutability":"view"},{"type":"function","name":"symbol","inputs":[],"outputs":[{"name":"","type":"string","internalType":"string"}],"stateMutability":"view"},{"type":"function","name":"totalSupply","inputs":[],"outputs":[{"name":"","type":"uint256","internalType":"uint256"}],"stateMutability":"view"},{"type":"function","name":"transfer","inputs":[{"name":"to","type":"address","internalType":"address"},{"name":"value","type":"uint256","internalType":"uint256"}],"outputs":[{"name":"","type":"bool","internalType":"bool"}],"stateMutability":"nonpayable"},{"type":"function","name":"transferFrom","inputs":[{"name":"from","type":"address","internalType":"address"},{"name":"to","type":"address","internalType":"address"},{"name":"value","type":"uint256","internalType":"uint256"}],"outputs":[{"name":"","type":"bool","internalType":"bool"}],"stateMutability":"nonpayable"},{"type":"event","name":"Approval","inputs":[{"name":"owner","type":"address","indexed":true,"internalType":"address"},{"name":"spender","type":"address","indexed":true,"internalType":"address"},{"name":"value","type":"uint256","indexed":false,"internalType":"uint256"}],"anonymous":false},{"type":"event","name":"Transfer","inputs":[{"name":"from","type":"address","indexed":true,"internalType":"address"},{"name":"to","type":"address","indexed":true,"internalType":"address"},{"name":"value","type":"uint256","indexed":false,"internalType":"uint256"}],"anonymous":false},{"type":"error","name":"ERC20InsufficientAllowance","inputs":[{"name":"spender","type":"address","internalType":"address"},{"name":"allowance","type":"uint256","internalType":"uint256"},{"name":"needed","type":"uint256","internalType":"uint256"}]},{"type":"error","name":"ERC20InsufficientBalance","inputs":[{"name":"sender","type":"address","internalType":"address"},{"name":"balance","type":"uint256","internalType":"uint256"},{"name":"needed","type":"uint256","internalType":"uint256"}]},{"type":"error","name":"ERC20InvalidApprover","inputs":[{"name":"approver","type":"address","internalType":"address"}]},{"type":"error","name":"ERC20InvalidReceiver","inputs":[{"name":"receiver","type":"address","internalType":"address"}]},{"type":"error","name":"ERC20InvalidSender","inputs":[{"name":"sender","type":"address","internalType":"address"}]},{"type":"error","name":"ERC20InvalidSpender","inputs":[{"name":"spender","type":"address","internalType":"address"}]}];
+
+            // Get a contract instance
+            erc20 = new ethers.Contract(contractAddress, contractABI, signer);
+            console.log("end of before");
+        });
+
+        it("Transfer function", async function() {
+            const receiver = '0x70997970C51812dc3A010C7d01b50e0d17dc79C8';
+            const beforeBalance = await erc20.balanceOf(owner);
+            const tx = await erc20.transfer(receiver, 1);
+            const receipt = await tx.wait();
+            expect(receipt.status).to.equal(1);
+            const afterBalance = await erc20.balanceOf(owner);
+            const diff = beforeBalance - afterBalance;
+            expect(diff).to.equal(1);
+        });
+
+        it("Approve and TransferFrom functions", async function() {
+            const receiver = '0x70997970C51812dc3A010C7d01b50e0d17dc79C8';
+            // lets have owner approve the transfer and have owner2 do the transferring
+            const approvalAmount = await erc20.allowance(owner, owner2);
+            expect(approvalAmount).to.equal(0);
+            const approveTx = await erc20.approve(owner2, 100);
+            const approveReceipt = await approveTx.wait();
+            expect(approveReceipt.status).to.equal(1);
+            expect(await erc20.allowance(owner, owner2)).to.equal(100);
+
+            // transfer from owner to owner2
+            const balanceBefore = await erc20.balanceOf(receiver);
+            const transferFromTx = await erc20.transferFrom(owner, receiver, 100, {from: owner2});
+
+            console.log("transferFromTx = ", transferFromTx);
+            // await sleep(3000);
+            const transferFromReceipt = await transferFromTx.wait();
+            expect(transferFromReceipt.status).to.equal(1);
+            const balanceAfter = await erc20.balanceOf(receiver);
+            const diff = balanceAfter - balanceBefore;
+            expect(diff).to.equal(100);
+        });
+
+        it("Balance of function", async function() {
+            const balance = await erc20.balanceOf(owner);
+            expect(balance).to.be.greaterThan(Number(0));
+        });
+
+        it("Name function", async function () {
+            const name = await erc20.name()
+            expect(name).to.equal('UATOM');
+        });
+
+        it("Symbol function", async function () {
+            const symbol = await erc20.symbol()
+            // expect symbol to be 'UATOM'
+            expect(symbol).to.equal('UATOM');
+        });
+    });
+});
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function runSetupScript(setupScriptPath) {
+    let contractAddress;
+    try {
+        const output = execSync('bash ' + setupScriptPath).toString();
+
+        // Parse the output to find the contract address
+        console.log("output:", output);
+        const match = output.match(/ERC20_DEPLOY_ADDR=(\S+)/);
+        contractAddress = match ? match[1] : undefined;
+
+        if (contractAddress === undefined) {
+            console.error("contractAddress not found in script output");
+        } else {
+            console.log("EVMPrecompileTester setup complete with contractAddress:", contractAddress);
+        }
+    } catch (error) {
+        console.error(`Error executing bash script: ${error}`);
+    }
+    return contractAddress;
+}
---
+++ contracts/test/deploy_atom_erc20.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# This script is used to deploy the UATOM ERC20 contract and associate it with the SEI account.
+set -e
+
+endpoint=${EVM_RPC:-"http://127.0.0.1:8545"}
+owner1=0xF87A299e6bC7bEba58dbBe5a5Aa21d49bCD16D52
+associated_sei_account1=sei1m9qugvk4h66p6hunfajfg96ysc48zeq4m0d82c
+owner2=0x70997970C51812dc3A010C7d01b50e0d17dc79C8
+
+echo "Funding account $account with UATOM for testing..."
+seid tx bank send $(seid keys show admin -a) $associated_sei_account1 10000uatom --fees 20000usei -b block -y > /dev/null
+
+echo "Fund owners with some SEI"
+seid tx evm send $owner1 1000000000000000000 --from admin
+seid tx evm send $owner2 1000000000000000000 --from admin
+
+echo "Deploying ERC20 pointer contract for UATOM..."
+deployment_output=$(seid tx evm deploy-erc20 uatom UATOM UATOM 6 --from admin --evm-rpc=$endpoint)
+
+erc20_deploy_addr=$(echo "$deployment_output" | grep 'Deployed to:' | awk '{print $3}')
+echo "ERC20_DEPLOY_ADDR=$erc20_deploy_addr"
+
+# wait for deployment to finish on live chain
+sleep 3
