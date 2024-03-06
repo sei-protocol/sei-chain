c35a056834ddb8db1f3a99b853af33a17e5a3f9a
Philip Su <philip.su.522@gmail.com>
2024-03-06 15:48:26 -0800

    debug

--- loadtest/contracts/evm/src/ERC721.sol
+++ loadtest/contracts/evm/src/ERC721.sol
@@ -4,12 +4,5 @@ pragma solidity ^0.8.20;
 import "../lib/creator-token-contracts/contracts/erc721c/ERC721C.sol";
 
 contract MyNFT is ERC721C {
-    function mint(address to, uint id) external {
-        _mint(to, id);
-    }
 
-    function burn(uint id) external {
-        require(msg.sender == _ownerOf[id], "not owner");
-        _burn(id);
-    }
 }
