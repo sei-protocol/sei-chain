fc88668b8f2dae5c3058d526d317adbe4298104d
Philip Su <philip.su.522@gmail.com>
2024-03-06 15:35:34 -0800

    debug

--- loadtest/contracts/evm/src/ERC721.sol
+++ loadtest/contracts/evm/src/ERC721.sol
@@ -1,10 +1,15 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.20;
 
 import "../lib/creator-token-contracts/contracts/erc721c/ERC721C.sol";
 
-contract MyERC721C is ERC721C {
-    constructor(string memory name, string memory symbol) ERC721(name, symbol) {}
+contract MyNFT is ERC721C {
+    function mint(address to, uint id) external {
+        _mint(to, id);
+    }
 
-
+    function burn(uint id) external {
+        require(msg.sender == _ownerOf[id], "not owner");
+        _burn(id);
+    }
 }
