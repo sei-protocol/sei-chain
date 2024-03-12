15a54b03298fba1ca7d516938f6783b3b7e822be
Philip Su <philip.su.522@gmail.com>
2024-03-12 07:53:35 -0700

    Remove command output that breaks loadtest deploying (#1424)
    * Remove command output that breaks loadtest deploying
    
    * fund

--- loadtest/contracts/deploy_erc20.sh
+++ loadtest/contracts/deploy_erc20.sh
@@ -5,10 +5,17 @@
 
 evm_endpoint=$1
 
+# first fund account if necessary
+THRESHOLD=100000000000000000000 # 100 Eth
+ACCOUNT="0xF87A299e6bC7bEba58dbBe5a5Aa21d49bCD16D52"
+BALANCE=$(cast balance $ACCOUNT --rpc-url "$evm_endpoint")
+if (( $(echo "$BALANCE < $THRESHOLD" | bc -l) )); then
+  printf "12345678\n" | ~/go/bin/seid tx evm send $ACCOUNT 100000000000000000000 --from admin --evm-rpc "$evm_endpoint"
+fi
 cd loadtest/contracts/evm || exit 1
 
-./setup.sh
+./setup.sh > /dev/null
 
-git submodule update --init --recursive
+git submodule update --init --recursive > /dev/null
 
 /root/.foundry/bin/forge create -r "$evm_endpoint" --private-key 57acb95d82739866a5c29e40b0aa2590742ae50425b7dd5b5d279a986370189e src/NoopToken.sol:NoopToken --json --constructor-args "NoopToken" "NT" | jq -r '.deployedTo'
