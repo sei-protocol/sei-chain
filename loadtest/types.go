f423c8d03303b9648a0390e1122622e765fd2e39
Jeremy Wei <jeremy.t.wei@gmail.com>
2024-03-13 10:06:26 -0400

    [EVM] univ2 loadtest (#1423)
    * checkpoint
    
    * add univ2 contracts + deploy_uinv2.sh
    
    * able to deploy uniswapv2
    
    * Unifap -> Uniswap
    
    * univ2 set up
    
    * trying to do swaps
    
    * swaps work
    
    * swaps in both directions work
    
    * increase liquidity in pool
    
    * remove prints
    
    * revert changes to make loadtest work locally
    
    * add legacy to forge cmds
    
    * add print statement
    
    * debug
    
    * add UNIV2 msg type
    
    * turn off ensure tx success
    
    * comment out prints
    
    * print
    
    * print
    
    * cleanup
    
    * cleanup
    
    * print script output
    
    * debug
    
    * forge install + parallelize token mints and approvals
    
    * create intermediary univ2 swapper
    
    * cleanup + remove bindings for univ2 pair and router
    
    * cleanup
    
    * fix
    
    * cleanup
    
    * query wallet balance in script
    
    * appease linter

--- loadtest/types.go
+++ loadtest/types.go
@@ -32,6 +32,7 @@ const (
 	Limit                string = "limit"
 	Market               string = "market"
 	WasmMintNft          string = "wasm_mint_nft"
+	UNIV2                string = "univ2"
 	Vortex               string = "vortex"
 	WasmInstantiate      string = "wasm_instantiate"
 	WasmOccIteratorWrite string = "wasm_occ_iterator_write"
@@ -44,8 +45,9 @@ type WasmIteratorWriteMsg struct {
 }
 
 type EVMAddresses struct {
-	ERC20  common.Address
-	ERC721 common.Address
+	ERC20        common.Address
+	ERC721       common.Address
+	UniV2Swapper common.Address
 }
 
 type Config struct {
