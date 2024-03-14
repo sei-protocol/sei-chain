39720536a4f677dd7388a2ed27e5ff3cd4f027bd
kbhat1 <kartikbhatri@gmail.com>
2024-03-14 14:58:00 -0400

    tx wait

--- contracts/test/EVMPrecompileTester.js
+++ contracts/test/EVMPrecompileTester.js
@@ -43,7 +43,9 @@ describe("EVM Test", function () {
                 const receiver = '0x70997970C51812dc3A010C7d01b50e0d17dc79C8';
                 const afterBalance = await erc20.balanceOf(owner);
                 expect(afterBalance).to.equal(100);
-                await expect(erc20.transfer(receiver, 10000)).to.be.reverted;
+                const tx = await erc20.transfer(receiver, 10000);
+                const receipt = await tx.wait();
+                expect(receipt.status).to.equal(1);
             });
 
             it("No Approve and TransferFrom fails", async function() {
