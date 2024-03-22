36500d2ff3dbd80903b8afdb11ceb5374b0a0358
codchen <codchen03@gmail.com>
2024-03-22 13:12:49 +0800

    Account for withdrawals in replay (#1449)
    * Account for withdrawals in replay
    
    * cancun prompt

--- app/eth_replay.go
+++ app/eth_replay.go
@@ -10,7 +10,9 @@ import (
 
 	"github.com/cosmos/cosmos-sdk/client"
 	ethtypes "github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/params"
 	"github.com/sei-protocol/sei-chain/utils"
+	"github.com/sei-protocol/sei-chain/x/evm/state"
 	evmtypes "github.com/sei-protocol/sei-chain/x/evm/types"
 	"github.com/sei-protocol/sei-chain/x/evm/types/ethtx"
 	abci "github.com/tendermint/tendermint/abci/types"
@@ -48,6 +50,13 @@ func Replay(a *App) {
 			continue
 		}
 		a.Logger().Info(fmt.Sprintf("Replaying block height %d", h+initHeight))
+		if h+initHeight >= 19426587 && evmtypes.DefaultChainConfig().CancunTime < 0 {
+			a.Logger().Error("Reaching Cancun upgrade height. Turn on Cancun by setting CancunTime in x/evm/types/config.go:DefaultChainConfig() to 0")
+			break
+		} else if h+initHeight < 19426587 && evmtypes.DefaultChainConfig().CancunTime >= 0 {
+			a.Logger().Error("Haven't reached Cancun upgrade height. Turn off Cancun by setting CancunTime in x/evm/types/config.go:DefaultChainConfig() to -1")
+			break
+		}
 		b, err := a.EvmKeeper.EthClient.BlockByNumber(context.Background(), big.NewInt(h+initHeight))
 		if err != nil {
 			panic(err)
@@ -65,6 +74,13 @@ func Replay(a *App) {
 			panic(err)
 		}
 		ctx := a.GetContextForDeliverTx([]byte{})
+		s := state.NewDBImpl(ctx, &a.EvmKeeper, false)
+		for _, w := range b.Withdrawals() {
+			amount := new(big.Int).SetUint64(w.Amount)
+			amount = amount.Mul(amount, big.NewInt(params.GWei))
+			s.AddBalance(w.Address, amount)
+		}
+		_, _ = s.Finalize()
 		for _, tx := range b.Txs {
 			a.Logger().Info(fmt.Sprintf("Verifying tx %s", tx.Hash().Hex()))
 			if tx.To() != nil {
