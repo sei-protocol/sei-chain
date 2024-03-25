2fb242176fc87a8cc8563fd797fe58402f40ec34
Jeremy Wei <jeremy.t.wei@gmail.com>
2024-03-25 15:03:15 -0400

    remove blocktest evm keeper prefixes

--- x/evm/types/keys.go
+++ x/evm/types/keys.go
@@ -42,12 +42,9 @@ var (
 	TxHashPrefix  = []byte{0x10}
 	TxBloomPrefix = []byte{0x11}
 
-	ReplaySeenAddrPrefix    = []byte{0x12}
-	ReplayedHeight          = []byte{0x13}
-	ReplayInitialHeight     = []byte{0x14}
-	BlockTestSeenAddrPrefix = []byte{0x15}
-	BlockTestHeight         = []byte{0x16}
-	BlockTestInitialHeight  = []byte{0x17}
+	ReplaySeenAddrPrefix = []byte{0x12}
+	ReplayedHeight       = []byte{0x13}
+	ReplayInitialHeight  = []byte{0x14}
 )
 
 func EVMAddressToSeiAddressKey(evmAddress common.Address) []byte {
