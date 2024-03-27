15c7f7214b49a551d0060b8e8cd07d9def53158f
Jeremy Wei <jeremy.t.wei@gmail.com>
2024-03-26 23:49:45 -0400

    add run_blocktests script

---
+++ run_blocktests.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+
+block_tests_dir=$1
+
+# Define an array of tests to skip
+declare -a skip_list=(
+    "shanghaiExample" # failing
+    "blockWithAllTransactionTypes" # failing
+    "DelegateCallSpam" # takes super long
+    "blockhashTests" # failing
+    "blockhashNonConstArg" # failing
+)
+
+# list out all paths to json files starting from the block_tests_dir
+block_tests=$(find "$block_tests_dir" -name "*.json")
+
+# for each json file, run the block test
+for test_file in $block_tests; do
+    test_name=$(basename "$test_file" .json)
+
+    # Check if the test name is in the skip list
+    if printf '%s\n' "${skip_list[@]}" | grep -qx "$test_name"; then
+        echo "Skipping test: $test_file"
+        continue
+    fi
+
+    # Check if "${test_name}_Cancun" is not in the test file
+    if ! grep -q "${test_name}_Cancun" "$test_file"; then
+        echo "Skipping test due to missing Cancun tag: $test_file"
+        continue
+    fi
+
+    echo -e "\n*********************************************************\n"
+    echo "Running block test: $test_file"
+    echo "test name: ${test_name}_Cancun"
+    echo -e "\n*********************************************************\n"
+    rm -r ~/.sei || true
+    ./scripts/initialize_local_chain_no_run.sh 2>&1 | tee /dev/null
+    seid blocktest --block-test $test_file --test-name "${test_name}_Cancun"
+done
