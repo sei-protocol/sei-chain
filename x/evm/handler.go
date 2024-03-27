ae864740fa51446242af28d82929acd88795e694
codchen <codchen03@gmail.com>
2024-03-27 12:59:22 +0800

    Add cosmos send to EVM addr transaction type (#1470)

--- x/evm/handler.go
+++ x/evm/handler.go
@@ -20,6 +20,9 @@ func NewHandler(k *keeper.Keeper) sdk.Handler {
 		case *types.MsgEVMTransaction:
 			res, err := msgServer.EVMTransaction(sdk.WrapSDKContext(ctx), msg)
 			return sdk.WrapServiceResult(ctx, res, err)
+		case *types.MsgSend:
+			res, err := msgServer.Send(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
 		default:
 			errMsg := fmt.Sprintf("unrecognized %s message type: %T", types.ModuleName, msg)
 			return nil, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, errMsg)
