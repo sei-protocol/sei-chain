37641228ba03f67a641f6663bc16081c37d4c899
Jeremy Wei <jeremy.t.wei@gmail.com>
2024-03-28 18:00:53 -0400

    Revert "Revert "add prints""
    This reverts commit 98ea147eb8e6107f2ebdb90f8028d5f48c73de8e.

--- app/app.go
+++ app/app.go
@@ -1473,6 +1473,9 @@ func (app *App) BuildDependenciesAndRunTxs(ctx sdk.Context, txs [][]byte, typedT
 }
 
 func (app *App) ProcessBlock(ctx sdk.Context, txs [][]byte, req BlockProcessRequest, lastCommit abci.CommitInfo) ([]abci.Event, []*abci.ExecTxResult, abci.ResponseEndBlock, error) {
+	fmt.Println("****************************")
+	fmt.Println("In ProcessBlock")
+	fmt.Println("****************************")
 	ctx = ctx.WithIsOCCEnabled(app.OccEnabled())
 	goCtx := app.decorateContextWithDexMemState(ctx.Context())
 	ctx = ctx.WithContext(goCtx)
