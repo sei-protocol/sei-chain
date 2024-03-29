0d7d74dfadad7c6a4feec958efe192056c9e5115
Matthieu Vachon <matthieu.o.vachon@gmail.com>
2024-03-29 09:17:35 -0400

    Update that brings `go-ethereum` with live tracer support (#1375)
    * Update that brings `go-ethereum` with live tracer support
    
    This only brings the dependency (will need to wait for a merge + tag of the branch before merging this PR) and the required changes to adapt `DBImpl` to support the new interface.
    
    Relates to:
    - https://github.com/sei-protocol/go-ethereum/pull/15
    - https://github.com/sei-protocol/sei-chain/pull/1344
    
    * Added missing instrumentation when state changes in `DBImpl`

--- x/evm/state/balance_test.go
+++ x/evm/state/balance_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/ethereum/go-ethereum/core/tracing"
 	testkeeper "github.com/sei-protocol/sei-chain/testutil/keeper"
 	"github.com/sei-protocol/sei-chain/x/evm/state"
 	"github.com/sei-protocol/sei-chain/x/evm/types"
@@ -16,17 +17,17 @@ func TestAddBalance(t *testing.T) {
 	db := state.NewDBImpl(ctx, k, false)
 	seiAddr, evmAddr := testkeeper.MockAddressPair()
 	require.Equal(t, big.NewInt(0), db.GetBalance(evmAddr))
-	db.AddBalance(evmAddr, big.NewInt(0))
+	db.AddBalance(evmAddr, big.NewInt(0), tracing.BalanceChangeUnspecified)
 
 	// set association
 	k.SetAddressMapping(db.Ctx(), seiAddr, evmAddr)
 	require.Equal(t, big.NewInt(0), db.GetBalance(evmAddr))
-	db.AddBalance(evmAddr, big.NewInt(10000000000000))
+	db.AddBalance(evmAddr, big.NewInt(10000000000000), tracing.BalanceChangeUnspecified)
 	require.Nil(t, db.Err())
 	require.Equal(t, db.GetBalance(evmAddr), big.NewInt(10000000000000))
 
 	_, evmAddr2 := testkeeper.MockAddressPair()
-	db.SubBalance(evmAddr2, big.NewInt(-5000000000000)) // should redirect to AddBalance
+	db.SubBalance(evmAddr2, big.NewInt(-5000000000000), tracing.BalanceChangeUnspecified) // should redirect to AddBalance
 	require.Nil(t, db.Err())
 	require.Equal(t, db.GetBalance(evmAddr), big.NewInt(10000000000000))
 	require.Equal(t, db.GetBalance(evmAddr2), big.NewInt(5000000000000))
@@ -37,7 +38,7 @@ func TestSubBalance(t *testing.T) {
 	db := state.NewDBImpl(ctx, k, false)
 	seiAddr, evmAddr := testkeeper.MockAddressPair()
 	require.Equal(t, big.NewInt(0), db.GetBalance(evmAddr))
-	db.SubBalance(evmAddr, big.NewInt(0))
+	db.SubBalance(evmAddr, big.NewInt(0), tracing.BalanceChangeUnspecified)
 
 	// set association
 	k.SetAddressMapping(db.Ctx(), seiAddr, evmAddr)
@@ -45,7 +46,7 @@ func TestSubBalance(t *testing.T) {
 	amt := sdk.NewCoins(sdk.NewCoin(k.GetBaseDenom(ctx), sdk.NewInt(20)))
 	k.BankKeeper().MintCoins(db.Ctx(), types.ModuleName, amt)
 	k.BankKeeper().SendCoinsFromModuleToAccount(db.Ctx(), types.ModuleName, seiAddr, amt)
-	db.SubBalance(evmAddr, big.NewInt(10000000000000))
+	db.SubBalance(evmAddr, big.NewInt(10000000000000), tracing.BalanceChangeUnspecified)
 	require.Nil(t, db.Err())
 	require.Equal(t, db.GetBalance(evmAddr), big.NewInt(10000000000000))
 
@@ -53,13 +54,13 @@ func TestSubBalance(t *testing.T) {
 	amt = sdk.NewCoins(sdk.NewCoin(k.GetBaseDenom(ctx), sdk.NewInt(10)))
 	k.BankKeeper().MintCoins(db.Ctx(), types.ModuleName, amt)
 	k.BankKeeper().SendCoinsFromModuleToAccount(db.Ctx(), types.ModuleName, sdk.AccAddress(evmAddr2[:]), amt)
-	db.AddBalance(evmAddr2, big.NewInt(-5000000000000)) // should redirect to SubBalance
+	db.AddBalance(evmAddr2, big.NewInt(-5000000000000), tracing.BalanceChangeUnspecified) // should redirect to SubBalance
 	require.Nil(t, db.Err())
 	require.Equal(t, db.GetBalance(evmAddr), big.NewInt(10000000000000))
 	require.Equal(t, db.GetBalance(evmAddr2), big.NewInt(5000000000000))
 
 	// insufficient balance
-	db.SubBalance(evmAddr2, big.NewInt(10000000000000))
+	db.SubBalance(evmAddr2, big.NewInt(10000000000000), tracing.BalanceChangeUnspecified)
 	require.NotNil(t, db.Err())
 }
 
@@ -67,12 +68,12 @@ func TestSetBalance(t *testing.T) {
 	k, ctx := testkeeper.MockEVMKeeper()
 	db := state.NewDBImpl(ctx, k, true)
 	_, evmAddr := testkeeper.MockAddressPair()
-	db.SetBalance(evmAddr, big.NewInt(10000000000000))
+	db.SetBalance(evmAddr, big.NewInt(10000000000000), tracing.BalanceChangeUnspecified)
 	require.Equal(t, big.NewInt(10000000000000), db.GetBalance(evmAddr))
 
 	seiAddr2, evmAddr2 := testkeeper.MockAddressPair()
 	k.SetAddressMapping(db.Ctx(), seiAddr2, evmAddr2)
-	db.SetBalance(evmAddr2, big.NewInt(10000000000000))
+	db.SetBalance(evmAddr2, big.NewInt(10000000000000), tracing.BalanceChangeUnspecified)
 	require.Equal(t, big.NewInt(10000000000000), db.GetBalance(evmAddr2))
 }
 
@@ -82,55 +83,55 @@ func TestSurplus(t *testing.T) {
 
 	// test negative usei surplus negative wei surplus
 	db := state.NewDBImpl(ctx, k, false)
-	db.AddBalance(evmAddr, big.NewInt(1_000_000_000_001))
+	db.AddBalance(evmAddr, big.NewInt(1_000_000_000_001), tracing.BalanceChangeUnspecified)
 	_, err := db.Finalize()
 	require.NotNil(t, err)
 	require.Contains(t, err.Error(), "negative surplus value")
 
 	// test negative usei surplus positive wei surplus (negative total)
 	db = state.NewDBImpl(ctx, k, false)
-	db.AddBalance(evmAddr, big.NewInt(1_000_000_000_000))
-	db.SubBalance(evmAddr, big.NewInt(1))
+	db.AddBalance(evmAddr, big.NewInt(1_000_000_000_000), tracing.BalanceChangeUnspecified)
+	db.SubBalance(evmAddr, big.NewInt(1), tracing.BalanceChangeUnspecified)
 	_, err = db.Finalize()
 	require.NotNil(t, err)
 	require.Contains(t, err.Error(), "negative surplus value")
 
 	// test negative usei surplus positive wei surplus (positive total)
 	db = state.NewDBImpl(ctx, k, false)
-	db.AddBalance(evmAddr, big.NewInt(1_000_000_000_000))
-	db.SubBalance(evmAddr, big.NewInt(2))
-	db.SubBalance(evmAddr, big.NewInt(999_999_999_999))
+	db.AddBalance(evmAddr, big.NewInt(1_000_000_000_000), tracing.BalanceChangeUnspecified)
+	db.SubBalance(evmAddr, big.NewInt(2), tracing.BalanceChangeUnspecified)
+	db.SubBalance(evmAddr, big.NewInt(999_999_999_999), tracing.BalanceChangeUnspecified)
 	surplus, err := db.Finalize()
 	require.Nil(t, err)
 	require.Equal(t, sdk.OneInt(), surplus)
 
 	// test positive usei surplus negative wei surplus (negative total)
 	db = state.NewDBImpl(ctx, k, false)
-	db.SubBalance(evmAddr, big.NewInt(1_000_000_000_000))
-	db.AddBalance(evmAddr, big.NewInt(2))
-	db.AddBalance(evmAddr, big.NewInt(999_999_999_999))
+	db.SubBalance(evmAddr, big.NewInt(1_000_000_000_000), tracing.BalanceChangeUnspecified)
+	db.AddBalance(evmAddr, big.NewInt(2), tracing.BalanceChangeUnspecified)
+	db.AddBalance(evmAddr, big.NewInt(999_999_999_999), tracing.BalanceChangeUnspecified)
 	_, err = db.Finalize()
 	require.NotNil(t, err)
 	require.Contains(t, err.Error(), "negative surplus value")
 
 	// test positive usei surplus negative wei surplus (positive total)
 	db = state.NewDBImpl(ctx, k, false)
-	db.SubBalance(evmAddr, big.NewInt(1_000_000_000_000))
-	db.AddBalance(evmAddr, big.NewInt(999_999_999_999))
+	db.SubBalance(evmAddr, big.NewInt(1_000_000_000_000), tracing.BalanceChangeUnspecified)
+	db.AddBalance(evmAddr, big.NewInt(999_999_999_999), tracing.BalanceChangeUnspecified)
 	surplus, err = db.Finalize()
 	require.Nil(t, err)
 	require.Equal(t, sdk.OneInt(), surplus)
 
 	// test snapshots
 	db = state.NewDBImpl(ctx, k, false)
-	db.SubBalance(evmAddr, big.NewInt(1_000_000_000_000))
-	db.AddBalance(evmAddr, big.NewInt(999_999_999_999))
+	db.SubBalance(evmAddr, big.NewInt(1_000_000_000_000), tracing.BalanceChangeUnspecified)
+	db.AddBalance(evmAddr, big.NewInt(999_999_999_999), tracing.BalanceChangeUnspecified)
 	db.Snapshot()
-	db.SubBalance(evmAddr, big.NewInt(1_000_000_000_000))
-	db.AddBalance(evmAddr, big.NewInt(999_999_999_999))
+	db.SubBalance(evmAddr, big.NewInt(1_000_000_000_000), tracing.BalanceChangeUnspecified)
+	db.AddBalance(evmAddr, big.NewInt(999_999_999_999), tracing.BalanceChangeUnspecified)
 	db.Snapshot()
-	db.SubBalance(evmAddr, big.NewInt(1_000_000_000_000))
-	db.AddBalance(evmAddr, big.NewInt(999_999_999_999))
+	db.SubBalance(evmAddr, big.NewInt(1_000_000_000_000), tracing.BalanceChangeUnspecified)
+	db.AddBalance(evmAddr, big.NewInt(999_999_999_999), tracing.BalanceChangeUnspecified)
 	surplus, err = db.Finalize()
 	require.Nil(t, err)
 	require.Equal(t, sdk.NewInt(3), surplus)
