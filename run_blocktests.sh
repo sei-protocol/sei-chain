60c592caee3da2b41ea511ff9132e87c00b0afe6
Uday Patil <udpatil@gmail.com>
2024-04-01 07:37:34 -0700

    Eth blocktests ci (#1495)
    Refactor EVM blocktests to run partitioned for faster CI performance

--- run_blocktests.sh
+++ run_blocktests.sh
@@ -2,7 +2,18 @@
 
 set -e
 
-block_tests_dir=$1
+# mode options are list, run, or all
+block_tests_path=$1
+runner_index=$2
+runner_total=$3
+
+if [ -z "$runner_index" ]; then
+    runner_index=0
+    runner_total=1
+fi
+
+echo $mode
+echo $block_tests_path
 
 # Define an array of tests to skip
 declare -a skip_list=(
@@ -14,7 +25,11 @@ declare -a skip_list=(
 )
 
 # list out all paths to json files starting from the block_tests_dir
-block_tests=$(find "$block_tests_dir" -name "*.json")
+block_tests=$(find "$block_tests_path" -name "*.json")
+
+test_files=""
+
+i=0
 
 # for each json file, run the block test
 for test_file in $block_tests; do
@@ -31,6 +46,13 @@ for test_file in $block_tests; do
         echo "Skipping test due to missing Cancun tag: $test_file"
         continue
     fi
+
+    if [ $((i % runner_total)) -ne $runner_index ]; then
+        i=$((i+1))
+        continue
+    fi
+
+    i=$((i+1))
 
     echo -e "\n*********************************************************\n"
     echo "Running block test: $test_file"
