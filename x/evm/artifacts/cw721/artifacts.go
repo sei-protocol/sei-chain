f101bfba976ac8f1d046f3e09f670b88546eb78f
codchen <codchen03@gmail.com>
2024-04-08 22:23:12 +0800

    Require `from` in TransferFrom in CW721 pointer to be the owner (#1516)
    * Require `from` in TransferFrom in CW721 pointer to be the owner
    
    * test

--- x/evm/artifacts/cw721/artifacts.go
+++ x/evm/artifacts/cw721/artifacts.go
@@ -10,7 +10,7 @@ import (
 	"github.com/ethereum/go-ethereum/accounts/abi"
 )
 
-const CurrentVersion uint16 = 1
+const CurrentVersion uint16 = 2
 
 //go:embed CW721ERC721Pointer.abi
 //go:embed CW721ERC721Pointer.bin
