cbb930630d6a87152344bf7be30d847161bdf058
codchen <codchen03@gmail.com>
2024-04-11 11:24:53 +0800

    Add mechanism for benchmarking EVM queries (#1530)
    * Add mechanism for benchmarking EVM queries
    
    * allow ticks to be specified

--- loadtest/loadtest_client.go
+++ loadtest/loadtest_client.go
@@ -202,6 +202,7 @@ func (c *LoadTestClient) BuildTxs(
 			switch messageType {
 			case EVM, ERC20, ERC721, UNIV2:
 				signedTx = SignedTx{EvmTx: c.generateSignedEvmTx(keyIndex, messageType), MsgType: messageType}
+				EvmTxHashes = append(EvmTxHashes, signedTx.EvmTx.Hash())
 			default:
 				msgTypeCount := atomic.LoadInt64(producedCountPerMsgType[messageType])
 				signedTx = SignedTx{TxBytes: c.generateSignedCosmosTxs(keyIndex, messageType, msgTypeCount), MsgType: messageType}
