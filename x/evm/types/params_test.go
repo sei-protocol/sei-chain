f0cd7d7c654565a737e409e674f28ebfa95217c0
Philip Su <philip.su.522@gmail.com>
2024-04-12 10:54:08 -0700

    Add x/evm/types unit tests (#1537)

--- x/evm/types/params_test.go
+++ x/evm/types/params_test.go
@@ -1,6 +1,7 @@
 package types_test
 
 import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"testing"
 
 	"github.com/sei-protocol/sei-chain/x/evm/types"
@@ -17,4 +18,40 @@ func TestDefaultParams(t *testing.T) {
 	}, types.DefaultParams())
 
 	require.Nil(t, types.DefaultParams().Validate())
+}
+
+func TestValidateParamsInvalidPriorityNormalizer(t *testing.T) {
+	params := types.DefaultParams()
+	params.PriorityNormalizer = sdk.NewDec(-1) // Set to invalid negative value
+
+	err := params.Validate()
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "nonpositive priority normalizer")
+}
+
+func TestValidateParamsNegativeBaseFeePerGas(t *testing.T) {
+	params := types.DefaultParams()
+	params.BaseFeePerGas = sdk.NewDec(-1) // Set to invalid negative value
+
+	err := params.Validate()
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "negative base fee per gas")
+}
+
+func TestValidateParamsNegativeChainID(t *testing.T) {
+	params := types.DefaultParams()
+	params.ChainId = sdk.NewInt(-1) // Set to invalid negative value
+
+	err := params.Validate()
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "negative chain ID")
+}
+
+func TestBaseFeeMinimumFee(t *testing.T) {
+	params := types.DefaultParams()
+	params.MinimumFeePerGas = sdk.NewDec(1)
+	params.BaseFeePerGas = params.MinimumFeePerGas.Add(sdk.NewDec(1))
+	err := params.Validate()
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "minimum fee cannot be lower than base fee")
 }
