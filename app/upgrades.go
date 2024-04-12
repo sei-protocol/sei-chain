b8511418e38ac0aef8ff070d6e807c69d9f9f062
Philip Su <philip.su.522@gmail.com>
2024-04-12 08:43:07 -0700

    V4.1.8 evm devnet release 2 (#1517)
    Add upgrade handler for new version

--- app/upgrades.go
+++ app/upgrades.go
@@ -87,6 +87,7 @@ var upgradesList = []string{
 	"v4.1.5-evm-devnet",
 	"v4.1.6-evm-devnet",
 	"v4.1.7-evm-devnet",
+	"v4.1.8-evm-devnet",
 }
 
 // if there is an override list, use that instead, for integration tests
