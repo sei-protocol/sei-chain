f24ecfa499b8dbc23fa96ed3998087cb4d31ae4a
Jeremy Wei <jeremy.t.wei@gmail.com>
2024-04-18 14:42:55 -0400

    very simplified contract

--- x/evm/client/wasm/query.go
+++ x/evm/client/wasm/query.go
@@ -9,7 +9,7 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/sei-protocol/sei-chain/example/contracts/erc20"
+	// "github.com/sei-protocol/sei-chain/example/contracts/erc20"
 	"github.com/sei-protocol/sei-chain/x/evm/artifacts/cw721"
 	"github.com/sei-protocol/sei-chain/x/evm/artifacts/native"
 	"github.com/sei-protocol/sei-chain/x/evm/client/wasm/bindings"
@@ -75,8 +75,8 @@ func (h *EVMQueryHandler) HandleERC20TokenInfo(ctx sdk.Context, contractAddress 
 		return nil, err
 	}
 	contract := common.HexToAddress(contractAddress)
-	// abi, err := native.NativeMetaData.GetAbi()
-	abi, err := erc20.Erc20MetaData.GetAbi()
+	abi, err := native.NativeMetaData.GetAbi()
+	// abi, err := erc20.Erc20MetaData.GetAbi()
 	if err != nil {
 		return nil, err
 	}
@@ -88,15 +88,16 @@ func (h *EVMQueryHandler) HandleERC20TokenInfo(ctx sdk.Context, contractAddress 
 	}
 	res, err := h.k.StaticCallEVM(ctx, callerAddr, &contract, bz)
 	if err != nil {
-		fmt.Println("static call error = ", err)
 		return nil, err
 	}
+	fmt.Println("got res = ", string(res))
 	unpacked, err := abi.Unpack("totalSupply", res)
 	if err != nil {
 		fmt.Println("unpacking totalSupply error = ", err)
 		return nil, err
 	}
 	totalSupply := sdk.NewIntFromBigInt(unpacked[0].(*big.Int))
+	fmt.Println("totalSupply = ", totalSupply)
 	response.TotalSupply = &totalSupply
 
 	bz, err = abi.Pack("name")
