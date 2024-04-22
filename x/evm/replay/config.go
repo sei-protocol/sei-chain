6db285a212fed1fe5123ec50575ff0248e9e482c
Jeremy Wei <jeremy.t.wei@gmail.com>
2024-04-22 14:31:23 -0400

    eth replay - add flag to disable contract state checks

--- x/evm/replay/config.go
+++ x/evm/replay/config.go
@@ -9,18 +9,21 @@ type Config struct {
 	Enabled    bool   `mapstructure:"eth_replay_enabled"`
 	EthRPC     string `mapstructure:"eth_rpc"`
 	EthDataDir string `mapstructure:"eth_data_dir"`
+	ContractStateChecks bool `mapstructure:"contract_state_checks"`
 }
 
 var DefaultConfig = Config{
 	Enabled:    false,
 	EthRPC:     "http://44.234.105.54:18545",
 	EthDataDir: "/root/.ethereum/chaindata",
+	ContractStateChecks: false,
 }
 
 const (
 	flagEnabled    = "eth_replay.eth_replay_enabled"
 	flagEthRPC     = "eth_replay.eth_rpc"
 	flagEthDataDir = "eth_replay.eth_data_dir"
+	flagContractStateChecks = "eth_replay.contract_state_checks"
 )
 
 func ReadConfig(opts servertypes.AppOptions) (Config, error) {
@@ -38,6 +41,11 @@ func ReadConfig(opts servertypes.AppOptions) (Config, error) {
 	}
 	if v := opts.Get(flagEthDataDir); v != nil {
 		if cfg.EthDataDir, err = cast.ToStringE(v); err != nil {
+			return cfg, err
+		}
+	}
+	if v := opts.Get(flagContractStateChecks); v != nil {
+		if cfg.ContractStateChecks, err = cast.ToBoolE(v); err != nil {
 			return cfg, err
 		}
 	}
