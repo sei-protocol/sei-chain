847db84cfeecdba14f91a6123eecd8d76c39bb70
Philip Su <philip.su.522@gmail.com>
2024-04-30 21:12:24 -0700

    lint

--- x/evm/client/cli/tx.go
+++ x/evm/client/cli/tx.go
@@ -375,7 +375,7 @@ func CmdERC20Send() *cobra.Command {
 			recipient := common.HexToAddress(args[1])
 			amt, ok := new(big.Int).SetString(args[2], 10)
 			if !ok {
-				return errors.New(fmt.Sprintf("Unable to parse amount: %s\n", args[2]))
+				return fmt.Errorf("Unable to parse amount: %s\n", args[2])
 			}
 			abi, err := native.NativeMetaData.GetAbi()
 			if err != nil {
