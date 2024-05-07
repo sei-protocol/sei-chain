abd51ea0a9202ee73e9ef9db1e507b58cf979b81
Jeremy Wei <jeremy.t.wei@gmail.com>
2024-05-07 17:18:47 -0400

    unassociated addrs tests

--- contracts/test/ERC20toCW20PointerTest.js
+++ contracts/test/ERC20toCW20PointerTest.js
@@ -96,9 +96,21 @@ describe("ERC20 to CW20 Pointer", function () {
             await cleanupTx.wait();
         });
 
-        it("should fail transfer() if sender has insufficient balance", async function () {
-            let recipient = accounts[1];
-            await expect(pointer.transfer(recipient.evmAddress, 20000000)).to.be.revertedWith("CosmWasm execute failed");
+        it.only("should fail transfer() if sender has insufficient balance", async function () {
+            const recipient = accounts[1];
+            await expect(pointer.transfer(recipient.evmAddress, 20000000)).to.be.reverted;
+            // await expect(pointer.transfer(recipient.evmAddress, 20000000)).to.be.revertedWith("execution reverted");
+        });
+
+        it.only("transfer to unassociated address should fail", async function() {
+            const unassociatedRecipient = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
+            await expect(pointer.transfer(unassociatedRecipient, 1)).to.be.revertedWith("execution reverted");
+        });
+
+        it.only("transfer to contract address should fail since unassociated", async function() {
+            const contract = await pointer.getAddress();
+            console.log("contract = ", contract);
+            await expect(pointer.transfer(contract, 1)).to.be.revertedWith("execution reverted");
         });
     });
 
