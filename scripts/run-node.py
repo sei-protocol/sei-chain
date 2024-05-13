d2fb8b25dcb9523ab57a4fb7545e005d81c8a8b1
cordt-sei <165932662+cordt-sei@users.noreply.github.com>
2024-05-13 07:05:06 -0600

    Update run-node.py
    auto version fetching based on abci_info
    
    variables for moniker / state sync trust height, unsafe_reset_all [bool] and manual version override near top of script

--- scripts/run-node.py
+++ scripts/run-node.py
@@ -8,7 +8,7 @@ import json
 import zipfile
 from io import BytesIO
 
-# Required packages
+# req packages
 dependencies = ['requests']
 
 for package in dependencies:
@@ -18,12 +18,18 @@ for package in dependencies:
         print(f"Installing the '{package}' package...")
         subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
-# Version configuration for easy updates
-MAINNET_VERSION = "v3.9.0"
-DEVNET_VERSION = "v5.0.1"
-TESTNET_VERSION = "v5.0.1"
+# advanced user configs
+moniker = "pynode" # optional custom moniker for the node
+trust_height_delta = 40000  # negative height offset for state sync
+enable_unsafe_reset = True  # wipe database and keys before setup
+version_override = False  # override version fetching. if true, specify version(s) below
 
-# Mapping of environment to chain ID and version
+# chain binary version ["version_override" must be true to use]
+MAINNET_VERSION = "v3.9.0"
+DEVNET_VERSION = "v5.2.2"
+TESTNET_VERSION = "v5.2.2"
+
+# map env to chain ID and optional manual version override
 ENV_TO_CONFIG = {
     "local": {"chain_id": None, "version": "latest"},
     "devnet": {"chain_id": "arctic-1", "version": DEVNET_VERSION},
@@ -64,7 +70,7 @@ This tool will build from scratch seid and wipe away existing state.
 Please backup any important existing data before proceeding.
 """)
 
-# interactive setup prompts
+# user setup prompts
 def take_manual_inputs():
     env = input("Choose an environment (1: local, 2: devnet, 3: testnet, 4: mainnet): ")
     while env not in ['1', '2', '3', '4']:
@@ -72,45 +78,10 @@ def take_manual_inputs():
         env = input("Choose an environment: ")
 
     env = ["local", "devnet", "testnet", "mainnet"][int(env) - 1]
-
-    # Change the default option for the database backend to "sei-db", which is option 2
     db_choice = input("Choose the database backend (1: legacy, 2: sei-db [default]): ").strip() or "2"
-
     return env, db_choice
 
-def install_release(version):
-    """
-    Install a specific version of the release based on the given version tag.
-    """
-    try:
-        # Download zip
-        zip_url = f"https://github.com/sei-protocol/sei-chain/archive/refs/tags/{version}.zip"
-        response = requests.get(zip_url)
-        response.raise_for_status()
-        zip_file = zipfile.ZipFile(BytesIO(response.content))
-        zip_file.extractall(".")
-
-        # Install
-        os.chdir(zip_file.namelist()[0])
-        subprocess.run("make install", shell=True, check=True)
-        print("Successfully installed version:", version)
-
-    except requests.exceptions.HTTPError as e:
-        print(f"HTTP error occurred: {e}")  # handle http error
-        sys.exit(1)
-    except requests.exceptions.RequestException as e:
-        print(f"Error downloading files: {e}")  # handle other errors
-        sys.exit(1)
-    except zipfile.BadZipFile:
-        print("Error unzipping file. The downloaded file may be corrupt.")
-        sys.exit(1)
-    except subprocess.CalledProcessError as e:
-        print(f"Installation failed during 'make install': {e}")
-        sys.exit(1)
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
-        sys.exit(1)
-
+# fetch chain data
 def get_rpc_server(chain_id):
     chains_json_url = "https://raw.githubusercontent.com/sei-protocol/chain-registry/main/chains.json"
     response = requests.get(chains_json_url)
@@ -143,9 +114,55 @@ def get_rpc_server(chain_id):
 
     return None
 
+# install release based on version tag.
+def install_release(version):
+
+
+    try:
+        zip_url = f"https://github.com/sei-protocol/sei-chain/archive/refs/tags/{version}.zip"
+        response = requests.get(zip_url)
+        response.raise_for_status()
+        zip_file = zipfile.ZipFile(BytesIO(response.content))
+        zip_file.extractall(".")
+
+        os.chdir(zip_file.namelist()[0])
+        subprocess.run("make install", shell=True, check=True)
+        print("Successfully installed version:", version)
+
+    except requests.exceptions.HTTPError as e:
+        print(f"HTTP error occurred: {e}")  # handle http error
+        sys.exit(1)
+    except requests.exceptions.RequestException as e:
+        print(f"Error downloading files: {e}")  # handle other errors
+        sys.exit(1)
+    except zipfile.BadZipFile:
+        print("Error unzipping file. The downloaded file may be corrupt.")
+        sys.exit(1)
+    except subprocess.CalledProcessError as e:
+        print(f"Installation failed during 'make install': {e}")
+        sys.exit(1)
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        sys.exit(1)
+
+# fetch version from RPC unless "version_override = true"
+def fetch_node_version(rpc_url):
+    if not version_override:
+        try:
+            response = requests.get(f"{rpc_url}/abci_info")
+            response.raise_for_status()
+            version = response.json()['response']['version']
+            print(f"Fetched node version {version} from {rpc_url}")
+            return version
+        except Exception as e:
+            print(f"Failed to fetch node version from RPC URL {rpc_url}: {e}")
+            return None
+    else:
+        print("Using user-specified version override.")
+        return None
+
 # fetch state sync params
 def get_state_sync_params(rpc_url):
-    trust_height_delta = 40000 # may need to tune
     response = requests.get(f"{rpc_url}/status")
     latest_height = int(response.json()['sync_info']['latest_block_height'])
     sync_block_height = latest_height - trust_height_delta if latest_height > trust_height_delta else latest_height
@@ -188,42 +205,46 @@ def ensure_file_path(file_path):
         open(file_path, 'a').close()
         print(f"Created missing file: {file_path}")
 
-# Main execution function
 def main():
     print_ascii_and_intro()
     env, db_choice = take_manual_inputs()
-    moniker = "demo"
     print(f"Setting up a node in {env}")
 
-    # Fetching chain_id from the ENV_TO_CONFIG using the selected environment
+    # fetch chain_id and version from ENV_TO_CONFIG
     config = ENV_TO_CONFIG[env]
-    chain_id = config['chain_id']  # Ensuring chain_id is defined
+    chain_id = config['chain_id']
+    version = config['version']
 
-    # Install the selected release based on environment
-    install_release(config['version'])
-
-    # Get RPC server URL or default to localhost for local environment
+    # determine version by RPC or override
     rpc_url = get_rpc_server(chain_id) if chain_id else "http://localhost:26657"
+    dynamic_version = fetch_node_version(rpc_url) if not version_override else None
+    version = dynamic_version or version  # Use the fetched version if not overridden
 
-    # Clean up previous data and initialize seid with the given chain ID and moniker
+    # install selected release
+    install_release(version)
+
+    if enable_unsafe_reset:
+        subprocess.run("seid tendermint unsafe-reset-all", shell=True, check=True)
+
+    # clean up previous data, init seid with given chain ID and moniker
     subprocess.run(f"rm -rf $HOME/.sei && seid init {moniker} --chain-id {chain_id}", shell=True, check=True)
 
-    # Fetch synchronization parameters and persistent peers
+    # fetch state-sync params and persistent peers
     sync_block_height, sync_block_hash = get_state_sync_params(rpc_url)
     persistent_peers = get_persistent_peers(rpc_url)
 
-    # Fetch and write genesis
+    # fetch and write genesis
     write_genesis_file(chain_id)
 
-    # Configuration settings
+    # config changes
     config_path = os.path.expanduser('~/.sei/config/config.toml')
     app_config_path = os.path.expanduser('~/.sei/config/app.toml')
 
-    # confirm  exists before modifying config files
+    # confirm exists before modifying config files
     ensure_file_path(config_path)
     ensure_file_path(app_config_path)
 
-    # Read and modify config.toml
+    # read and modify config.toml
     with open(config_path, 'r') as file:
         config_data = file.read()
         config_data = config_data.replace('rpc-servers = ""', f'rpc-servers = "{rpc_url},{rpc_url}"')
@@ -236,7 +257,7 @@ def main():
     with open(config_path, 'w') as file:
         file.write(config_data)
 
-    # Read, modify, and write app.toml if sei-db is selected
+    # read modify and write app.toml if sei-db is selected
     if db_choice == "1":
         with open(app_config_path, 'r') as file:
             app_data = file.read()
@@ -245,10 +266,9 @@ def main():
         with open(app_config_path, 'w') as file:
             file.write(app_data)
 
-    # Start seid
+    # start seid
     print("Starting seid...")
     run_command("seid start")
-
 
 if __name__ == "__main__":
     main()
