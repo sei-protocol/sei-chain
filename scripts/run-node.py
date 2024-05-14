2a43cd5acb436ac55ffb8972314292f72835c091
cordt-sei <165932662+cordt-sei@users.noreply.github.com>
2024-05-14 10:21:49 -0600

    Update run-node.py
    multiple fixes+minor UX improvement
    
    db-backend [legacy] now default
    
    cleaner exiting from script with keyboard interrupt
    
    setting custom moniker (fixed)

--- scripts/run-node.py
+++ scripts/run-node.py
@@ -19,7 +19,7 @@ for package in dependencies:
         subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
 # advanced user configs
-moniker = "pynode" # optional custom moniker for the node
+moniker = "pynode"  # optional custom moniker for the node
 trust_height_delta = 40000  # negative height offset for state sync
 enable_unsafe_reset = True  # wipe database and keys before setup
 version_override = False  # override version fetching. if true, specify version(s) below
@@ -78,7 +78,9 @@ def take_manual_inputs():
         env = input("Choose an environment: ")
 
     env = ["local", "devnet", "testnet", "mainnet"][int(env) - 1]
-    db_choice = input("Choose the database backend (1: legacy, 2: sei-db [default]): ").strip() or "2"
+    db_choice = input("Choose the database backend (1: legacy [default], 2: sei-db): ").strip() or "1"
+    if db_choice not in ["1", "2"]:
+        db_choice = "1"  # Default to "1" if the input is invalid or empty
     return env, db_choice
 
 # fetch chain data
@@ -116,8 +118,6 @@ def get_rpc_server(chain_id):
 
 # install release based on version tag.
 def install_release(version):
-
-
     try:
         zip_url = f"https://github.com/sei-protocol/sei-chain/archive/refs/tags/{version}.zip"
         response = requests.get(zip_url)
@@ -194,10 +194,10 @@ def write_genesis_file(chain_id):
 def run_command(command):
     try:
         subprocess.run(command, shell=True, check=True)
-        print(f"Command executed successfully: {command}")
     except subprocess.CalledProcessError as e:
-        print(f"Failed to execute command '{command}': {e}")
-        sys.exit(1)
+        print(f"Command '{command}' failed with return code {e.returncode}")
+    except KeyboardInterrupt:
+        print("Process interrupted by user. Exiting gracefully...")
 
 def ensure_file_path(file_path):
     os.makedirs(os.path.dirname(file_path), exist_ok=True)
@@ -206,74 +206,80 @@ def ensure_file_path(file_path):
         print(f"Created missing file: {file_path}")
 
 def main():
-    print_ascii_and_intro()
-    env, db_choice = take_manual_inputs()
-    print(f"Setting up a node in {env}")
+    try:
+        print_ascii_and_intro()
+        env, db_choice = take_manual_inputs()
+        print(f"Setting up a node in {env}")
 
-    # fetch chain_id and version from ENV_TO_CONFIG
-    config = ENV_TO_CONFIG[env]
-    chain_id = config['chain_id']
-    version = config['version']
+        # fetch chain_id and version from ENV_TO_CONFIG
+        config = ENV_TO_CONFIG[env]
+        chain_id = config['chain_id']
+        version = config['version']
 
-    # determine version by RPC or override
-    rpc_url = get_rpc_server(chain_id) if chain_id else "http://localhost:26657"
-    dynamic_version = fetch_node_version(rpc_url) if not version_override else None
-    version = dynamic_version or version  # Use the fetched version if not overridden
+        # determine version by RPC or override
+        rpc_url = get_rpc_server(chain_id) if chain_id else "http://localhost:26657"
+        dynamic_version = fetch_node_version(rpc_url) if not version_override else None
+        version = dynamic_version or version  # Use the fetched version if not overridden
 
-    # install selected release
-    install_release(version)
+        # install selected release
+        install_release(version)
 
-    # unsafe-reset-all only if directory exists, and by config at top of script
-    if enable_unsafe_reset and os.path.exists(os.path.expanduser('~/.sei')):
-        try:
-            subprocess.run("seid tendermint unsafe-reset-all", shell=True, check=True)
-        except subprocess.CalledProcessError as e:
-            print(f"Failed to execute 'seid tendermint unsafe-reset-all': {e}")
-            sys.exit(1)
+        # unsafe-reset-all only if directory exists, and by config at top of script
+        if enable_unsafe_reset and os.path.exists(os.path.expanduser('~/.sei')):
+            try:
+                subprocess.run("seid tendermint unsafe-reset-all", shell=True, check=True)
+            except subprocess.CalledProcessError as e:
+                print(f"Failed to execute 'seid tendermint unsafe-reset-all': {e}")
+                sys.exit(1)
 
-    # clean up previous data, init seid with given chain ID and moniker
-    subprocess.run(f"rm -rf $HOME/.sei && seid init {moniker} --chain-id {chain_id}", shell=True, check=True)
+        # clean up previous data, init seid with given chain ID and moniker
+        subprocess.run(f"rm -rf $HOME/.sei && seid init {moniker} --chain-id {chain_id}", shell=True, check=True)
 
-    # fetch state-sync params and persistent peers
-    sync_block_height, sync_block_hash = get_state_sync_params(rpc_url)
-    persistent_peers = get_persistent_peers(rpc_url)
+        # fetch state-sync params and persistent peers
+        sync_block_height, sync_block_hash = get_state_sync_params(rpc_url)
+        persistent_peers = get_persistent_peers(rpc_url)
 
-    # fetch and write genesis
-    write_genesis_file(chain_id)
+        # fetch and write genesis
+        write_genesis_file(chain_id)
 
-    # config changes
-    config_path = os.path.expanduser('~/.sei/config/config.toml')
-    app_config_path = os.path.expanduser('~/.sei/config/app.toml')
+        # config changes
+        config_path = os.path.expanduser('~/.sei/config/config.toml')
+        app_config_path = os.path.expanduser('~/.sei/config/app.toml')
 
-    # confirm exists before modifying config files
-    ensure_file_path(config_path)
-    ensure_file_path(app_config_path)
+        # confirm exists before modifying config files
+        ensure_file_path(config_path)
+        ensure_file_path(app_config_path)
 
-    # read and modify config.toml
-    with open(config_path, 'r') as file:
-        config_data = file.read()
-        config_data = config_data.replace('rpc-servers = ""', f'rpc-servers = "{rpc_url},{rpc_url}"')
-        config_data = config_data.replace('trust-height = 0', f'trust-height = {sync_block_height}')
-        config_data = config_data.replace('trust-hash = ""', f'trust-hash = "{sync_block_hash}"')
-        config_data = config_data.replace('persistent-peers = ""', f'persistent-peers = "{persistent_peers}"')
-        config_data = config_data.replace('enable = false', 'enable = true')
-        config_data = config_data.replace('db-sync-enable = true', 'db-sync-enable = false')
-        config_data = config_data.replace('use-p2p = false', 'use-p2p = true')
-    with open(config_path, 'w') as file:
-        file.write(config_data)
+        # read and modify config.toml
+        with open(config_path, 'r') as file:
+            config_data = file.read()
+            config_data = config_data.replace('rpc-servers = ""', f'rpc-servers = "{rpc_url},{rpc_url}"')
+            config_data = config_data.replace('trust-height = 0', f'trust-height = {sync_block_height}')
+            config_data = config_data.replace('trust-hash = ""', f'trust-hash = "{sync_block_hash}"')
+            config_data = config_data.replace('persistent-peers = ""', f'persistent-peers = "{persistent_peers}"')
+            config_data = config_data.replace('enable = false', 'enable = true')
+            config_data = config_data.replace('db-sync-enable = true', 'db-sync-enable = false')
+            config_data = config_data.replace('use-p2p = false', 'use-p2p = true')
+        with open(config_path, 'w') as file:
+            file.write(config_data)
 
-    # read modify and write app.toml if sei-db is selected
-    if db_choice == "1":
-        with open(app_config_path, 'r') as file:
-            app_data = file.read()
-        app_data = app_data.replace('sc-enable = false', 'sc-enable = true')
-        app_data = app_data.replace('ss-enable = false', 'ss-enable = true')
-        with open(app_config_path, 'w') as file:
-            file.write(app_data)
+        # read modify and write app.toml if sei-db is selected
+        if db_choice == "2":
+            with open(app_config_path, 'r') as file:
+                app_data = file.read()
+            app_data = app_data.replace('sc-enable = false', 'sc-enable = true')
+            app_data = app_data.replace('ss-enable = false', 'ss-enable = true')
+            with open(app_config_path, 'w') as file:
+                file.write(app_data)
 
-    # start seid
-    print("Starting seid...")
-    run_command("seid start")
+        # start seid
+        print("Starting seid...")
+        run_command("seid start")
+    except KeyboardInterrupt:
+        print("Main process interrupted by user. Exiting gracefully...")
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Script interrupted by user. Exiting gracefully...")
