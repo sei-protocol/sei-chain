23bab217292c7a48c853aca319d8530f7b51acd0
Yiming Zang <50607998+yzang2019@users.noreply.github.com>
2024-05-16 15:32:47 +0800

    Fix genesis export command (#1653)
    * Fix genesis export command
    
    * Bump cosmos and seidb version
    
    * Fix go mod dependency issue

--- app/app.go
+++ app/app.go
@@ -1651,7 +1651,7 @@ func (app *App) getFinalizeBlockResponse(appHash []byte, events []abci.Event, tx
 
 // LoadHeight loads a particular height
 func (app *App) LoadHeight(height int64) error {
-	return app.LoadVersion(height)
+	return app.LoadVersionWithoutInit(height)
 }
 
 // ModuleAccountAddrs returns all the app's module account addresses.
