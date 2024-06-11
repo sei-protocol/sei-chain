5c9d9eb834ae2c313fd92db93217553cd4e9bb21
Steven Landers <steven.landers@gmail.com>
2024-06-11 10:40:16 -0400

    goimports

--- x/evm/client/wasm/query.go
+++ x/evm/client/wasm/query.go
@@ -5,7 +5,6 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"errors"
-	"github.com/sei-protocol/sei-chain/x/evm/types"
 	"math/big"
 	"strings"
 
@@ -15,6 +14,7 @@ import (
 	"github.com/sei-protocol/sei-chain/x/evm/artifacts/native"
 	"github.com/sei-protocol/sei-chain/x/evm/client/wasm/bindings"
 	"github.com/sei-protocol/sei-chain/x/evm/keeper"
+	"github.com/sei-protocol/sei-chain/x/evm/types"
 )
 
 type EVMQueryHandler struct {
