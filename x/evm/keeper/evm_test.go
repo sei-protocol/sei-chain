d065752ff931347f4148b4938c687de399ee85c3
codchen <codchen03@gmail.com>
2024-06-13 04:40:17 +0800

    Remove interoperability hop limit with isolated EVM instances (#1729)
    * remove hop limit
    
    * Remove interop hop limit and handle events correctly

--- x/evm/keeper/evm_test.go
+++ x/evm/keeper/evm_test.go
@@ -35,9 +35,6 @@ func TestInternalCallCreateContract(t *testing.T) {
 		Data:   contractData,
 	}
 	// circular interop call
-	ctx = ctx.WithIsEVM(true)
-	_, err = k.HandleInternalEVMCall(ctx, req)
-	require.Equal(t, "sei does not support EVM->CW->EVM call pattern", err.Error())
 	ctx = ctx.WithIsEVM(false)
 	_, err = k.HandleInternalEVMCall(ctx, req)
 	require.Nil(t, err)
@@ -62,9 +59,6 @@ func TestInternalCall(t *testing.T) {
 		Sender: testAddr.String(),
 		Data:   contractData,
 	}
-	ctx = ctx.WithIsEVM(true)
-	_, err = k.HandleInternalEVMCall(ctx, req)
-	require.Equal(t, "sei does not support EVM->CW->EVM call pattern", err.Error())
 	ctx = ctx.WithIsEVM(false)
 	ret, err := k.HandleInternalEVMCall(ctx, req)
 	require.Nil(t, err)
