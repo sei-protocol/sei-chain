026962738452d8f9a2f4fdbf5b34ea076927cbfa
davyli <dvli2007@gmail.com>
2024-06-15 16:04:14 -0400

    Fix up cwerc1155 compiled files

---
+++ x/evm/artifacts/erc1155/artifacts.go
@@ -0,0 +1,22 @@
+package erc721
+
+import "embed"
+
+const CurrentVersion uint16 = 1
+
+//go:embed cwerc1155.wasm
+var f embed.FS
+
+var cachedBin []byte
+
+func GetBin() []byte {
+	if cachedBin != nil {
+		return cachedBin
+	}
+	bz, err := f.ReadFile("cwerc1155.wasm")
+	if err != nil {
+		panic("failed to read ERC1155 wrapper contract wasm")
+	}
+	cachedBin = bz
+	return bz
+}
