f1b7bf3566d21ff0017857795b06567cb91a463f
Steven Landers <steven.landers@gmail.com>
2024-06-25 19:45:34 -0400

    include prefix on write

--- x/evm/keeper/receipt.go
+++ x/evm/keeper/receipt.go
@@ -2,7 +2,7 @@ package keeper
 
 import (
 	"errors"
-	"github.com/cosmos/cosmos-sdk/store/prefix"
+	
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/iavl"
 	"github.com/ethereum/go-ethereum/common"
@@ -70,13 +70,11 @@ func (k *Keeper) SetReceipt(ctx sdk.Context, txHash common.Hash, receipt *types.
 }
 
 func (k *Keeper) FlushTransientReceipts(ctx sdk.Context) error {
-	ctx.Logger().Info("[Debug] Flushing transient receipts")
-	iter := prefix.NewStore(ctx.TransientStore(k.transientStoreKey), types.ReceiptKeyPrefix).Iterator(nil, nil)
+	iter := ctx.TransientStore(k.transientStoreKey).Iterator(nil, nil)
 	defer iter.Close()
 	var pairs []*iavl.KVPair
 	var changesets []*proto.NamedChangeSet
 	for ; iter.Valid(); iter.Next() {
-		ctx.Logger().Info("[Debug] FlushTransientReceipts", "tx", common.BytesToHash(iter.Key()).Hex())
 		kvPair := &iavl.KVPair{Key: iter.Key(), Value: iter.Value()}
 		pairs = append(pairs, kvPair)
 	}
