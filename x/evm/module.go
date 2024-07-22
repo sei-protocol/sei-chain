91e5c7ed4c0809616222d916147ab1907d392dc6
codchen <codchen03@gmail.com>
2024-07-22 13:04:42 +0800

    migration

--- x/evm/module.go
+++ x/evm/module.go
@@ -152,7 +152,7 @@ func (am AppModule) RegisterServices(cfg module.Configurator) {
 	})
 
 	_ = cfg.RegisterMigration(types.ModuleName, 6, func(ctx sdk.Context) error {
-		return migrations.StoreCWPointerCode(ctx, am.keeper, true, true)
+		return migrations.StoreCWPointerCode(ctx, am.keeper, false, true)
 	})
 
 	_ = cfg.RegisterMigration(types.ModuleName, 7, func(ctx sdk.Context) error {
@@ -167,6 +167,16 @@ func (am AppModule) RegisterServices(cfg module.Configurator) {
 			return err
 		}
 		return migrations.MigrateERCCW721Pointers(ctx, am.keeper)
+	})
+
+	_ = cfg.RegisterMigration(types.ModuleName, 9, func(ctx sdk.Context) error {
+		if err := migrations.StoreCWPointerCode(ctx, am.keeper, true, true); err != nil {
+			return err
+		}
+		if err := migrations.MigrateCWERC20Pointers(ctx, am.keeper); err != nil {
+			return err
+		}
+		return migrations.MigrateCWERC721Pointers(ctx, am.keeper)
 	})
 }
 
@@ -192,7 +202,7 @@ func (am AppModule) ExportGenesis(ctx sdk.Context, cdc codec.JSONCodec) json.Raw
 }
 
 // ConsensusVersion implements ConsensusVersion.
-func (AppModule) ConsensusVersion() uint64 { return 9 }
+func (AppModule) ConsensusVersion() uint64 { return 10 }
 
 // BeginBlock executes all ABCI BeginBlock logic respective to the capability module.
 func (am AppModule) BeginBlock(ctx sdk.Context, _ abci.RequestBeginBlock) {
