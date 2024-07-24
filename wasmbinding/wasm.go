aa36cc0b9d0df23adbc54582323c9ce9d95ebfa5
codchen <codchen03@gmail.com>
2024-07-24 22:28:31 +0800

    Revert "Reuse existing EVM instance in interop calls (#1731)"
    This reverts commit f285e6f43549bdba63eebde5f3662c31744a7d1e.

--- wasmbinding/wasm.go
+++ wasmbinding/wasm.go
@@ -4,9 +4,7 @@ import (
 	"github.com/CosmWasm/wasmd/x/wasm"
 	wasmkeeper "github.com/CosmWasm/wasmd/x/wasm/keeper"
 	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
-	"github.com/cosmos/cosmos-sdk/baseapp"
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	aclkeeper "github.com/cosmos/cosmos-sdk/x/accesscontrol/keeper"
 	authkeeper "github.com/cosmos/cosmos-sdk/x/auth/keeper"
 	epochwasm "github.com/sei-protocol/sei-chain/x/epoch/client/wasm"
@@ -19,24 +17,12 @@ import (
 	tokenfactorykeeper "github.com/sei-protocol/sei-chain/x/tokenfactory/keeper"
 )
 
-type routerWithContext struct {
-	router *baseapp.MsgServiceRouter
-}
-
-func (rc routerWithContext) Handler(msg sdk.Msg) wasmkeeper.MsgHandler {
-	h := rc.router.Handler(msg)
-	return func(ctx sdk.Context, req sdk.Msg) (sdk.Context, *sdk.Result, error) {
-		result, err := h(ctx, msg)
-		return ctx, result, err
-	}
-}
-
 func RegisterCustomPlugins(
 	oracle *oraclekeeper.Keeper,
 	epoch *epochkeeper.Keeper,
 	tokenfactory *tokenfactorykeeper.Keeper,
 	_ *authkeeper.AccountKeeper,
-	router *baseapp.MsgServiceRouter,
+	router wasmkeeper.MessageRouter,
 	channelKeeper wasmtypes.ChannelKeeper,
 	capabilityKeeper wasmtypes.CapabilityKeeper,
 	bankKeeper wasmtypes.Burner,
@@ -54,9 +40,8 @@ func RegisterCustomPlugins(
 	queryPluginOpt := wasmkeeper.WithQueryPlugins(&wasmkeeper.QueryPlugins{
 		Custom: CustomQuerier(wasmQueryPlugin),
 	})
-	routerWithCtx := routerWithContext{router}
 	messengerHandlerOpt := wasmkeeper.WithMessageHandler(
-		CustomMessageHandler(routerWithCtx, channelKeeper, capabilityKeeper, bankKeeper, evmKeeper, unpacker, portSource, aclKeeper),
+		CustomMessageHandler(router, channelKeeper, capabilityKeeper, bankKeeper, evmKeeper, unpacker, portSource, aclKeeper),
 	)
 
 	return []wasm.Option{
