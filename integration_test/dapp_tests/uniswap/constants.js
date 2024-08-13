6579e776e0d69052e6e57e2e979e87174ed63670
mj <riku850@gmail.com>
2024-08-13 18:02:39 -0400

    deps

--- integration_test/dapp_tests/uniswap/constants.js
+++ integration_test/dapp_tests/uniswap/constants.js
@@ -5,7 +5,7 @@ const tokenFactoryDenoms = {
 
 const cw20Addresses = {
     "testnet": {"evm": "0xcD10A4FdeE9CefB7732161f4B20b018bA3F4e7fF", "sei": "sei1d5cs4y0cfdm8dvak4fnmcudqd9htgsnw0djryuvpwhhmyvywypmsxv7vnq"},
-    "devnet": {"evm": "", "sei": "sei10609np8vn9udq3lg2rp3k2ymtzrxnq9dnezyag9ygklnnua4rqks89np9j"}
+    "devnet": {"evm": "0x2118f39DB4a32327523eA9ED6299EE5E2dee7b3d", "sei": "sei10609np8vn9udq3lg2rp3k2ymtzrxnq9dnezyag9ygklnnua4rqks89np9j"}
 }
 
 const rpcUrls = {
