16246e7b14d3c962e815e247deafca47464881f7
mj <riku850@gmail.com>
2024-08-15 00:12:04 -0400

    no sh

--- integration_test/dapp_tests/dapp_tests.sh
+++ integration_test/dapp_tests/dapp_tests.sh
@@ -21,8 +21,6 @@ npx hardhat compile
 export DAPP_TEST_ENV=$1
 
 seid config keyring-backend test
-printf "$DAPP_TESTS_MNEMONIC" | seid keys add dapptest --recover --hd-path "m/44'/60'/0'/0/0" --keyring-backend test
-seid keys show dapptest
 
 npx hardhat test --network $1 uniswap/uniswapTest.js
 npx hardhat test --network $1 steak/SteakTests.js
