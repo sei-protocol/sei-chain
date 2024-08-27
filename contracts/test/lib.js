600f4c48f994441026433ea64557757d3565534b
yzang2019 <zymfrank@gmail.com>
2024-08-26 19:48:36 -0700

    Add log

--- contracts/test/lib.js
+++ contracts/test/lib.js
@@ -435,10 +435,12 @@ function execCommand(command) {
     return new Promise((resolve, reject) => {
         exec(command, (error, stdout, stderr) => {
             if (error) {
+                console.log(`Got error: ${error}`)
                 reject(error);
                 return;
             }
             if (stderr) {
+                console.log(`Got stderr: ${stderr}`)
                 reject(new Error(stderr));
                 return;
             }
