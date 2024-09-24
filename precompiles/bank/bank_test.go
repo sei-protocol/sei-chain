5a4accf77b49cfbb18f1f411126c9d667b13834e
Jeremy Wei <jeremy.t.wei@gmail.com>
2024-09-24 16:36:32 -0400

    fix tests

--- precompiles/bank/bank_test.go
+++ precompiles/bank/bank_test.go
@@ -179,7 +179,7 @@ func TestRun(t *testing.T) {
 			sdk.NewAttribute(banktypes.AttributeKeySender, senderAddr.String()),
 		),
 		// gas refund to the sender
-		banktypes.NewCoinReceivedEvent(senderAddr, sdk.NewCoins(sdk.NewCoin("usei", sdk.NewInt(172056)))),
+		banktypes.NewCoinReceivedEvent(senderAddr, sdk.NewCoins(sdk.NewCoin("usei", sdk.NewInt(41916)))),
 		// tip is paid to the validator
 		banktypes.NewCoinReceivedEvent(sdk.MustAccAddressFromBech32("sei1v4mx6hmrda5kucnpwdjsqqqqqqqqqqqqlve8dv"), sdk.NewCoins(sdk.NewCoin("usei", sdk.NewInt(27944)))),
 	}
@@ -231,7 +231,7 @@ func TestRun(t *testing.T) {
 		Denom:  "ufoo",
 	}, bank.CoinBalance(parsedBalances[0]))
 	require.Equal(t, bank.CoinBalance{
-		Amount: big.NewInt(9972045),
+		Amount: big.NewInt(9841905),
 		Denom:  "usei",
 	}, bank.CoinBalance(parsedBalances[1]))
 
