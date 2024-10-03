e05f3e12526919e6e57837b50304986164b4b4f6
codchen <codchen03@gmail.com>
2024-10-03 11:19:19 +0800

    use codespace-d errors for wasm->evm calls

--- x/evm/types/errors.go
+++ x/evm/types/errors.go
@@ -3,6 +3,8 @@ package types
 import (
 	"fmt"
 	"strings"
+
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 type AssociationMissingErr struct {
@@ -23,3 +25,23 @@ func (e AssociationMissingErr) AddressType() string {
 	}
 	return "sei"
 }
+
+const EvmCodespace = "evm"
+
+var (
+	errInternal = sdkerrors.Register(EvmCodespace, 1, "internal")
+
+	ErrInvalidBech32 = sdkerrors.Register(EvmCodespace, 2, "invalid bech32 address representation")
+
+	ErrMoreThanOneHop = sdkerrors.Register(EvmCodespace, 3, "sei does not support EVM->CW->EVM call pattern")
+
+	ErrMaxInitCodeSize = sdkerrors.Register(EvmCodespace, 4, "max init code size exceeded")
+
+	ErrEVMExecution = sdkerrors.Register(EvmCodespace, 5, "execution error")
+
+	ErrResult = sdkerrors.Register(EvmCodespace, 6, "error in execution result")
+
+	ErrFinalizeState = sdkerrors.Register(EvmCodespace, 7, "error in stateDB finalization")
+
+	ErrWriteReceipt = sdkerrors.Register(EvmCodespace, 8, "error writing receipt")
+)
