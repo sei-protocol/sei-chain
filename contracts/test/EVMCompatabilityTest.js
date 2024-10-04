01c696528aa23aa76fe7dde90165cd2f4a3309fd
Jeremy Wei <jeremy.t.wei@gmail.com>
2024-10-04 15:06:54 -0400

    fix more gas prices in EVMCompatibility

--- contracts/test/EVMCompatabilityTest.js
+++ contracts/test/EVMCompatabilityTest.js
@@ -148,7 +148,7 @@ describe("EVM Test", function () {
 
           await delay()
           // Set balance
-          await testToken.setBalance(owner.address, setAmount);
+            await testToken.setBalance(owner.address, setAmount, { gasPrice: ethers.parseUnits('100', 'gwei') });
 
           // Prepare call data for balanceOf function of MyToken
           const balanceOfData = testToken.interface.encodeFunctionData("balanceOf", [owner.address]);
@@ -169,7 +169,7 @@ describe("EVM Test", function () {
     describe("Msg Properties", function() {
       it("Should store and retrieve msg properties correctly", async function() {
         // Store msg properties
-        const txResponse = await evmTester.storeMsgProperties({ value: 1 });
+        const txResponse = await evmTester.storeMsgProperties({ value: 1, gasPrice: ethers.parseUnits('100', 'gwei') });
         await txResponse.wait();
 
         // Retrieve stored msg properties
