c61318175699ad670d9d63c51c7e5785827c1b01
yirenz <blindchaser@users.noreply.github.com>
2024-10-08 14:26:16 -0400

    update gas tip cap param range (#1884)
    * update gas tip cap param range
    
    * unit test

--- x/evm/ante/fee_test.go
+++ x/evm/ante/fee_test.go
@@ -112,6 +112,24 @@ func TestEVMFeeCheckDecorator(t *testing.T) {
 		return ctx, nil
 	})
 	require.NotNil(t, err)
+
+	// should fail because of negative gas tip cap
+	txData.GasTipCap = big.NewInt(-1)
+	txData.GasFeeCap = big.NewInt(10000000000000)
+	tx, err = ethtypes.SignTx(ethtypes.NewTx(&txData), signer, key)
+	require.Nil(t, err)
+	typedTx = newDynamicFeeTxWithoutValidation(tx)
+	msg, err = types.NewMsgEVMTransaction(typedTx)
+	require.Nil(t, err)
+	ctx, err = preprocessor.AnteHandle(ctx, mockTx{msgs: []sdk.Msg{msg}}, false, func(ctx sdk.Context, _ sdk.Tx, _ bool) (sdk.Context, error) {
+		return ctx, nil
+	})
+	require.Nil(t, err)
+	_, err = handler.AnteHandle(ctx, mockTx{msgs: []sdk.Msg{msg}}, false, func(ctx sdk.Context, _ sdk.Tx, _ bool) (sdk.Context, error) {
+		return ctx, nil
+	})
+	require.NotNil(t, err)
+	require.Contains(t, err.Error(), "gas fee cap cannot be negative")
 }
 
 func TestCalculatePriorityScenarios(t *testing.T) {
@@ -226,4 +244,23 @@ func TestCalculatePriorityScenarios(t *testing.T) {
 			}
 		})
 	}
+}
+
+func newDynamicFeeTxWithoutValidation(tx *ethtypes.Transaction) *ethtx.DynamicFeeTx {
+	txData := &ethtx.DynamicFeeTx{
+		Nonce:    tx.Nonce(),
+		Data:     tx.Data(),
+		GasLimit: tx.Gas(),
+	}
+
+	v, r, s := tx.RawSignatureValues()
+	ethtx.SetConvertIfPresent(tx.To(), func(to *common.Address) string { return to.Hex() }, txData.SetTo)
+	ethtx.SetConvertIfPresent(tx.Value(), sdk.NewIntFromBigInt, txData.SetAmount)
+	ethtx.SetConvertIfPresent(tx.GasFeeCap(), sdk.NewIntFromBigInt, txData.SetGasFeeCap)
+	ethtx.SetConvertIfPresent(tx.GasTipCap(), sdk.NewIntFromBigInt, txData.SetGasTipCap)
+	al := tx.AccessList()
+	ethtx.SetConvertIfPresent(&al, ethtx.NewAccessList, txData.SetAccesses)
+
+	txData.SetSignatureValues(tx.ChainId(), v, r, s)
+	return txData
 }
