acb87fcf3c684d43a52dc010818fc8c4d9d8026a
_dssei_ <denys@seinetwork.io>
2024-10-16 16:12:26 -0700

    codec + some validation + routing

---
+++ x/confidentialtransfers/types/codec.go
@@ -0,0 +1,32 @@
+package types
+
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/msgservice"
+)
+
+func RegisterCodec(cdc *codec.LegacyAmino) {
+	cdc.RegisterConcrete(&MsgTransfer{}, "confidentialtransfers/MsgTransfer", nil)
+}
+
+func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
+	registry.RegisterImplementations((*sdk.Msg)(nil),
+		&MsgTransfer{},
+	)
+
+	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
+}
+
+var (
+	amino     = codec.NewLegacyAmino()
+	ModuleCdc = codec.NewAminoCodec(amino)
+)
+
+func init() {
+	RegisterCodec(amino)
+	sdk.RegisterLegacyAminoCodec(amino)
+
+	amino.Seal()
+}
--- x/confidentialtransfers/types/msgs.go
+++ x/confidentialtransfers/types/msgs.go
@@ -1,6 +1,9 @@
 package types
 
-import sdk "github.com/cosmos/cosmos-sdk/types"
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+)
 
 // confidential transfers message types
 const (
@@ -8,3 +11,38 @@ const (
 )
 
 var _ sdk.Msg = &MsgTransfer{}
+
+// Route Implements Msg.
+func (m *MsgTransfer) Route() string { return RouterKey }
+
+// Type Implements Msg.
+func (m *MsgTransfer) Type() string { return TypeMsgTransfer }
+
+func (m *MsgTransfer) ValidateBasic() error {
+	_, err := sdk.AccAddressFromBech32(m.FromAddress)
+	if err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "Invalid sender address (%s)", err)
+	}
+
+	_, err = sdk.AccAddressFromBech32(m.ToAddress)
+	if err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "Invalid recipient address (%s)", err)
+	}
+
+	err = sdk.ValidateDenom(m.Denom)
+	if err != nil {
+		return err
+	}
+
+	// TODO: Add the rest of the validation logic here
+	return nil
+}
+
+func (m *MsgTransfer) GetSignBytes() []byte {
+	return sdk.MustSortJSON(ModuleCdc.MustMarshalJSON(m))
+}
+
+func (m *MsgTransfer) GetSigners() []sdk.AccAddress {
+	sender, _ := sdk.AccAddressFromBech32(m.FromAddress)
+	return []sdk.AccAddress{sender}
+}
