97346a2abd83eba7b2f03d500794aea976eccb35
codchen <codchen03@gmail.com>
2024-10-31 01:16:51 +0800

    stop flushing deferred info into receipt store (#1909)
    Co-authored-by: Philip Su <philip.su.522@gmail.com>

--- x/evm/keeper/receipt_test.go
+++ x/evm/keeper/receipt_test.go
@@ -3,7 +3,9 @@ package keeper_test
 import (
 	"testing"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/ethereum/go-ethereum/common"
+	ethtypes "github.com/ethereum/go-ethereum/core/types"
 	testkeeper "github.com/sei-protocol/sei-chain/testutil/keeper"
 	"github.com/sei-protocol/sei-chain/x/evm/types"
 	"github.com/stretchr/testify/require"
@@ -16,7 +18,10 @@ func TestReceipt(t *testing.T) {
 	_, err := k.GetReceipt(ctx, txHash)
 	require.NotNil(t, err)
 	k.MockReceipt(ctx, txHash, &types.Receipt{TxHashHex: txHash.Hex()})
+	k.AppendToEvmTxDeferredInfo(ctx, ethtypes.Bloom{}, common.Hash{1}, sdk.NewInt(1)) // make sure this isn't flushed into receipt store
 	r, err := k.GetReceipt(ctx, txHash)
 	require.Nil(t, err)
 	require.Equal(t, txHash.Hex(), r.TxHashHex)
+	_, err = k.GetReceipt(ctx, common.Hash{1})
+	require.Equal(t, "not found", err.Error())
 }
