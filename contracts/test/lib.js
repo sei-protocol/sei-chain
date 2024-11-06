0340a7247cb1feddd0019decf788d47385154678
Yasin <yasin132@gmail.com>
2024-11-07 00:16:15 +0300

    more logs

--- contracts/test/lib.js
+++ contracts/test/lib.js
@@ -439,10 +439,12 @@ function execCommand(command) {
     return new Promise((resolve, reject) => {
         exec(command, (error, stdout, stderr) => {
             if (error) {
+                console.log(error)
                 reject(error);
                 return;
             }
             if (stderr) {
+                console.log(stderr);
                 reject(new Error(stderr));
                 return;
             }
