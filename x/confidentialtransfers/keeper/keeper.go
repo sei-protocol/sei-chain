c9c0828fcc31497be791b64c6ceff5f9c2750f18
_dssei_ <denys@seinetwork.io>
2024-11-12 13:33:40 -0800

    formatting

--- x/confidentialtransfers/keeper/keeper.go
+++ x/confidentialtransfers/keeper/keeper.go
@@ -2,6 +2,7 @@ package keeper
 
 import (
 	"fmt"
+
 	"github.com/cosmos/cosmos-sdk/store/prefix"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	"github.com/sei-protocol/sei-chain/x/confidentialtransfers/types"
