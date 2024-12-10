ff362ca58886b264c310e7fe7feb6743b8a4b442
Matthieu Vachon <matt@streamingfast.io>
2024-12-10 16:46:12 -0500

    Tweaked again to show block output

--- x/evm/tracers/firehose.go
+++ x/evm/tracers/firehose.go
@@ -40,6 +40,7 @@ import (
 	evmtypes "github.com/sei-protocol/sei-chain/x/evm/types"
 	"golang.org/x/exp/maps"
 	"golang.org/x/exp/slices"
+	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
@@ -453,6 +454,15 @@ func getActivePrecompilesChecker(rules params.Rules) func(addr common.Address) b
 func (f *Firehose) OnBlockEnd(err error) {
 	blockNumber := f.block.Number
 	firehoseInfo("block ending (number=%d, trx=%d, err=%s)", blockNumber, len(f.block.TransactionTraces), errorView(err))
+
+	marshalled, marshalErr := protojson.MarshalOptions{}.Marshal(f.block)
+	if marshalErr == nil {
+		fmt.Fprintf(os.Stderr, "[Firehose] %s", string(marshalled))
+	}
+
+	if f.block.Number >= 119822071 {
+		panic("Block number is too high for now")
+	}
 
 	if err == nil {
 		f.ensureInBlockAndNotInTrx()
