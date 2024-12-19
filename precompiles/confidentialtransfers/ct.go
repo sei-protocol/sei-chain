9c97859cb41300d5a7a0c32609479d7f21a20947
_dssei_ <denys@seinetwork.io>
2024-12-19 14:57:05 -0800

    - fix slice size

--- precompiles/confidentialtransfers/ct.go
+++ precompiles/confidentialtransfers/ct.go
@@ -264,7 +264,7 @@ func (p PrecompileExecutor) getAuditorsFromArg(ctx sdk.Context, arg interface{})
 		TransferAmountHiEqualityProof []byte         `json:"transferAmountHiEqualityProof"`
 	})
 
-	auditors := make([]*cttypes.Auditor, len(res))
+	auditors := make([]*cttypes.Auditor, 0)
 	for _, auditor := range res {
 		auditorAddr, err := p.accAddressFromArg(ctx, auditor.AuditorAddress)
 		if err != nil {
