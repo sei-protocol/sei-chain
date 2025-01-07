9196b485bee601f2156f3115524cac96d298411d
yzang2019 <zymfrank@gmail.com>
2025-01-07 07:56:30 -0800

    Fix lint

--- sei-db/sc/memiavl/tree.go
+++ sei-db/sc/memiavl/tree.go
@@ -132,7 +132,7 @@ func (t *Tree) StartBackgroundWrite() {
 		defer t.pendingWg.Done()
 		for nextChange := range t.pendingChanges {
 			t.ApplyChangeSet(nextChange)
-			t.SaveVersion(false)
+			_, _, _ = t.SaveVersion(false)
 		}
 	}()
 }
