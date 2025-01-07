29317d70c84a6b1e4b8fcc6ca3d73d17dfee8164
Jeremy Wei <jeremy.t.wei@gmail.com>
2025-01-07 08:56:51 -0500

    remove unused prefix

--- x/evm/types/keys.go
+++ x/evm/types/keys.go
@@ -57,8 +57,7 @@ var (
 
 	LegacyBlockBloomCutoffHeightKey = []byte{0x1a}
 	BaseFeePerGasPrefix             = []byte{0x1b}
-	PrevBlockGasUsedPrefix          = []byte{0x1c}
-	PrevBlockBaseFeePerGasPrefix    = []byte{0x1d}
+	PrevBlockBaseFeePerGasPrefix    = []byte{0x1c}
 )
 
 var (
