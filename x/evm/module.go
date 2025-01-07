b1d3f6be4993fc6f953292e93e33ec61dcf832b5
Yiming Zang <50607998+yzang2019@users.noreply.github.com>
2025-01-07 09:24:50 -0800

    Revert evm migration for RemoveTxHashes (#2022)

--- x/evm/module.go
+++ x/evm/module.go
@@ -229,10 +229,6 @@ func (am AppModule) RegisterServices(cfg module.Configurator) {
 	_ = cfg.RegisterMigration(types.ModuleName, 15, func(ctx sdk.Context) error {
 		return migrations.StoreCWPointerCode(ctx, am.keeper, false, false, true)
 	})
-
-	_ = cfg.RegisterMigration(types.ModuleName, 16, func(ctx sdk.Context) error {
-		return migrations.RemoveTxHashes(ctx, am.keeper)
-	})
 }
 
 // RegisterInvariants registers the capability module's invariants.
@@ -270,7 +266,7 @@ func (am AppModule) ExportGenesisStream(ctx sdk.Context, cdc codec.JSONCodec) <-
 }
 
 // ConsensusVersion implements ConsensusVersion.
-func (AppModule) ConsensusVersion() uint64 { return 16 }
+func (AppModule) ConsensusVersion() uint64 { return 15 }
 
 // BeginBlock executes all ABCI BeginBlock logic respective to the capability module.
 func (am AppModule) BeginBlock(ctx sdk.Context, _ abci.RequestBeginBlock) {
