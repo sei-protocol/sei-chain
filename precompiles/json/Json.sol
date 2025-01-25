21ebd6839c28f9ed55ee085e3a7a1a4824935750
_dssei_ <denys@seinetwork.io>
2025-01-24 16:19:08 -0800

    adding extractAsBytesFromArray method

--- precompiles/json/Json.sol
+++ precompiles/json/Json.sol
@@ -7,11 +7,39 @@ IJson constant JSON_CONTRACT = IJson(
     JSON_PRECOMPILE_ADDRESS
 );
 
+/**
+ * @dev Interface for interacting with the JSON precompile contract.
+ */
 interface IJson {
-    // Queries
+    /**
+     * @dev Extracts a value as bytes from the JSON input using the specified key.
+     * @param input The JSON input as bytes.
+     * @param key The key to extract the value for.
+     * @return response The extracted value as bytes.
+     */
     function extractAsBytes(bytes memory input, string memory key) external view returns (bytes memory response);
 
+    /**
+     * @dev Extracts a list of values as bytes from the JSON input using the specified key.
+     * @param input The JSON input as bytes.
+     * @param key The key to extract the values for.
+     * @return response The extracted values as a list of bytes.
+     */
     function extractAsBytesList(bytes memory input, string memory key) external view returns (bytes[] memory response);
 
+    /**
+     * @dev Extracts a value as uint256 from the JSON input using the specified key.
+     * @param input The JSON input as bytes.
+     * @param key The key to extract the value for.
+     * @return response The extracted value as uint256.
+     */
     function extractAsUint256(bytes memory input, string memory key) external view returns (uint256 response);
+
+    /**
+     * @dev Extracts a value as bytes from an array in the JSON input using the specified index.
+     * @param input The JSON array as bytes.
+     * @param arrayIndex The index in the array to extract the value from.
+     * @return response The extracted value as bytes.
+     */
+    function extractAsBytesFromArray(bytes memory input, uint16 memory arrayIndex) external view returns (bytes memory response);
 }
--- precompiles/json/json_test.go
+++ precompiles/json/json_test.go
@@ -1,6 +1,7 @@
 package json_test
 
 import (
+	"github.com/ethereum/go-ethereum/accounts/abi"
 	"math/big"
 	"testing"
 
@@ -33,9 +34,6 @@ func TestExtractAsBytes(t *testing.T) {
 		}, {
 			[]byte("{\"key\":[1,2,3]}"),
 			[]byte("[1,2,3]"),
-		}, {
-			[]byte("{\"key\":{\"nested\":1}}"),
-			[]byte("{\"nested\":1}"),
 		},
 	} {
 		args, err := method.Inputs.Pack(test.body, "key")
@@ -120,4 +118,188 @@ func TestExtractAsUint256(t *testing.T) {
 		require.Equal(t, 1, len(output))
 		require.Equal(t, 0, output[0].(*big.Int).Cmp(test.expectedOutput))
 	}
+}
+
+func TestPrecompileExecutor_extractAsBytesFromArray(t *testing.T) {
+	stateDB := &state.DBImpl{}
+	stateDB.WithCtx(sdk.Context{})
+	evm := &vm.EVM{StateDB: stateDB}
+
+	type args struct {
+		in0    sdk.Context
+		method *abi.Method
+		args   []interface{}
+		value  *big.Int
+	}
+
+	type input struct {
+		body       []byte
+		indexArray uint16
+	}
+	tests := []struct {
+		name       string
+		input      input
+		args       args
+		want       []byte
+		wantErr    bool
+		wantErrMsg string
+	}{
+		{
+			name: "extracts string el from array",
+			input: input{
+				body:       []byte("[\"1\", \"2\"]"),
+				indexArray: 1,
+			},
+			want: []byte("2"),
+		},
+		{
+			name: "extracts number el from array",
+			input: input{
+				body:       []byte("[1, \"2\"]"),
+				indexArray: 0,
+			},
+			want: []byte("1"),
+		},
+		{
+			name: "extracts nested array el from array",
+			input: input{
+				body:       []byte("[1, \"2\", [1,2]]"),
+				indexArray: 2,
+			},
+			want: []byte("[1,2]"),
+		},
+		{
+			name: "extracts nested object el from array",
+			input: input{
+				body:       []byte("[1, \"2\", {\"key\":1}]"),
+				indexArray: 2,
+			},
+			want: []byte("{\"key\":1}"),
+		},
+		{
+			name: "extracts bool el from array",
+			input: input{
+				body:       []byte("[true, \"2\"]"),
+				indexArray: 0,
+			},
+			want: []byte("true"),
+		},
+		{
+			name: "extracts null el from array",
+			input: input{
+				body:       []byte("[null, \"2\"]"),
+				indexArray: 0,
+			},
+			want: []byte("null"),
+		},
+		{
+			name: "extracts empty array el from array",
+			input: input{
+				body:       []byte("[[], \"2\"]"),
+				indexArray: 0,
+			},
+			want: []byte("[]"),
+		},
+		{
+			name: "extracts empty object el from array",
+			input: input{
+				body: []byte("[{}, \"2\"]"),
+			},
+			want: []byte("{}"),
+		},
+		{
+			name: "extracts empty string el from array",
+			input: input{
+				body: []byte("[\"\", \"2\"]"),
+			},
+			want: []byte(""),
+		},
+		{
+			name: "returns error if indexArray is out of bounds",
+			input: input{
+				body:       []byte("[\"1\", \"2\"]"),
+				indexArray: 2,
+			},
+			wantErr:    true,
+			wantErrMsg: "index 2 is out of bounds",
+		},
+		{
+			name: "returns error if indexArray is out of bounds for empty array",
+			input: input{
+				body:       []byte("[]"),
+				indexArray: 0,
+			},
+			wantErr:    true,
+			wantErrMsg: "index 0 is out of bounds",
+		},
+		{
+			name: "returns error if value is passed",
+			args: args{
+				value: big.NewInt(1),
+			},
+			wantErr:    true,
+			wantErrMsg: "sending funds to a non-payable function",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p, err := json.NewPrecompile()
+			require.Nil(t, err)
+			method, err := p.MethodById(p.GetExecutor().(*json.PrecompileExecutor).ExtractAsBytesFromArrayID)
+			inputArgs, err := method.Inputs.Pack(tt.input.body, tt.input.indexArray)
+			require.Nil(t, err)
+			in := append(p.GetExecutor().(*json.PrecompileExecutor).ExtractAsBytesFromArrayID, inputArgs...)
+			res, err := p.Run(evm, common.Address{}, common.Address{}, in, tt.args.value, true, false)
+
+			if tt.wantErr {
+				require.Error(t, err)
+				require.Equal(t, tt.wantErrMsg, string(res))
+				return
+			} else {
+				output, err := method.Outputs.Unpack(res)
+				require.NoError(t, err)
+				require.Equal(t, tt.want, output[0].([]byte))
+			}
+		})
+	}
+}
+
+func TestExtractElementFromNestedArray(t *testing.T) {
+	stateDB := &state.DBImpl{}
+	stateDB.WithCtx(sdk.Context{})
+	evm := &vm.EVM{StateDB: stateDB}
+	p, err := json.NewPrecompile()
+	require.NoError(t, err)
+	methodExtractAsBytes, err := p.MethodById(p.GetExecutor().(*json.PrecompileExecutor).ExtractAsBytesID)
+	methodExtractAsBytesList, err := p.MethodById(p.GetExecutor().(*json.PrecompileExecutor).ExtractAsBytesListID)
+	methodExtractAsBytesFromArray, err := p.MethodById(p.GetExecutor().(*json.PrecompileExecutor).ExtractAsBytesFromArrayID)
+	require.NoError(t, err)
+
+	body := []byte("{\"data\":{\"exchange_rates\": [[1727762390,\"1.033207463698531844\"],[1727545459,\"1.032887092691178063\"]],\"apr\": \"0.000101145698240442\"}}")
+
+	args, err := methodExtractAsBytes.Inputs.Pack(body, "data")
+	input := append(p.GetExecutor().(*json.PrecompileExecutor).ExtractAsBytesID, args...)
+	res, err := p.Run(evm, common.Address{}, common.Address{}, input, nil, true, false)
+	require.NoError(t, err)
+	data, err := methodExtractAsBytes.Outputs.Unpack(res)
+	require.NoError(t, err)
+
+	args, err = methodExtractAsBytesList.Inputs.Pack(data[0].([]byte), "exchange_rates")
+	input2 := append(p.GetExecutor().(*json.PrecompileExecutor).ExtractAsBytesListID, args...)
+	res, err = p.Run(evm, common.Address{}, common.Address{}, input2, nil, true, false)
+	require.NoError(t, err)
+	exchangeRates, err := methodExtractAsBytesList.Outputs.Unpack(res)
+	require.NoError(t, err)
+
+	array := exchangeRates[0].([][]byte)[0] // [1727762390,"1.033207463698531844"]
+
+	require.NoError(t, err)
+	inputArgs, err := methodExtractAsBytesFromArray.Inputs.Pack(array, uint16(1))
+	require.NoError(t, err)
+	in := append(p.GetExecutor().(*json.PrecompileExecutor).ExtractAsBytesFromArrayID, inputArgs...)
+	res, err = p.Run(evm, common.Address{}, common.Address{}, in, nil, true, false)
+	require.NoError(t, err)
+	output, err := methodExtractAsBytesFromArray.Outputs.Unpack(res)
+	require.NoError(t, err)
+	require.Equal(t, []byte("1.033207463698531844"), output[0].([]byte))
 }
