36aba35f4071b718262fc532d8c50a2c8267f1fc
Jeremy Wei <jeremy.t.wei@gmail.com>
2025-02-24 14:38:39 -0500

    remove prints

--- x/evm/ante/gas.go
+++ x/evm/ante/gas.go
@@ -1,8 +1,6 @@
 package ante
 
 import (
-	"fmt"
-
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	evmkeeper "github.com/sei-protocol/sei-chain/x/evm/keeper"
 	evmtypes "github.com/sei-protocol/sei-chain/x/evm/types"
@@ -18,7 +16,6 @@ func NewGasDecorator(evmKeeper *evmkeeper.Keeper) *GasDecorator {
 
 // Called at the end of the ante chain to set gas limit and gas used estimate properly
 func (gl GasDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (sdk.Context, error) {
-	fmt.Println("[DEBUG]: In x/evm/ante/gas.go/AnteHandle: tx.GetGasEstimate() = ", tx.GetGasEstimate())
 	msg := evmtypes.MustGetEVMTransactionMessage(tx)
 	txData, err := evmtypes.UnpackTxData(msg.Data)
 	if err != nil {
@@ -27,7 +24,6 @@ func (gl GasDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, nex
 
 	adjustedGasLimit := gl.evmKeeper.GetPriorityNormalizer(ctx).MulInt64(int64(txData.GetGas()))
 	gasMeter := sdk.NewGasMeterWithMultiplier(ctx, adjustedGasLimit.TruncateInt().Uint64())
-	fmt.Printf("[DEBUG]: In x/evm/ante/gas.go/NewGasLimitDecorator: gasMeter limit = %+v\n", gasMeter.Limit())
 	ctx = ctx.WithGasMeter(gasMeter)
 	if tx.GetGasEstimate() >= 21000 {
 		ctx = ctx.WithGasEstimate(tx.GetGasEstimate())
