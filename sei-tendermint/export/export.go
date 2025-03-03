db61084c35de2bc19b837e0bff523eb4f1be86ec
codchen <codchen03@gmail.com>
2025-03-03 23:38:39 +0800

    make variables public (#266)

--- sei-tendermint/export/export.go
+++ sei-tendermint/export/export.go
@@ -1,9 +1,16 @@
 package export
 
 import (
+	"github.com/tendermint/tendermint/internal/jsontypes"
+	"github.com/tendermint/tendermint/internal/pubsub/query"
 	"github.com/tendermint/tendermint/internal/state"
 	"github.com/tendermint/tendermint/internal/store"
 )
 
+type Query = query.Query
+
 var NewBlockStore = store.NewBlockStore
 var NewStore = state.NewStore
+var NewQuery = query.New
+var QueryAll = query.All
+var JsonMarshal = jsontypes.Marshal
