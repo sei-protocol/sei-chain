7e55a4ae8e0366e122a002b52e1c1e8219800d54
Matthieu Vachon <matt@streamingfast.io>
2025-03-11 10:57:06 -0400

    Re-write how we run Firehose test reducing bootstrap problem and re-using more code

--- integration_test/firehose_test/run_firehose.sh
+++ integration_test/firehose_test/run_firehose.sh
@@ -1,38 +1,40 @@
 
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+source "$ROOT/lib.sh"
+
 set -e
 
 main() {
-    data_dir="$ROOT/.firehose-data"
-    seid="${SEID:-seid}"
+    check_fireeth
+    check_sd
+
     seid_args="start --home \"$HOME/.sei\" --trace --chain-id sei-chain"
     parallel_tx_enabled=${PARALLEL_TX_ENABLED:-"true"}
 
-    fireeth="fireeth"
-    if ! command -v "$fireeth" &> /dev/null; then
-        echo "The '$fireeth' binary could not be found, you can install it through one of those means:"
-        echo ""
-        echo "- By running 'brew install streamingfast/tap/firehose-ethereum' on Mac or Linux system (with Homebrew installed)"
-        echo "- By building it from source cloning https://github.com/streamingfast/firehose-ethereum.git and then 'go install ./cmd/fireeth'"
-        echo "- By downloading a pre-compiled binary from https://github.com/streamingfast/firehose-ethereum/releases"
-        exit 1
+    occ_enabled="true"
+    if [[ "${parallel_tx_enabled}" != "true" ]]; then
+        occ_enabled="false"
     fi
 
     echo "Running Sei node with Firehose tracer activated via 'fireeth' and parallel tx enabled: $parallel_tx_enabled"
-    rm -rf "$data_dir"
+    rm -rf "$firehose_data_dir"
 
-    "$fireeth" \
+    NO_RUN=1 "$ROOT/../../scripts/initialize_local_chain.sh"
+
+    sd '\[evm\]' "[evm]\nlive_evm_tracer = \"firehose\"" "$HOME/.sei/config/app.toml"
+    sd 'occ-enabled *=.*' "occ-enabled = ${occ_enabled}" "$HOME/.sei/config/app.toml"
+
+    exec "$fireeth" \
         start \
         reader-node,relayer,merger,firehose \
         -c '' \
-        -d "$data_dir" \
+        -d "$firehose_data_dir" \
         --advertise-chain-name=battlefield \
         --ignore-advertise-validation \
         --common-first-streamable-block=1 \
         --reader-node-path="$seid" \
         --reader-node-arguments="$seid_args" \
-        --reader-node-bootstrap-data-url="bash://$ROOT/bootstrap.sh?env_PARALLEL_TX_ENABLED=${parallel_tx_enabled}" \
         --firehose-grpc-listen-addr="localhost:8089"
 }
 
--- integration_test/firehose_test/run_test_case.sh
+++ integration_test/firehose_test/run_test_case.sh
@@ -3,23 +3,25 @@ ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CONTRACTS="$ROOT/../../contracts"
 TOP_PID=$$
 
+source "$ROOT/lib.sh"
+
 set -e
 
 main() {
+    check_fireeth
+    check_sd
+
     trap "exit 1" TERM
 
-    if ! command -v "sd" &> /dev/null; then
-        echo "The 'sd' command is required for this script, please install it"
-        echo "by following instructions at https://github.com/chmln/sd?tab=readme-ov-file#installation"
-        exit 1
-    fi
-
-    data_dir="$ROOT/.firehose-data"
-    seid="${SEID:-seid}"
     seid_args="start --home \"$HOME/.sei\" --trace --chain-id sei-chain"
     fireeth_log="$ROOT/.fireeth.log"
     start_firehose="true"
     parallel_tx_enabled=${PARALLEL_TX_ENABLED:-"true"}
+
+    occ_enabled="true"
+    if [[ "${parallel_tx_enabled}" != "true" ]]; then
+        occ_enabled="false"
+    fi
 
     while getopts "so:" opt; do
         case $opt in
@@ -29,32 +31,9 @@ main() {
     done
     shift $((OPTIND-1))
 
-    fireeth="fireeth"
-    if ! command -v "$fireeth" &> /dev/null; then
-        echo "The '$fireeth' binary could not be found, you can install it through one of those means:"
-        echo ""
-        echo "- By running 'brew install streamingfast/tap/firehose-ethereum' on Mac or Linux system (with Homebrew installed)"
-        echo "- By building it from source cloning https://github.com/streamingfast/firehose-ethereum.git and then 'go install ./cmd/fireeth'"
-        echo "- By downloading a pre-compiled binary from https://github.com/streamingfast/firehose-ethereum/releases"
-        exit 1
-    fi
-
     if [[ $start_firehose == "true" ]]; then
         echo "Running Sei node with Firehose tracer activated via 'fireeth' and parallel tx enabled: $parallel_tx_enabled"
-        rm -rf "$data_dir"
-
-        ("$fireeth" \
-            start \
-            reader-node,relayer,merger,firehose \
-            -c '' \
-            -d "$data_dir" \
-            --advertise-chain-name=battlefield \
-            --ignore-advertise-validation \
-            --common-first-streamable-block=1 \
-            --reader-node-path="$seid" \
-            --reader-node-arguments="$seid_args" \
-            --reader-node-bootstrap-data-url="bash://$ROOT/bootstrap.sh?env_PARALLEL_TX_ENABLED=${parallel_tx_enabled}" \
-            --firehose-grpc-listen-addr="localhost:8089" &> "$fireeth_log") &
+        ("$ROOT/run_firehose.sh" &> "$fireeth_log") &
         fireeth_pid=$!
         trap "cleanup" EXIT
 
