2026a9f009bc13b96dbae91f1c7676efd059bd88
Tony Chen <codchen03@gmail.com>
2025-05-18 18:06:27 +0800

    add missing wasm legacy precompiles

--- precompiles/setup.go
+++ precompiles/setup.go
@@ -79,6 +79,8 @@ import (
 	wasmdv575 "github.com/sei-protocol/sei-chain/precompiles/wasmd/legacy/v575"
 	wasmdv580 "github.com/sei-protocol/sei-chain/precompiles/wasmd/legacy/v580"
 	wasmdv600 "github.com/sei-protocol/sei-chain/precompiles/wasmd/legacy/v600"
+	wasmdv601 "github.com/sei-protocol/sei-chain/precompiles/wasmd/legacy/v601"
+	wasmdv603 "github.com/sei-protocol/sei-chain/precompiles/wasmd/legacy/v603"
 )
 
 var SetupMtx = &sync.Mutex{}
@@ -139,6 +141,8 @@ func GetCustomPrecompiles(
 		"v5.7.5":      check(wasmdv575.NewPrecompile(evmKeeper, wasmdKeeper, wasmdViewKeeper, bankKeeper)),
 		"v5.8.0":      check(wasmdv580.NewPrecompile(evmKeeper, wasmdKeeper, wasmdViewKeeper, bankKeeper)),
 		"v6.0.0":      check(wasmdv600.NewPrecompile(evmKeeper, wasmdKeeper, wasmdViewKeeper, bankKeeper)),
+		"v6.0.1":      check(wasmdv601.NewPrecompile(evmKeeper, wasmdKeeper, wasmdViewKeeper, bankKeeper)),
+		"v6.0.3":      check(wasmdv603.NewPrecompile(evmKeeper, wasmdKeeper, wasmdViewKeeper, bankKeeper)),
 	}
 	jsonVersions := VersionedPrecompiles{
 		latestUpgrade: check(json.NewPrecompile()),
