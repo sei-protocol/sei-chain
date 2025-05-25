2df47333d03f6008a60f05cbed4aac9c94e3c3f6
Tony Chen <codchen03@gmail.com>
2025-05-25 20:51:09 +0800

    set baseFee to nil during tracing

--- evmrpc/simulate.go
+++ evmrpc/simulate.go
@@ -457,7 +457,7 @@ func (b *Backend) getHeader(blockNumber *big.Int) *ethtypes.Header {
 	header := &ethtypes.Header{
 		Difficulty:    common.Big0,
 		Number:        blockNumber,
-		BaseFee:       b.keeper.GetCurrBaseFeePerGas(b.ctxProvider(LatestCtxHeight)).TruncateInt().BigInt(),
+		BaseFee:       nil,
 		GasLimit:      b.config.GasCap,
 		Time:          uint64(time.Now().Unix()),
 		ExcessBlobGas: &zeroExcessBlobGas,
