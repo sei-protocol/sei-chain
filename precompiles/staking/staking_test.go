68ec0476b5dd5560f7271354a0d0be6777b5a628
ogarciarevett <ogarciarevett@gmail.com>
2025-07-09 12:11:04 +0200

    Fix: beforeEach test setup

--- precompiles/staking/staking_test.go
+++ precompiles/staking/staking_test.go
@@ -653,10 +653,9 @@ func TestCreateValidator(t *testing.T) {
 		},
 	}
 
-	setup := setupCreateValidatorTest(t)
-
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			setup := setupCreateValidatorTest(t)
 
 			inputs, err := setup.abi.Pack("createValidator",
 				tt.args.pubKeyHex,
