23a887af07116b1ac0d1074f46c0bd1c2b1789e2
Steven Landers <steven.landers@gmail.com>
2025-07-28 15:07:59 -0400

    add unit tests

--- loadtest_v2/generator/scenarios/factory.go
+++ loadtest_v2/generator/scenarios/factory.go
@@ -11,9 +11,9 @@ var scenarioFactories = map[string]ScenarioFactory{
 	// Auto-generated entries will be added below this line by make generate
 	// DO NOT EDIT BELOW THIS LINE - AUTO-GENERATED CONTENT
 	ERC20Conflict: NewERC20ConflictScenario,
-	ERC20Noop: NewERC20NoopScenario,
-	ERC20: NewERC20Scenario,
-	ERC721: NewERC721Scenario,
+	ERC20Noop:     NewERC20NoopScenario,
+	ERC20:         NewERC20Scenario,
+	ERC721:        NewERC721Scenario,
 
 	// DO NOT EDIT ABOVE THIS LINE - AUTO-GENERATED CONTENT
 }
@@ -25,19 +25,4 @@ func CreateScenario(name string) TxGenerator {
 		panic("Unknown scenario: " + name)
 	}
 	return factory()
-}
-
-// GetAvailableScenarios returns a list of all available scenario names
-func GetAvailableScenarios() []string {
-	names := make([]string, 0, len(scenarioFactories))
-	for name := range scenarioFactories {
-		names = append(names, name)
-	}
-	return names
-}
-
-// RegisterScenario allows manual registration of scenario factories
-// This is useful for adding custom scenarios that aren't auto-generated
-func RegisterScenario(name string, factory ScenarioFactory) {
-	scenarioFactories[name] = factory
 }
