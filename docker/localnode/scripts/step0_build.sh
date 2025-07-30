e62f62d72ae9ea2c779f458f7b7eb2024e9b8f63
Uday Patil <udpatil@gmail.com>
2025-07-30 11:39:44 -0500

    Enable mock balances on docker cluster

--- docker/localnode/scripts/step0_build.sh
+++ docker/localnode/scripts/step0_build.sh
@@ -9,7 +9,8 @@ echo "Building seid from local branch"
 git config --global --add safe.directory /sei-protocol/sei-chain
 export LEDGER_ENABLED=false
 make clean
-make build-linux
+# build seid with the mock balance function enabled
+make build-linux LDFLAGS="-X github.com/sei-protocol/sei-chain/x/evm/state.mockBalanceTesting=enabled"
 make build-price-feeder-linux
 mkdir -p build/generated
 echo "DONE" > build/generated/build.complete
