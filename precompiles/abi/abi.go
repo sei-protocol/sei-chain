0bffbb03c0be85e49847b2dfeac3da91870217e7
Pray4Lovee <totalwine2338@gmail.com>
2025-08-08 00:53:02 +0000

    🧩 Fix: Make abi.U256 return (value, error) to match usage

--- precompiles/abi/abi.go
+++ precompiles/abi/abi.go
@@ -1,9 +1,11 @@
 package abi
 
-import "math/big"
+import (
+	"math/big"
+)
 
-// U256 is a stub function to mimic conversion to big.Int
-func U256(x interface{}) *big.Int {
-	return big.NewInt(0)
+// U256 is a stubbed version that returns a dummy value and nil error
+func U256(input interface{}) (*big.Int, error) {
+	return big.NewInt(0), nil
 }
 
