8ed16141f6fabf4703fa33bab98090302e9ab1e1
Pray4Lovee <totalwine2338@gmail.com>
2025-09-03 20:19:16 -0500

    Update keys.go

--- x/seinet/types/keys.go
+++ x/seinet/types/keys.go
@@ -1,8 +1,18 @@
 package types
 
 const (
-	ModuleName   = "seinet"
-	StoreKey     = ModuleName
-	RouterKey    = ModuleName
+	// ModuleName defines the module name
+	ModuleName = "seinet"
+
+	// StoreKey defines the primary module store key
+	StoreKey = ModuleName
+
+	// RouterKey is the message route for slashing
+	RouterKey = ModuleName
+
+	// QuerierRoute defines the module's query routing key
 	QuerierRoute = ModuleName
+
+	// Module account name for royalty
+	SeinetRoyaltyAccount = "seinet_module_account"
 )
