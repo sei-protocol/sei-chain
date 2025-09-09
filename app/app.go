1fd9f5bb09b15a60f4eabcc440e973bf026bc518
Jeremy Wei <jeremy.t.wei@gmail.com>
2025-09-09 17:13:58 -0400

    fix lint

--- app/app.go
+++ app/app.go
@@ -1999,9 +1999,16 @@ func (app *App) checkTotalBlockGas(ctx sdk.Context, txs [][]byte) bool {
 
 		totalGasWanted += gasWanted
 
-		// If the gas estimate is set and at least 21k (the minimum gas needed for an EVM tx) and less than the block max gas, use the gas estimate.
-		// Otherwise, use the gas wanted. Typically the gas estimate is set for EVM txs and not set for Cosmos txs.
-		if decodedTx.GetGasEstimate() >= MinGasEVMTx && decodedTx.GetGasEstimate() <= uint64(ctx.ConsensusParams().Block.MaxGas) {
+		// If the gas estimate is set and at least 21k (the minimum gas needed for an EVM tx)
+		// and less than or equal to the block max gas, use the gas estimate. Otherwise, use gasWanted.
+		useEstimate := false
+		if decodedTx.GetGasEstimate() >= MinGasEVMTx {
+			maxBlockGas := ctx.ConsensusParams().Block.MaxGas
+			if maxBlockGas > 0 && decodedTx.GetGasEstimate() <= uint64(maxBlockGas) { // safe: guarded by maxBlockGas>0
+				useEstimate = true
+			}
+		}
+		if useEstimate {
 			totalGas += decodedTx.GetGasEstimate()
 		} else {
 			totalGas += gasWanted
