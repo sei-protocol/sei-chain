3263c6d110019fdd1dfcb43acc69e02377a06277
Tony Chen <codchen03@gmail.com>
2025-10-10 17:26:06 +0800

    apply wasm fix

--- precompiles/solo/solo_test.go
+++ precompiles/solo/solo_test.go
@@ -177,7 +177,7 @@ func TestClaimSpecificCW721(t *testing.T) {
 	k.AccountKeeper().SetAccount(origCtx, acc)
 	contractAddr := setupCW721Contract(origCtx, claimeeKey, *wKeeper)
 	ctx, _ := origCtx.CacheContext()
-	ctx = ctx.WithGasMeter(sdk.NewGasMeter(2000000, 1, 1))
+	ctx = ctx.WithGasMeter(sdk.NewGasMeter(3000000, 1, 1))
 	_, remainingGas, err := p.ClaimSpecific(ctx, claimer, &method, []interface{}{signClaimMsg(t, evmtypes.NewMsgClaimSpecific(claimee, claimer, &evmtypes.Asset{AssetType: evmtypes.AssetType_TYPECW721, ContractAddress: contractAddr.String()}), claimee, claimer, acc, claimeeKey)}, false)
 	ctx = ctx.WithGasMeter(sdk.NewInfiniteGasMeterWithMultiplier(ctx))
 	require.NoError(t, err)
@@ -207,7 +207,7 @@ func TestClaimSingleCW721(t *testing.T) {
 	_, remainingGas, err := p.ClaimSpecific(ctx, claimer, &method, []interface{}{signClaimMsg(t, evmtypes.NewMsgClaimSpecific(claimee, claimer, &evmtypes.Asset{AssetType: evmtypes.AssetType_TYPECW721, ContractAddress: contractAddr.String(), Denom: "5"}), claimee, claimer, acc, claimeeKey)}, false)
 	ctx = ctx.WithGasMeter(sdk.NewInfiniteGasMeterWithMultiplier(ctx))
 	require.NoError(t, err)
-	require.Equal(t, uint64(1903347), remainingGas)
+	require.Equal(t, uint64(1838040), remainingGas)
 	for i := 0; i < 15; i++ {
 		if i == 5 {
 			require.Equal(t, k.GetSeiAddressOrDefault(ctx, claimer).String(), queryCW721Owner(ctx, testkeeper.EVMTestApp.WasmKeeper, contractAddr, fmt.Sprintf("%d", i)))
