4a3f93cd29953bfd01ea2050adc178e40f80633c
Grzegorz Prusak <pompon.pompon@gmail.com>
2025-10-27 12:32:22 +0100

    simplified p2p.Channel (#2471)
    Simplified the channel implementation. Now channels don't have
    background tasks. Sending messages to channel is non-blocking and never
    fails. Messages sent to channels go directly to relevant peer queues.
    Peer errors are reported directly to PeerManager. Additionally:
    * wrapped the PeerManager.ready set into a Watch, so that changes to the
    connection set are directly observable
    * added PreexistingPeers to PeerUpdates, so that an observer can
    reconstruct the connection set no matter when they subscribe to updates
    (PeerUpdates will be eventually removed in favor of observing the
    connection set directly).
    * made a lot of test actually use the Router instead of faking the peers
    on the channel.
    * made Router pass around net.TCPConn instead of net.Conn (implicitly
    assumed to be a TCPConn anyway)
    * replaced LimitListener with a regular listener + semaphore.

--- sei-tendermint/node/node_test.go
+++ sei-tendermint/node/node_test.go
@@ -754,7 +754,7 @@ func loadStatefromGenesis(ctx context.Context, t *testing.T) sm.State {
 	require.NoError(t, err)
 	require.True(t, loadedState.IsEmpty())
 
-	valSet, _ := factory.ValidatorSet(ctx, t, 0, 10)
+	valSet, _ := factory.ValidatorSet(ctx, 0, 10)
 	genDoc := factory.GenesisDoc(cfg, time.Now(), valSet.Validators, factory.ConsensusParams())
 
 	state, err := LoadStateFromDBOrGenesisDocProvider(
