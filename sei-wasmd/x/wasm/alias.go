a465bc2a0437c89480f2b3ead692950cf2ca8846
pdrobnjak <pavle@tenderly.co>
2025-10-29 15:22:14 +0100

    Removed unused code [part 2] (#2513)
    ## Describe your changes and provide context
    
    Removing unused code.
    
    ## Testing performed to validate your change
    
    No testing needed, code is not used anywhere.

--- sei-wasmd/x/wasm/alias.go
+++ sei-wasmd/x/wasm/alias.go
@@ -14,91 +14,42 @@ const (
 	firstCodeID                     = 1
 	ModuleName                      = types.ModuleName
 	StoreKey                        = types.StoreKey
-	TStoreKey                       = types.TStoreKey
 	QuerierRoute                    = types.QuerierRoute
 	RouterKey                       = types.RouterKey
-	WasmModuleEventType             = types.WasmModuleEventType
-	AttributeKeyContractAddr        = types.AttributeKeyContractAddr
 	ProposalTypeStoreCode           = types.ProposalTypeStoreCode
 	ProposalTypeInstantiateContract = types.ProposalTypeInstantiateContract
-	ProposalTypeMigrateContract     = types.ProposalTypeMigrateContract
-	ProposalTypeUpdateAdmin         = types.ProposalTypeUpdateAdmin
-	ProposalTypeClearAdmin          = types.ProposalTypeClearAdmin
-	QueryListContractByCode         = keeper.QueryListContractByCode
-	QueryGetContract                = keeper.QueryGetContract
-	QueryGetContractState           = keeper.QueryGetContractState
-	QueryGetCode                    = keeper.QueryGetCode
-	QueryListCode                   = keeper.QueryListCode
-	QueryMethodContractStateSmart   = keeper.QueryMethodContractStateSmart
-	QueryMethodContractStateAll     = keeper.QueryMethodContractStateAll
-	QueryMethodContractStateRaw     = keeper.QueryMethodContractStateRaw
+
+	QueryListContractByCode = keeper.QueryListContractByCode
+	QueryGetContract        = keeper.QueryGetContract
+	QueryGetContractState   = keeper.QueryGetContractState
+	QueryGetCode            = keeper.QueryGetCode
+	QueryListCode           = keeper.QueryListCode
 )
 
 var (
 	// functions aliases
-	RegisterCodec             = types.RegisterLegacyAminoCodec
-	RegisterInterfaces        = types.RegisterInterfaces
-	ValidateGenesis           = types.ValidateGenesis
-	ConvertToProposals        = types.ConvertToProposals
-	GetCodeKey                = types.GetCodeKey
-	GetContractAddressKey     = types.GetContractAddressKey
-	GetContractStorePrefixKey = types.GetContractStorePrefix
-	NewCodeInfo               = types.NewCodeInfo
-	NewAbsoluteTxPosition     = types.NewAbsoluteTxPosition
-	NewContractInfo           = types.NewContractInfo
-	NewEnv                    = types.NewEnv
-	NewWasmCoins              = types.NewWasmCoins
-	DefaultWasmConfig         = types.DefaultWasmConfig
-	DefaultParams             = types.DefaultParams
-	InitGenesis               = keeper.InitGenesis
-	ExportGenesis             = keeper.ExportGenesis
-	ExportGenesisStream       = keeper.ExportGenesisStream
-	NewMessageHandler         = keeper.NewDefaultMessageHandler
-	DefaultEncoders           = keeper.DefaultEncoders
-	EncodeBankMsg             = keeper.EncodeBankMsg
-	NoCustomMsg               = keeper.NoCustomMsg
-	EncodeStakingMsg          = keeper.EncodeStakingMsg
-	EncodeWasmMsg             = keeper.EncodeWasmMsg
-	NewKeeper                 = keeper.NewKeeper
-	NewLegacyQuerier          = keeper.NewLegacyQuerier
-	DefaultQueryPlugins       = keeper.DefaultQueryPlugins
-	BankQuerier               = keeper.BankQuerier
-	NoCustomQuerier           = keeper.NoCustomQuerier
-	StakingQuerier            = keeper.StakingQuerier
-	WasmQuerier               = keeper.WasmQuerier
-	CreateTestInput           = keeper.CreateTestInput
-	TestHandler               = keeper.TestHandler
-	NewWasmProposalHandler    = keeper.NewWasmProposalHandler
-	NewQuerier                = keeper.Querier
-	ContractFromPortID        = keeper.ContractFromPortID
-	WithWasmEngine            = keeper.WithWasmEngine
-	NewCountTXDecorator       = keeper.NewCountTXDecorator
+	RegisterCodec      = types.RegisterLegacyAminoCodec
+	ValidateGenesis    = types.ValidateGenesis
+	ConvertToProposals = types.ConvertToProposals
+	DefaultWasmConfig  = types.DefaultWasmConfig
+	DefaultParams      = types.DefaultParams
 
-	// variable aliases
-	ModuleCdc            = types.ModuleCdc
-	DefaultCodespace     = types.DefaultCodespace
-	ErrCreateFailed      = types.ErrCreateFailed
-	ErrAccountExists     = types.ErrAccountExists
-	ErrInstantiateFailed = types.ErrInstantiateFailed
-	ErrExecuteFailed     = types.ErrExecuteFailed
-	ErrGasLimit          = types.ErrGasLimit
-	ErrInvalidGenesis    = types.ErrInvalidGenesis
-	ErrNotFound          = types.ErrNotFound
-	ErrQueryFailed       = types.ErrQueryFailed
-	ErrInvalidMsg        = types.ErrInvalidMsg
-	KeyLastCodeID        = types.KeyLastCodeID
-	KeyLastInstanceID    = types.KeyLastInstanceID
-	CodeKeyPrefix        = types.CodeKeyPrefix
-	ContractKeyPrefix    = types.ContractKeyPrefix
-	ContractStorePrefix  = types.ContractStorePrefix
-	EnableAllProposals   = types.EnableAllProposals
-	DisableAllProposals  = types.DisableAllProposals
+	InitGenesis            = keeper.InitGenesis
+	ExportGenesis          = keeper.ExportGenesis
+	ExportGenesisStream    = keeper.ExportGenesisStream
+	NewKeeper              = keeper.NewKeeper
+	CreateTestInput        = keeper.CreateTestInput
+	NewWasmProposalHandler = keeper.NewWasmProposalHandler
+	NewQuerier             = keeper.Querier
+	ContractFromPortID     = keeper.ContractFromPortID
+
+	EnableAllProposals  = types.EnableAllProposals
+	DisableAllProposals = types.DisableAllProposals
 )
 
 type (
 	ProposalType                   = types.ProposalType
 	GenesisState                   = types.GenesisState
-	Code                           = types.Code
 	Contract                       = types.Contract
 	MsgStoreCode                   = types.MsgStoreCode
 	MsgStoreCodeResponse           = types.MsgStoreCodeResponse
