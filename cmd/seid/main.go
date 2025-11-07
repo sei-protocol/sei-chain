efaa0d3f29eaa3d754ab58d28b9e8a02f8268cd4
Masih H. Derkani <m@derkani.org>
2025-11-07 12:40:13 +0000

    Warn when sei-wasmd paths are t rimmed
    Add a warning in `seid` main to warn if paths are trimmed

--- cmd/seid/main.go
+++ cmd/seid/main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
-	"github.com/sei-protocol/sei-chain/app/params"
-	"github.com/sei-protocol/sei-chain/cmd/seid/cmd"
-
+	"github.com/CosmWasm/wasmd/check"
 	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
 	"github.com/sei-protocol/sei-chain/app"
+	"github.com/sei-protocol/sei-chain/app/params"
+	"github.com/sei-protocol/sei-chain/cmd/seid/cmd"
 )
 
 func main() {
+
+	if trimmed, ok := check.RuntimePathTrimmed(); ok && trimmed {
+		_, _ = fmt.Fprintln(os.Stderr, "WARNING: paths are likely trimmed. This can result in inconsistent error stacktrace.")
+	}
+
 	params.SetAddressPrefixes()
 	rootCmd, _ := cmd.NewRootCmd()
 	if err := svrcmd.Execute(rootCmd, app.DefaultNodeHome); err != nil {
