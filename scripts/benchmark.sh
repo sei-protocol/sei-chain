1226ce30455506640fb9133256f8167a2a4b6bc4
Steven Landers <steven.landers@gmail.com>
2025-11-11 16:49:13 -0500

    add benchmark scirpt

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -14,21 +14,7 @@ fi
 
 # set key name
 keyname=admin
-# Uncomment the following if you'd like to run jaeger
-#docker stop jaeger
-#docker rm jaeger
-#docker run -d --name jaeger \
-#  -e COLLECTOR_ZIPKIN_HOST_PORT=:9411 \
-#  -p 5775:5775/udp \
-#  -p 6831:6831/udp \
-#  -p 6832:6832/udp \
-#  -p 5778:5778 \
-#  -p 16686:16686 \
-#  -p 14250:14250 \
-#  -p 14268:14268 \
-#  -p 14269:14269 \
-#  -p 9411:9411 \
-#  jaegertracing/all-in-one:1.33
+
 # clean up old sei directory
 rm -rf ~/.sei
 echo "Building..."
@@ -131,4 +117,4 @@ if [ $NO_RUN = 1 ]; then
 fi
 
 # start the chain with log tracing
-GORACE="log_path=/tmp/race/seid_race" ~/go/bin/seid start --benchmark --chain-id sei-chain
+~/go/bin/seid start --benchmark --chain-id sei-chain
