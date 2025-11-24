8e01c6d042f7aa6f9673f295152f64d4779396ea
Steven Landers <steven.landers@gmail.com>
2025-11-24 13:51:43 -0500

    add test for reverse lookup

--- go.mod
+++ go.mod
@@ -161,7 +161,6 @@ require (
 	github.com/gogo/gateway v1.1.0 // indirect
 	github.com/golang/glog v1.1.0 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
-	github.com/golang/mock v1.7.0-rc.1 // indirect
 	github.com/golang/snappy v0.0.5-0.20220116011046-fa5810519dcb // indirect
 	github.com/golangci/check v0.0.0-20180506172741-cfe4005ccda2 // indirect
 	github.com/golangci/dupl v0.0.0-20180902072040-3e9179ac440a // indirect
--- x/evm/config/config.go
+++ x/evm/config/config.go
@@ -17,6 +17,15 @@ var ChainIDMapping = map[string]int64{
 	"arctic-1":   int64(713715),
 }
 
+// EVMChainIDMapping is a mapping of cosmos chain IDs to their respective chain IDs.
+var EVMChainIDMapping = map[int64]string{
+	// pacific-1 chain ID == 0x531
+	int64(1329): "pacific-1",
+	// atlantic-2 chain ID == 0x530
+	int64(1328):   "atlantic-2",
+	int64(713715): "arctic-1",
+}
+
 func GetEVMChainID(cosmosChainID string) *big.Int {
 	if evmChainID, ok := ChainIDMapping[cosmosChainID]; ok {
 		return big.NewInt(evmChainID)
@@ -40,10 +49,6 @@ func IsLiveChainID(ctx sdk.Context) bool {
 
 // IsLiveEVMChainID returns true is this chainID is reserved for one of the live chains.
 func IsLiveEVMChainID(evmChainID int64) bool {
-	for _, v := range ChainIDMapping {
-		if v == evmChainID {
-			return true
-		}
-	}
-	return false
+	_, ok := EVMChainIDMapping[evmChainID]
+	return ok
 }
