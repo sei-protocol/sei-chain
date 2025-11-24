3a2d7076da49029c4657ac8b8e830d9959c2d8b5
Grzegorz Prusak <pompon.pompon@gmail.com>
2025-11-24 15:46:26 +0100

    test fix

--- sei-tendermint/internal/statesync/message_test.go
+++ sei-tendermint/internal/statesync/message_test.go
@@ -12,6 +12,90 @@ import (
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 )
 
+func TestValidateMsg(t *testing.T) {
+	testcases := map[string]struct {
+		msg   *pb.Message
+		valid bool
+	}{
+		"nil": {nil, false},
+
+		"ChunkRequest valid":    {wrap(&pb.ChunkRequest{Height: 1, Format: 1, Index: 1}), true},
+		"ChunkRequest 0 height": {wrap(&pb.ChunkRequest{Height: 0, Format: 1, Index: 1}), false},
+		"ChunkRequest 0 format": {wrap(&pb.ChunkRequest{Height: 1, Format: 0, Index: 1}), true},
+		"ChunkRequest 0 chunk":  {wrap(&pb.ChunkRequest{Height: 1, Format: 1, Index: 0}), true},
+
+		"ChunkResponse valid": {
+			wrap(&pb.ChunkResponse{Height: 1, Format: 1, Index: 1, Chunk: []byte{1}}),
+			true,
+		},
+		"ChunkResponse 0 height": {
+			wrap(&pb.ChunkResponse{Height: 0, Format: 1, Index: 1, Chunk: []byte{1}}),
+			false,
+		},
+		"ChunkResponse 0 format": {
+			wrap(&pb.ChunkResponse{Height: 1, Format: 0, Index: 1, Chunk: []byte{1}}),
+			true,
+		},
+		"ChunkResponse 0 chunk": {
+			wrap(&pb.ChunkResponse{Height: 1, Format: 1, Index: 0, Chunk: []byte{1}}),
+			true,
+		},
+		"ChunkResponse empty body": {
+			wrap(&pb.ChunkResponse{Height: 1, Format: 1, Index: 1, Chunk: []byte{}}),
+			true,
+		},
+		"ChunkResponse nil body": {
+			wrap(&pb.ChunkResponse{Height: 1, Format: 1, Index: 1, Chunk: nil}),
+			false,
+		},
+		"ChunkResponse missing": {
+			wrap(&pb.ChunkResponse{Height: 1, Format: 1, Index: 1, Missing: true}),
+			true,
+		},
+		"ChunkResponse missing with empty": {
+			wrap(&pb.ChunkResponse{Height: 1, Format: 1, Index: 1, Missing: true, Chunk: []byte{}}),
+			true,
+		},
+		"ChunkResponse missing with body": {
+			wrap(&pb.ChunkResponse{Height: 1, Format: 1, Index: 1, Missing: true, Chunk: []byte{1}}),
+			false,
+		},
+
+		"SnapshotsRequest valid": {wrap(&pb.SnapshotsRequest{}), true},
+
+		"SnapshotsResponse valid": {
+			wrap(&pb.SnapshotsResponse{Height: 1, Format: 1, Chunks: 2, Hash: []byte{1}}),
+			true,
+		},
+		"SnapshotsResponse 0 height": {
+			wrap(&pb.SnapshotsResponse{Height: 0, Format: 1, Chunks: 2, Hash: []byte{1}}),
+			false,
+		},
+		"SnapshotsResponse 0 format": {
+			wrap(&pb.SnapshotsResponse{Height: 1, Format: 0, Chunks: 2, Hash: []byte{1}}),
+			true,
+		},
+		"SnapshotsResponse 0 chunks": {
+			wrap(&pb.SnapshotsResponse{Height: 1, Format: 1, Hash: []byte{1}}),
+			false,
+		},
+		"SnapshotsResponse no hash": {
+			wrap(&pb.SnapshotsResponse{Height: 1, Format: 1, Chunks: 2, Hash: []byte{}}),
+			false,
+		},
+	}
+
+	for name, tc := range testcases {
+		t.Run(name, func(t *testing.T) {
+			if tc.valid {
+				require.NoError(t, tc.msg.Validate())
+			} else {
+				require.Error(t, tc.msg.Validate())
+			}
+		})
+	}
+}
+
 // TODO(gprusak): why do we need this test?
 // AFAICT we don't need these messages to have stable representations.
 func TestStateSyncVectors(t *testing.T) {
