e4ecde628b4e9f5c1051bce9533fb3d35cede520
Grzegorz Prusak <pompon.pompon@gmail.com>
2025-12-11 10:12:42 +0100

    fmt

--- sei-tendermint/cmd/buf_plugin/main.go
+++ sei-tendermint/cmd/buf_plugin/main.go
@@ -12,7 +12,7 @@ import (
 	"google.golang.org/protobuf/types/dynamicpb"
 )
 
-type md struct { protoreflect.MessageDescriptor }
+type md struct{ protoreflect.MessageDescriptor }
 type mds = map[protoreflect.FullName]md
 
 func getExtType(p *protogen.Plugin, name protoreflect.FullName) (protoreflect.ExtensionType, bool) {
@@ -20,15 +20,17 @@ func getExtType(p *protogen.Plugin, name protoreflect.FullName) (protoreflect.Ex
 		es := file.Desc.Extensions()
 		for i := range es.Len() {
 			if e := es.Get(i); e.FullName() == name {
-				return dynamicpb.NewExtensionType(e),true
+				return dynamicpb.NewExtensionType(e), true
 			}
-		}	
+		}
 	}
 	return nil, false
 }
 
 func (d md) walk(yield func(md) bool) bool {
-	if !yield(d) { return false }
+	if !yield(d) {
+		return false
+	}
 	descs := d.Messages()
 	for i := range descs.Len() {
 		if !(md{descs.Get(i)}).walk(yield) {
@@ -47,7 +49,7 @@ func allMDs(plugin *protogen.Plugin) iter.Seq[md] {
 					return
 				}
 			}
-		}	
+		}
 	}
 }
 
@@ -56,7 +58,7 @@ func (d md) GetBoolOption(opt protoreflect.ExtensionType) bool {
 	if !ok || !proto.HasExtension(options, opt) {
 		return false
 	}
-	has,ok := proto.GetExtension(options, opt).(bool)
+	has, ok := proto.GetExtension(options, opt).(bool)
 	return ok && has
 }
 
@@ -67,7 +69,7 @@ func (d md) GetBoolOption(opt protoreflect.ExtensionType) bool {
 // * fields of can_hash messages cannot be maps
 func run(p *protogen.Plugin) error {
 	canHashName := protoreflect.FullName("tendermint.utils.can_hash")
-	canHashOpt,ok := getExtType(p,canHashName)
+	canHashOpt, ok := getExtType(p, canHashName)
 	if !ok {
 		// When the module being processed does not declare the extension we have nothing to validate.
 		return nil
@@ -79,22 +81,22 @@ func run(p *protogen.Plugin) error {
 		}
 	}
 	log.Printf("buf_plugin: found can_hash option; %d message type(s) marked with it", len(descs))
-	for _,d := range descs {
-		if d.Syntax()!=protoreflect.Proto3 {
-			return fmt.Errorf("%q: can_hash messages have to be in proto3 syntax",d.FullName())
+	for _, d := range descs {
+		if d.Syntax() != protoreflect.Proto3 {
+			return fmt.Errorf("%q: can_hash messages have to be in proto3 syntax", d.FullName())
 		}
 		fields := d.Fields()
 		for i := 0; i < fields.Len(); i++ {
 			f := fields.Get(i)
 			if f.IsMap() {
-				return fmt.Errorf("%q: maps are not allowed in can_hash messages",f.FullName())
+				return fmt.Errorf("%q: maps are not allowed in can_hash messages", f.FullName())
 			}
 			if !f.IsList() && !f.HasPresence() {
-				return fmt.Errorf("%q: all fields of can_hash messages should be optional or repeated",f.FullName())
+				return fmt.Errorf("%q: all fields of can_hash messages should be optional or repeated", f.FullName())
 			}
 			if f.Kind() == protoreflect.MessageKind {
-				if _,ok := descs[f.Message().FullName()]; !ok {
-					return fmt.Errorf("%q: message fields of can_hash messages have to be can_hash",f.FullName())
+				if _, ok := descs[f.Message().FullName()]; !ok {
+					return fmt.Errorf("%q: message fields of can_hash messages have to be can_hash", f.FullName())
 				}
 			}
 		}
