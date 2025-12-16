7df6c59fa28b2a5b1fa76e42b7171f669443e880
Masih H. Derkani <m@derkani.org>
2025-12-16 08:44:00 +0000

    Fix `go fmt` issue

--- sei-ibc-go/modules/core/05-port/types/query.pb.go
+++ sei-ibc-go/modules/core/05-port/types/query.pb.go
@@ -6,9 +6,9 @@ package types
 import (
 	context "context"
 	fmt "fmt"
-	types "github.com/sei-protocol/sei-chain/sei-ibc-go/modules/core/04-channel/types"
 	grpc1 "github.com/gogo/protobuf/grpc"
 	proto "github.com/gogo/protobuf/proto"
+	types "github.com/sei-protocol/sei-chain/sei-ibc-go/modules/core/04-channel/types"
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
