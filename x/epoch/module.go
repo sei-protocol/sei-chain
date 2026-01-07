5f0c9a829e756559468a2696fe6af3343c56c6e8
Olexandr88 <radole1203@gmail.com>
2026-01-07 18:50:32 +0200

    chore(epoch): remove unused starport scaffolding comment

--- x/epoch/module.go
+++ x/epoch/module.go
@@ -5,8 +5,6 @@ import (
 	"encoding/json"
 	"fmt"
 
-	// this line is used by starport scaffolding # 1
-
 	"github.com/gorilla/mux"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"github.com/spf13/cobra"
