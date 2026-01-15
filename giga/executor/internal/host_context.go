5327cdad1a4d7adb295c14dd484de063761fea4e
Aayush <arajasek94@gmail.com>
2026-01-15 10:44:03 -0500

    go fmt

--- giga/executor/internal/host_context.go
+++ giga/executor/internal/host_context.go
@@ -3,12 +3,12 @@ package internal
 import (
 	"github.com/ethereum/evmc/v12/bindings/go/evmc"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/params"
-	"github.com/sei-protocol/sei-chain/giga/executor/precompiles"
 	"github.com/ethereum/go-ethereum/core/tracing"
 	ethtypes "github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/core/vm"
+	"github.com/ethereum/go-ethereum/params"
 	"github.com/holiman/uint256"
+	"github.com/sei-protocol/sei-chain/giga/executor/precompiles"
 )
 
 var _ evmc.HostContext = (*HostContext)(nil)
