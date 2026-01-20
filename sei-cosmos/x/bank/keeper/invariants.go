df6be55b1053b11b5c6c860f0e1b6ecdc248dbef
Jeremy Wei <jeremy.t.wei@gmail.com>
2026-01-20 13:42:47 -0500

    fix

---
+++ sei-cosmos/x/bank/keeper/invariants.go
@@ -0,0 +1,58 @@
+package keeper
+
+import (
+	"fmt"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/query"
+	"github.com/cosmos/cosmos-sdk/x/bank/types"
+)
+
+// TotalSupply checks that the total supply reflects all the coins held in accounts
+func TotalSupply(k Keeper) sdk.Invariant {
+	return func(ctx sdk.Context) (string, bool) {
+		expectedTotal := sdk.Coins{}
+		weiTotal := sdk.NewInt(0)
+		supply, _, err := k.GetPaginatedTotalSupply(ctx, &query.PageRequest{Limit: query.MaxLimit})
+
+		if err != nil {
+			return sdk.FormatInvariant(types.ModuleName, "query supply",
+				fmt.Sprintf("error querying total supply %v", err)), false
+		}
+
+		k.IterateAllBalances(ctx, func(_ sdk.AccAddress, balance sdk.Coin) bool {
+			expectedTotal = expectedTotal.Add(balance)
+			return false
+		})
+		// also iterate over deferred balances
+		k.IterateDeferredBalances(ctx, func(addr sdk.AccAddress, coin sdk.Coin) bool {
+			expectedTotal = expectedTotal.Add(coin)
+			return false
+		})
+		k.IterateAllWeiBalances(ctx, func(addr sdk.AccAddress, balance sdk.Int) bool {
+			weiTotal = weiTotal.Add(balance)
+			return false
+		})
+		weiInUsei, weiRemainder := SplitUseiWeiAmount(weiTotal)
+		if !weiRemainder.IsZero() {
+			return sdk.FormatInvariant(types.ModuleName, "total supply",
+				fmt.Sprintf(
+					"\twei remainder: %v\n",
+					weiRemainder)), true
+		}
+		baseDenom, err := sdk.GetBaseDenom()
+		if err == nil {
+			expectedTotal = expectedTotal.Add(sdk.NewCoin(baseDenom, weiInUsei))
+		} else if !weiInUsei.IsZero() {
+			return sdk.FormatInvariant(types.ModuleName, "total supply", "non-zero wei balance without base denom"), true
+		}
+
+		broken := !expectedTotal.IsEqual(supply)
+
+		return sdk.FormatInvariant(types.ModuleName, "total supply",
+			fmt.Sprintf(
+				"\tsum of accounts coins: %v\n"+
+					"\tsupply.Total:          %v\n",
+				expectedTotal, supply)), broken
+	}
+}
