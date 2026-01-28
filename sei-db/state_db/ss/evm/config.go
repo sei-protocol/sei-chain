57817ce1f7a476cca4bdb519ac653544b6e97736
kbhat1 <kartikbhatri@gmail.com>
2026-01-27 21:34:39 -0500

    refactor(sei-db): Split store types to types.go, add config tests
    - Move EVMStoreType and related constants to types.go
    - Keep only config-related code in config.go
    - Remove StoreCodeSize (not part of standard EVM state)
    - Add StoreBalance placeholder for future migration
    - Add NumEVMStoreTypes constant for map pre-allocation
    - Add unit tests for config and types

--- sei-db/state_db/ss/evm/config.go
+++ sei-db/state_db/ss/evm/config.go
@@ -1,51 +1,13 @@
 package evm
 
 import (
-	commonevm "github.com/sei-protocol/sei-chain/sei-db/common/evm"
+	"github.com/sei-protocol/sei-chain/sei-db/config"
 )
 
-// EVMStoreKey is the cosmos store key for EVM module
-const EVMStoreKey = "evm"
+// EVMStoreConfig defines configuration for the EVM state stores.
+type EVMStoreConfig = config.EVMStateStoreConfig
 
-// EVMStoreType identifies the type of EVM sub-database.
-// Alias to EVMKeyKind from common/evm - use commonevm.ParseEVMKey for routing.
-type EVMStoreType = commonevm.EVMKeyKind
-
-// Re-export EVMKeyKind constants for convenience
-const (
-	StoreUnknown  = commonevm.EVMKeyUnknown
-	StoreNonce    = commonevm.EVMKeyNonce
-	StoreCodeHash = commonevm.EVMKeyCodeHash
-	StoreCode     = commonevm.EVMKeyCode
-	StoreCodeSize = commonevm.EVMKeyCodeSize
-	StoreStorage  = commonevm.EVMKeyStorage
-)
-
-// AllEVMStoreTypes returns all EVM store types that have separate DBs
-func AllEVMStoreTypes() []EVMStoreType {
-	return []EVMStoreType{
-		StoreNonce,
-		StoreCodeHash,
-		StoreCode,
-		StoreCodeSize,
-		StoreStorage,
-	}
-}
-
-// StoreTypeName returns a human-readable name for the store type (used for DB directories)
-func StoreTypeName(st EVMStoreType) string {
-	switch st {
-	case StoreNonce:
-		return "nonce"
-	case StoreCodeHash:
-		return "codehash"
-	case StoreCode:
-		return "code"
-	case StoreCodeSize:
-		return "codesize"
-	case StoreStorage:
-		return "storage"
-	default:
-		return "unknown"
-	}
+// DefaultEVMStoreConfig returns the default EVM store configuration.
+func DefaultEVMStoreConfig() EVMStoreConfig {
+	return config.DefaultEVMStateStoreConfig()
 }
