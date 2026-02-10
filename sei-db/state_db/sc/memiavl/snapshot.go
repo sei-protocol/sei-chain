3d1435272419ad01d63182b8ba46cbea905fc714
blindchaser <zengyiren0@gmail.com>
2026-02-10 11:58:06 -0500

    remove MADV_WILLNEED during loadtree

--- sei-db/state_db/sc/memiavl/snapshot.go
+++ sei-db/state_db/sc/memiavl/snapshot.go
@@ -180,13 +180,15 @@ func OpenSnapshot(snapshotDir string, opts Options) (*Snapshot, error) {
 		return errors.Join(errs...)
 	}
 
-	if nodesMap, err = NewMmap(filepath.Join(snapshotDir, FileNameNodes)); err != nil {
+	// Load snapshot mmap files with MADV_RANDOM.
+	// Snapshot prefetch is handled separately by prefetchSnapshot() at the end of this function.
+	if nodesMap, err = NewMmapRandom(filepath.Join(snapshotDir, FileNameNodes)); err != nil {
 		return nil, cleanupHandles(err)
 	}
-	if leavesMap, err = NewMmap(filepath.Join(snapshotDir, FileNameLeaves)); err != nil {
+	if leavesMap, err = NewMmapRandom(filepath.Join(snapshotDir, FileNameLeaves)); err != nil {
 		return nil, cleanupHandles(err)
 	}
-	if kvsMap, err = NewMmap(filepath.Join(snapshotDir, FileNameKVs)); err != nil {
+	if kvsMap, err = NewMmapRandom(filepath.Join(snapshotDir, FileNameKVs)); err != nil {
 		return nil, cleanupHandles(err)
 	}
 
