a9fc87e7820dc4eb466186579c65fc9421557d5a
Grzegorz Prusak <pompon.pompon@gmail.com>
2026-02-19 16:02:35 +0100

    missing file

---
+++ sei-tendermint/abci/types/plain_types.go
@@ -0,0 +1,10 @@
+package types
+
+// RequestLoadLatest is the plain Go replacement for the legacy protobuf message.
+type RequestLoadLatest struct{}
+
+// ResponseLoadLatest is the plain Go replacement for the legacy protobuf message.
+type ResponseLoadLatest struct{}
+
+// RequestListSnapshots is the plain Go replacement for the legacy protobuf message.
+type RequestListSnapshots struct{}
