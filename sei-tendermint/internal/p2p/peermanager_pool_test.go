5ea16eb2b13a5f5c4c736919188123e512b415c6
Grzegorz Prusak <pompon.pompon@gmail.com>
2026-02-25 15:19:55 +0100

    more tests

--- sei-tendermint/internal/p2p/peermanager_pool_test.go
+++ sei-tendermint/internal/p2p/peermanager_pool_test.go
@@ -5,13 +5,14 @@ import (
 
 	"github.com/sei-protocol/sei-chain/sei-tendermint/libs/utils"
 	"github.com/sei-protocol/sei-chain/sei-tendermint/libs/utils/require"
+	"github.com/sei-protocol/sei-chain/sei-tendermint/types"
 )
 
 func TestPool_AddAddr_deduplicate(t *testing.T) {
 	rng := utils.TestRng()
 	selfID := makeNodeID(rng)
 	p := newPool[*fakeConn](poolConfig{selfID: selfID})
-	require.False(t, p.AddAddr(makeAddrFor(rng,selfID)))
+	require.False(t, p.AddAddr(makeAddrFor(rng, selfID)))
 	require.Nil(t, p.addrs[selfID])
 
 	peer := makeNodeID(rng)
@@ -25,7 +26,7 @@ func TestPool_AddAddr_prune_failed_addrs(t *testing.T) {
 	rng := utils.TestRng()
 	selfID := makeNodeID(rng)
 	p := newPool[*fakeConn](poolConfig{
-		selfID: selfID,
+		selfID:          selfID,
 		maxAddrsPerPeer: utils.Some(1),
 	})
 	peer := makeNodeID(rng)
@@ -33,12 +34,12 @@ func TestPool_AddAddr_prune_failed_addrs(t *testing.T) {
 		// Insert a new address which should replace the old one.
 		addr := makeAddrFor(rng, peer)
 		require.True(t, p.AddAddr(addr))
-		
+
 		// Dial and fail multiple times.
 		// Only the newest address is expected, since maxAddrsPerPeer == 1
 		for range 5 {
-			dialAddr,ok := p.TryStartDial()
-			require.Equal(t, utils.Some(addr), opt(dialAddr,ok))
+			dialAddr, ok := p.TryStartDial()
+			require.Equal(t, utils.Some(addr), opt(dialAddr, ok))
 			p.DialFailed(dialAddr)
 		}
 	}
@@ -48,7 +49,7 @@ func TestPool_AddAddr_prune_failed_peers(t *testing.T) {
 	rng := utils.TestRng()
 	selfID := makeNodeID(rng)
 	p := newPool[*fakeConn](poolConfig{
-		selfID: selfID,
+		selfID:   selfID,
 		maxAddrs: utils.Some(1),
 	})
 	for range 3 {
@@ -56,12 +57,12 @@ func TestPool_AddAddr_prune_failed_peers(t *testing.T) {
 		// Insert a new peer which should replace the old one.
 		addr := makeAddrFor(rng, peer)
 		require.True(t, p.AddAddr(addr))
-		
+
 		// Dial and fail multiple times.
 		// Only the newest address is expected, since maxAddrsPerPeer == 1
 		for range 5 {
-			dialAddr,ok := p.TryStartDial()
-			require.Equal(t, utils.Some(addr), opt(dialAddr,ok))
+			dialAddr, ok := p.TryStartDial()
+			require.Equal(t, utils.Some(addr), opt(dialAddr, ok))
 			p.DialFailed(dialAddr)
 		}
 	}
@@ -98,13 +99,13 @@ func TestPool_Connected_deduplicate(t *testing.T) {
 	rng := utils.TestRng()
 	p := newPool[*fakeConn](poolConfig{selfID: makeNodeID(rng)})
 	peer := makeNodeID(rng)
-	oldConn := makeConnFor(rng,peer,utils.GenBool(rng))
+	oldConn := makeConnFor(rng, peer, utils.GenBool(rng))
 	require.NoError(t, p.Connected(oldConn))
 	for range 100 {
 		require.False(t, oldConn.Closed())
-		newConn := makeConnFor(rng,peer,utils.GenBool(rng))
-		if err := p.Connected(newConn); err==nil {
-			newConn,oldConn = oldConn,newConn
+		newConn := makeConnFor(rng, peer, utils.GenBool(rng))
+		if err := p.Connected(newConn); err == nil {
+			newConn, oldConn = oldConn, newConn
 		}
 		require.True(t, newConn.Closed())
 		p.Disconnected(newConn)
@@ -112,7 +113,9 @@ func TestPool_Connected_deduplicate(t *testing.T) {
 }
 
 func opt[T any](v T, ok bool) utils.Option[T] {
-	if ok { return utils.Some(v) }
+	if ok {
+		return utils.Some(v)
+	}
 	return utils.None[T]()
 }
 
@@ -123,29 +126,31 @@ func TestPool_Connected_race(t *testing.T) {
 		p1 := newPool[*fakeConn](poolConfig{selfID: makeNodeID(rng)})
 		p2 := newPool[*fakeConn](poolConfig{selfID: makeNodeID(rng)})
 		// They know each others addresses.
-		p1addr := makeAddrFor(rng,p1.selfID)
-		p2addr := makeAddrFor(rng,p2.selfID)
-		require.True(t,p1.AddAddr(p2addr))
-		require.True(t,p2.AddAddr(p1addr))
+		p1addr := makeAddrFor(rng, p1.selfID)
+		p2addr := makeAddrFor(rng, p2.selfID)
+		require.True(t, p1.AddAddr(p2addr))
+		require.True(t, p2.AddAddr(p1addr))
 		// They dial each other.
-		require.Equal(t,utils.Some(p2addr),opt(p1.TryStartDial()))
-		require.Equal(t,utils.Some(p1addr),opt(p2.TryStartDial()))
-		p1c1,p2c1 := makeConnFor(rng,p2.selfID,false),makeConnTo(p1addr)
-		p1c2,p2c2 := makeConnTo(p2addr),makeConnFor(rng,p1.selfID,false)
-		// Connections are established in random order on each side. 
-		conns := utils.Slice(p1c1,p2c1,p1c2,p2c2)
-		utils.Shuffle(rng,conns)
-		for _,c := range conns {
+		require.Equal(t, utils.Some(p2addr), opt(p1.TryStartDial()))
+		require.Equal(t, utils.Some(p1addr), opt(p2.TryStartDial()))
+		p1c1, p2c1 := makeConnFor(rng, p2.selfID, false), makeConnTo(p1addr)
+		p1c2, p2c2 := makeConnTo(p2addr), makeConnFor(rng, p1.selfID, false)
+		// Connections are established in random order on each side.
+		conns := utils.Slice(p1c1, p2c1, p1c2, p2c2)
+		utils.Shuffle(rng, conns)
+		for _, c := range conns {
 			switch c.info.ID {
-			case p1.selfID: _ = p2.Connected(c)
-			case p2.selfID: _ = p1.Connected(c)
+			case p1.selfID:
+				_ = p2.Connected(c)
+			case p2.selfID:
+				_ = p1.Connected(c)
 			}
 		}
 		// Connections should be closed consistently on both sides.
-		require.Equal(t,p1c1.Closed(),p2c1.Closed())
-		require.Equal(t,p1c2.Closed(),p2c2.Closed())
+		require.Equal(t, p1c1.Closed(), p2c1.Closed())
+		require.Equal(t, p1c2.Closed(), p2c2.Closed())
 		// One connection should survice.
-		require.Equal(t,p1c1.Closed(),!p1c2.Closed())
+		require.Equal(t, p1c1.Closed(), !p1c2.Closed())
 	}
 }
 
@@ -156,7 +161,7 @@ func TestPool_Evict(t *testing.T) {
 	peer := makeNodeID(rng)
 	addr := makeAddrFor(rng, peer)
 	require.True(t, p.AddAddr(addr))
-	require.Equal(t,utils.Some(addr),opt(p.TryStartDial()))
+	require.Equal(t, utils.Some(addr), opt(p.TryStartDial()))
 	conn := makeConnTo(addr)
 	require.NoError(t, p.Connected(conn))
 
@@ -164,4 +169,39 @@ func TestPool_Evict(t *testing.T) {
 	p.Evict(peer)
 	require.True(t, conn.Closed())
 	require.Equal(t, utils.None[NodeAddress](), opt(p.TryStartDial()))
+}
+
+func TestPool_OutboundCount(t *testing.T) {
+	rng := utils.TestRng()
+	p := newPool[*fakeConn](poolConfig{
+		selfID:   makeNodeID(rng),
+		maxConns: utils.Some(3),
+	})
+	var ids []types.NodeID
+	for range 5 {
+		ids = append(ids, makeNodeID(rng))
+	}
+	conns := map[types.NodeID]*fakeConn{}
+	for range 100 {
+		id := ids[rng.Intn(len(ids))]
+		if conns[id] != nil && utils.GenBool(rng) {
+			p.Disconnected(conns[id])
+			require.True(t, conns[id].Closed())
+			conns[id] = nil
+		} else {
+			conn := makeConnFor(rng, id, utils.GenBool(rng))
+			if err := p.Connected(conn); err == nil {
+				conns[id], conn = conn, conns[id]
+			}
+			require.True(t, conn == nil || conn.Closed())
+			require.True(t, conns[id] == nil || !conns[id].Closed())
+		}
+		outbound := 0
+		for _, c := range conns {
+			if c != nil && c.info.DialAddr.IsPresent() {
+				outbound += 1
+			}
+		}
+		require.Equal(t, outbound, p.outbound)
+	}
 }
