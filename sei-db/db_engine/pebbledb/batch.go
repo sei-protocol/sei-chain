267feae911bbca7dd0f64bca540288039ad27db9
Cody Littley <cody.littley@seinetwork.io>
2026-03-06 11:45:56 -0600

    bugfix

--- sei-db/db_engine/pebbledb/batch.go
+++ sei-db/db_engine/pebbledb/batch.go
@@ -2,38 +2,67 @@ package pebbledb
 
 import (
 	"github.com/cockroachdb/pebble/v2"
+	"github.com/sei-protocol/sei-chain/sei-db/db_engine/pebbledb/flatcache"
 	"github.com/sei-protocol/sei-chain/sei-db/db_engine/types"
 )
+
+type pendingCacheWrite struct {
+	key      []byte
+	value    []byte
+	isDelete bool
+}
 
 // pebbleBatch wraps a Pebble batch for atomic writes.
 // Important: Callers must call Close() after Commit() to release batch resources,
 // even if Commit() succeeds. Failure to Close() will leak memory.
 type pebbleBatch struct {
-	b *pebble.Batch
+	b     *pebble.Batch
+	cache flatcache.Cache
+
+	// Writes are tracked so the cache can be updated after a successful commit.
+	pendingCacheWrites []pendingCacheWrite
 }
 
 var _ types.Batch = (*pebbleBatch)(nil)
 
-func newPebbleBatch(db *pebble.DB) *pebbleBatch {
-	return &pebbleBatch{b: db.NewBatch()}
+func newPebbleBatch(db *pebble.DB, cache flatcache.Cache) *pebbleBatch {
+	return &pebbleBatch{b: db.NewBatch(), cache: cache}
 }
 
 func (p *pebbleDB) NewBatch() types.Batch {
-	return newPebbleBatch(p.db)
+	return newPebbleBatch(p.db, p.cache)
 }
 
 func (pb *pebbleBatch) Set(key, value []byte) error {
-	// Durability options are applied on Commit.
+	pb.pendingCacheWrites = append(pb.pendingCacheWrites, pendingCacheWrite{
+		key:   key,
+		value: value,
+	})
 	return pb.b.Set(key, value, nil)
 }
 
 func (pb *pebbleBatch) Delete(key []byte) error {
-	// Durability options are applied on Commit.
+	pb.pendingCacheWrites = append(pb.pendingCacheWrites, pendingCacheWrite{
+		key:      key,
+		isDelete: true,
+	})
 	return pb.b.Delete(key, nil)
 }
 
 func (pb *pebbleBatch) Commit(opts types.WriteOptions) error {
-	return pb.b.Commit(toPebbleWriteOpts(opts))
+	err := pb.b.Commit(toPebbleWriteOpts(opts))
+	if err != nil {
+		return err
+	}
+	for _, w := range pb.pendingCacheWrites {
+		if w.isDelete {
+			pb.cache.Delete(w.key)
+		} else {
+			pb.cache.Set(w.key, w.value)
+		}
+	}
+	pb.pendingCacheWrites = nil
+	return nil
 }
 
 func (pb *pebbleBatch) Len() int {
@@ -42,6 +71,7 @@ func (pb *pebbleBatch) Len() int {
 
 func (pb *pebbleBatch) Reset() {
 	pb.b.Reset()
+	pb.pendingCacheWrites = nil
 }
 
 func (pb *pebbleBatch) Close() error {
