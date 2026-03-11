7160f772cb41980a3f19702458a5ca76c465923d
kbhat1 <kartikbhatri@gmail.com>
2026-03-11 15:15:07 -0400

    Revert "Avoid store tracing for ordinary debug traces"
    This reverts commit dc448a2ba7a7aad408e3f0b008024403db78c8b1.

--- app/app.go
+++ app/app.go
@@ -2299,7 +2299,7 @@ func (app *App) RegisterTxService(clientCtx client.Context) {
 
 func (app *App) RPCContextProvider(i int64) sdk.Context {
 	if i == evmrpc.LatestCtxHeight {
-		return app.GetCheckCtx().WithIsEVM(true).WithTraceMode(true).WithIsCheckTx(false).WithClosestUpgradeName(LatestUpgrade)
+		return app.GetCheckCtx().WithIsEVM(true).WithIsTracing(true).WithIsCheckTx(false).WithClosestUpgradeName(LatestUpgrade)
 	}
 	ctx, err := app.CreateQueryContext(i, false)
 	if err != nil {
@@ -2310,7 +2310,7 @@ func (app *App) RPCContextProvider(i int64) sdk.Context {
 		closestUpgrade = LatestUpgrade
 	}
 	ctx = ctx.WithClosestUpgradeName(closestUpgrade)
-	return ctx.WithIsEVM(true).WithTraceMode(true).WithIsCheckTx(false)
+	return ctx.WithIsEVM(true).WithIsTracing(true).WithIsCheckTx(false)
 }
 
 // RegisterTendermintService implements the Application.RegisterTendermintService method.
