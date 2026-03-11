111459f5ea2ea32bb851ba64fcc95a83e478814f
Cody Littley <cody.littley@seinetwork.io>
2026-03-11 12:41:36 -0500

    Helper files for flatKV cache

---
+++ sei-db/db_engine/pebbledb/pebblecache/lru_queue.go
@@ -0,0 +1,83 @@
+package pebblecache
+
+import "container/list"
+
+// Implements a queue-like abstraction with LRU semantics. Not thread safe.
+type lruQueue struct {
+	order     *list.List
+	entries   map[string]*list.Element
+	totalSize int
+}
+
+type lruQueueEntry struct {
+	key  string
+	size int
+}
+
+// Create a new LRU queue.
+func NewLRUQueue() *lruQueue {
+	return &lruQueue{
+		order:   list.New(),
+		entries: make(map[string]*list.Element),
+	}
+}
+
+// Add a new entry to the LRU queue. Can also be used to update an existing value with a new weight.
+func (lru *lruQueue) Push(
+	// the key in the cache that was recently interacted with
+	key []byte,
+	// the size of the key + value
+	size int,
+) {
+	if elem, ok := lru.entries[string(key)]; ok {
+		entry := elem.Value.(*lruQueueEntry)
+		lru.totalSize += size - entry.size
+		entry.size = size
+		lru.order.MoveToBack(elem)
+		return
+	}
+
+	keyStr := string(key)
+	elem := lru.order.PushBack(&lruQueueEntry{
+		key:  keyStr,
+		size: size,
+	})
+	lru.entries[keyStr] = elem
+	lru.totalSize += size
+}
+
+// Signal that an entry has been interated with, moving it to the to the back of the queue
+// (i.e. making it so it doesn't get popped soon).
+func (lru *lruQueue) Touch(key []byte) {
+	elem, ok := lru.entries[string(key)]
+	if !ok {
+		return
+	}
+	lru.order.MoveToBack(elem)
+}
+
+// Returns the total size of all entries in the LRU queue.
+func (lru *lruQueue) GetTotalSize() int {
+	return lru.totalSize
+}
+
+// Returns a count of the number of entries in the LRU queue, where each entry counts for 1 regardless of size.
+func (lru *lruQueue) GetCount() int {
+	return len(lru.entries)
+}
+
+// Pops a single element out of the queue. The element removed is the entry least recently passed to Update().
+// Returns the key in string form to avoid copying the key an additional time.
+// Panics if the queue is empty.
+func (lru *lruQueue) PopLeastRecentlyUsed() string {
+	elem := lru.order.Front()
+	if elem == nil {
+		panic("cannot pop from empty LRU queue")
+	}
+
+	lru.order.Remove(elem)
+	entry := elem.Value.(*lruQueueEntry)
+	delete(lru.entries, entry.key)
+	lru.totalSize -= entry.size
+	return entry.key
+}
