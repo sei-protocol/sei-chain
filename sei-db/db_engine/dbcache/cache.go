94ae673ee27050a6617df94deea1658591598d93
Cody Littley <cody.littley@seinetwork.io>
2026-03-16 11:36:39 -0500

    made suggested changes

--- sei-db/db_engine/dbcache/cache.go
+++ sei-db/db_engine/dbcache/cache.go
@@ -16,10 +16,25 @@ import (
 type Reader func(key []byte) (value []byte, found bool, err error)
 
 // Cache describes a read-through cache backed by a Reader.
+//
+// Warning: it is not safe to mutate byte slices (keys or values) passed to or received from the cache.
+// A cache is not required to make defensive copies, and so these slices must be treated as immutable.
+//
+// Although several methods on this interface return errors, the conditions when a cache
+// is permitted to actually return an error is limited at the API level. A cache method
+// may return an error under the following conditions:
+// - malformed input (e.g. a nil key)
+// - the Reader method returns an error (for methods that accpet a Reader)
+// - the cache is shutting down
+// - the cache's work pools are shutting down
 type Cache interface {
 
 	// Get returns the value for the given key, or (nil, false, nil) if not found.
-	// On a cache miss the provided Reader is called to fetch from the backing store.
+	// On a cache miss the provided Reader is called to fetch from the backing store,
+	// and the result is loaded into the cache.
+	//
+	// It is not safe to mutate the key slice after calling this method, nor is it safe to mutate the value slice
+	// that is returned.
 	Get(
 		// Reads a value from the backing store on cache miss.
 		read Reader,
@@ -34,16 +49,23 @@ type Cache interface {
 	// Perform a batch read operation. Given a map of keys to read, performs the reads and updates the
 	// map with the results. On cache misses the provided Reader is called to fetch from the backing store.
 	//
-	// It is not thread safe to read or mutate the map while this method is running.
+	// It is not thread safe to read or mutate the map while this method is running. It is also not safe to mutate the
+	// key or value slices in the map after calling this method.
 	BatchGet(read Reader, keys map[string]types.BatchGetResult) error
 
 	// Set sets the value for the given key.
+	//
+	// It is not safe to mutate the key or value slices after calling this method.
 	Set(key []byte, value []byte)
 
 	// Delete deletes the value for the given key.
+	//
+	// It is not safe to mutate the key slice after calling this method.
 	Delete(key []byte)
 
 	// BatchSet applies the given updates to the cache.
+	//
+	// It is not safe to mutate the key or value slices in the CacheUpdate structs after calling this method.
 	BatchSet(updates []CacheUpdate) error
 }
 
--- sei-db/db_engine/dbcache/cached_key_value_db.go
+++ sei-db/db_engine/dbcache/cached_key_value_db.go
@@ -18,6 +18,9 @@ type cachedKeyValueDB struct {
 }
 
 // Combine a cache and a key-value database to create a new key-value database with caching.
+//
+// Due to the nature of a Cache, it is not safe to mutate byte slices (keys or values) passed to or received from
+// any of the methods on a cachedKeyValueDB after calling them.
 func NewCachedKeyValueDB(db types.KeyValueDB, cache Cache) types.KeyValueDB {
 	read := func(key []byte) ([]byte, bool, error) {
 		val, err := db.Get(key)
@@ -32,6 +35,10 @@ func NewCachedKeyValueDB(db types.KeyValueDB, cache Cache) types.KeyValueDB {
 	return &cachedKeyValueDB{db: db, cache: cache, read: read}
 }
 
+// Get returns the value for the given key, or ErrNotFound if not found.
+//
+// It is not safe to mutate the key slice after calling this method, nor is it safe to mutate the value slice
+// that is returned.
 func (c *cachedKeyValueDB) Get(key []byte) ([]byte, error) {
 	val, found, err := c.cache.Get(c.read, key, true)
 	if err != nil {
@@ -43,6 +50,11 @@ func (c *cachedKeyValueDB) Get(key []byte) ([]byte, error) {
 	return val, nil
 }
 
+// BatchGet performs a batch read operation. Given a map of keys to read, performs the reads and updates the
+// map with the results. On cache misses the provided Reader is called to fetch from the backing store.
+//
+// It is not thread safe to read or mutate the map while this method is running. It is also not safe to mutate the
+// key or value slices in the map after calling this method.
 func (c *cachedKeyValueDB) BatchGet(keys map[string]types.BatchGetResult) error {
 	err := c.cache.BatchGet(c.read, keys)
 	if err != nil {
@@ -51,6 +63,9 @@ func (c *cachedKeyValueDB) BatchGet(keys map[string]types.BatchGetResult) error 
 	return nil
 }
 
+// Set sets the value for the given key.
+//
+// It is not safe to mutate the key or value slices after calling this method.
 func (c *cachedKeyValueDB) Set(key []byte, value []byte, opts types.WriteOptions) error {
 	err := c.db.Set(key, value, opts)
 	if err != nil {
@@ -60,6 +75,9 @@ func (c *cachedKeyValueDB) Set(key []byte, value []byte, opts types.WriteOptions
 	return nil
 }
 
+// Delete deletes the value for the given key.
+//
+// It is not safe to mutate the key slice after calling this method.
 func (c *cachedKeyValueDB) Delete(key []byte, opts types.WriteOptions) error {
 	err := c.db.Delete(key, opts)
 	if err != nil {
@@ -73,6 +91,10 @@ func (c *cachedKeyValueDB) NewIter(opts *types.IterOptions) (types.KeyValueDBIte
 	return c.db.NewIter(opts)
 }
 
+// NewBatch returns a new batch for atomic writes.
+//
+// It is not safe to mutate the key/value slices passed to the batch once inserted. This remains true even
+// after the batch is committed.
 func (c *cachedKeyValueDB) NewBatch() types.Batch {
 	return newCachedBatch(c.db.NewBatch(), c.cache)
 }
