acfe8009da7f33514b60697d5559367b39adbc30
Cody Littley <cody.littley@seinetwork.io>
2026-04-15 12:59:07 -0500

    resolve TODO

--- sei-db/block_db/blocksim/blocksim_config.go
+++ sei-db/block_db/blocksim/blocksim_config.go
@@ -126,7 +126,7 @@ func DefaultBlocksimConfig() *BlocksimConfig {
 		MaxRuntimeSeconds:               0,
 		MetricsAddr:                     ":9090",
 		EnableSuspension:                true,
-		FlushIntervalBlocks:             0, // TODO
+		FlushIntervalBlocks:             1024,
 		BackgroundMetricsScrapeInterval: 60,
 		LogDir:                          "logs",
 		LogLevel:                        "info",
