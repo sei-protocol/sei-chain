ba347258d989f4cb74751eb51455295c0d4c78c7
Cody Littley <56973212+cody-littley@users.noreply.github.com>
2026-04-30 14:36:03 -0500

    Cjl/expanded route api (#3335)
    ## Describe your changes and provide context
    
    Adds methods to the `Router` interface that allows a router to handle
    requests to iterate and requests for membership proofs.
    
    ## Testing performed to validate your change
    
    unit tests
    
    ---------
    
    Co-authored-by: Cody Littley <cody.littley@seinetwork.io>

--- sei-db/state_db/sc/migration/migration_types.go
+++ sei-db/state_db/sc/migration/migration_types.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"fmt"
 
+	ics23 "github.com/confio/ics23/go"
 	"github.com/sei-protocol/sei-chain/sei-db/proto"
+	dbm "github.com/tendermint/tm-db"
 )
 
 // MigrationStatus is the lifecycle status of a migration.
@@ -40,6 +42,12 @@ type DBWriter func(ctx context.Context, changesets []*proto.NamedChangeSet) erro
 // Read a value from the database.
 type DBReader func(store string, key []byte) ([]byte, bool, error)
 
+// Get an iterator over a range of keys in a store.
+type DBIteratorBuilder func(store string, start []byte, end []byte, ascending bool) (dbm.Iterator, error)
+
+// Builds a proof of the value for a key in a store.
+type DBProofBuilder func(store string, key []byte) (*ics23.CommitmentProof, error)
+
 // An object capable of routing/splitting reads and writes between databases.
 type Router interface {
 	// Read a value from the database.
@@ -52,4 +60,12 @@ type Router interface {
 	// If this method returns an error, it is not safe to attempt to retry. An error should be considered
 	// fatal, and should result in any managed databases being shut down and crash recovered.
 	ApplyChangeSets(ctx context.Context, changesets []*proto.NamedChangeSet) error
+
+	// Get an iterator over a range of keys in a store. Some stores may not support iteration,
+	// and this method will return an error in that case.
+	Iterator(store string, start []byte, end []byte, ascending bool) (dbm.Iterator, error)
+
+	// Get a proof of the value for a key in a store. Some stores may not support proofs,
+	// and this method will return an error in that case.
+	GetProof(store string, key []byte) (*ics23.CommitmentProof, error)
 }
