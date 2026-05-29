07cb06447943b5fa7dc6e71676fb96705e14fa86
Amir Deris <amir@seinetwork.io>
2026-05-29 11:33:56 -0700

    Added default limit 0 to GetBlockWithTxs

--- sei-cosmos/x/auth/tx/service.go
+++ sei-cosmos/x/auth/tx/service.go
@@ -195,8 +195,8 @@ func (s txServer) GetBlockWithTxs(ctx context.Context, req *txtypes.GetBlockWith
 		if err = pagination.VerifyPaginationLimit(limit); err != nil {
 			return nil, sdkerrors.ErrInvalidRequest.Wrapf("invalid pagination limit: %d. Max allowed limit is %d", limit, pagination.MaxLimit)
 		}
-	} else {
-		offset = 0
+	}
+	if limit == 0 {
 		limit = pagination.DefaultLimit
 	}
 
