c4e1a2ac69d0ec268d199664ba70bcf01ce00bad
Grzegorz Prusak <pompon.pompon@gmail.com>
2026-05-29 19:04:08 +0200

    split blocksync reactor into 2 modes. (#3511)
    Autobahn nodes will need to be able to send pre-giga blocks to pre-giga
    nodes during transition period (so that pre-giga nodes can blocksync to
    the upgrade height). However all the remaining parts of the blocksync
    reactor should be disabled. This pr extracts the tendermint-only
    blocksync logic to syncController which is optional part of the
    blocksync reactor. Additionally I have
    * refactored the blocksync reactor to use structured concurrency
    * fixed bug in poolRoutine which was silently terminating blocksync in
    case of block validation failure (now it will retry fetching a block)
    * fixed busy loop in blocksync pool.run.

--- sei-tendermint/internal/blocksync/reactor_test.go
+++ sei-tendermint/internal/blocksync/reactor_test.go
@@ -2,6 +2,7 @@ package blocksync
 
 import (
 	"context"
+	"errors"
 	"runtime"
 	"strings"
 	"testing"
@@ -26,6 +27,7 @@ import (
 	"github.com/sei-protocol/sei-chain/sei-tendermint/internal/store"
 	"github.com/sei-protocol/sei-chain/sei-tendermint/internal/test/factory"
 	"github.com/sei-protocol/sei-chain/sei-tendermint/libs/utils"
+	pb "github.com/sei-protocol/sei-chain/sei-tendermint/proto/tendermint/blocksync"
 	"github.com/sei-protocol/sei-chain/sei-tendermint/types"
 )
 
@@ -82,6 +84,7 @@ func makeReactor(
 	t *testing.T,
 	genDoc *types.GenesisDoc,
 	router *p2p.Router,
+	blockSync bool,
 	restartEvent func(),
 	selfRemediationConfig *config.SelfRemediationConfig,
 ) *Reactor {
@@ -98,8 +101,8 @@ func makeReactor(
 	require.NoError(t, err)
 	require.NoError(t, stateStore.Save(state))
 	mp := mempool.NewTxMempool(mempool.TestConfig(), proxyApp, mempool.NopMetrics(), mempool.NopTxConstraintsFetcher)
-	eventbus := eventbus.NewDefault()
-	require.NoError(t, eventbus.Start(ctx))
+	bus := eventbus.NewDefault()
+	require.NoError(t, bus.Start(ctx))
 
 	blockExec := sm.NewBlockExecutor(
 		stateStore,
@@ -107,22 +110,24 @@ func makeReactor(
 		mp,
 		sm.EmptyEvidencePool{},
 		blockStore,
-		eventbus,
+		bus,
 		sm.NopMetrics(),
 		types.DefaultConsensusPolicy(),
 	)
 
 	r, err := NewReactor(
 		stateStore,
-		blockExec,
 		blockStore,
-		nil,
 		router,
-		true,
-		consensus.NopMetrics(),
-		nil, // eventbus, can be nil
-		restartEvent,
-		selfRemediationConfig,
+		utils.Some(SyncerConfig{
+			BlockExec:             blockExec,
+			ConsReactor:           nil,
+			BlockSync:             blockSync,
+			Metrics:               consensus.NopMetrics(),
+			EventBus:              nil, // eventbus can be nil
+			RestartEvent:          restartEvent,
+			SelfRemediationConfig: selfRemediationConfig,
+		}),
 	)
 	if err != nil {
 		t.Fatalf("NewReactor(): %v", err)
@@ -150,8 +155,9 @@ func (rts *reactorTestSuite) addNode(
 		t,
 		genDoc,
 		rts.network.Node(nodeID).Router,
+		true,
 		func() {},
-		config.DefaultSelfRemediationConfig(),
+		remediationConfig,
 	)
 	lastCommit := &types.Commit{}
 
@@ -160,7 +166,8 @@ func (rts *reactorTestSuite) addNode(
 	for blockHeight := int64(1); blockHeight <= maxBlockHeight; blockHeight++ {
 		block, blockID, partSet, seenCommit := makeNextBlock(ctx, t, state, privVal, blockHeight, lastCommit)
 
-		state, err = reactor.blockExec.ApplyBlock(ctx, state, blockID, block, nil)
+		syncer := reactor.syncer.OrPanic("syncer should be configured in tests")
+		state, err = syncer.blockExec.ApplyBlock(ctx, state, blockID, block, nil)
 		require.NoError(t, err)
 
 		reactor.store.SaveBlock(block, partSet, seenCommit)
@@ -226,13 +233,17 @@ func TestReactor_AbruptDisconnect(t *testing.T) {
 
 	rts.start(t)
 
-	secondaryPool := rts.reactors[rts.nodes[1]].pool
+	secondarySyncer := rts.reactors[rts.nodes[1]].syncer.OrPanic("syncer should be configured in tests")
 
 	require.Eventually(
 		t,
 		func() bool {
-			height, _, _ := secondaryPool.GetStatus()
-			return secondaryPool.MaxPeerHeight() == maxBlockHeight && height > 0 && height <= maxBlockHeight
+			pool := secondarySyncer.pool.Load()
+			if pool == nil {
+				return false
+			}
+			height, _, _ := pool.GetStatus()
+			return pool.MaxPeerHeight() == maxBlockHeight && height > 0 && height <= maxBlockHeight
 		},
 		10*time.Second,
 		10*time.Millisecond,
@@ -340,8 +351,12 @@ func TestReactor_SyncTime(t *testing.T) {
 	require.Eventually(
 		t,
 		func() bool {
+			pool := rts.reactors[rts.nodes[1]].syncer.OrPanic("syncer should be configured in tests").pool.Load()
+			if pool == nil {
+				return false
+			}
 			return rts.reactors[rts.nodes[1]].GetRemainingSyncTime() > time.Nanosecond &&
-				rts.reactors[rts.nodes[1]].pool.getLastSyncRate() > 0.001
+				pool.getLastSyncRate() > 0.001
 		},
 		10*time.Second,
 		10*time.Millisecond,
@@ -420,25 +435,277 @@ func TestAutoRestartIfBehind(t *testing.T) {
 			}
 
 			restart := utils.NewAtomicSend(false)
-			r := &Reactor{
+			syncer := &syncController{
 				store:                     mockBlockStore,
-				pool:                      blockPool,
 				blocksBehindThreshold:     tt.blocksBehindThreshold,
 				blocksBehindCheckInterval: tt.blocksBehindCheckInterval,
 				restartEvent:              func() { restart.Store(true) },
-				blockSync:                 newAtomicBool(tt.isBlockSync),
 			}
+			if tt.isBlockSync {
+				syncer.blockSync.Store(true)
+			}
+			r := &Reactor{syncer: utils.Some(syncer)}
 
 			ctx := t.Context()
 			if tt.restartExpected {
-				r.autoRestartIfBehind(ctx)
+				r.syncer.OrPanic("syncer").autoRestartIfBehind(ctx, blockPool)
 				assert.True(t, restart.Load(), "Expected restart but did not occur")
 			} else {
 				ctx, cancel := context.WithTimeout(t.Context(), 50*time.Millisecond)
 				defer cancel()
-				r.autoRestartIfBehind(ctx)
+				r.syncer.OrPanic("syncer").autoRestartIfBehind(ctx, blockPool)
 				assert.False(t, restart.Load(), "Unexpected restart")
 			}
 		})
 	}
+}
+
+func makeValidationFailurePair(
+	ctx context.Context,
+	t *testing.T,
+	testRootName string,
+) (sm.State, *types.Block, *types.Block) {
+	t.Helper()
+
+	cfg, err := config.ResetTestRoot(t.TempDir(), testRootName)
+	require.NoError(t, err)
+
+	valSet, privVals := factory.ValidatorSet(ctx, 1, 30)
+	genDoc := factory.GenesisDoc(cfg, time.Now(), valSet.Validators, factory.ConsensusParams())
+	initialState, err := sm.MakeGenesisState(genDoc)
+	require.NoError(t, err)
+
+	lastCommit := &types.Commit{}
+	block1, _, _, seenCommit1 := makeNextBlock(ctx, t, initialState, privVals[0], 1, lastCommit)
+	block2, _, _, _ := makeNextBlock(ctx, t, initialState, privVals[0], 2, seenCommit1)
+
+	badBlock2Proto, err := block2.ToProto()
+	require.NoError(t, err)
+	badBlock2Proto.LastCommit.Signatures[0].Signature[0] ^= 0xFF
+	badCommit, err := types.CommitFromProto(badBlock2Proto.LastCommit)
+	require.NoError(t, err)
+	badBlock2Proto.Header.LastCommitHash = badCommit.Hash()
+	badBlock2, err := types.BlockFromProto(badBlock2Proto)
+	require.NoError(t, err)
+
+	return initialState, block1, badBlock2
+}
+
+func TestPoolRoutine_DoesNotReturnOnValidationFailure(t *testing.T) {
+	ctx := t.Context()
+
+	initialState, block1, badBlock2 := makeValidationFailurePair(ctx, t, "block_sync_validation_failure_does_not_return")
+
+	badPeer := types.NodeID(strings.Repeat("a", 40))
+	goodPeer := types.NodeID(strings.Repeat("b", 40))
+	router := makeRouter(testPeers{
+		badPeer:  {id: badPeer, base: 1, height: 2, inputChan: make(chan inputData, 1)},
+		goodPeer: {id: goodPeer, base: 1, height: 2, inputChan: make(chan inputData, 1)},
+	})
+	pool := NewBlockPool(1, router)
+	done := make(chan error, 1)
+	go func() { done <- pool.run(ctx) }()
+	t.Cleanup(func() {
+		if err := <-done; err != nil && !errors.Is(err, context.Canceled) {
+			t.Fatalf("pool.run(): %v", err)
+		}
+	})
+	pool.SetPeerRange(badPeer, 1, 2)
+
+	evictNetwork := p2p.MakeTestNetwork(t, p2p.TestNetworkOptions{NumNodes: 1})
+	syncer := &syncController{
+		router:  evictNetwork.Node(evictNetwork.NodeIDs()[0]).Router,
+		metrics: consensus.NopMetrics(),
+	}
+
+	results := make(chan error, 1)
+	go func() {
+		_, err := syncer.poolRoutine(ctx, pool, initialState, false)
+		results <- err
+	}()
+	t.Cleanup(func() {
+		err := <-results
+		require.ErrorIs(t, err, context.Canceled)
+	})
+
+	introducedGoodPeer := false
+	for {
+		select {
+		case err := <-results:
+			t.Fatalf("poolRoutine returned early after validation failure: %v", err)
+		case request := <-pool.Requests():
+			if request.PeerID == goodPeer {
+				return
+			}
+
+			switch request.Height {
+			case 1:
+				_ = pool.AddBlock(request.PeerID, block1, block1.Size())
+			case 2:
+				_ = pool.AddBlock(request.PeerID, badBlock2, badBlock2.Size())
+				if !introducedGoodPeer {
+					introducedGoodPeer = true
+					pool.SetPeerRange(goodPeer, 1, 2)
+				}
+			}
+		}
+	}
+}
+
+func TestPoolRoutine_RetriesAfterValidationFailure(t *testing.T) {
+	ctx := t.Context()
+
+	initialState, block1, badBlock2 := makeValidationFailurePair(ctx, t, "block_sync_retry_after_validation_failure")
+	network := p2p.MakeTestNetwork(t, p2p.TestNetworkOptions{NumNodes: 1})
+
+	badPeer := types.NodeID(strings.Repeat("a", 40))
+	goodPeer1 := types.NodeID(strings.Repeat("b", 40))
+	goodPeer2 := types.NodeID(strings.Repeat("c", 40))
+	peers := testPeers{
+		badPeer:   {id: badPeer, base: 1, height: 2, inputChan: make(chan inputData, 1)},
+		goodPeer1: {id: goodPeer1, base: 1, height: 2, inputChan: make(chan inputData, 1)},
+		goodPeer2: {id: goodPeer2, base: 1, height: 2, inputChan: make(chan inputData, 1)},
+	}
+	pool := NewBlockPool(1, makeRouter(peers))
+	runPoolForTest(t, pool)
+	pool.SetPeerRange(badPeer, 1, 2)
+
+	syncer := &syncController{
+		router:  network.Node(network.NodeIDs()[0]).Router,
+		metrics: consensus.NopMetrics(),
+	}
+
+	results := make(chan error, 1)
+	go func() {
+		_, err := syncer.poolRoutine(ctx, pool, initialState, false)
+		results <- err
+	}()
+	t.Cleanup(func() {
+		err := <-results
+		require.ErrorIs(t, err, context.Canceled)
+	})
+
+	introducedGoodPeers := false
+	height1Requests := map[types.NodeID]int{}
+
+	for {
+		select {
+		case err := <-results:
+			t.Fatalf("poolRoutine returned before retry was observed: %v", err)
+		case request := <-pool.Requests():
+			if request.Height == 1 {
+				height1Requests[request.PeerID]++
+				if request.PeerID != badPeer && height1Requests[request.PeerID] == 1 {
+					return
+				}
+			}
+
+			if request.PeerID == badPeer && request.Height == 2 && !introducedGoodPeers {
+				introducedGoodPeers = true
+				pool.SetPeerRange(goodPeer1, 1, 2)
+				pool.SetPeerRange(goodPeer2, 1, 2)
+			}
+
+			if request.PeerID == badPeer {
+				switch request.Height {
+				case 1:
+					_ = pool.AddBlock(request.PeerID, block1, block1.Size())
+				case 2:
+					_ = pool.AddBlock(request.PeerID, badBlock2, badBlock2.Size())
+				}
+			}
+		}
+	}
+}
+
+func TestQueryResponder_ServesBlockRequestsWhenBlockSyncDisabled(t *testing.T) {
+	ctx := t.Context()
+
+	cfg, err := config.ResetTestRoot(t.TempDir(), "block_sync_query_responder_test")
+	require.NoError(t, err)
+
+	valSet, privVals := factory.ValidatorSet(ctx, 1, 30)
+	genDoc := factory.GenesisDoc(cfg, time.Now(), valSet.Validators, factory.ConsensusParams())
+	network := p2p.MakeTestNetwork(t, p2p.TestNetworkOptions{NumNodes: 2})
+	nodeIDs := network.NodeIDs()
+
+	server := makeReactor(
+		ctx,
+		t,
+		genDoc,
+		network.Node(nodeIDs[0]).Router,
+		false,
+		func() {},
+		config.DefaultSelfRemediationConfig(),
+	)
+	lastCommit := &types.Commit{}
+	state, err := server.stateStore.Load()
+	require.NoError(t, err)
+	for height := int64(1); height <= 3; height++ {
+		block, blockID, partSet, seenCommit := makeNextBlock(ctx, t, state, privVals[0], height, lastCommit)
+		state, err = server.syncer.OrPanic("syncer should be configured in tests").blockExec.ApplyBlock(ctx, state, blockID, block, nil)
+		require.NoError(t, err)
+		server.store.SaveBlock(block, partSet, seenCommit)
+		lastCommit = seenCommit
+	}
+	require.NoError(t, server.Start(ctx))
+	t.Cleanup(server.Wait)
+
+	client := p2p.TestMakeChannelNoCleanup(t, network.Node(nodeIDs[1]), GetChannelDescriptor())
+	network.Start(t)
+
+	client.Send(wrap(&pb.BlockRequest{Height: 2}), nodeIDs[0])
+	for range 2 {
+		msg, err := client.Recv(ctx)
+		require.NoError(t, err)
+		if blockResponse, ok := msg.Message.Sum.(*pb.Message_BlockResponse); ok {
+			require.Equal(t, int64(2), blockResponse.BlockResponse.GetBlock().Header.Height)
+			require.Equal(t, nodeIDs[0], msg.From)
+			return
+		}
+	}
+	t.Fatal("did not receive block response")
+}
+
+func TestQueryResponder_ServesStatusRequestsWhenBlockSyncDisabled(t *testing.T) {
+	ctx := t.Context()
+
+	cfg, err := config.ResetTestRoot(t.TempDir(), "block_sync_query_status_test")
+	require.NoError(t, err)
+
+	valSet, _ := factory.ValidatorSet(ctx, 1, 30)
+	genDoc := factory.GenesisDoc(cfg, time.Now(), valSet.Validators, factory.ConsensusParams())
+	network := p2p.MakeTestNetwork(t, p2p.TestNetworkOptions{NumNodes: 2})
+	nodeIDs := network.NodeIDs()
+
+	server := makeReactor(
+		ctx,
+		t,
+		genDoc,
+		network.Node(nodeIDs[0]).Router,
+		false,
+		func() {},
+		config.DefaultSelfRemediationConfig(),
+	)
+	require.NoError(t, server.Start(ctx))
+	t.Cleanup(server.Wait)
+
+	client := p2p.TestMakeChannelNoCleanup(t, network.Node(nodeIDs[1]), GetChannelDescriptor())
+	network.Start(t)
+
+	peerUpMsg, err := client.Recv(ctx)
+	require.NoError(t, err)
+	_, ok := peerUpMsg.Message.Sum.(*pb.Message_StatusRequest)
+	require.True(t, ok)
+	require.Equal(t, nodeIDs[0], peerUpMsg.From)
+
+	client.Send(wrap(&pb.StatusRequest{}), nodeIDs[0])
+	msg, err := client.Recv(ctx)
+	require.NoError(t, err)
+
+	statusResponse, ok := msg.Message.Sum.(*pb.Message_StatusResponse)
+	require.True(t, ok)
+	require.Equal(t, server.store.Base(), statusResponse.StatusResponse.GetBase())
+	require.Equal(t, server.store.Height(), statusResponse.StatusResponse.GetHeight())
+	require.Equal(t, nodeIDs[0], msg.From)
 }
