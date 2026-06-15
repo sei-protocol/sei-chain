eeda0481bcde4ac98e31b423ac00fdf2582cbc78
Tony Chen <codchen03@gmail.com>
2026-06-15 13:38:30 +0800

    port native evm-only giga executor

--- giga/evmonly/seiv3/config.go
+++ giga/evmonly/seiv3/config.go
@@ -1,6 +1,13 @@
 package seiv3
 
-import "math"
+import (
+	"math"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/params"
+
+	"github.com/sei-protocol/sei-chain/giga/evmonly/precompiles"
+)
 
 // Config captures the sei-v3 executor knobs needed by the EVM-only path.
 type Config struct {
@@ -8,12 +15,16 @@ type Config struct {
 	FlushBatchSize       int
 	DisableNonceCheck    bool
 	DisableGasPriceCheck bool
+	MinGasPrice          *big.Int
+	ChainConfig          *params.ChainConfig
+	CustomPrecompiles    precompiles.Registry
 }
 
 func DefaultConfig() Config {
 	return Config{
 		OCCWorkers:     int(math.Min(12, float64(runtimeCPU()))),
 		FlushBatchSize: 100,
+		MinGasPrice:    big.NewInt(1_000_000_000),
 	}
 }
 
@@ -24,6 +35,9 @@ func (c Config) WithDefaults() Config {
 	}
 	if c.FlushBatchSize == 0 {
 		c.FlushBatchSize = defaults.FlushBatchSize
+	}
+	if c.MinGasPrice == nil {
+		c.MinGasPrice = new(big.Int).Set(defaults.MinGasPrice)
 	}
 	return c
 }
