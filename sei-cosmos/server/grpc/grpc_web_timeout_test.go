63fcd89040e8ade1c2ed97faa953bbdb44df0a74
Amir Deris <amir@seinetwork.io>
2026-06-17 16:34:44 -0700

    Fixed test

--- sei-cosmos/server/grpc/grpc_web_timeout_test.go
+++ sei-cosmos/server/grpc/grpc_web_timeout_test.go
@@ -40,5 +40,5 @@ func TestStartGRPCWebTimeouts(t *testing.T) {
 	require.Equal(t, 10*time.Second, srv.ReadHeaderTimeout, "ReadHeaderTimeout")
 	require.Equal(t, 30*time.Second, srv.ReadTimeout, "ReadTimeout")
 	require.Equal(t, 2*time.Minute, srv.WriteTimeout, "WriteTimeout")
-	require.Equal(t, 120*time.Second, srv.IdleTimeout, "IdleTimeout")
+	require.Equal(t, 30*time.Second, srv.IdleTimeout, "IdleTimeout")
 }
