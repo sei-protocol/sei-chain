8c79b1133758f7f9b2ba9ab83a6f852fa66b41e0
Sergey Mishin <sergei.a.mishin@gmail.com>
2026-06-20 12:30:52 +0200

    Don't use hardcoded path to the evmc lib
    The runtime.Caller(0) returns paths to the source code, not picking
    them up during runtime.

--- giga/executor/lib/evmlib.go
+++ giga/executor/lib/evmlib.go
@@ -2,8 +2,6 @@ package lib
 
 import (
 	"fmt"
-	"path/filepath"
-	"runtime"
 
 	"github.com/ethereum/evmc/v12/bindings/go/evmc"
 )
@@ -13,15 +11,9 @@ import (
 // InitEvmoneVM initializes the EVMC VM by loading the platform-specific evmone library.
 // It does not verify that the loaded version is compatible with evmc version.
 func InitEvmoneVM() (*evmc.VM, error) {
-	_, path, _, ok := runtime.Caller(0)
-	if !ok {
-		return nil, fmt.Errorf("failed to get caller information")
-	}
-
-	libPath := filepath.Join(filepath.Dir(path), libName)
-	vm, err := evmc.Load(libPath)
+	vm, err := evmc.Load(libName)
 	if err != nil {
-		return nil, fmt.Errorf("evmc.Load(%q): %w", libPath, err)
+		return nil, fmt.Errorf("evmc.Load(%q): %w", libName, err)
 	}
 
 	return vm, nil
