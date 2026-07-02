4d23269634c7d90d1a01daaba39a494ef2a254db
Tony Chen <codchen03@gmail.com>
2026-07-02 20:37:09 +0800

    remove loadtest worker pinning controls

--- giga/evmonly/cmd/evmonly-loadtest/worker_affinity_other.go
+++
@@ -1,9 +0,0 @@
-//go:build !linux
-
-package main
-
-import "fmt"
-
-func pinCurrentWorkerThread(int) (func(), error) {
-	return func() {}, fmt.Errorf("worker CPU pinning is only supported on linux")
-}
