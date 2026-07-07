c5c6d39d9d0efe6f688a92479acead7bbb0c3ddf
Masih H. Derkani <m@derkani.org>
2026-07-07 10:03:47 +0100

    Fix apphash breakage

--- sei-cosmos/x/auth/vesting/types/msgs.go
+++ sei-cosmos/x/auth/vesting/types/msgs.go
@@ -8,10 +8,6 @@ import (
 // TypeMsgCreateVestingAccount defines the type value for a MsgCreateVestingAccount.
 const TypeMsgCreateVestingAccount = "msg_create_vesting_account"
 
-// maxVestingCoinAmountBitLen bounds the magnitude of a single coin amount in
-// MsgCreateVestingAccount. 2^200 is well above any realistic token supply.
-const maxVestingCoinAmountBitLen = 200
-
 var _ sdk.Msg = &MsgCreateVestingAccount{}
 
 // NewMsgCreateVestingAccount returns a reference to a new MsgCreateVestingAccount.
@@ -34,15 +30,27 @@ func (msg MsgCreateVestingAccount) Type() string { return TypeMsgCreateVestingAc
 
 // ValidateBasic Implements Msg.
 func (msg MsgCreateVestingAccount) ValidateBasic() error {
-
-	if err := validateAddr(msg.FromAddress, "sender"); err != nil {
+	from, err := sdk.AccAddressFromBech32(msg.FromAddress)
+	if err != nil {
 		return err
 	}
-	if err := validateAddr(msg.ToAddress, "recipient"); err != nil {
+	to, err := sdk.AccAddressFromBech32(msg.ToAddress)
+	if err != nil {
 		return err
 	}
+	if err := sdk.VerifyAddressFormat(from); err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid sender address: %s", err)
+	}
 
-	if !msg.Amount.IsValid() || !msg.Amount.IsAllPositive() {
+	if err := sdk.VerifyAddressFormat(to); err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid recipient address: %s", err)
+	}
+
+	if !msg.Amount.IsValid() {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidCoins, msg.Amount.String())
+	}
+
+	if !msg.Amount.IsAllPositive() {
 		return sdkerrors.Wrap(sdkerrors.ErrInvalidCoins, msg.Amount.String())
 	}
 
@@ -50,23 +58,6 @@ func (msg MsgCreateVestingAccount) ValidateBasic() error {
 		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "invalid end time")
 	}
 
-	for _, c := range msg.Amount {
-		if c.Amount.BigInt().BitLen() > maxVestingCoinAmountBitLen {
-			return sdkerrors.Wrapf(sdkerrors.ErrInvalidCoins, "%s amount is out of range", c.Denom)
-		}
-	}
-
-	return nil
-}
-
-func validateAddr(bech32, label string) error {
-	addr, err := sdk.AccAddressFromBech32(bech32)
-	if err != nil {
-		return err
-	}
-	if err := sdk.VerifyAddressFormat(addr); err != nil {
-		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid %s address: %s", label, err)
-	}
 	return nil
 }
 
