33562a107e5f530d71cab4aa936100c212362884
Tony Chen <codchen03@gmail.com>
2026-07-13 23:59:16 +0800

    Use DBImpl for giga mock balance validation

---
+++ giga/tests/giga_mock_balances_test.go
@@ -0,0 +1,33 @@
+//go:build mock_balances
+
+package giga_test
+
+import (
+	"math/big"
+	"testing"
+	"time"
+
+	gigaevmstate "github.com/sei-protocol/sei-chain/giga/deps/xevm/state"
+	"github.com/sei-protocol/sei-chain/occ_tests/utils"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGigaMockBalancesValidationUsesDBImpl(t *testing.T) {
+	blockTime := time.Now()
+	accts := utils.NewTestAccounts(3)
+	signer := utils.NewSigner()
+	recipient := utils.NewSigner()
+
+	gigaCtx := NewGigaTestContext(t, accts, blockTime, 1, ModeGigaSequential)
+	gigaCtx.TestApp.GigaEvmKeeper.SetAddressMapping(gigaCtx.Ctx, signer.AccountAddress, signer.EvmAddress)
+
+	to := recipient.EvmAddress
+	value := new(big.Int).Mul(gigaevmstate.TopOffAmount, big.NewInt(2))
+	fee := big.NewInt(100000000000)
+	tx := createCustomEVMTx(t, gigaCtx, signer, &to, value, 21000, fee, fee, 0)
+
+	_, results, err := RunBlock(t, gigaCtx, [][]byte{tx})
+	require.NoError(t, err)
+	require.Len(t, results, 1)
+	require.Equal(t, uint32(0), results[0].Code, results[0].Log)
+}
