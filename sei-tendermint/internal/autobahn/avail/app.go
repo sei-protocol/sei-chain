b5c7bf320f60d2f6255007a958d0e73de4b304d1
Grzegorz Prusak <pompon.pompon@gmail.com>
2026-07-31 14:38:03 +0200

    deadcode

--- sei-tendermint/internal/autobahn/avail/app.go
+++ sei-tendermint/internal/autobahn/avail/app.go
@@ -25,19 +25,6 @@ func (s *State) LastAppQC() utils.Option[*types.AppQC] {
 	return utils.None[*types.AppQC]()
 }
 
-// WaitForAppQC waits until there is an AppQC for the given index or higher.
-// Returns this AppQC and the corresponding CommitQC.
-// Together they provide enough information to prune the availability state.
-func (s *State) WaitForAnchor(ctx context.Context, idx types.RoadIndex) (*PruneAnchor, error) {
-	for inner, ctrl := range s.inner.Lock() {
-		if err := ctrl.WaitUntil(ctx, func() bool { return types.NextOpt(inner.app.anchor) > idx }); err!= nil {
-			return nil,err
-		}
-		return inner.app.anchor.OrPanic("missing anchor"), nil
-	}
-	panic("unreachable")
-}
-
 // PushAppVote pushes an AppVote to the state.
 // Same admit-then-verify as PushAppQC: far-future roads park until the duo
 // and CommitQC tip catch up (one stream goroutine; does not block others).
