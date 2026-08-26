514b99369776419dd9b902df45b1cef11b1f47eb
Masih H. Derkani <m@derkani.org>
2026-08-26 09:54:00 +0100

    Remove stale ibc from v6.7 module version map
    Remove stale IBC from v6.7 module version map and apply minor cleanups
    that are missed in previous IBC removal.
    
    Remove obsolete relayer tests.

--- sei-wasmd/x/wasm/types/errors.go
+++ sei-wasmd/x/wasm/types/errors.go
@@ -52,9 +52,6 @@ var (
 	// ErrDuplicate error for content that exists
 	ErrDuplicate = sdkErrors.Register(DefaultCodespace, 15, "duplicate")
 
-	// ErrMaxIBCChannels error for maximum number of ibc channels reached
-	ErrMaxIBCChannels = sdkErrors.Register(DefaultCodespace, 16, "max transfer channels")
-
 	// ErrUnsupportedForContract error when a feature is used that is not supported for/ by this contract
 	ErrUnsupportedForContract = sdkErrors.Register(DefaultCodespace, 17, "unsupported for this contract")
 
