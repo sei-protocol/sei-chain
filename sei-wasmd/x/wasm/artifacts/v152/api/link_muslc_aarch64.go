604380c36f4ad9a5cb78fef1ade2fe5c6ad1956f
masih <m@derkani.org>
2026-09-16 12:40:34 +0000

    Remove historical CosmWasm VM artifacts

--- sei-wasmd/x/wasm/artifacts/v152/api/link_muslc_aarch64.go
+++
@@ -1,6 +0,0 @@
-//go:build linux && muslc && arm64 && !sys_wasmvm
-
-package api
-
-// #cgo LDFLAGS: -Wl,-rpath,${SRCDIR} -L${SRCDIR} -lwasmvm152_muslc.aarch64
-import "C"
--- sei-wasmd/x/wasm/artifacts/v155/api/link_mac_static.go
+++
@@ -1,6 +0,0 @@
-//go:build darwin && static_wasm && !sys_wasmvm
-
-package api
-
-// #cgo LDFLAGS: -L${SRCDIR} -lwasmvm155static_darwin
-import "C"
